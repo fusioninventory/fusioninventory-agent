d04b26d78c3e174369e4866ab4a08f8e9f084b69
Gonéri Le Bouder <goneri@rulezlan.org>
2007-02-19 15:31:03 +0000

    If the BIOS doesn't provide a serial number, that's critic to identify the machine, I read CPU ID instead (The issue appends with dedibox.fr hosted servers). regex clean up

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/Bios.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/Bios.pm
@@ -19,21 +19,51 @@ sub run {
   my $flag=0;
   for(@dmidecode){
     $flag=1 if /dmi type 0,/i;
-    if((/dmi type (\d+),/i) && ($flag)){($1!='0')?last:1;}
-    if((/^vendor\s*:\s*(.+)/i) && ($flag)) { $BiosManufacturer = $1 }
-    if((/^release date\s*:\s*(.+)/i) && ($flag)) { $BiosDate = $1 }
-    if((/^version\s*:\s*(.+)/i) && ($flag)) { $BiosVersion = $1 }
+    last if($flag && (/dmi type (\d+),/i) && ($1!=0));
+    if((/^vendor:\s*(\S+)/i) && ($flag)) { $BiosManufacturer = $1 }
+    if((/^release dates:\s*(\S+)/i) && ($flag)) { $BiosDate = $1 }
+    if((/^version:\s*(\S+)/i) && ($flag)) { $BiosVersion = $1 }
   }
 
   $flag=0;
   for(@dmidecode){
     if(/dmi type 1,/i){$flag=1;}
-    if((/dmi type (\d+),/i) && ($flag)){($1!='1')?last:1;}
-    if((/^serial number\s*:\s*(.+?)\s*$/i) && ($flag)) { $SystemSerial = $1 }
-    if((/^product name\s*:\s*(.+?)\s*$/i) && ($flag)) { $SystemModel = $1 }
-    if((/^manufacturer\s*:\s*(.+?)\s*$/i) && ($flag)) { $SystemManufacturer = $1 }
+    last if($flag && (/dmi type (\d+),/i) && ($1!=1));
+    if((/^serial number*:\s*(\S+)/i) && ($flag)) { $SystemSerial = $1 }
+    if((/^product name:\s*(\S+)/i) && ($flag)) { $SystemModel = $1 }
+    if((/^manufacturer:\s*(\S+)/i) && ($flag)) { $SystemManufacturer = $1 }
   }
 
+  $flag=0;
+  for(@dmidecode){
+    if(/dmi type 2,/i){$flag=1;}
+    last if($flag && (/dmi type (\d+),/i) && ($1!=2));
+    if((/^serial number:\s*(\S+)/i) && ($flag) && (!$SystemSerial)) { $SystemSerial = $1 }
+    if((/^product name:\s*(\S+)/i) && ($flag) && (!$SystemModel)) { $SystemModel = $1 }
+    if((/^manufacturer:\s*(\S+)/i) && ($flag) && (!$SystemManufacturer)) { $SystemManufacturer = $1 }
+  }
+
+# Some bad bioses don't provide a serial number so I check for CPU ID (e.g: server from dedibox.fr)
+  if (!$SystemSerial) {
+    $flag=0;
+    for(@dmidecode){
+      if(/dmi type 4,/i){$flag=1;}
+      elsif(/^processor information:/i){$flag=2;}
+      elsif((/^ID:\s*(.*)/i) && ($flag)) {
+	$SystemSerial = $1;
+	$SystemSerial =~ s/\ /-/g;
+	last
+      }
+    }
+  }
+
+  print "
+      SMANUFACTURER => $SystemManufacturer\n
+      SMODEL => $SystemModel\n
+      SSN => $SystemSerial\n
+      BMANUFACTURER => $BiosManufacturer\n
+      BVERSION => $BiosVersion\n
+      BDATE => $BiosDate\n";
 # Writing data
   $inventory->setBios ({
       SMANUFACTURER => $SystemManufacturer,
