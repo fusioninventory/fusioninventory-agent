8a50dd66be0a057063d859e0b4d31a931e5cb017
Gonéri Le Bouder <goneri@rulezlan.org>
2007-02-27 22:48:49 +0000

    improve dmidecode parsing

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/Bios.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/Bios.pm
@@ -12,39 +12,37 @@ sub run {
   my( $SystemSerial , $SystemModel, $SystemManufacturer, $BiosManufacturer,
     $BiosVersion, $BiosDate, $YEAR, $MONTH, $DAY, $HOUR, $MIN, $SEC);
 
-  my @dmidecode = `dmidecode`; # TODO retrive error
+  my @dmidecode = `dmidecode`;
   s/^\s+// for (@dmidecode);
-
-
   my $flag=0;
   for(@dmidecode){
     $flag=1 if /dmi type 0,/i;
     last if($flag && (/dmi type (\d+),/i) && ($1!=0));
-    if((/^vendor:\s*(\S+)/i) && ($flag)) { $BiosManufacturer = $1 }
-    if((/^release dates:\s*(\S+)/i) && ($flag)) { $BiosDate = $1 }
-    if((/^version:\s*(\S+)/i) && ($flag)) { $BiosVersion = $1 }
+    if((/^vendor:\s*(.+?)(\s*)$/i) && ($flag)) { $BiosManufacturer = $1 }
+    if((/^release\ date:\s*(.+?)(\s*)$/i) && ($flag)) { $BiosDate = $1 }
+    if((/^version:\s*(.+?)(\s*)$/i) && ($flag)) { $BiosVersion = $1 }
   }
-
+  
   $flag=0;
   for(@dmidecode){
     if(/dmi type 1,/i){$flag=1;}
     last if($flag && (/dmi type (\d+),/i) && ($1!=1));
-    if((/^serial number*:\s*(\S+)/i) && ($flag)) { $SystemSerial = $1 }
-    if((/^product name:\s*(\S+)/i) && ($flag)) { $SystemModel = $1 }
-    if((/^manufacturer:\s*(\S+)/i) && ($flag)) { $SystemManufacturer = $1 }
+    if((/^serial number:\s*(.+?)(\s*)$/i) && ($flag)) { $SystemSerial = $1 }
+    if((/^product name:\s*(.+?)(\s*)$/i) && ($flag)) { $SystemModel = $1 }
+    if((/^manufacturer:\s*(.+?)(\s*)$/i) && ($flag)) { $SystemManufacturer = $1 }
   }
 
   $flag=0;
   for(@dmidecode){
     if(/dmi type 2,/i){$flag=1;}
     last if($flag && (/dmi type (\d+),/i) && ($1!=2));
-    if((/^serial number:\s*(\S+)/i) && ($flag) && (!$SystemSerial)) { $SystemSerial = $1 }
-    if((/^product name:\s*(\S+)/i) && ($flag) && (!$SystemModel)) { $SystemModel = $1 }
-    if((/^manufacturer:\s*(\S+)/i) && ($flag) && (!$SystemManufacturer)) { $SystemManufacturer = $1 }
+    if((/^serial number:\s*(.+?)(\s*)/i) && ($flag) && (!$SystemSerial)) { $SystemSerial = $1 }
+    if((/^product name:\s*(.+?)(\s*)/i) && ($flag) && (!$SystemModel)) { $SystemModel = $1 }
+    if((/^manufacturer:\s*(.+?)(\s*)/i) && ($flag) && (!$SystemManufacturer)) { $SystemManufacturer = $1 }
   }
 
-# Some bad bioses don't provide a serial number so I check for CPU ID (e.g: server from dedibox.fr)
-  if (!$SystemSerial) {
+# Some bioses don't provide a serial number so I check for CPU ID (e.g: server from dedibox.fr)
+  if (!$SystemSerial ||$SystemSerial =~ /0+/) {
     $flag=0;
     for(@dmidecode){
       if(/dmi type 4,/i){$flag=1;}
