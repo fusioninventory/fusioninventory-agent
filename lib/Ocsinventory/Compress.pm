adaa266024105c28ac5a322002dcd451ec45ed8f
Gonéri Le Bouder <goneri@rulezlan.org>
2007-02-27 14:53:12 +0000

    remove lib/Ocsinventory/Agent/Backend/OS/BSD/Softwares.pm (move to Generic/Packaging) use of gzip if Compress::Zlib is not avalaible on the system

--- lib/Ocsinventory/Compress.pm
+++ lib/Ocsinventory/Compress.pm
@@ -1,5 +1,6 @@
 package Ocsinventory::Compress;
 # TODO I want to be able to send to the server uncompressed stream
+use strict;
 
 use File::Temp qw/ tempdir tempfile /;
 
@@ -10,19 +11,26 @@ sub new {
 
   my $logger = $self->{logger} = $params->{logger};
 
-  # TODO: If Compress::Zlib is not avalaible
-  # I'll emulate it by calling gzip
-  # directly 
+
   eval{require Compress::Zlib};
   $self->{natif} = ($@)?0:1;
 
+  my @tmpgzip;
   if ($self->{natif}) {
     $logger->debug ('Compress::Zlib is avalaible.');
+  } elsif (@tmpgzip = `gzip -h>>/dev/null` && @tmpgzip) {
+    $logger->debug ( # Today the sever only understands Zlib compressed data 
+	'Compress::Zlib is not avalaible! The data will be compressed with
+	gzip instead but won\'t be accepted by server prior 1.1'); # TODO is 1.1
+	# the correct release for this feature?
+      $self->{tmpdir} = tempdir( CLEANUP => 1 );
+      mkdir $self->{tmpdir};
+      if ( ! -d $self->{tmpdir} ) {
+	$logger->fault("Failed to create the temp dir `$self->{tmpdir}'");
+      }
   } else {
-    $logger->fault ( # Today the sever only understands Zlib compressed data 
-	'Compress::Zlib is not avalaible! The data won\'t be accepted '.
-	'by the server. Please install the Compress-Zlib package and '.
-	'restard the agent.');
+    $logger->fault ('I need the Compress::Zlib library or the gzip'.
+    ' command to compress the data');
   }
 
   bless $self;
@@ -34,29 +42,25 @@ sub compress {
   if ($self->{natif}) {
     return Compress::Zlib::compress($content);
   }
-#
-#  my $dir = tempdir( CLEANUP => 1 );
-#  my ($fh, $filename) = tempfile( DIR => $dir );
-#
-#  print $filename."\n";
-#  print $fh "toto";
-#
-#  close $fh;
-#
-#  open FILE, "<$filename";
-#  print foreach (<FILE>);
-#  close FILE;
-#
-#  system ("gzip $filename");
-#
+# Else I use gzip directly
+
+  my ($fh, $filename) = tempfile( DIR => $self->{tmpdir} );
+
+  print $fh $content;
+  close $fh;
+
+  system ("gzip --best $filename");
+
 #  print "filename ".$filename."\n"; 
-#
-#  my $ret;
-#  open FILE, "<$filename.gz";
-#  $ret .= $_ foreach (<FILE>);
-#  close FILE;
-#
-#  $ret;
+
+  my $ret;
+  open FILE, "<$filename.gz";
+  $ret .= $_ foreach (<FILE>);
+  close FILE;
+  if ( ! unlink "$filename.gz" ) {
+    $self->logger("Failed to remove `$filename.gz'");
+  }
+  $ret;
 }
 
 sub uncompress {
@@ -65,30 +69,27 @@ sub uncompress {
   if ($self->{natif}) {
     return Compress::Zlib::uncompress($data);
   }
-#
-#  my $dir = tempdir( CLEANUP => 1 );
-#  my ($fh, $filename) = tempfile( DIR => $dir, SUFFIX => '.gz' );
-#
-#  print $filename."\n";
-#  print $fh $data;
-#  close $fh;
-#
-#  open FILE, "<$filename";
-#  print foreach (<FILE>);
-#  close FILE;
-#
-#  print "filename ".$filename."\n"; 
-#  my ($uncompressed_filename) = $filename =~ /(.*)\.gz$/;
-#
-#  print "uncompressed_filename ".$uncompressed_filename."\n"; 
-#
-#  system ("gzip -d $uncompressed_filename");
-#
-#  my $ret;
-#  open FILE, "<$filename.gz";
-#  $ret .= $_ foreach (<FILE>);
-#  close FILE;
-#
-#  $ret;
+# Else I use gzip directly
+  my ($fh, $filename) = tempfile( DIR => $self->{tmpdir}, SUFFIX => '.gz' );
+
+  print $filename."\n";
+  print $fh $data;
+  close $fh;
+
+  open FILE, "<$filename";
+  print foreach (<FILE>);
+  close FILE;
+
+  system ("gzip -d $filename");
+  my ($uncompressed_filename) = $filename =~ /(.*)\.gz$/;
+
+  my $ret;
+  open FILE, "<$uncompressed_filename";
+  $ret .= $_ foreach (<FILE>);
+  close FILE;
+  if ( ! unlink "$uncompressed_filename" ) {
+    $self->logger("Failed to remove `$uncompressed_filename'");
+  }
+  $ret;
 }
 1;
