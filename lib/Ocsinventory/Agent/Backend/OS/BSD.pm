80d0a770c5b39eb9e2d52970a6233627428b8f0c
Thierry Lacoste <admin_miage@users.sourceforge.net>
2007-02-28 02:46:41 +0000

    improve OSComment

--- lib/Ocsinventory/Agent/Backend/OS/BSD.pm
+++ lib/Ocsinventory/Agent/Backend/OS/BSD.pm
@@ -21,10 +21,12 @@ sub run {
   chomp($OSVersion=`uname -r`);
 
   # Retrieve the origin of the kernel configuration file
-  my @kern_version = `sysctl -n kern.version`;
-  chomp ($OSComment = $kern_version[1]); # second line
-  $OSComment =~ s/^\s*//; # skip leading spaces
-
+  my ($date, $origin);
+  for (`sysctl -n kern.version`) {
+      $date = $1 if /^\S.*\#\d+:\s*(.*)/;
+      $origin = $1 if /^\s+(.+)$/;
+  }
+  $OSComment = $origin."\n".$date;
   # if there is a problem use uname -v
   chomp($OSComment=`uname -v`) unless $OSComment; 
   
