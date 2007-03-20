53f5e8b685820368e13abf9772dcb16098d3ff55
Gonéri Le Bouder <goneri@rulezlan.org>
2007-03-20 13:57:59 +0000

    use lslpp -c -l to find installed packages

---
+++ lib/Ocsinventory/Agent/Backend/OS/AIX/Software.pm
@@ -0,0 +1,32 @@
+package Ocsinventory::Agent::Backend::OS::Generic::Packaging::Software;
+
+use strict;
+use warnings;
+
+sub check {
+  `lslpp 2>&1`;
+  return if ($? >> 8)!=0;
+  1;
+}
+
+sub run {
+  my $params = shift;
+  my $inventory = $params->{inventory};
+
+  my @list;
+  my $buff;
+  foreach (`lslpp -c -l`) {
+    my @entry = split /:/,$_;
+    next unless (@entry);
+    next unless ($entry[1]);
+    next if $entry[1] =~ /^device/;
+
+    $inventory->addSoftwares({
+	'NAME'          => $entry[1],
+	'VERSION'       => $entry[2],
+	'COMMENTS'      => $entry[6],
+	});
+  }
+}
+
+1;
