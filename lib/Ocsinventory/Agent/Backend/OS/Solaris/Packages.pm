34b788ecb117f480d2bef99b323baea8cc403954
Gonéri Le Bouder <goneri@rulezlan.org>
2007-03-21 14:28:47 +0000

    retrieve Solaris package with pkginfo

---
+++ lib/Ocsinventory/Agent/Backend/OS/Solaris/Packages.pm
@@ -0,0 +1,41 @@
+package Ocsinventory::Agent::Backend::OS::Solaris::Packages;
+
+use strict;
+use warnings;
+
+sub check {
+  `which pkginfo 2>&1`;
+  return if ($? >> 8)!=0;
+  1;
+}
+
+sub run {
+  my $params = shift;
+  my $inventory = $params->{inventory};
+
+  my $name;
+  my $version;
+  my $comments;
+  foreach (`pkginfo -l`) {
+    if (/^\s*$/) {
+      $inventory->addSoftwares({
+          'NAME'          => $name,
+          'VERSION'       => $version,
+          'COMMENTS'      => $comments,
+          });
+
+      $name = '';
+      $version = '';
+      $comments = '';
+
+    } elsif (/PKGINST:\s+(.+)/) {
+      $name = $1;
+    } elsif (/VERSION:\s+(.+)/) {
+      $version = $1;
+    } elsif (/DESC:\s+(.+)/) {
+      $comments = $1;
+    }
+  }
+}
+
+1;
