b55dede2a090e1d8b634fe1c5d2228b61f8203ec
Gonéri Le Bouder <goneri@rulezlan.org>
2007-04-16 16:45:48 +0000

    * Turn of STDERR during the module execution to avoid useless message (e.g: foo: not found)          use --debug to turn them back

--- lib/Ocsinventory/LoggerBackend/Stderr.pm
+++ lib/Ocsinventory/LoggerBackend/Stderr.pm
@@ -4,20 +4,21 @@ sub new {
   my (undef, $params) = @_;
 
   my $self = {};
-
+  # STDERR has been hijacked, I take its saved ref
+  $self->{params} = $params->{params};
   bless $self;
 }
 
 sub addMsg {
 
-  my (undef, $args) = @_;
+  my ($self, $args) = @_;
 
   my $level = $args->{level};
   my $message = $args->{message};
 
   return if $message =~ /^$/;
-
-  print STDERR "[$level] $message\n";
+  my $tmp = $self->{params}->{savedstderr};
+  print $tmp "[$level] $message\n";
 }
 
 1;
