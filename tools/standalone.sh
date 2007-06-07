53715cd598b5f9122370f1a0172f574dae700c21
Gonéri Le Bouder <goneri@rulezlan.org>
2007-06-07 13:43:59 +0000

    add the clean flag to pp so the /tmp/par-* directory will be purged

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 MODULES="`cat MANIFEST  | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
-pp --lib lib $MODULES -o ocsinventory-agent.bin ocsinventory-agent 
+pp -C --lib lib $MODULES -o ocsinventory-agent.bin ocsinventory-agent 
 #pp --clean --lib lib $MODULES -p ocsinventory-agent 
