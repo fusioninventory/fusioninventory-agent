aa0855e1d12f456a55e8d26ac847b1fdcf5f925c
Gonéri Le Bouder <goneri@rulezlan.org>
2007-06-12 13:32:03 +0000

    Fix the comment

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-MODULES="`cat MANIFEST  | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
+MODULES="`cat MANIFEST  | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::PurePerl -M PerlIO "
+
+for i in `echo $MODULES| perl -nle 's/\-M//g;print'`; do  perl -I "lib" -M$i -e "1" || MISSING="$MISSING $i" ;done
+
+if [ ! -z $MISSING ]; then
+  echo "Some modules are missing in your installation, please install them first."
+  echo "->$MISSING"
+  exit 1
+fi
+
 pp --lib lib $MODULES -o ocsinventory-agent.bin ocsinventory-agent 
 #pp --lib lib $MODULES -p ocsinventory-agent 
