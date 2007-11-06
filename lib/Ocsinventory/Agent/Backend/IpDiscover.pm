63ffbd6ee2abfbb000a523852c4f6dc49f53c436
Gonéri Le Bouder <goneri@rulezlan.org>
2007-11-06 16:52:38 +0000

    initial import of the file

---
+++ lib/Ocsinventory/Agent/Backend/IpDiscover.pm
@@ -0,0 +1,16 @@
+package Ocsinventory::Agent::Backend::IpDiscover;
+
+use strict;
+
+sub check {
+    my $params = shift;
+
+    my $prologresp = $params->{prologresp};
+    my $mem = $params->{mem};
+    return unless ($prologresp && $prologresp->getOptionInfoByName("IPDISCOVER"));
+
+    1;
+}
+
+
+1;
