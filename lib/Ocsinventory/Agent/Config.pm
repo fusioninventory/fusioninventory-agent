f20bfff11f3f7ae1086bf58500da5fd34801a596
Gonéri Le Bouder <goneri@rulezlan.org>
2008-01-13 01:27:39 +0000

    try to read the config in /usr/local/etc/ocsinventory/ocsinventory-agent.cfg if the standard one is missing.

--- lib/Ocsinventory/Agent/Config.pm
+++ lib/Ocsinventory/Agent/Config.pm
@@ -1,15 +1,24 @@
 package Ocsinventory::Agent::Config;
 
+use strict;
+
 sub get {
   my $file = shift;
   my $config;
 
+  my @configfile = (
+      '/etc/ocsinventory/ocsinventory-agent.cfg',
+      '/usr/local/etc/ocsinventory/ocsinventory-agent.cfg',
+      $ENV{HOME}.'/.ocsinventory/ocsinventory-agent.cfg'
+  );
   if (!$file || !-f $file) {
-    $file = $ENV{HOME}.'/.ocsinventory/ocsinventory-agent.cfg';
-    if (!-f $file) {
-      $file = '/etc/ocsinventory/ocsinventory-agent.cfg';
-      return {} if (! -f $file);
+    foreach (@configfile) {
+        if (-f) {
+            $file = $_;
+            last;
+        }
     }
+    return {} unless -f $file;
   }
 
   $config->{configfile} = $file;
