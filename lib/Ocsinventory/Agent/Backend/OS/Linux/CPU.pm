58cfce16b5c375437e2d6a5d96860a96a466bc9a
Gonéri Le Bouder <goneri@rulezlan.org>
2008-02-01 21:38:14 +0000

    merge PowerPC and PC code to detect the CPU and improve/fix the regex

--- lib/Ocsinventory/Agent/Backend/OS/Linux/CPU.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/CPU.pm
@@ -16,11 +16,16 @@ sub run {
   my $processors;
   open CPUINFO, "</proc/cpuinfo" or warn;
   foreach(<CPUINFO>){
-    $processort = $1 if (/^(cpu|model\sname)\s*:\s*(.+)/i);
+    $processort = $2 if (/^(cpu|model\sname)\s*:\s*(.+)/i);
     $processorn++ if (/^processor/);
-    $processors = $1 if (/^(clock|cpu\sMHZ)\s*:\s*(\d+?)(|\.)$/i);
+    $processors = $2 if (/^(clock|cpu\sMHz)\s*:\s*(\d+)(|\.\d+)$/i);
   }
   close CPUINFO;
+
+  print STDERR "
+      PROCESSORT => $processort,
+      PROCESSORN => $processorn,
+      PROCESSORS => $processors\n";
 
 
   $inventory->setHardware({
@@ -31,59 +36,6 @@ sub run {
 
     });
 
-
-############ Motherboard
-  my $SystemManufacturer;
-  my $SystemModel;
-  my $SystemSerial;
-  my $BiosManufacturer;
-  my $BiosVersion;
-  my $BiosDate;
-
- if (open SERIAL,"</proc/device-tree/serial-number") {
-   $SystemSerial = <SERIAL>;
-   $SystemSerial =~ s/[^\,^\.^\w^\ ]//g; # I remove some unprintable char
-   close SERIAL;
- }
-
- if (open MODEL,"</proc/device-tree/model") {
-   $SystemModel = <MODEL>;
-   $SystemModel =~ s/[^\,^\.^\w^\ ]//g;
-   close MODEL;
- }
-
- if (open COLOR,"</proc/device-tree/color-code") {
-   my $tmp = <COLOR>;
-   close COLOR;
-   my ($color) = unpack "h7" , $tmp;
-   $SystemModel = $SystemModel." color: $color" if $color;
- }
-
- if (open OPENBOOT,"</proc/device-tree/openprom/model") {
-   $BiosVersion = <OPENBOOT>;
-   $BiosVersion =~ s/[^\,^\.^\w^\ ]//g;
-   close OPENBOOT;
- }
-
- if (open COPYRIGHT,"</proc/device-tree/copyright") {
-   my $tmp = <COPYRIGHT>;
-   close COPYRIGHT;
-
-   if ($tmp =~ /Apple/) {
-   # What about the Apple clone?
-     $BiosManufacturer = "Apple Computer, Inc.";
-     $SystemManufacturer = "Apple Computer, Inc." 
-   }
- }
-  
-  $inventory->setBios ({
-      SMANUFACTURER => $SystemManufacturer,
-      SMODEL => $SystemModel,
-      SSN => $SystemSerial,
-      BMANUFACTURER => $BiosManufacturer,
-      BVERSION => $BiosVersion,
-      BDATE => $BiosDate,
-    });
 
 }
 
