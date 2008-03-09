c827c5eb55b790cbbb7cc8282a72fd5422385a6e
Gonéri Le Bouder <goneri@rulezlan.org>
2008-03-09 20:45:45 +0000

    improve (a bit) tools/standalone.sh to avoid error if people launch it from the tools directory

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+[ -f standalone.sh ] && cd ..
+if [ ! -f tools/standalone.sh ]; then
+    echo "Can't find tools/standalone.sh"
+    exit 1
+fi
+
 MODULES="`cat MANIFEST | grep -v ^inc/ | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
 
 for i in `echo $MODULES| perl -nle 's/\-M//g;print'`; do  perl -I "lib" -M$i -e "1" || MISSING="$MISSING $i" ;done
@@ -11,4 +17,8 @@ if [ ! -z "$MISSING" ]; then
 fi
 
 pp --lib lib $MODULES -o ocsinventory-agent.bin ocsinventory-agent 
-#pp --lib lib $MODULES -p ocsinventory-agent 
+if [ -f ocsinventory-agent.bin ]; then
+    echo "ocsinventory-agent.bin generated!"
+else
+    echo "Failed to generate ocsinventory-agent.bin!"
+fi
