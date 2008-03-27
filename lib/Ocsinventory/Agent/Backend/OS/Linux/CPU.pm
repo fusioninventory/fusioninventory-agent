1032aa1dec46c58b5749ab03fc9f74764a1ade3f
Gonéri Le Bouder <goneri@rulezlan.org>
2008-03-27 12:53:20 +0000

    fix the CPU detection on Linux Sparc64

--- lib/Ocsinventory/Agent/Backend/OS/Linux/CPU.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/CPU.pm
@@ -17,10 +17,20 @@ sub run {
   open CPUINFO, "</proc/cpuinfo" or warn;
   foreach(<CPUINFO>){
     $processort = $2 if (/^(cpu|model\sname)\s*:\s*(.+)/i);
-    $processorn++ if (/^processor/);
+    $processorn++ if (/^processor|CPU\d+:\s+online/);
     $processors = $2 if (/^(clock|cpu\sMHz)\s*:\s*(\d+)(|\.\d+)$/i);
   }
   close CPUINFO;
+
+  # on some system (ARM, Sparc64), the CPU frequency is not in /proc/cpuinfo
+  # whereas $clocktickfile seems to give the correct information
+  my $clocktickfile = "/sys/devices/system/cpu/cpu0/clock_tick";
+  if (-f $clocktickfile) {
+    open CLOCKTICK, "<".$clocktickfile or warn;
+    chomp (my $clocktick = <CLOCKTICK> );
+    $processors = $1 if $clocktick =~ /^(\d+?)\d{6}$/; # 360010281
+    close CLOCKTICK;
+  }
 
   $inventory->setHardware({
 
