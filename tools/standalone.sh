d27c2613dc46c79440ffc42f6da5f5fc7705be31
Gonéri Le Bouder <goneri@rulezlan.org>
2008-05-07 15:34:53 +0000

    standalone.sh checks PAR::Packer presence now

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -13,6 +13,15 @@ if [ ! -f MANIFEST ]; then
     exit 1
 fi
 
+if [ ! -x "`which zpp 2>/dev/null`" ]; then
+    echo "Can't find the pp command. Please install PAR::Packer:"
+    echo " -CPAN: 'cpan PAR::Packer'"
+    echo " -source: sownload the archive from"
+    echo "  http://search.cpan.org/dist/PAR-Packer/"
+    echo " -Debian/Ubuntu: aptitude install libpar-packer-perl"
+    exit 1
+fi
+
 MODULES="`cat MANIFEST | grep -v ^inc/ | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
 
 for i in `echo $MODULES| perl -nle 's/\-M//g;print'`; do  perl -I "lib" -M$i -e "1" || MISSING="$MISSING $i" ;done
