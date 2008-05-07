534ff3b340047c33df6ee7567e97c9883143323f
Gonéri Le Bouder <goneri@rulezlan.org>
2008-05-07 15:52:07 +0000

    only inventory installed .deb

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
@@ -9,9 +9,8 @@ sub run {
   my $params = shift;
   my $inventory = $params->{inventory};
 
-  foreach(`COLUMNS=200 dpkg -l`){
-    if (/^[uirph]/){
-      /^(\w+)\s+(\S+)\s+(\S+)\s+(.*)/;
+  foreach(`COLUMNS=200 dpkg -l`) {
+     if (/^ii\s+(\S+)\s+(\S+)\s+(.*)/) {
       $inventory->addSoftwares ({
 	  'NAME'          => $2,
 	  'VERSION'       => $3,
