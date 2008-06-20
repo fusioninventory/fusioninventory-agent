4a70e561f8a213ddd3a167538d5ef799b9da0aa8
Wes Young <saxjazman9@users.sourceforge.net>
2008-06-20 00:13:41 +0000

    added STANDALONE build scripts for Darwin/OSX

---
+++ tools/darwin/STANDALONE_STEP2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# package maker might spit out some permissions errors if the app or it's folders are on your system already, this is usually OK, read them to make sure 
+echo "building package"
+sudo rm -R -f ./OCSNG.pkg
+sudo /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -proj OCSNG.pmproj -p ./OCSNG.pkg
+
+echo "taring up package and installer files"
+tar -zvcf OCSNG.pkg.tar.gz org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg
+
+echo "done"
