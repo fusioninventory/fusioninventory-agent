95c2c854e3d3d238f58189c33ae04cfcc0b1b2f1
Gonéri Le Bouder <goneri@rulezlan.org>
2008-06-23 13:48:46 +0000

    * standalone.sh, don't use grep -v since it's not POSIX

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -22,7 +22,7 @@ if [ ! -x "`which pp 2>/dev/null`" ]; then
     exit 1
 fi
 
-MODULES="`cat MANIFEST | grep -v ^inc/ | grep pm$ | perl -pe 's/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
+MODULES="`cat MANIFEST | perl -pe 's/.*// unless (!/^inc/ && /pm$/); s/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO "
 
 for i in `echo $MODULES| perl -nle 's/\-M//g;print'`; do  perl -I "lib" -M$i -e "1" || MISSING="$MISSING $i" ;done
 
