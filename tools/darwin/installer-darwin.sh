1ecf6d6addb96fb21c13e205db61027c80bb58d6
Wes Young <saxjazman9@users.sourceforge.net>
2008-06-23 15:32:50 +0000

    updates for jaguar compatibility

--- tools/darwin/installer-darwin.sh
+++ tools/darwin/installer-darwin.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo 'determining OS Version'
+OSVER=`uname -r`
+echo "OS: $OSVER"
+
 echo 'Running user creation script'
 
 ./dscl-adduser.sh
@@ -14,7 +18,7 @@ sudo chmod 770 $TPATH/
 sudo cp ./ocsinventory-agent.cfg $TPATH/
 
 TPATH="/var/lib/ocsinventory-agent"
-sudo mkdir $TPATH
+sudo mkdir -p $TPATH
 sudo chown 3995:admin $TPATH
 
 TPATH="/var/log/ocsng.log"
@@ -22,15 +26,29 @@ sudo touch $TPATH
 sudo chown root:admin $TPATH
 sudo chmod 660 $TPATH
 
-TPATH="/Library/LaunchAgents/"
-sudo cp org.ocsng.agent.plist $TPATH
-sudo chown root:wheel $TPATH/org.ocsng.agent.plist
-sudo chmod 644 $TPATH/org.ocsng.agent.plist
+if [ "$OSVER" == "7.9.0" ]; then
+	echo "Found Jaguar OS, using 10.3 StartupItems setup"
+	TPATH="/System/Library/StartupItems"
+	sudo cp -R ./jag-startup/OCSInventory $TPATH/
+	sudo chown -R root:wheel $TPATH/OCSInventory
+	sudo chmod 755 $TPATH/OCSInventory
+	sudo chmod 644 $TPATH/OCSInventory/StartupParameters.plist
+	sudo chmod 755 $TPATH/OCSInventory/OCSInventory
 
-echo 'Loading Service'
-sudo launchctl load $TPATH
+	echo 'Starting Service using Sudo'
+	sudo /System/Library/StartupItems/OCSInventory/OCSInventory start
+else
+	echo "Found Tiger or newer OS, using LaunchAgent plists"
+	TPATH="/Library/LaunchAgents/"
+	sudo cp org.ocsng.agent.plist $TPATH
+	sudo chown root:wheel $TPATH/org.ocsng.agent.plist
+	sudo chmod 644 $TPATH/org.ocsng.agent.plist
 
-echo 'Starting Service'
-sudo launchctl start org.ocsng.agent
+	echo 'Loading Service'
+	sudo launchctl load $TPATH
+
+	echo 'Starting Service'
+	sudo launchctl start org.ocsng.agent
+fi
 
 echo 'done'
--- tools/darwin/uninstall-darwin.sh
+++ tools/darwin/uninstall-darwin.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-PID=`ps -e | grep OCSNG | grep -v grep | awk '{print $2}'`
+OSVER=`uname -r`
+echo "OSVer is $OSVER"
+
+PID=`ps ax -e | grep OCSNG | grep -v grep | awk '{print $1}'`
 if [ "$PID" !=  "" ]; then
-	echo 'killing process'
+	echo "killing process: $PID"
 	sudo kill $PID
 fi
 
-FILES="/Library/LaunchAgents/org.ocsng.agent.plist /etc/ocsinventory-agent/ /var/lib/ocsinventory-agent/ /Applications/OCSNG.app /var/log/ocsng.log"
+FILES="/etc/ocsinventory-agent/ /var/lib/ocsinventory-agent/ /Applications/OCSNG.app /var/log/ocsng.log"
 
-echo 'Stopping and unloading service'
-launchctl stop org.ocsng.agent
-launchctl unload /Library/LaunchAgents/org.ocsng.agent.plist
+if [ "$OSVER" == "7.9.0" ]; then
+	FILES="$FILES /Library/StartupItems/OCSInventory"
+else
+	FILES="$FILES /Library/LaunchAgents/org.ocsng.agent.plist"
+
+	echo 'Stopping and unloading service'
+	launchctl stop org.ocsng.agent
+	launchctl unload /Library/LaunchAgents/org.ocsng.agent.plist
+fi
 
 for FILE in $FILES; do
   echo 'removing '.$FILE
