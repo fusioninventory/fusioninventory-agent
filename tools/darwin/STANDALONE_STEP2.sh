9918beb2ba930848de9e38a4309e1ca9515f44fe
Wes Young <saxjazman9@users.sourceforge.net>
2008-07-28 13:43:22 +0000

    minor changes

--- tools/darwin/STANDALONE_STEP2.sh
+++ tools/darwin/STANDALONE_STEP2.sh
@@ -5,9 +5,15 @@ echo "building package"
 sudo rm -R -f ./OCSNG.pkg
 sudo /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -proj OCSNG.pmproj -p ./OCSNG.pkg
 
-echo "taring up package and installer files"
-tar -zvcf Agent-MacOSX.tar.gz --exclude=CVS 10_3_9-startup/ org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg modules.conf cacert.pem
+FILES="INSTALL.txt ver 10_3_9-startup org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg modules.conf cacert.pem"
 
-zip Agent-MacOSX org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg modules.conf cacert.pem
+#echo "taring up package and installer files"
+#tar -zvcf Agent-MacOSX.tar.gz --exclude=CVS ver 10_3_9-startup/ org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg modules.conf cacert.pem
 
+#zip -r Agent-MacOSX ver org.ocsng.agent.plist OCSNG.pkg installer-darwin.sh uninstall-darwin.sh dscl-adduser.sh dscl-remove-user.sh ocsinventory-agent.cfg modules.conf cacert.pem
+
+mkdir Agent-MacOSX
+cp -R $FILES Agent-MacOSX/
+zip -r Agent-MacOSX Agent-MacOSX/
+rm -R -f Agent-MacOSX/
 echo "done"
