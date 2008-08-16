4d9a7366559ffba55652632282071669111a38e5
Gonéri Le Bouder <goneri@rulezlan.org>
2008-08-16 16:44:02 +0000

    really use the --nosoftware flag

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging.pm
@@ -6,7 +6,7 @@ sub check {
   my $params = shift;
   
   # Do not run an package inventory if there is the --nosoft parameter
-  return if ($params->{params}->{nosoft});
+  return if ($params->{params}->{nosoftware});
    
   1;
 }
