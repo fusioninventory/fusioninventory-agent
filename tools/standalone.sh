b618b4f16df172112c397310914a6a4c6cdb1a0d
Gonéri Le Bouder <goneri@rulezlan.org>
2009-01-27 12:23:18 +0000

    add ModuleToLoad.pm to pass the list of the mod to load

--- tools/standalone.sh
+++ tools/standalone.sh
@@ -22,18 +22,31 @@ if [ ! -x "`which pp 2>/dev/null`" ]; then
     exit 1
 fi
 
-MODULES="`cat MANIFEST | perl -pe 's/.*// unless (!/^inc/ && /pm$/); s/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::Expat -M XML::SAX::PurePerl -M PerlIO -M Getopt::Long -M Digest::MD5"
+BACKENDMODULE=`cat MANIFEST | perl -pe 's/.*// unless (/Ocsinventory\/Agent\/Backend\// && !/^inc/ && /pm$/); s/lib\/(.*)\.pm/ $1/; s/\//::/g; chomp'`
+
+cat > lib/Ocsinventory/Agent/Backend/ModuleToLoad.pm <<EOF
+# This is a workaround for PAR::Packer. Since it resets @INC
+# I can't find the backend modules to load dynamically. So
+# I prepare a list and include it.
+package Ocsinventory::Agent::Backend::ModuleToLoad;
+
+our @list = qw/ $BACKENDMODULE /; 
+
+1;
+EOF
+
+MODULES="`cat MANIFEST | perl -pe 's/.*// unless (!/^inc/ && /pm$/); s/lib\/(.*)\.pm/ -M $1/; s/\//::/g; chomp'` -M XML::SAX::PurePerl -M PerlIO -M Getopt::Long -M Digest::MD5 -M Ocsinventory::Agent::Backend::ModuleToLoad"
 
 for i in `echo $MODULES| perl -nle 's/\-M//g;print'`; do  perl -I "lib" -M$i -e "1" || MISSING="$MISSING $i" ;done
 
 if [ ! -z "$MISSING" ]; then
-  echo "Some modules are missing in your installation, please install them first."
+  echo "Some modules are missing in your installation or failed to build, please install them first."
   echo "->$MISSING"
   exit 1
 fi
 
 #pp --lib lib $MODULES -o ocsinventory-agent.bin ocsinventory-agent
-pp --lib lib $MODULES -p ocsinventory-agent -o ocsinventory-agent.par
+pp --lib lib $MODULES -B -p ocsinventory-agent -vvv -o ocsinventory-agent.par
 pp -o ocsinventory-agent.bin ocsinventory-agent.par
 
 if [ -f ocsinventory-agent.bin ]; then
