a38c809f7d624e5b3e29d66c6b85a526cc15768c
Julien Safar <jsa99@users.sourceforge.net>
2009-05-09 08:48:48 +0000

    Linux: Detect DNS Servers

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Domains.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Domains.pm
@@ -12,30 +12,33 @@ sub run {
   my $inventory = $params->{inventory};
 
   my $domain;
-
+  my %domain;
+  my @dns_list;
+  my $srvdns;
   chomp($domain = `hostname -d`);
 
-  if (!$domain) {
-    my %domain;
-
-    open RESOLV, "/etc/resolv.conf" or warn;
+  open RESOLV, "/etc/resolv.conf" or warn;
     while(<RESOLV>){
-         if (/^(domain|search)\s+(.+)/) {
-               foreach (split(/\s+/,$2)) {
-                       $domain{$_} = 1;
-               }
-         }
+      if (/^nameserver\s+(\S+)/i) {
+        push(@dns_list,$1);
+      }
+      elsif (!$domain) {
+        $domain{$2} = 1 if (/^(domain|search)\s+(.+)/);
+      }
     }
-    close RESOLV;
+  close RESOLV;
 
+  if (!$domain) {
     $domain = join "/", keys %domain;
   }
-
+  
+  $srvdns=join("/",@dns_list);
   # If no domain name, we send "WORKGROUP"
   $domain = 'WORKGROUP' unless $domain;
 
   $inventory->setHardware({
-      WORKGROUP => $domain
+      WORKGROUP => $domain,
+      DNS => $srvdns
     });
 
 }
