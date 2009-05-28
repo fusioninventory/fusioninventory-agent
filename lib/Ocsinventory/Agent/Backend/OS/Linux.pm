b7818971f5eaf96d3ed7c63475c1f7aa2c2bc330
Gonéri Le Bouder <goneri@rulezlan.org>
2009-05-28 17:41:30 +0000

    Linux: Collect LASTLOGGEDUSER and DATELASTLOGGEDUSER (Nicolas EISEN)

--- lib/Ocsinventory/Agent/Backend/OS/Linux.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux.pm
@@ -12,11 +12,24 @@ sub run {
 
   chomp (my $osversion = `uname -r`);
 
+  my $lastloggeduser;
+  my $datelastlog;
+  my @query = `last -R`;
+  
+  foreach ($query[0]) {
+    if ( s/^(\S+)\s+\S+\s+(\S+\s+\S+\s+\S+\s+\S+)\s+.*// ) {
+      $lastloggeduser = $1;
+      $datelastlog = $2;
+    }
+  }
+  
   # This will probably be overwritten by a Linux::Distro module.
   $inventory->setHardware({
       OSNAME => "Linux",
       OSCOMMENTS => "Unknow Linux distribution",
-      OSVERSION => $osversion
+      OSVERSION => $osversion,
+      LASTLOGGEDUSER => $lastloggeduser,
+      DATELASTLOGGEDUSER => $datelastlog
     });
 }
 
