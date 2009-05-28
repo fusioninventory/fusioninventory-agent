fc7a54c491930f872bfcef52a964e0283443f45a
Gonéri Le Bouder <goneri@rulezlan.org>
2009-05-28 17:42:06 +0000

    DPKG: collect the Installed-Size (Nicolas EISEN)

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
@@ -8,19 +8,19 @@ sub check { can_run("dpkg") }
 sub run {
   my $params = shift;
   my $inventory = $params->{inventory};
-
+  
 # use dpkg-query -W -f='${Package}|||${Version}\n'
-  foreach(`COLUMNS=200 dpkg -l`) {
-     if (/^ii\s+(\S+)\s+(\S+)\s+(.*)/) {
-      $inventory->addSoftwares ({
-	  'NAME'          => $1,
-	  'VERSION'       => $2,
-	  'COMMENTS'      => $3,
-	  });
+  foreach(`dpkg-query -W -f='\${Package}---\${Version}---\${Installed-Size}---\${Description}\n'`) {
+     if (/^(\S+)---(\S+)---(\S+)---(.*)/) {     	     	
+       $inventory->addSoftwares ({
+         'NAME'          => $1,
+         'VERSION'       => $2,
+         'FILESIZE'      => $3,
+         'COMMENTS'      => $4,
+         'FROM'          => 'deb'
+       });
     }
-
   }
-
 }
 
 1;
