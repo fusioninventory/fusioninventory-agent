8ae97133ab11f5fc195cb1ac4e0cd10a282108fa
Gonéri Le Bouder <goneri@rulezlan.org>
2009-06-12 11:45:15 +0000

    slot detection for sun_class 1 system (Sylvain LUCE)

--- lib/Ocsinventory/Agent/Backend/OS/Solaris/Slots.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Solaris/Slots.pm
@@ -14,9 +14,26 @@ sub run {
 
   my $flag;
   my $flag_pci;
+  my $model;
+  my $sun_class;
 
-  foreach(`prtdiag`) {
-#print $_."\n";
+  $model=`uname -i`;
+  # debug print model
+  #print "Model: '$model'";
+  # cut the CR from string model
+  $model = substr($model, 0, length($model)-1);
+  # we map (hopfully) our server model to a known class
+  if ($model eq "SUNW,SPARC-Enterprise") { $sun_class = 1; }
+  else { $sun_class = 0; }
+  #Debug
+  #print "sun_class : $sun_class\n";
+
+
+    foreach (`prtdiag`) {
+    #print $_."\n";
+ 
+   if ( $sun_class == 0 )
+   {
     last if(/^\=+/ && $flag_pci);
     next if(/^\s+/ && $flag_pci);
     if($flag && $flag_pci && /^(\S+)\s+/){
@@ -26,22 +43,53 @@ sub run {
       $designation = $1;
     }
     if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
-      $description = $1; 	  
+      $description = $1;
     }
     if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
-      $status = $1; 	  
+      $status = $1;
     }
     if($flag && $flag_pci){
       $inventory->addSlots({
-	  DESCRIPTION =>  $description,
-	  DESIGNATION =>  $designation,
-	  NAME 		  =>  $name,
-	  STATUS	  =>  $status,
-	  }); 
+          DESCRIPTION =>  $description,
+          DESIGNATION =>  $designation,
+          NAME            =>  $name,
+          STATUS          =>  $status,
+          });
     }
-    if(/^=+\s+IO Cards/){$flag_pci = 1;}	
-    if($flag_pci && /^-+/){$flag = 1;}   	
+    if(/^=+\s+IO Cards/){$flag_pci = 1;}
+    if($flag_pci && /^-+/){$flag = 1;}
 
+   }
+   if ( $sun_class == 1 )
+   {
+	last if(/^\=+/ && $flag_pci && $flag);
+	
+	if($flag && $flag_pci && /^\s+(\d+)/){
+           $name = "LSB " . $1;	 
+	}
+ 	if($flag && $flag_pci && /^\s+\S+\s+(\S+)/){
+                $description = $1;
+        }
+        if($flag && $flag_pci && /^\s+\S+\s+\S+\s+(\S+)/){
+                $designation = $1;
+        }
+	$status = " ";
+	
+	#Debug
+	#if ($flag && $flag_pci){print "$name" . "||||" . "$designation" . "||" . "$description\n";}
+    	#print $_."\n";
+
+        if($flag && $flag_pci){
+        $inventory->addSlots({
+          DESCRIPTION =>  $description,
+          DESIGNATION =>  $designation,
+          NAME            =>  $name,
+          STATUS          =>  $status,
+          });
+        }
+        if(/^=+\S+\s+IO Cards/){$flag_pci = 1;  }
+        if($flag_pci && /^-+/){$flag = 1;}
+   }
   }
 }
 1;
