52f2893f5238059195ce24e12d7e2185fd57343a
David Olivier <adadov@users.sourceforge.net>
2009-07-29 12:46:36 +0000

    Adding PATA support to Storages.pm for MacOSX

--- lib/Ocsinventory/Agent/Backend/OS/MacOS/Storages.pm
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS/Storages.pm
@@ -6,20 +6,8 @@ sub check {return can_load('Mac::SysProfile');}
 
 sub getManufacturer {
   my $model = shift;
-  if($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i) {
+  if($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend|matshita)/i) {
     return ucfirst(lc($1));
-  }
-  elsif ($model =~ /^HP/) {
-    return "Hewlett Packard";
-  }
-  elsif ($model =~ /^WDC/) {
-    return "Western Digital";
-  }
-  elsif ($model =~ /^ST/) {
-    return "Seagate";
-  }
-  elsif ($model =~ /^HD/ or $model =~ /^IC/ or $model =~ /^HU/) {
-    return "Hitachi";
   }
 }
 
@@ -33,15 +21,27 @@ sub run {
   my $devices = {};
 
   my $prof = Mac::SysProfile->new();
+
+  # Get SATA Drives
   my $sata = $prof->gettype('SPSerialATADataType');
 
   return undef unless( ref($sata) eq 'HASH' );
   
+  use Data::Dumper;
+
   foreach my $x ( keys %$sata ) {
     my $controller = $sata->{$x};
     foreach my $y ( keys %$controller ) {
       next unless( ref($sata->{$x}->{$y}) eq 'HASH' );
       my $drive = $sata->{$x}->{$y};
+
+      my $description;
+      if ( $y =~ /DVD/i || $y =~ /CD/i ) {
+        $description = 'CD-ROM Drive';
+      }
+      else {
+        $description = 'Disk drive';
+      }
 
       my $size = $drive->{'Capacity'};
       $size =~ s/ GB//;
@@ -58,16 +58,52 @@ sub run {
         DISKSIZE => $size,
         FIRMWARE => $drive->{'Revision'},
         MANUFACTURER => $manufacturer,
-        DESCRIPTION => 'disk drive',
+        DESCRIPTION => $description,
         MODEL => $model
       };
-
+    }
+  }
+  
+  # Get PATA Drives
+  my $pata = $prof->gettype('SPParallelATADataType');
+  
+  foreach my $x ( keys %$pata ) {
+    my $controller = $pata->{$x};
+    foreach my $y ( keys %$controller ) {
+      next unless ( ref($pata->{$x}->{$y}) eq 'HASH' );
+      my $drive = $pata->{$x}->{$y};
+      
+      my $description;
+      if ( $y =~ /DVD/i || $y =~ /CD/i ) {
+        $description = 'CD-ROM Drive';
+      }
+      else {
+        $description = 'Disk drive';
+      }
+      
+      my $manufacturer = getManufacturer($y);
+      
+      my $model = $drive->{'Model'};
+      
+      my $size;
+      
+      $devices->{$y} = {
+        NAME => $y,
+        SERIAL => $drive->{'Serial Number'},
+        DISKSIZE => $size,
+        FIRMWARE => $drive->{'Revision'},
+        MANUFACTURER => $manufacturer,
+        DESCRIPTION => $description,
+        MODEL => $model
+      };
     }
   }
 
   foreach my $device ( keys %$devices ) {
     $inventory->addStorages($devices->{$device});
   }
+  
+#  warn Dumper($devices);
 
 }
 
