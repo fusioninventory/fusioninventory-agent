c1252c1f55504c3a6f00ddd3e87f8d4dea628dd5
David Olivier <adadov@users.sourceforge.net>
2009-07-29 08:36:18 +0000

    Adding Storages.pm for MacOS

---
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS/Storages.pm
@@ -0,0 +1,65 @@
+package Ocsinventory::Agent::Backend::OS::MacOS::Storages;
+
+use strict;
+
+sub check {return can_load('Mac::SysProfile');}
+
+sub getManufacturer {
+  my $model = shift;
+  if($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i) {
+    return ucfirst(lc($1));
+  }
+}
+
+sub run {
+
+  use Mac::SysProfile;
+  my $params = shift;
+  my $logger = $params->{logger};
+  my $inventory = $params->{inventory};
+
+  my $devices = {};
+
+  my $prof = Mac::SysProfile->new();
+  my $sata = $prof->gettype('SPSerialATADataType');
+
+  return undef unless( ref($sata) eq 'HASH' );
+  
+  use Data::Dumper;
+
+  foreach my $x ( keys %$sata ) {
+    my $controller = $sata->{$x};
+    foreach my $y ( keys %$controller ) {
+      next unless( ref($sata->{$x}->{$y}) eq 'HASH' );
+      my $drive = $sata->{$x}->{$y};
+
+      my $size = $drive->{'Capacity'};
+      $size =~ s/ GB//;
+      $size *= 1024;
+
+      my $manufacturer = getManufacturer($y);
+
+      my $model = $drive->{'Model'};
+      $model =~ s/\s*$manufacturer\s*//i;
+
+      $devices->{$y} = {
+        NAME => $y,
+        SERIAL => $drive->{'Serial Number'},
+        DISKSIZE => $size,
+        FIRMWARE => $drive->{'Revision'},
+        MANUFACTURER => $manufacturer,
+        DESCRIPTION => 'disk drive',
+        MODEL => $model
+      };
+      warn Dumper($devices);
+
+    }
+  }
+
+  foreach my $device ( keys %$devices ) {
+    $inventory->addStorages($devices->{$device});
+  }
+
+}
+
+1;
