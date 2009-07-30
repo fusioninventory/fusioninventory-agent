0549df87cc8ecc70cb14161e5113262eb9e41716
David Olivier <adadov@users.sourceforge.net>
2009-07-30 09:33:36 +0000

    Adding Hostname.pm to take Hostname from system_profiler

---
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS/Hostname.pm
@@ -0,0 +1,26 @@
+package Ocsinventory::Agent::Backend::OS::MacOS::Hostname;
+
+sub check {
+  return 1 if can_load ("Mac::SysProfile");
+  0;
+}
+
+# Initialise the distro entry
+sub run {
+  my $params = shift;
+  my $inventory = $params->{inventory};
+
+  my $hostname;
+
+  use Mac::SysProfile;
+  my $prof = Mac::SysProfile->new();
+  my $nfo = $prof->gettype('SPSoftwareDataType');
+  
+  return undef unless(ref($nfo) eq 'HASH');
+  
+  $hostname = $nfo->{'System Software Overview'}->{'Computer Name'};
+  
+  $inventory->setHardware ({NAME => $hostname}) if $hostname;
+}
+
+1;
