890f74312d33e77162c4b85b0dd2d0c42bd8e7df
Philippe Libat <pli01@users.sourceforge.net>
2009-07-31 20:45:41 +0000

    Add Virtualization::VmwareFusion module (Walid Nouh)

---
+++ lib/Ocsinventory/Agent/Backend/Virtualization/VmWareFusion.pm
@@ -0,0 +1,52 @@
+package Ocsinventory::Agent::Backend::Virtualization::VmwareFusion;
+#
+# initial version: Walid Nouh
+#
+
+use strict;
+
+sub check { return can_run('/Library/Application\ Support/VMware\ Fusion/vmrun') }
+
+sub run {
+    my $params = shift;
+    my $inventory = $params->{inventory};
+	
+    my $uuid;
+    my $mem;
+    my $status;
+    my $name;
+    my $index = 0;
+
+    my $commande = "/Library/Application\\ Support/VMware\\ Fusion\/vmrun list";
+    foreach my $vmxpath ( `$commande` ) {
+            if ($index>0) {
+                    $vmxpath =~ s/ /\\\ /g;
+                    my @vminfos = `cat $vmxpath`;
+                    foreach my $line (@vminfos) {
+                            if ($line =~ m/^displayName =\s\"+(.*)\"/) {
+                                    $name = $1;
+                            }
+                            elsif ($line =~ m/^memsize =\s\"+(.*)\"/) {
+                                    $mem = $1;
+                            }
+                            elsif ($line =~ m/^uuid.bios =\s\"+(.*)\"/) {
+                                    $uuid = $1;
+                            }
+                    }
+
+            $uuid =~ s/\ //g;
+            $inventory->addVirtualMachine ({
+                        NAME      => $name,
+                        VCPU      => 1,
+                        UUID      => $uuid,
+                        MEMORY    => $mem,
+                        STATUS    => "Running",
+                        SUBSYSTEM => "VmWare Fusion",
+                        VMTYPE    => "VmWare",
+                    });
+            }
+            $index++;
+    }
+}
+
+1;
