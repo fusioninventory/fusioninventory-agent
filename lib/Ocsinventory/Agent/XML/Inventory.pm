2e43e50462d0e60cdf814bde295306e1fa18ef73
David Olivier <adadov@users.sourceforge.net>
2009-08-04 12:18:16 +0000

    Correct unprintable caractere detection

--- lib/Ocsinventory/Agent/XML/Inventory.pm
+++ lib/Ocsinventory/Agent/XML/Inventory.pm
@@ -568,9 +568,23 @@ sub getContent {
   my $content = XMLout( $self->{h}, RootName => 'REQUEST', XMLDecl => '<?xml version="1.0" encoding="UTF-8"?>', SuppressEmpty => undef );
 
   my $clean_content;
+  
   # To avoid strange breakage I remove the unprintable caractere in the XML
   foreach (split "\n", $content) {
-      s/[[:cntrl:]]//g;
+#      s/[[:cntrl:]]//g;
+    if (! m/\A(
+      [\x09\x0A\x0D\x20-\x7E]            # ASCII
+      | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+      |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+      | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+      |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+      |  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+      | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+      |  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+      )*\z/x) { s/[[:cntrl:]]//g; print 'non utf-8'.$_."\n"; }
+
+      s/\r|\n//g;
+
       $clean_content .= $_."\n";
   }
 
