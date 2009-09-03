42d7297f787279f16a4353286c815682b35fc2e9
Nicolas EISEN <nicolas.eisen@gmail.com>
2009-09-03 16:11:19 +0200

    Fix HARDWARE:UUID empty field

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/UUID.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Dmidecode/UUID.pm
@@ -2,6 +2,8 @@ package Ocsinventory::Agent::Backend::OS::Generic::Dmidecode::UUID;
 
 use strict;
 
+sub check { return can_run('dmidecode') }
+
 sub run {
   my $params = shift;
   my $inventory = $params->{inventory};
@@ -13,36 +15,30 @@ sub run {
   my @dmidecode = split (/\n/, $dmidecode);
   # add a new line at the end
   push @dmidecode, "\n";
-
+  # delete all space at the beginning of each line
   s/^\s+// for (@dmidecode);
 
   my $flag;
-
   my $uuid;
 
   foreach (@dmidecode) {
-
      if (/dmi type 1,/i) {
          $flag = 1;
          next;
      }
 
      if ($flag) {
-        if (/^\s+UUID:\s+(\S+)/) {
+        if (/^UUID:\s+(\S+)/) {
             $uuid = $1;
             last;
         } elsif (/^$/) { # End of the TYPE 1 section
             last;
         }
-
       }
    }
 
-
    $inventory->setHardware({
-
       UUID => $uuid,
-
    });
 
 }
