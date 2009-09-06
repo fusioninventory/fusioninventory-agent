338666068167f87bd8a558b2c870f65c2668944e
Goneri Le Bouder <goneri@rulezlan.org>
2009-09-06 21:44:07 +0200

    indent

--- lib/Ocsinventory/Agent/Backend/Virtualization/VmWareDesktop.pm
+++ lib/Ocsinventory/Agent/Backend/Virtualization/VmWareDesktop.pm
@@ -10,7 +10,7 @@ sub check { return can_run('/Library/Application\ Support/VMware\ Fusion/vmrun')
 sub run {
     my $params = shift;
     my $inventory = $params->{inventory};
-	
+
     my $uuid;
     my $mem;
     my $status;
@@ -20,29 +20,29 @@ sub run {
     my $commande = "/Library/Application\\ Support/VMware\\ Fusion\/vmrun list";
     foreach my $vmxpath ( `$commande` ) {
         next unless $i++ > 0; # Ignore the first line
-                    $vmxpath =~ s/ /\\\ /g;
-                    my @vminfos = `cat $vmxpath`;
-                    foreach my $line (@vminfos) {
-                            if ($line =~ m/^displayName =\s\"+(.*)\"/) {
-                                    $name = $1;
-                            }
-                            elsif ($line =~ m/^memsize =\s\"+(.*)\"/) {
-                                    $mem = $1;
-                            }
-                            elsif ($line =~ m/^uuid.bios =\s\"+(.*)\"/) {
-                                    $uuid = $1;
-                            }
-                    }
+        $vmxpath =~ s/ /\\\ /g;
+        my @vminfos = `cat $vmxpath`;
+        foreach my $line (@vminfos) {
+            if ($line =~ m/^displayName =\s\"+(.*)\"/) {
+                $name = $1;
+            }
+            elsif ($line =~ m/^memsize =\s\"+(.*)\"/) {
+                $mem = $1;
+            }
+            elsif ($line =~ m/^uuid.bios =\s\"+(.*)\"/) {
+                $uuid = $1;
+            }
+        }
 
-            $inventory->addVirtualMachine ({
-                        NAME      => $name,
-                        VCPU      => 1,
-                        UUID      => $uuid,
-                        MEMORY    => $mem,
-                        STATUS    => "running",
-                        SUBSYSTEM => "VmWare Fusion",
-                        VMTYPE    => "VmWare",
-                    });
+        $inventory->addVirtualMachine ({
+                NAME      => $name,
+                VCPU      => 1,
+                UUID      => $uuid,
+                MEMORY    => $mem,
+                STATUS    => "running",
+                SUBSYSTEM => "VmWare Fusion",
+                VMTYPE    => "VmWare",
+            });
     }
 }
 
