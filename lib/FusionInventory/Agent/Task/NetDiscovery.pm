21f9ad6f10c2a79032652300fcb6ff9b0b420fc9
David Durieux <d.durieux@siprossii.com>
2010-02-18 16:39:26 +0100

    Real correction of NMAP host name in NETDISCOVERY

--- lib/FusionInventory/Agent/Task/NetDiscovery.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery.pm
@@ -548,14 +548,8 @@ sub discovery_ip_threaded {
          $datadevice->{NETPORTVENDOR} = special_char($scan->{HOSTS}->{$params->{ip}}->{addrs}->{mac}->{vendor});
       }
 
-      if (ref($scan->{HOSTS}->{$params->{ip}}->{hostnames}) eq "HASH"){
-         if (exists($scan->{HOSTS}->{$params->{ip}}->{hostnames}->{name})) {
-            $datadevice->{DNSHOSTNAME} = special_char($scan->{HOSTS}->{$params->{ip}}->{hostnames}->{name});
-         }
-      } elsif (exists($scan->{HOSTS}->{$params->{ip}}->{hostnames}->[0])) {
-         if (exists($scan->{HOSTS}->{$params->{ip}}->{hostnames}->[0]->{name})) {
-            $datadevice->{DNSHOSTNAME} = special_char($scan->{HOSTS}->{$params->{ip}}->{hostnames}->[0]->{name});
-         }
+      if (exists($scan->{HOSTS}->{$params->{ip}}->{hostnames}->[0])) {
+         $datadevice->{DNSHOSTNAME} = special_char($scan->{HOSTS}->{$params->{ip}}->{hostnames}->[0]);
       }
    } elsif ($params->{ModuleNmapScanner} eq "1") {
       my $scan = new Nmap::Scanner;
