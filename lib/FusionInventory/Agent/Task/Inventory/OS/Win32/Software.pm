b66fc50b0eda7a1475e80baba34e763e5a97575d
Gonéri Le Bouder <goneri@rulezlan.org>
2010-02-19 22:19:49 +0100

    Windows inventory works on Vista

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Software.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Software.pm
@@ -3,31 +3,35 @@ package FusionInventory::Agent::Task::Inventory::OS::Win32::Software;
 
 # http://techtasks.com/code/viewbookcode/1417
 
-use strict;
 
-# No check here. If Win32::OLE and Win32::OLE::Variant not available, the module
-# will fail to load.
-use Win32::OLE;
-use Win32::OLE::Variant;
-
-my $Registry;
 use Win32::TieRegistry ( Delimiter=>"/", ArrayValues=>0 );
 
-sub check {1}
+sub isInventoryEnabled {1}
 
-sub run {
+sub hexToDec {
+    my $val = shift;
+
+    return $val unless /^0x/; 
+
+    my $tmp = $val;
+
+    $tmp =~ s/^0x0*//i;
+    return hex($tmp); 
+}
+
+sub doInventory {
     my $params = shift;
     my $inventory = $params->{inventory};
-
-
 
     my $softwares=
         $Registry->{"HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall"};
 
     foreach my $name ( keys %$softwares ) {
         my $data = $softwares->{$name};
+        next unless keys %$data;
 
-        my $name = $name;
+
+        my $name = $data->{'/DisplayName'};
         my $comments = $data->{'/Comments'};
         my $version = $data->{'/DisplayVersion'};
         my $publisher = $data->{'/Publisher'};
@@ -37,6 +41,8 @@ sub run {
         my $noRemove = ($data->{'/NoRemove'} =~ /1/)?1:0;
         my $releaseType = $data->{'/ReleaseType'};
         my $installDate = $data->{'/InstallDate'};
+        my $versionMinor = hexToDec($data->{'/VersionMinor'});
+        my $versionMajor = hexToDec($data->{'/VersionMajor'});
 
         $inventory->addSoftware ({
             COMMENTS => $comments,
@@ -51,7 +57,9 @@ sub run {
             PUBLISHER => $publisher,
             UNINSTALL_STRING => $uninstallString,
             URL_INFO_ABOUT => $urlInfoAbout,
-            VERSION => $version
+            VERSION => $version,
+            VERSION_MINOR => $versionMinor,
+            VERSION_MAJOR => $versionMajor,
         });
     }
 
