b9f4ea5d889a826bd952357449e6cb46d31e3680
Emengao <emengao@hotmail.com>
2010-03-31 21:18:47 +0200

    Collect Solaris process informations

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Processes.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Processes.pm
@@ -25,7 +25,11 @@ sub doInventory {
     );
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
     my $the_year=$year+1900;
-    open(PS, "ps aux|");
+    if ($^O =~ /^solaris$/) {
+        open(PS, "ps -A -o user,pid,pcpu,pmem,vsz,rss,tty,s,stime,time,comm|");
+    } else {
+        open(PS, "ps aux|");
+    }
     while ($line = <PS>) {
         next if ($. ==1);
         if ($line =~
