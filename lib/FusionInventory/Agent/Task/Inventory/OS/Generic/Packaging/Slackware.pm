96c446b6c3ef644eb4d7988d666546ee4b9275ac
David DURIEUX <d.durieux@siprossii.com>
2010-04-09 16:24:22 +0200

    detects Slackware packages

---
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Packaging/Slackware.pm
@@ -0,0 +1,39 @@
+package FusionInventory::Agent::Task::Inventory::OS::Generic::Packaging::Slackware;
+
+use strict;
+use warnings;
+
+sub isInventoryEnabled { can_run("pkgtool") }
+
+sub doInventory {
+  my $params = shift;
+  my $inventory = $params->{inventory};
+
+  my $name;
+  
+  opendir(DIR, "/var/log/packages/");
+  my @files= readdir(DIR);
+  closedir(DIR);
+
+  foreach my $file (@files) {
+     if (($file ne ".") && ($file ne "..")) {
+         my @array = split("-", $file);
+         if ((@array - 4) > 0) {
+            $name = $array[0];
+            for (my $i = 1; $i <= (@array - 4); $i++) {
+               $name .= "-".$array[$i];
+            }
+         } else {
+            $name = $array[0];
+         }
+         my $version = $array[(@array - 3)]."-".$array[(@array - 2)]."-".$array[(@array - 1)];
+
+         $inventory->addSoftware({
+        'NAME' => $name,
+        'VERSION' => $version
+         });
+     }
+  }
+}
+
+1;
