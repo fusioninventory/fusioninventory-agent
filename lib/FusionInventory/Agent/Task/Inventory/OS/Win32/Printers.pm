723c589767d09f445e55e40db921ed97ecd42384
Gonéri Le Bouder <goneri@rulezlan.org>
2010-04-17 20:30:43 +0200

    initial import of Printer.pm

---
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Printers.pm
@@ -0,0 +1,44 @@
+package FusionInventory::Agent::Task::Inventory::OS::Win32::Printers;
+
+use strict;
+use Win32::OLE qw(in CP_UTF8);
+use Win32::OLE::Const;
+
+Win32::OLE-> Option(CP=>CP_UTF8);
+
+use Win32::OLE::Enum;
+
+use Encode qw(encode);
+
+sub isInventoryEnabled {1}
+
+sub doInventory {
+
+    my $params = shift;
+    my $logger = $params->{logger};
+    my $inventory = $params->{inventory};
+
+    my $WMIServices = Win32::OLE->GetObject(
+            "winmgmts:{impersonationLevel=impersonate,(security)}!//./" );
+
+    if (!$WMIServices) {
+        print Win32::OLE->LastError();
+    }
+
+
+    my @slots;
+    foreach my $Properties ( Win32::OLE::in( $WMIServices->InstancesOf(
+                    'Win32_Printer' ) ) )
+    {
+
+
+        $inventory->addPrinter({
+                NAME => $Properties->{Name},
+                DESCRIPTION => $Properties->{Description},
+                DRIVER => $Properties->{DriverName},
+                PORT => $Properties->{PortName},
+                });
+
+    }    
+}
+1;
