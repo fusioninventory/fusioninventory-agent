0deeb01185441ada95d3ecd588087f993332bfb7
Gonéri Le Bouder <goneri@rulezlan.org>
2010-04-17 22:20:37 +0200

    collect port information from Win32_Portconnector

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Ports.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Ports.pm
@@ -58,5 +58,77 @@ sub doInventory {
         });
 
     }
+
+
+
+    my @portType = (
+
+'None',
+'Parallel Port XT/AT Compatible',
+'Parallel Port PS/2',
+'Parallel Port ECP',
+'Parallel Port EPP',
+'Parallel Port ECP/EPP',
+'Serial Port XT/AT Compatible',
+'Serial Port 16450 Compatible',
+'Serial Port 16550 Compatible',
+'Serial Port 16550A Compatible',
+'SCSI Port',
+'MIDI Port',
+'Joy Stick Port',
+'Keyboard Port',
+'Mouse Port',
+'SSA SCSI ',
+'USB',
+'FireWire (IEEE P1394)',
+'PCMCIA Type II',
+'PCMCIA Type II',
+'PCMCIA Type III',
+'CardBus',
+'Access Bus Port',
+'SCSI II',
+'SCSI Wide',
+'PC-98',
+'PC-98-Hireso',
+'PC-H98',
+'Video Port',
+'Audio Port',
+'Modem Port',
+'Network Port',
+'8251 Compatible',
+'8251 FIFO Compatible',
+
+);
+
+    foreach my $Properties ( Win32::OLE::in( $WMIServices->InstancesOf(
+                    'Win32_PortConnector' ) ) )
+    {
+
+
+        my $type;
+        if ($Properties->{ConnectorType}) {
+            $type = $portType[$Properties->{ConnectorType}]; 
+        } else {
+            $type = $Properties->{InternalReferenceDesignator};
+            $type =~ s/\ \d.//; # Drop the port number
+        }
+
+        if($Properties->{InternalReferenceDesignator} =~ /SERIAL/) {
+            next; # Already done
+        } elsif($Properties->{InternalReferenceDesignator} =~ /PARALLEL/) {
+            next; # Already done
+        }
+
+        $inventory->addPorts({
+
+            NAME => $Properties->{InternalReferenceDesignator},
+            CAPTION => $Properties->{InternalReferenceDesignator},
+            DESCRIPTION => $Properties->{InternalReferenceDesignator},
+            TYPE => $type
+
+        });
+
+    }
+
 }
 1;
