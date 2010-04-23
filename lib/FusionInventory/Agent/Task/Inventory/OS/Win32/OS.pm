c56e0f6d791e1b0e91d1012b14a5137c89d45532
Gonéri Le Bouder <goneri@rulezlan.org>
2010-04-23 17:39:01 +0200

    move user detection code in User.pm

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/OS.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/OS.pm
@@ -58,37 +58,5 @@ qw/Workgroup UserName PrimaryOwnerName/)) {
 
     }
 
-
-
-    my $objWMIService = Win32::OLE->GetObject("winmgmts:\\\\.\\root\\CIMV2") or die "WMI connection failed.\n";
-    my $colItems = $objWMIService->ExecQuery("SELECT * FROM Win32_Process", "WQL",
-            wbemFlagReturnImmediately | wbemFlagForwardOnly);
-
-    foreach my $objItem (in $colItems) {
-    
-        my $cmdLine = $objItem->{CommandLine};
-    
-        if ($cmdLine =~ /\\Explorer\.exe/i) {
-            my $name = Variant (VT_BYREF | VT_BSTR, '');
-            my $domain = Variant (VT_BYREF | VT_BSTR, '');
-    
-            $objItem->GetOwner($name, $domain);
-    
-    
-           if (Win32::GetOSName() ne 'Win7') {
-               $name = encode("UTF-8", $name);
-               $domain = encode("UTF-8", $domain);
-           }
-            $inventory->addUser({ LOGIN => $name, DOMAIN => $domain });
-        }
-    
-    }
-    
-
-    foreach (`query session`) {
-        if (/^(\s|)\S+\s+(\S+)\s+\d+/) {
-            $inventory->addUser({ LOGIN => $2 });
-        }
-    }
 }
 1;
