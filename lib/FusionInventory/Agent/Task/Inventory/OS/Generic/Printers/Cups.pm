25f9c4cce93ba9a7a6cc45a19ac9992006a6634f
Aurelien Bondis <aurelien.bondis@gmail.com>
2010-05-03 18:48:53 -0400

    Find printer's name from its URI The name should be the same for a network printer installed on different computers

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Printers/Cups.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Printers/Cups.pm
@@ -1,4 +1,5 @@
 package FusionInventory::Agent::Task::Inventory::OS::Generic::Printers::Cups;
+use URI::Split qw(uri_split uri_join);
 use strict;
 
 sub isInventoryEnabled {
@@ -11,19 +12,26 @@ sub isInventoryEnabled {
 sub doInventory {
     my $params = shift;
     my $inventory = $params->{inventory};
+    my $logger = $params->{logger}; 
 
     my $cups = Net::CUPS->new();
     my @printers = $cups->getDestinations();
 
     return unless scalar(@printers);
-
     foreach my $printer  (@printers)
     {
+
+        my $printername = $printer->getUri();
+        $logger->debug("printer name avant: $printername");
+        $printername =~ s/^.*\/\/([^\.]*).*$/$1/eg ;
+        $printername =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+        $logger->debug("printer name apres: $printername");
         $inventory->addPrinter({
-            NAME    => $printer->getName(),
+            #NAME    => $printer->getName(),
+            NAME    => $printername,
             DESCRIPTION => $printer->getDescription(),
             DRIVER => $printer->getOptionValue("printer-make-and-model"),
-            PORT => $printer->getUri() 
+            PORT => $printer->getUri(), 
         });
     }
 
