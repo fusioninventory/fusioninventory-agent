7d83ddca01c205290c167da1904cb93edc382289
Amir Pakdel <a.pakdel@karafarinbank.net>
2010-05-18 12:23:01 +0200

    IPV4.pm Moved its functionality to Networks.pm so that I could get network related information in one place (since all are TCP/IP related data)
    Memory.pm
    
    echo 'sc product mem;il'| /usr/sbin/cstm
    cstm>sc product mem;il
                    ^-- Unknown list entry. --
    The list entry (mem) is undefined.
    
                        ^-- (InfoLog) is currently disabled. --

--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Memory.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Memory.pm
@@ -7,45 +7,51 @@ sub doInventory {
   my $params = shift;
   my $inventory = $params->{inventory};
 
-  my $capacity=0;
-  my $caption;
-  my $description;
-  my $numslot;
-  my $subnumslot;
-  my $serialnumber;
-  my $type;
-  my @list_mem=`echo 'sc product mem;il'| /usr/sbin/cstm`;
 
-  my $ok=0;
+  if ( `uname -m` =~ /ia64/ ) {
+    for ( `echo 'sc product IPF_MEMORY;il' | /usr/sbin/cstm` ) {
+      if ( /\w+IMM\s+Location/ ) {
+        next
+      } elsif (/Total Configured Memory:\s(\d+)\sMB/i) {
+        my $TotalMemSize = $1;
+        my $TotalSwapSize = `swapinfo -dt | tail -n1`;
+        $TotalSwapSize =~ s/^total\s+(\d+)\s+\d+\s+\d+\s+\d+%\s+\-\s+\d+\s+\-/$1/i;
+        $inventory->setHardware({ MEMORY =>  $TotalMemSize, SWAP =>    sprintf("%i", $TotalSwapSize/1024), });
+      } elsif ( /(\w+IMM)\s+(\w+)\s+(\d+|\-+)\s+(\w+IMM)\s+(\w+)\s+(\d+|\-+)/ ) {
+        $inventory->addMemory({
+          CAPACITY => $3,
+          DESCRIPTION => $1,
+          CAPTION => $1 . ' ' . $2,
+          SPEED => 'No Speed data vailable!',
+          TYPE => $1,
+          NUMSLOTS => $2,
+          SERIALNUMBER => 'No Serial Number available!',
+        });
+        $inventory->addMemory({
+          CAPACITY => $6,
+          DESCRIPTION => $4,
+          CAPTION => $4 . ' ' . $5,
+          SPEED => 'No Speed data vailable!',
+          TYPE => $4,
+          NUMSLOTS => $5,
+          SERIALNUMBER => 'No Serial Number available!',
+        }); 
 
-  if ( `uname -m` =~ /ia64/ )
-  {
-      for ( `echo 'sc product IPF_MEMORY;il' | /usr/sbin/cstm` )
-      {
-         if ( /\w+IMM\s+Location/ )
-         {
-	   ;
-         }
-         elsif ( /(\w+IMM)\s+(\w+)\s+(\S+)\s+(\w+IMM)\s+(\w+)\s+(\S+)/ )
-         {
-             $inventory->addMemories({
-                   CAPACITY => $3,
-                   CAPTION => $2 ,
-                   NUMSLOTS => "1" ,
-                   TYPE => $1,
-			    });
-             $inventory->addMemories({
-                   CAPACITY => $6,
-                   CAPTION => $5 ,
-                   NUMSLOTS => "1" ,
-                   TYPE => $4,
-			    });
-          } 
       }
-  }
-  else
-  {
-     for ( `echo 'sc product system;il' | /usr/sbin/cstm ` ) {
+    }
+  } else {
+     my $capacity = 0;
+     my $caption; 
+     my $description;
+     my $numslot = 1;
+     my $subnumslot;
+     my $serialnumber = 'No Serial Number available!';
+     my $type;
+     my @list_mem=`echo 'sc product mem;il'| /usr/sbin/cstm`;
+     my $ok=0;
+
+     for ( `echo 'sc product system;il' | /usr/sbin/cstm` ) {
+
        if ( /FRU\sSource\s+=\s+\S+\s+\(memory/ ) {
           $ok=0;
           #print "FRU Source memory\n";
@@ -64,52 +70,46 @@ sub doInventory {
        }
     
        if ( /FRU\s+Name\.*:\s+(\S+)/ ) {
-          if ( /(\S+)_(\S+)/ ) {
-             $type=$1;
-             $capacity=$2;
-             #print "Type $type capa $capacity\n";
-          }
-          elsif ( /(\wIMM)(\S+)/ )
-          {
-             $ok=1;
-             $type=$1;
-             $numslot=$2;
-             #print "Type $type numslot $numslot\n";
-          }
+         if ( /(\S+)_(\S+)/ ) {
+           $type=$1;
+           $capacity=$2;
+           #print "Type $type capa $capacity\n";
+         } elsif ( /(\wIMM)(\S+)/ ) {
+           $ok=1;
+           $type=$1;
+           $numslot=$2;
+           #print "Type $type numslot $numslot\n";
+         }
        }
        if ( /Part\s+Number\.*:\s*(\S+)\s+/ ) {
          $description=$1;
          #print "ref $description\n";
-       };
+       }
        if ( /Serial\s+Number\.*:\s*(\S+)\s+/ ) {
-          $serialnumber=$1;
-          if ( $ok eq 1 ) 
-          {
-             if ( $capacity eq 0 )
-             {
-                foreach ( @list_mem )
-                {
-                   if ( /\s+$numslot\s+(\d+)/ )
-                   {
-                      $capacity=$1;
-                      #print "Capacity $capacity\n";
-                   }
-                }
+         $serialnumber=$1;
+         if ( $ok eq 1 ) {
+           if ( $capacity eq 0 ) {
+             foreach ( @list_mem ) {
+               if ( /\s+$numslot\s+(\d+)/ ) {
+                 $capacity=$1;
+                 #print "Capacity $capacity\n";
+               }
              }
-             $inventory->addMemories({
-                   CAPACITY => $capacity,
-	           CAPTION => "Ext $subnumslot Slot $numslot" ,
-                   DESCRIPTION => "Part Number $description",
-                   NUMSLOTS => "1" ,
-		   SERIALNUMBER => $serialnumber,
-                   TYPE => $type,
-			    });
-              $ok=0;
-              $capacity=0;
-	   };
-          #print "Serial $serialnumber\n\n";
-       };
-     };
+           }
+           $inventory->addMemory({
+             CAPACITY => $capacity,
+             DESCRIPTION => "Part Number $description",
+             CAPTION => "Ext $subnumslot Slot $numslot",
+             SPEED => 'No Speed data vailable!',
+             TYPE => $type,
+             NUMSLOTS => '1',
+             SERIALNUMBER => $serialnumber,
+           });
+           $ok=0;
+           $capacity=0;
+         } # $ok eq 1
+       } # /Serial\s+Number\.*:\s*(\S+)\s+/ 
+     } # echo 'sc product system;il' | /usr/sbin/cstm
    }
 
 }
