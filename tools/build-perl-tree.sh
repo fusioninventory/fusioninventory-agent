44c1474f36aca8a33009e51c8fe974010d29da20
Gonéri Le Bouder <goneri@rulezlan.org>
2010-05-23 15:34:31 +0200

    use cpanm to pull the dependencies

--- tools/build-perl-tree.sh
+++ tools/build-perl-tree.sh
@@ -7,15 +7,20 @@
 set -e
 
 MAKE="make"
-TMP="$HOME/tmp"
-PREFIX="$HOME/perl"
+TMP="$PWD/tmp"
+PREFIX="$TMP/perl"
+BUILDDIR="$TMP/build"
+MODULES="Compress::Zlib Digest::MD5 Net::IP XML::Simple Crypt::SSLeay File::ShareDir File::Copy::Recursive Net::SNMP HTTP::Daemon"
 
-PERLVERSION="5.10.1"
+PERLVERSION="5.12.1"
 
-if [ ! -d $TMP ]; then
-  echo "tmp $TMP dir is missing"
+mkdir $TMP
+mkdir $BUILDDIR
+
+if [ ! -d $BUILDDIR ]; then
+  echo "$BUILDDIR dir is missing"
 fi
-cd $TMP
+cd $BUILDDIR
 if [ ! -f perl-$PERLVERSION.tar.gz ]; then
   wget -O perl-$PERLVERSION.tar.gz.part http://cpan.perl.org/src/perl-$PERLVERSION.tar.gz
   mv perl-$PERLVERSION.tar.gz.part perl-$PERLVERSION.tar.gz 
@@ -26,7 +31,36 @@ cd perl-$PERLVERSION
 # AIX
 #./Configure -Dusethreads -Dusenm -des -Dinstallprefix=$PREFIX -Dsiteprefix=$PREFIX -Dprefix=$PREFIX
 #./Configure -Dusethreads -Dcc="gcc" -des -Dinstallprefix=$PREFIX -Dsiteprefix=$PREFIX -Dprefix=$PREFIX
-./Configure -Dusethreads -des -Dinstallprefix=$PREFIX -Dsiteprefix=$PREFIX -Dprefix=$PREFIX
+./Configure -Duserelocatableinc -Dusethreads -des -Dinstallprefix=$PREFIX -Dsiteprefix=$PREFIX -Dprefix=$PREFIX
 $MAKE
 $MAKE install
 
+
+export PATH=$PREFIX/bin:$PATH
+
+cd $BUILDDIR
+wget http://www.openssl.org/source/openssl-0.9.8n.tar.gz
+gunzip < openssl-0.9.8n.tar.gz | tar xvf -
+cd openssl-0.9.8n
+./config no-shared --prefix=$TMP/openssl
+make depend
+make install
+export OPENSSL_PREFIX=$TMP/openssl # Pour Net::SSLeay
+export CRYPT_SSLEAY_DEFAULT=$TMP/openssl # Utile pour Crypt::SSLeay
+
+# Net::SSLeay's Makefile.PL the OpenSSL directory as parmeter, so we can't
+# use cpanm directly
+cd $TMP
+wget http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/Net-SSLeay-1.36.tar.gz
+gunzip < Net-SSLeay-1.36.tar.gz | tar xvf -
+cd Net-SSLeay-1.36
+PERL_MM_USE_DEFAULT=1 perl Makefile.PL
+make install
+
+cd $TMP
+
+wget http://xrl.us/cpanm
+perl cpanm -L $MODULES
+
+TARBALLNAME=`./perl/bin/perl -MConfig -e'print $Config{osname}."_".$Config{archname}."_".$Config{osvers}.".tar"'`
+tar cf $TARBALLNAME perl
