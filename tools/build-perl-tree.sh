f4483842a7e3435a07725c8db41c071f8c169609
Gonéri Le Bouder <goneri@rulezlan.org>
2010-05-24 16:32:35 +0200

    keep the downloaded files

--- tools/build-perl-tree.sh
+++ tools/build-perl-tree.sh
@@ -14,18 +14,28 @@ MODULES="Compress::Zlib Digest::MD5 Net::IP XML::Simple Crypt::SSLeay File::Shar
 
 PERLVERSION="5.12.1"
 
-mkdir $TMP
-mkdir $BUILDDIR
-
-if [ ! -d $BUILDDIR ]; then
-  echo "$BUILDDIR dir is missing"
+[ -d $TMP ] || mkdir $TMP
+if [ -d $BUILDDIR ]; then
+    echo "Please remove $BUILDDIR first"
+    exit 0
 fi
-cd $BUILDDIR
+
+cd $TMP
 if [ ! -f perl-$PERLVERSION.tar.gz ]; then
   wget -O perl-$PERLVERSION.tar.gz.part http://cpan.perl.org/src/perl-$PERLVERSION.tar.gz
   mv perl-$PERLVERSION.tar.gz.part perl-$PERLVERSION.tar.gz 
 fi
-gunzip < perl-$PERLVERSION.tar.gz | tar xvf -
+wget -c http://www.openssl.org/source/openssl-0.9.8n.tar.gz
+wget -c http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/Net-SSLeay-1.36.tar.gz
+
+
+mkdir $BUILDDIR
+if [ ! -d $BUILDDIR ]; then
+  echo "$BUILDDIR dir is missing"
+fi
+
+cd $BUILDDIR
+gunzip < ../perl-$PERLVERSION.tar.gz | tar xvf -
 cd perl-$PERLVERSION
 
 # AIX
@@ -39,8 +49,7 @@ $MAKE install
 export PATH=$PREFIX/bin:$PATH
 
 cd $BUILDDIR
-wget http://www.openssl.org/source/openssl-0.9.8n.tar.gz
-gunzip < openssl-0.9.8n.tar.gz | tar xvf -
+gunzip < ../openssl-0.9.8n.tar.gz | tar xvf -
 cd openssl-0.9.8n
 ./config no-shared --prefix=$TMP/openssl
 make depend
@@ -50,9 +59,8 @@ export CRYPT_SSLEAY_DEFAULT=$TMP/openssl # Utile pour Crypt::SSLeay
 
 # Net::SSLeay's Makefile.PL the OpenSSL directory as parmeter, so we can't
 # use cpanm directly
-cd $TMP
-wget http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/Net-SSLeay-1.36.tar.gz
-gunzip < Net-SSLeay-1.36.tar.gz | tar xvf -
+cd $BUILDDIR
+gunzip < ../Net-SSLeay-1.36.tar.gz | tar xvf -
 cd Net-SSLeay-1.36
 PERL_MM_USE_DEFAULT=1 perl Makefile.PL
 make install
