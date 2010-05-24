53220e09ff6d1495f3a002bdaa6cc148b6642c57
Gonéri Le Bouder <goneri@rulezlan.org>
2010-05-24 23:34:33 +0200

    exit as soon as a package installation fails

--- tools/build-perl-tree.sh
+++ tools/build-perl-tree.sh
@@ -10,15 +10,14 @@ MAKE="make"
 TMP="$PWD/tmp"
 PREFIX="$TMP/perl"
 BUILDDIR="$TMP/build"
-MODULES="Compress::Zlib Digest::MD5 Net::IP XML::Simple Crypt::SSLeay File::ShareDir File::Copy::Recursive Net::SNMP HTTP::Daemon"
+MODULES="XML::NamespaceSupport HTML::Tagset Class::Inspector LWP Compress::Zlib Digest::MD5 Net::IP XML::Simple Crypt::SSLeay File::ShareDir File::Copy::Recursive Net::SNMP HTTP::Daemon"
 
 PERLVERSION="5.12.1"
 
+# Clean up
+rm -rf $BUILDDIR $TMP/openssl $TMP/perl $TMP/Compress::Zlib
+
 [ -d $TMP ] || mkdir $TMP
-if [ -d $BUILDDIR ]; then
-    echo "Please remove $BUILDDIR first"
-    exit 0
-fi
 
 cd $TMP
 if [ ! -f perl-$PERLVERSION.tar.gz ]; then
@@ -27,7 +26,7 @@ if [ ! -f perl-$PERLVERSION.tar.gz ]; then
 fi
 wget -c http://www.openssl.org/source/openssl-0.9.8n.tar.gz
 wget -c http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/Net-SSLeay-1.36.tar.gz
-
+wget -c http://search.cpan.org/CPAN/authors/id/D/DL/DLAND/Crypt-SSLeay-0.57.tar.gz
 
 mkdir $BUILDDIR
 if [ ! -d $BUILDDIR ]; then
@@ -54,8 +53,9 @@ cd openssl-0.9.8n
 ./config no-shared --prefix=$TMP/openssl
 make depend
 make install
+# hack for Crypt::SSLeay
+cp $TMP/openssl/include/openssl/*.h $TMP/openssl/include/openssl/openssl
 export OPENSSL_PREFIX=$TMP/openssl # Pour Net::SSLeay
-export CRYPT_SSLEAY_DEFAULT=$TMP/openssl # Utile pour Crypt::SSLeay
 
 # Net::SSLeay's Makefile.PL the OpenSSL directory as parmeter, so we can't
 # use cpanm directly
@@ -65,10 +65,25 @@ cd Net-SSLeay-1.36
 PERL_MM_USE_DEFAULT=1 perl Makefile.PL
 make install
 
+cd $BUILDDIR
+gunzip < ../Crypt-SSLeay-0.57.tar.gz | tar xvf -
+cd Crypt-SSLeay-0.57
+perl Makefile.PL --default --static --lib=$TMP/openssl
+make install
+mkdir $TMP/openssl/include/openssl/openssl
+
 cd $TMP
 
-wget http://xrl.us/cpanm
-perl cpanm -L $MODULES
+if [ ! -f cpanm ]; then
+    echo "Can't find cpanm in $TMP directory"
+    exit 1
+fi
+
+# Tree dependencies not pulled by cpanm
+for module in $MODULES; do
+    perl cpanm $module
+    perl -M$module -e1
+done
 
 TARBALLNAME=`./perl/bin/perl -MConfig -e'print $Config{osname}."_".$Config{archname}."_".$Config{osvers}.".tar"'`
 tar cf $TARBALLNAME perl
