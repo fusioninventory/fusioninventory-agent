b6e3fa08a562ce85e8a38851b8274778add8a354
Gonéri Le Bouder <goneri@rulezlan.org>
2010-06-05 00:46:07 +0200

    generate the tarball offline now

--- tools/build-perl-tree.sh
+++ tools/build-perl-tree.sh
@@ -6,6 +6,62 @@
 
 set -e
 
+installMod () {
+    modName=$1
+    distName=$2
+
+    if [ -z $distName ]; then
+        distName=`echo $modName|sed 's,::,-,g'`
+    fi
+    archive=`ls $TMP/$distName*.tar.gz`
+    $PERL_PREFIX/bin/perl $CPANM --skip-installed $archive
+    $PERL_PREFIX/bin/perl -M$modName -e1
+}
+
+cleanUp () {
+    rm -rf $BUILDDIR $TMP/openssl $TMP/perl $TMP/Compress::Zlib
+
+}
+
+buildPerl () {
+
+    cd $TMP
+    if [ ! -f perl-$PERLVERSION.tar.gz ]; then
+        wget -O perl-$PERLVERSION.tar.gz.part http://cpan.perl.org/src/perl-$PERLVERSION.tar.gz
+        mv perl-$PERLVERSION.tar.gz.part perl-$PERLVERSION.tar.gz 
+    fi
+
+    cd $BUILDDIR
+    gunzip < ../perl-$PERLVERSION.tar.gz | tar xvf -
+    cd perl-$PERLVERSION
+    
+    # AIX
+    #./Configure -Dusethreads -Dusenm -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
+    #./Configure -Dusethreads -Dcc="gcc" -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
+    
+    ./Configure -Duserelocatableinc -Dusethreads -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
+    $MAKE
+    $MAKE install
+    
+
+}
+
+buildOpenSSL () {
+
+    cd $TMP
+    [ -f openssl-0.9.8n.tar.gz ] || wget http://www.openssl.org/source/openssl-0.9.8n.tar.gz
+
+    cd $BUILDDIR
+    gunzip < ../openssl-0.9.8n.tar.gz | tar xvf -
+    cd openssl-0.9.8n
+    ./config no-shared --prefix=$TMP/openssl
+    make depend
+    make install
+    # hack for Crypt::SSLeay
+    mkdir $TMP/openssl/include/openssl/openssl
+    cp $TMP/openssl/include/openssl/*.h $TMP/openssl/include/openssl/openssl
+
+}
 if [ ! -d '../tools' ]; then
     echo "Please run the script in the ./tools directory"
     exit 1
@@ -16,53 +72,36 @@ MAKE="make"
 TMP="$PWD/tmp"
 PERL_PREFIX="$TMP/perl"
 BUILDDIR="$TMP/build"
-MODULES="XML::NamespaceSupport HTML::Tagset Class::Inspector LWP Compress::Zlib Digest::MD5 Net::IP XML::Simple File::ShareDir File::Copy::Recursive Net::SNMP Net::IP Proc::Daemon Proc::PID::File Compress::Zlib Compress::Raw::Zlib Archive::Extract Digest::MD5 File::Copy File::Glob File::Path File::stat File::Temp Net::NBName Net::SSLeay Parallel::ForkManager Nmap::Parser "
+MODULES="Compress::Raw::Bzip2 URI HTML::Parser HTML::Tagset XML::NamespaceSupport HTML::Tagset
+Class::Inspector Digest::MD5 Net::IP XML::SAX XML::Simple File::ShareDir File::Copy::Recursive Net::SNMP Net::IP Proc::Daemon Proc::PID::File Compress::Raw::Zlib Archive::Extract Digest::MD5 File::Path File::Temp Net::NBName Net::SSLeay Parallel::ForkManager "
 FINALDIR=$PWD
+NO_CLEANUP=0
+NO_PERL_REBUILD=0
+NO_OPENSSL_REBUILD=0
 
 PERLVERSION="5.12.1"
 
 # Clean up
-rm -rf $BUILDDIR $TMP/openssl $TMP/perl $TMP/Compress::Zlib $TMP/App-cpanminus-1.0004
+if [ -z $NO_CLEANUP ]; then
+    cleanUp
+fi
 
 [ -d $TMP ] || mkdir $TMP
+[ -d $BUILDDIR ] || mkdir $BUILDDIR
 
-cd $TMP
-if [ ! -f perl-$PERLVERSION.tar.gz ]; then
-  wget -O perl-$PERLVERSION.tar.gz.part http://cpan.perl.org/src/perl-$PERLVERSION.tar.gz
-  mv perl-$PERLVERSION.tar.gz.part perl-$PERLVERSION.tar.gz 
-fi
-wget -c http://www.openssl.org/source/openssl-0.9.8n.tar.gz
-wget -c http://search.cpan.org/CPAN/authors/id/F/FL/FLORA/Net-SSLeay-1.36.tar.gz
-wget -c http://search.cpan.org/CPAN/authors/id/D/DL/DLAND/Crypt-SSLeay-0.57.tar.gz
-wget -c http://search.cpan.org/CPAN/authors/id/M/MI/MIYAGAWA/App-cpanminus-1.0004.tar.gz
-
-mkdir $BUILDDIR
 if [ ! -d $BUILDDIR ]; then
   echo "$BUILDDIR dir is missing"
 fi
 
-cd $BUILDDIR
-gunzip < ../perl-$PERLVERSION.tar.gz | tar xvf -
-cd perl-$PERLVERSION
-
-# AIX
-#./Configure -Dusethreads -Dusenm -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
-#./Configure -Dusethreads -Dcc="gcc" -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
-
-./Configure -Duserelocatableinc -Dusethreads -des -Dinstallprefix=$PERL_PREFIX -Dsiteprefix=$PERL_PREFIX -Dprefix=$PERL_PREFIX
-$MAKE
-$MAKE install
 
 
-cd $BUILDDIR
-gunzip < ../openssl-0.9.8n.tar.gz | tar xvf -
-cd openssl-0.9.8n
-./config no-shared --prefix=$TMP/openssl
-make depend
-make install
-# hack for Crypt::SSLeay
-mkdir $TMP/openssl/include/openssl/openssl
-cp $TMP/openssl/include/openssl/*.h $TMP/openssl/include/openssl/openssl
+if [ -z $NO_PERL_REBUILD]; then
+    buildPerl
+fi
+
+if [ -z $NO_OPENSSL_REBUILD]; then
+    buildOpenSSL
+fi
 export OPENSSL_PREFIX=$TMP/openssl # Pour Net::SSLeay
 
 # Net::SSLeay's Makefile.PL the OpenSSL directory as parmeter, so we can't
@@ -79,23 +118,25 @@ cd Crypt-SSLeay-0.57
 PERL_MM_USE_DEFAULT=1 $PERL_PREFIX/bin/perl Makefile.PL --default --static --lib=$TMP/openssl
 make install
 
-cd $TMP
-
 cd $BUILDDIR
-gunzip < ../App-cpanminus-1.0004.tar.gz | tar xvf -
-CPANM=$PWD/App-cpanminus-1.0004/bin/cpanm
+echo $PWD
+archive=`ls $TMP/App-cpanminus-*.tar.gz`
+echo $archive
+gunzip < $archive | tar xvf -
+CPANM=$BUILDDIR/App-cpanminus-1.0004/bin/cpanm
 
 if [ -f "/usr/include/cups/cups.h" ]; then
     echo "CUPS found, enable Net::CUPS"
-    MODULES="$MODULE Net::CUPS"
+    installMod "Net::CUPS"
 fi
 
-
 # Tree dependencies not pulled by cpanm
-for module in $MODULES; do
-    $PERL_PREFIX/bin/perl $CPANM --skip-installed $module
-    $PERL_PREFIX/bin/perl -M$module -e1
+for modName in $MODULES; do
+    installMod $modName
 done
+
+installMod "LWP" "libwww-perl"
+installMod "Compress::Zlib" "IO-Compress"
 
 cd $TMP
 TARBALLNAME=` $PERL_PREFIX/bin/perl -MConfig -e'print $Config{osname}."_".$Config{archname}."_".$Config{osvers}.".tar"'`
