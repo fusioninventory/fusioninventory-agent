df99a48e15ff7fe18c1b125e840466e5f9f24b2e
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-09 14:34:44 +0200

    no need to rewrite all lines, the only relevant ones can be analysed directly

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Bios.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Bios.pm
@@ -36,7 +36,6 @@ sub parseDmidecode {
 
     foreach my $line (@$dmidecode) {
         chomp $line;
-        $line =~ s/^\s+//;
 
         if ($line =~ /DMI type (\d+)/i) {
             $type = $1;
@@ -47,7 +46,7 @@ sub parseDmidecode {
 
         if ($type == 0) {
             # BIOS values
-            if ($line =~ /^vendor:\s*(.+?)(\s*)$/i) {
+            if ($line =~ /^\s+vendor:\s*(.+?)(\s*)$/i) {
                 $result{BiosManufacturer} = $1;
                 if ($result{BiosManufacturer} =~ /QEMU/i) {
                     $result{vmsystem} = 'QEMU';
@@ -56,25 +55,25 @@ sub parseDmidecode {
                 } elsif ($result{BiosManufacturer} =~ /^Xen/i) {
                     $result{vmsystem} = 'Xen';
                 }
-            } elsif ($line =~ /^release\ date:\s*(.+?)(\s*)$/i) {
+            } elsif ($line =~ /^\s+release\ date:\s*(.+?)(\s*)$/i) {
                 $result{BiosDate} = $1
-            } elsif ($line =~ /^version:\s*(.+?)(\s*)$/i) {
+            } elsif ($line =~ /^\s+version:\s*(.+?)(\s*)$/i) {
                 $result{BiosVersion} = $1
             }
             next;
         }
 
         if ($type == 1) {
-            if ($line =~ /^serial number:\s*(.+?)(\s*)$/i) {
+            if ($line =~ /^\s+serial number:\s*(.+?)(\s*)$/i) {
                 $result{SystemSerial} = $1
-            } elsif ($line =~ /^(product name|product):\s*(.+?)(\s*)$/i) {
+            } elsif ($line =~ /^\s+(product name|product):\s*(.+?)(\s*)$/i) {
                 $result{SystemModel} = $2;
                 if ($result{SystemModel} =~ /VMware/i) {
                     $result{vmsystem} = 'VMware';
                 } elsif ($result{SystemModel} =~ /Virtual Machine/i) {
                     $result{vmsystem} = 'Virtual Machine';
                 }
-            } elsif ($line =~ /^(manufacturer|vendor):\s*(.+?)(\s*)$/i) {
+            } elsif ($line =~ /^\s+(manufacturer|vendor):\s*(.+?)(\s*)$/i) {
                 $result{SystemManufacturer} = $2
             }
             next;
@@ -82,18 +81,18 @@ sub parseDmidecode {
 
         if ($type == 2) {
             # Failback on the motherbord
-            if ($line =~ /^serial number:\s*(.+?)(\s*)/i) {
+            if ($line =~ /^\s+serial number:\s*(.+?)(\s*)/i) {
                 $result{SystemSerial} = $1 if !$result{SystemSerial};
-            } elsif ($line =~ /^product name:\s*(.+?)(\s*)/i) {
+            } elsif ($line =~ /^\s+product name:\s*(.+?)(\s*)/i) {
                 $result{SystemModel} = $1 if !$result{SystemModel};
-            } elsif ($line =~ /^manufacturer:\s*(.+?)(\s*)/i) {
+            } elsif ($line =~ /^\s+manufacturer:\s*(.+?)(\s*)/i) {
                 $result{SystemManufacturer} = $1
                     if !$result{SystemManufacturer};
             }
         }
 
         if ($type == 3) {
-            if ($line =~ /^Asset Tag:\s*(.+\S)/i) {
+            if ($line =~ /^\s+Asset Tag:\s*(.+\S)/i) {
                 $result{AssetTag} = $1 eq 'Not Specified'  ? '' : $1;
             }
             next;
@@ -102,7 +101,7 @@ sub parseDmidecode {
         if ($type == 4) {
             # Some bioses don't provide a serial number so I check for CPU ID
             # (e.g: server from dedibox.fr)
-            if ($line =~ /^ID:\s*(.*)/i) {
+            if ($line =~ /^\s+ID:\s*(.*)/i) {
                 if (!$result{SystemSerial}) {
                     $result{SystemSerial} = $1;
                     $result{SystemSerial} =~ s/\ /-/g;
