3a703cb2cb8384eb2187231dc4626f76492eedbc
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-15 18:40:48 +0200

    use meaningful names for magic variables

--- lib/FusionInventory/Agent/Task/WakeOnLan.pm
+++ lib/FusionInventory/Agent/Task/WakeOnLan.pm
@@ -9,6 +9,7 @@ use constant ETH_P_ALL => 0x0003;
 use constant PF_PACKET => 17;
 use constant SOCK_PACKET => 10;
 
+use English qw(-no_match_vars);
 use Socket;
 
 use FusionInventory::Agent::AccountInfo;
@@ -74,7 +75,7 @@ sub StartMachine {
     if ( eval { socket(SOCKET, PF_PACKET, SOCK_PACKET, 0); }) {
 
         setsockopt(SOCKET, SOL_SOCKET, SO_BROADCAST, 1)
-            or warn "Can't do setsockopt: $!\n";
+            or warn "Can't do setsockopt: $ERRNO\n";
 
         my $rec = `/sbin/ifconfig -a | grep "HWaddr"`;
         my @netcards = split(/\n/, $rec);
@@ -93,7 +94,7 @@ sub StartMachine {
             $magic_packet .= chr(0xFF) x 6 . (pack('H12', $macaddress) x 16);
             my $destination = pack("Sa14", 0, $netName);
             send(SOCKET, $magic_packet, 0, $destination)
-                or warn "Couldn't send packet: $!";
+                or warn "Couldn't send packet: $ERRNO";
         }
         # TODO : For FreeBSD, send to /dev/bpf ....
     } else { # degraded wol by UDP
