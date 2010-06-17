fac37e2ffaf021bd1882428e22a6df3add739c72
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-17 20:54:23 +0200

    code factorisation, one module is enough

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Distro/NonLSB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Distro/NonLSB.pm
@@ -3,6 +3,57 @@ package FusionInventory::Agent::Task::Inventory::OS::Linux::Distro::NonLSB;
 use strict;
 use warnings;
 
-$runMeIfTheseChecksFailed = ["FusionInventory::Agent::Task::Inventory::OS::Linux::Distro::LSB"];
+use English qw(-no_match_vars);
+
+my %files = (
+    '/etc/arch-release'      => 'ArchLinux %s',
+    '/etc/debian_version'    => 'Debian GNU/Linux %s',
+    '/etc/fedora-release'    => '%s',
+    '/etc/gentoo-release'    => 'Gentoo Linux %s',
+    '/etc/knoppix_version'   => 'Knoppix GNU/Linux $s',
+    '/etc/mandriva-release'  => '%s',
+    '/etc/mandrake-release'  => '%s',
+    '/etc/redhat-release'    => '%s',
+    '/etc/slackware-version' => '%s',
+    '/etc/SuSE-release'      => '%s',
+    '/etc/trustix-release'   => '%s',
+    '/etc/ubuntu_version'    => 'Ubuntu %s',
+    '/etc/vmware-release'    => '%s',
+);
+
+our $runMeIfTheseChecksFailed = ["FusionInventory::Agent::Task::Inventory::OS::Linux::Distro::LSB"];
+
+sub isInventoryEnabled {
+    return 1;
+}
+
+sub findRelease {
+  my $release;
+
+  foreach my $file (keys %files) {
+      next unless -f $file;
+      open (my $handle, '<', $file) or die "Can't open $file: $ERRNO";
+      my $version = <$handle>;
+      chomp $version;
+      close ($handle);
+      $release = sprintf $files{$file}, $version;
+      last;
+  }
+
+  return $release;
+}
+
+sub doInventory {
+    my $params = shift;
+    my $inventory = $params->{inventory};
+
+    my $OSComment = `uname -v`;
+    chomp $OSComment;
+
+    $inventory->setHardware({ 
+        OSNAME     => findRelease(),
+        OSCOMMENTS => $OSComment
+    });
+}
 
 1;
