98fc0c0dcdbdf49c74bd38fb537349ca59471969
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-17 18:05:21 +0200

    no need to override base class constructor with no behaviour change

--- lib/FusionInventory/Agent/XML/Response/Inventory.pm
+++ lib/FusionInventory/Agent/XML/Response/Inventory.pm
@@ -4,15 +4,6 @@ use strict;
 use warnings;
 use base 'FusionInventory::Agent::XML::Response';
 
-sub new {
-    my ($class, @params) = @_;
-
-    my $self = $class->SUPER::new(@params);
-    bless $self, $class;
-
-    return $self;
-}
-
 sub isAccountUpdated {
     my $self = shift;
 
--- lib/FusionInventory/Agent/XML/Response/SimpleMessage.pm
+++ lib/FusionInventory/Agent/XML/Response/SimpleMessage.pm
@@ -4,13 +4,4 @@ use strict;
 use warnings;
 use base 'FusionInventory::Agent::XML::Response';
 
-sub new {
-    my ($class, @params) = @_;
-
-    my $self = $class->SUPER::new(@params);
-    bless $self, $class;
-
-    return $self;
-}
-
 1;
