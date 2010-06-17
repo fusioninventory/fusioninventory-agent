fusioninventory/fusioninventory-agent

commit 84355fa297fe07fed3be54b2720a365d27609ef9

sanitize usage of file opening: - use 3-arguments style open() - use a local variable for file handle - check return value of open(), and emits a meaningfule warning if   needed - close file descriptor - use a while loop instead of a foreach loop for reading

lib/FusionInventory/Agent/AccountInfo.pm
@@ -124,22 +124,14 @@ sub write {
 
     my $xml=XML::Simple::XMLout( $tmp, RootName => 'ADM' );
 
-    my $fault;
-    if (!open ADM, ">".$target->{accountinfofile}) {
-        $fault = 1;
-
-    } else {
-
-        print ADM $xml;
-        $fault = 1 unless close ADM;
-
-    }
-
-    if (!$fault) {
+    if (open my $handle, ">", $target->{accountinfofile}) {
+        print $handle $xml;
+        close $handle;
         $logger->debug ("Account info updated successfully");
     } else {
+        warn "Can't open $target->{accountinfofile} for writing: $ERRNO";
         $logger->error ("Can't save account info in `".
-            $self->{config}->{accountinfofile});
+            $target->{accountinfofile});
     }
 }
 

lib/FusionInventory/Agent/Config.pm
@@ -149,14 +149,14 @@ if (!$file || !-f $file) {
     return $config unless -f $file;
   }
 
-  if (!open (CONFIG, "<".$file)) {
-    print(STDERR "Config: Failed to open $file: $ERRNO\n");
-	return $config;
+  if (!open my $handle, '<', $file) {
+    warn "Config: Failed to open $file: $ERRNO"
+    return $config;
   }
   
   $config->{'conf-file'} = $file;
 
-  foreach (<CONFIG>) {
+  while (<$handle>) {
     s/#.+//;
     if (/([\w-]+)\s*=\s*(.+)/) {
       my $key = $1;
@@ -168,7 +168,7 @@ if (!$file || !-f $file) {
       $config->{$key} = $val;
     }
   }
-  close CONFIG;
+  close $handle;
 }
 
 sub loadUserParams {

lib/FusionInventory/Agent/RPC.pm
@@ -98,13 +98,15 @@ sub handler {
         }
 
         my $indexFile = $htmlDir."/index.tpl";
-        if (!open FH, $indexFile) {
-            $logger->error("Can't open share $indexFile");
+        if (!open my $handle, '<', $indexFile) {
+            $logger->error("Can't open share $indexFile: $ERRNO");
             $c->send_error(404);
             return;
         }
         undef $/;
-        my $output = <FH>;
+        my $output = <$handle>;
+        close $handle;
+
         $output =~ s/%%STATUS%%/$status/;
         $output =~ s/%%AGENT_VERSION%%/$config->{VERSION}/;
         if (!$config->{'rpc-trust-localhost'}) {

lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Domains.pm
@@ -3,16 +3,23 @@ package FusionInventory::Agent::Task::Inventory::OS::AIX::Domains;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 sub doInventory {
   my $params = shift;
   my $inventory = $params->{inventory};
   my $domain;
 
   #Domain name 
-  open RESOLV, "/etc/resolv.conf";
-  while(<RESOLV>){
-		if (/^(domain|search)\s+(.+)/){$domain=$2;chomp($domain);}
+  if (!open my $handle, '<', '/etc/resolv.conf') {
+      warn "Can't open /etc/resolv.conf: $ERRNO";
+      return;
   }
+
+  while(<$handle>){
+    if (/^(domain|search)\s+(.+)/){$domain=$2;chomp($domain);}
+  }
+  close $handle;
   #If no domain name and no workgroup name (samba), we send "WORKGROUP"
   #TODO:Check if samba is present and get the windows workgroup or NT domain name
   unless (defined($domain)){chomp($domain="WORKGROUP");}

lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Bios.pm
@@ -18,9 +18,13 @@ sub doInventory {
 sub parseDmidecode {
     my ($file, $mode) = @_;
 
+    if (!open my $handle, $mode, $file) {
+        warn "Can't open $file: $ERRNO";
+        return;
+    }
+
     my ($bios, $hardware, $type);
 
-    open (my $handle, $mode, $file);
     while (my $line = <$handle>) {
         chomp $line;
 
@@ -95,10 +99,9 @@ sub parseDmidecode {
             next;
         }
     }
-    close ($handle);
+    close $handle;
 
     return $bios, $hardware;
-
 }
 
 1;

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Distro/NonLSB.pm
@@ -32,10 +32,13 @@ sub findRelease {
 
   foreach my $file (keys %files) {
       next unless -f $file;
-      open (my $handle, '<', $file) or die "Can't open $file: $ERRNO";
+      if (!open my $handle, '<', $file) {
+        warn "Can't open $file: $ERRNO";
+        return;
+      }
       my $version = <$handle>;
       chomp $version;
-      close ($handle);
+      close $handle;
       $release = sprintf $files{$file}, $version;
       last;
   }

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages.pm
@@ -40,12 +40,15 @@ sub getFromSysProc {
     }
 
     my $command = `fdisk -v` =~ '^GNU' ? 'fdisk -p -l' : 'fdisk -l';
-    open (my $handle, '-|', $command) or die "Can't run $command: $ERRNO";
-    while (my $line = <$handle>) {
-        next unless (/^\/dev\/([sh]d[a-z])/);
-        push(@names, $1);
+    if (!open my $handle, '-|', $command) {
+        warn "Can't run $command: $ERRNO";
+    } else {
+        while (my $line = <$handle>) {
+            next unless (/^\/dev\/([sh]d[a-z])/);
+            push(@names, $1);
+        }
+        close $handle;
     }
-    close ($handle);
 
     # filter duplicates
     my %seen;
@@ -78,7 +81,11 @@ sub getValueFromSysProc {
 
     return unless $file;
 
-    open (my $handle, '<', $file) or die "Can't open $file: $ERRNO";
+    if (!open my $handle, '<', $file) {
+        warn "Can't open $file: $ERRNO";
+        return;
+    }
+
     my $value = <$handle>;
     close ($handle);
 
@@ -181,25 +188,27 @@ sub doInventory {
         foreach my $device (@devices) {
             if (!$device->{SERIALNUMBER} || !$device->{FIRMWARE}) {
                 my $command = "hdparm -I /dev/$device->{NAME} 2>/dev/null";
-                open (my $handle, '-|', $command)
-                    or die "Can't run $command: $ERRNO";
-                while (my $line = <$handle>) {
-                    if ($line =~ /^\s+Serial Number\s*:\s*(.+)/i) {
-                        my $value = $1;
-                        $value =~ s/\s+$//;
-                        $device->{SERIALNUMBER} = $value
-                            if !$device->{SERIALNUMBER};
-                        next;
+                if (!open my $handle, '-|', $command) {
+                    warn "Can't run $command: $ERRNO";
+                } else {
+                    while (my $line = <$handle>) {
+                        if ($line =~ /^\s+Serial Number\s*:\s*(.+)/i) {
+                            my $value = $1;
+                            $value =~ s/\s+$//;
+                            $device->{SERIALNUMBER} = $value
+                                if !$device->{SERIALNUMBER};
+                            next;
+                        }
+                        if (/^\s+Firmware Revision\s*:\s*(.+)/i) { 
+                            my $value = $1;
+                            $value =~ s/\s+$//;
+                            $device->{FIRMWARE} = $value
+                                if !$device->{FIRMWARE};
+                            next;
+                        }
                     }
-                    if (/^\s+Firmware Revision\s*:\s*(.+)/i) { 
-                        my $value = $1;
-                        $value =~ s/\s+$//;
-                        $device->{FIRMWARE} = $value
-                            if !$device->{FIRMWARE};
-                        next;
-                    }
+                    close ($handle);
                 }
-                close ($handle);
             }
         }
     }
@@ -227,9 +236,13 @@ sub doInventory {
 sub parseUdev {
     my ($file, $device) = @_;
 
-    my ($result, $serial);
 
-    open (my $handle, '<', $file);
+    if (!open my $handle, '<', $file) {
+        warn "Can't open $file: $ERRNO";
+        return;
+    }
+
+    my ($result, $serial);
     while (my $line = <$handle>) {
         if ($line =~ /^S:.*-scsi-(\d+):(\d+):(\d+):(\d+)/) {
             $result->{SCSI_COID} = $1;
@@ -268,9 +281,14 @@ sub parseUdev {
 sub parseLshal {
     my ($file, $mode) = @_;
 
+
+    if (!open my $handle, $mode, $file) {
+        warn "Can't open $file: $ERRNO";
+        return;
+    }
+
     my ($devices, $device);
 
-    open (my $handle, $mode, $file);
     while (my $line = <$handle>) {
         chomp $line;
         if ($line =~ m{^udi = '/org/freedesktop/Hal/devices/(storage|legacy_floppy|block)}) {

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Uptime.pm
@@ -3,6 +3,8 @@ package FusionInventory::Agent::Task::Inventory::OS::Linux::Uptime;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 sub isInventoryEnabled { can_read("/proc/uptime") }
 
 sub doInventory {
@@ -10,10 +12,14 @@ sub doInventory {
   my $inventory = $params->{inventory};
 
   # Uptime
-  open UPTIME, "/proc/uptime";
-  my $uptime = <UPTIME>;
+  if (!open my $handle, '<', '/proc/uptime') {
+      warn "Can't open /proc/uptime: $ERRNO";
+      return;
+  }
+
+  my $uptime = <$handle>;
   $uptime =~ s/^(.+)\s+.+/$1/;
-  close UPTIME;
+  close $handle;
 
   # Uptime conversion
   my ($UYEAR, $UMONTH , $UDAY, $UHOUR, $UMIN, $USEC) = (gmtime ($uptime))[5,4,3,2,1,0];

commit 6201aad24d0b9ec71c6ca59c03e6170f20f17b36

fix issues introduced by commit 84355fa297fe07fed3be54b2720a365d27609ef9, for all case where created file handle is used outside the conditional block

lib/FusionInventory/Agent/AccountInfo.pm
@@ -3,6 +3,8 @@ package FusionInventory::Agent::AccountInfo;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 sub new {
     my ($class,$params) = @_;
 

lib/FusionInventory/Agent/Config.pm
@@ -149,8 +149,9 @@ if (!$file || !-f $file) {
     return $config unless -f $file;
   }
 
-  if (!open my $handle, '<', $file) {
-    warn "Config: Failed to open $file: $ERRNO"
+  my $handle;
+  if (!open $handle, '<', $file) {
+    warn "Config: Failed to open $file: $ERRNO";
     return $config;
   }
   

lib/FusionInventory/Agent/RPC.pm
@@ -98,7 +98,8 @@ sub handler {
         }
 
         my $indexFile = $htmlDir."/index.tpl";
-        if (!open my $handle, '<', $indexFile) {
+        my $handle;
+        if (!open $handle, '<', $indexFile) {
             $logger->error("Can't open share $indexFile: $ERRNO");
             $c->send_error(404);
             return;

lib/FusionInventory/Agent/Task/Inventory.pm
@@ -155,7 +155,7 @@ sub initModList {
             my $calling_namespace = caller(0);
             eval "package $calling_namespace; use $module;";
 #      print STDERR "$module not loaded in $calling_namespace! $ERRNO: $EVAL_ERROR\n" if $EVAL_ERROR\;
-            return if $EVAL_ERROR
+            return if $EVAL_ERROR;
 #      print STDERR "$module loaded in $calling_namespace!\n";
             1;
         },

lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Domains.pm
@@ -10,8 +10,9 @@ sub doInventory {
   my $inventory = $params->{inventory};
   my $domain;
 
-  #Domain name 
-  if (!open my $handle, '<', '/etc/resolv.conf') {
+  #Domain name
+  my $handle;
+  if (!open $handle, '<', '/etc/resolv.conf') {
       warn "Can't open /etc/resolv.conf: $ERRNO";
       return;
   }

lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Bios.pm
@@ -3,6 +3,8 @@ package FusionInventory::Agent::Task::Inventory::OS::Generic::Dmidecode::Bios;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 sub doInventory {
   my $params = shift;
   my $inventory = $params->{inventory};
@@ -18,7 +20,8 @@ sub doInventory {
 sub parseDmidecode {
     my ($file, $mode) = @_;
 
-    if (!open my $handle, $mode, $file) {
+    my $handle;
+    if (!open $handle, $mode, $file) {
         warn "Can't open $file: $ERRNO";
         return;
     }

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Distro/NonLSB.pm
@@ -32,7 +32,8 @@ sub findRelease {
 
   foreach my $file (keys %files) {
       next unless -f $file;
-      if (!open my $handle, '<', $file) {
+      my $handle;
+      if (!open $handle, '<', $file) {
         warn "Can't open $file: $ERRNO";
         return;
       }

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages.pm
@@ -81,7 +81,8 @@ sub getValueFromSysProc {
 
     return unless $file;
 
-    if (!open my $handle, '<', $file) {
+    my $handle;
+    if (!open $handle, '<', $file) {
         warn "Can't open $file: $ERRNO";
         return;
     }
@@ -237,7 +238,8 @@ sub parseUdev {
     my ($file, $device) = @_;
 
 
-    if (!open my $handle, '<', $file) {
+    my $handle;
+    if (!open $handle, '<', $file) {
         warn "Can't open $file: $ERRNO";
         return;
     }
@@ -282,7 +284,8 @@ sub parseLshal {
     my ($file, $mode) = @_;
 
 
-    if (!open my $handle, $mode, $file) {
+    my $handle;
+    if (!open $handle, $mode, $file) {
         warn "Can't open $file: $ERRNO";
         return;
     }

lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Uptime.pm
@@ -12,7 +12,8 @@ sub doInventory {
   my $inventory = $params->{inventory};
 
   # Uptime
-  if (!open my $handle, '<', '/proc/uptime') {
+  my $handle;
+  if (!open $handle, '<', '/proc/uptime') {
       warn "Can't open /proc/uptime: $ERRNO";
       return;
   }

lib/FusionInventory/Agent/Task/Inventory/Virtualization/SolarisZones.pm
@@ -16,7 +16,8 @@ sub check_solaris_valid_release{
   my $release;
   my $year;
   
-  if (!open my $handle, '<', '/etc/release') {
+  my $handle;
+  if (!open $handle, '<', '/etc/release') {
     warn "Can't open /etc/release: $ERRNO";
     return;
   }
@@ -62,7 +63,9 @@ sub doInventory {
 	# Memory considerations depends on rcapd or project definitions
 	# Little hack, I go directly in /etc/zones reading mcap physcap for each zone.
         $zonefile = "/etc/zones/$zonename.xml";
-        if (!open my $handle, '<', $zonefile) {
+
+        my $handle;
+        if (!open $handle, '<', $zonefile) {
             warn "Can't open $zonefile: $ERRNO";
             $logger->debug("Failed to open $zonefile");
             next;