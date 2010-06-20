dc5522846b45ad67f6d177c3190d1c468e760810
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-20 14:58:17 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Uptime.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Uptime.pm
@@ -8,29 +8,29 @@ use English qw(-no_match_vars);
 sub isInventoryEnabled { can_read("/proc/uptime") }
 
 sub doInventory {
-  my $params = shift;
-  my $inventory = $params->{inventory};
+    my $params = shift;
+    my $inventory = $params->{inventory};
 
-  # Uptime
-  my $handle;
-  if (!open $handle, '<', '/proc/uptime') {
-      warn "Can't open /proc/uptime: $ERRNO";
-      return;
-  }
+    # Uptime
+    my $handle;
+    if (!open $handle, '<', '/proc/uptime') {
+        warn "Can't open /proc/uptime: $ERRNO";
+        return;
+    }
 
-  my $uptime = <$handle>;
-  $uptime =~ s/^(.+)\s+.+/$1/;
-  close $handle;
+    my $uptime = <$handle>;
+    $uptime =~ s/^(.+)\s+.+/$1/;
+    close $handle;
 
-  # Uptime conversion
-  my ($UYEAR, $UMONTH , $UDAY, $UHOUR, $UMIN, $USEC) = (gmtime ($uptime))[5,4,3,2,1,0];
+    # Uptime conversion
+    my ($UYEAR, $UMONTH , $UDAY, $UHOUR, $UMIN, $USEC) = (gmtime ($uptime))[5,4,3,2,1,0];
 
-  # Write in ISO format
-  $uptime=sprintf "%02d-%02d-%02d %02d:%02d:%02d", ($UYEAR-70), $UMONTH, ($UDAY-1), $UHOUR, $UMIN, $USEC;
-  
-  chomp(my $DeviceType =`uname -m`);
+    # Write in ISO format
+    $uptime=sprintf "%02d-%02d-%02d %02d:%02d:%02d", ($UYEAR-70), $UMONTH, ($UDAY-1), $UHOUR, $UMIN, $USEC;
+
+    chomp(my $DeviceType =`uname -m`);
 #  TODO$h->{'CONTENT'}{'HARDWARE'}{'DESCRIPTION'} = [ "$DeviceType/$uptime" ];
-  $inventory->setHardware({ DESCRIPTION => "$DeviceType/$uptime" });
+    $inventory->setHardware({ DESCRIPTION => "$DeviceType/$uptime" });
 }
 
-1
+1;
