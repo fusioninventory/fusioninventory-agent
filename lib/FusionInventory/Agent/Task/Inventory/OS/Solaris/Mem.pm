b15c5ebd73ab64646c837afc4901b86a79644fee
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-20 14:48:54 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Mem.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Mem.pm
@@ -6,26 +6,26 @@ use warnings;
 sub isInventoryEnabled { can_run ("swap") && can_run ("prtconf") }
 
 sub doInventory {
-  my $params = shift;
-  my $inventory = $params->{inventory};
-#my $unit = 1024;
+    my $params = shift;
+    my $inventory = $params->{inventory};
+    #my $unit = 1024;
 
-  my $PhysicalMemory;
-  my $SwapFileSize;
+    my $PhysicalMemory;
+    my $SwapFileSize;
 
-# Memory informations
-  foreach(`prtconf`){
-    if(/^Memory\ssize:\s+(\S+)/){$PhysicalMemory = $1}; 	
-  } 
-#Swap Informations 
-  foreach(`swap -l`){
-    if(/\s+(\S+)$/){$SwapFileSize += $1}; 
-  }
+    # Memory informations
+    foreach(`prtconf`){
+        if(/^Memory\ssize:\s+(\S+)/){$PhysicalMemory = $1}; 	
+    } 
+    #Swap Informations 
+    foreach(`swap -l`){
+        if(/\s+(\S+)$/){$SwapFileSize += $1}; 
+    }
 
-  $inventory->setHardware({
-      MEMORY =>  $PhysicalMemory,
-      SWAP =>    $SwapFileSize
-      });
+    $inventory->setHardware({
+        MEMORY =>  $PhysicalMemory,
+        SWAP =>    $SwapFileSize
+    });
 }
 
-1
+1;
