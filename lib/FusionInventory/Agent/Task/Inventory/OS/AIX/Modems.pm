a0b3377767b3b4e7712fac9783c1269d479fdfb5
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-20 14:55:42 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Modems.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Modems.pm
@@ -6,19 +6,19 @@ use warnings;
 sub isInventoryEnabled {can_run("lsdev")}
 
 sub doInventory {
-  my $params = shift;
-  my $inventory = $params->{inventory};
+    my $params = shift;
+    my $inventory = $params->{inventory};
 
-  for(`lsdev -Cc adapter -F 'name:type:description'`){
-    if(/modem/i && /\d+\s(.+):(.+)$/){
-	  my $name = $1;
-	  my $description = $2;
-	  $inventory->addModems({
-	  	'DESCRIPTION'  => $description,
-	  	'NAME'          => $name,
-	  });
+    for(`lsdev -Cc adapter -F 'name:type:description'`){
+        if(/modem/i && /\d+\s(.+):(.+)$/){
+            my $name = $1;
+            my $description = $2;
+            $inventory->addModems({
+                'DESCRIPTION'  => $description,
+                'NAME'          => $name,
+            });
+        }
     }
-  }
 }
 
-1
+1;
