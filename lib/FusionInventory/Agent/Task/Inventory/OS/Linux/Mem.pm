7932173f81f251ddaadbe497032b37d2ca73d047
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-20 14:51:03 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Mem.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Mem.pm
@@ -8,31 +8,29 @@ use English qw(-no_match_vars);
 sub isInventoryEnabled { can_read ("/proc/meminfo") }
 
 sub doInventory {
-  my $params = shift;
-  my $inventory = $params->{inventory};
-  my $unit = 1024;
+    my $params = shift;
+    my $inventory = $params->{inventory};
+    my $unit = 1024;
 
-  my $PhysicalMemory;
-  my $SwapFileSize;
+    my $PhysicalMemory;
+    my $SwapFileSize;
 
-  # Memory informations
-  if (open my $handle, '<', '/proc/meminfo') {
-      while(<$handle>){
-        $PhysicalMemory=$1 if /^memtotal\s*:\s*(\S+)/i;
-        $SwapFileSize=$1 if /^swaptotal\s*:\s*(\S+)/i;
-      }
-      close $handle;
-  } else {
-      warn "Can't open /proc/meminfo: $ERRNO";
-  }
+    # Memory informations
+    if (open my $handle, '<', '/proc/meminfo') {
+        while(<$handle>){
+            $PhysicalMemory=$1 if /^memtotal\s*:\s*(\S+)/i;
+            $SwapFileSize=$1 if /^swaptotal\s*:\s*(\S+)/i;
+        }
+        close $handle;
+    } else {
+        warn "Can't open /proc/meminfo: $ERRNO";
+    }
 
-  # TODO
-  $inventory->setHardware({
-
-      MEMORY =>  sprintf("%i",$PhysicalMemory/$unit),
-      SWAP =>    sprintf("%i", $SwapFileSize/$unit),
-
+    # TODO
+    $inventory->setHardware({
+        MEMORY =>  sprintf("%i",$PhysicalMemory/$unit),
+        SWAP =>    sprintf("%i", $SwapFileSize/$unit),
     });
 }
 
-1
+1;
