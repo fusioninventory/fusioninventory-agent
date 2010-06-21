70cc4d8325f2a1bc9b977f2c1585b1c9d4e9264d
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-21 22:27:18 +0200

    don't use external grep in perl code

--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Software.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Software.pm
@@ -3,30 +3,37 @@ package FusionInventory::Agent::Task::Inventory::OS::HPUX::Software;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 sub isInventoryEnabled  {
     my $params = shift;
 
     # Do not run an package inventory if there is the --nosoft parameter
     return if $params->{params}->{nosoft};
 
-    return 
-        can_run('swlist') &&
-        can_run('grep');
+    return can_run('swlist');
 }
 
 sub doInventory {
     my $params = shift;
     my $inventory = $params->{inventory};
 
-    my @softList;
-    my $software;
+    my $command = 'swlist';
 
+    my $handle;
+    if (!open $handle, '-|', $command) {
+        warn "Can't run $command: $ERRNO";
+        return;
+    }
 
+    while (my $line = <$handle>) {
+        next if $line =~ /^#/;
+        next if $line =~ /^  PH/;
+        $line =~ tr/\t/ /s;
+        $line =~ tr/ //s;
+        chomp $line;
 
-    @softList = `swlist | grep -v '^  PH' | grep -v '^#' |tr -s "\t" " "|tr -s " "` ;
-    foreach my $software (@softList) {
-        chomp( $software );
-        if ( $software =~ /^ (\S+)\s(\S+)\s(.+)/ ) {
+        if ($line =~ /^ (\S+)\s(\S+)\s(.+)/ ) {
             $inventory->addSoftware({
                 NAME      => $1,
                 VERSION   => $2,
@@ -35,6 +42,8 @@ sub doInventory {
             });
         }
     }
+
+    close $handle;
 
 }
 
