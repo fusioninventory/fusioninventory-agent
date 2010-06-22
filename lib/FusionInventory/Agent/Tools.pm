df6deef6e465bf1e15a27fe34eb1d9ef40914c72
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-06-22 17:56:59 +0200

    use a named lexical iterator

--- lib/FusionInventory/Agent/Tools.pm
+++ lib/FusionInventory/Agent/Tools.pm
@@ -69,24 +69,30 @@ sub getIpDhcp {
     my ($lease, $dhcp);
 
     # find the last lease for the interface with its expire date
-    while(<$handle>){
-        $lease = 1 if /lease\s*{/i;
-        $lease = 0 if /^\s*}\s*$/;
+    while (my $line = <$handle>) {
+        $lease = 1 if $line=~ /lease\s*{/i;
+        $lease = 0 if $line=~ /^\s*}\s*$/;
 
         next unless $lease;
 
         # inside a lease section
-        if (/interface\s+"(.+?)"\s*/){
+        if ($line =~ /interface\s+"(.+?)"\s*/){
             $dhcp = ($1 eq $if);
+            next;
         }
 
         next unless $dhcp;
 
-        if (/option\s+dhcp-server-identifier\s+(\d{1,3}(?:\.\d{1,3}){3})\s*;/) {
+        if (
+            $line =~ 
+            /option\s+dhcp-server-identifier\s+(\d{1,3}(?:\.\d{1,3}){3})\s*;/
+        ) {
             # server IP
             $server_ip = $1;
-        }
-        if (/^\s*expire\s*\d\s*(\d*)\/(\d*)\/(\d*)\s*(\d*):(\d*):(\d*)/) {
+        } elsif (
+            $line =~
+            /^\s*expire\s*\d\s*(\d*)\/(\d*)\/(\d*)\s*(\d*):(\d*):(\d*)/
+        ) {
             $expiration_time =
                 sprintf "%04d%02d%02d%02d%02d%02d", $1, $2, $3, $4, $5, $6;
         }
