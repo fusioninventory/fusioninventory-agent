f1795046aafdfc1d520f6814487251d2e96ba9c2
David Durieux <d.durieux@siprossii.com>
2010-06-26 22:55:04 +0000

    Fix detection of VM (Virtualbox)

--- lib/FusionInventory/Agent/Task/Inventory/Virtualization/VirtualBox.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Virtualization/VirtualBox.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use XML::Simple;
 use File::Glob ':glob';
-
+use Data::Dumper;
 sub isInventoryEnabled {
     return
         can_run('VirtualBox') &&
@@ -17,7 +17,7 @@ sub isInventoryEnabled {
 sub doInventory {
     my $params = shift;
     my $inventory = $params->{inventory};
-    my $scanhomedirs = $params->{accountinfo}{config}{'scan-homedirs'};
+    my $scanhomedirs = $params->{config}{'scan-homedirs'};
 
     my $cmd_list_vms = "VBoxManage -nologo list vms";
 
@@ -78,7 +78,7 @@ sub doInventory {
     # try to found another VMs, not exectute by root
     my @vmRunnings = ();
     my $index = 0 ;
-    foreach my $line ( `ps -ef` ) {
+    foreach my $line ( `ps -efax` ) {
         chomp($line);
         if ( $line !~ m/^root/) {
             if ($line =~ m/^.*VirtualBox (.*)$/) {
@@ -152,8 +152,12 @@ sub doInventory {
 
             # ... and read it
             my $defaultMachineFolder = $data->{Global}->{SystemProperties}->{defaultMachineFolder};
-            if ( $defaultMachineFolder != 0 and $defaultMachineFolder != "Machines" 
-                    and $defaultMachineFolder =~ /^\/home\/S+\/.VirtualBox\/Machines$/ ) {
+
+            if ($defaultMachineFolder eq "Machines") {
+                $defaultMachineFolder =~ s/VirtualBox.xml/Machines/;
+            }
+
+            if ( $defaultMachineFolder =~ /^\/home\/S+\/.VirtualBox\/Machines$/ ) {
 
                 foreach my $xmlMachine (bsd_glob($defaultMachineFolder."/*/*.xml")) {
                     my $configFile = new XML::Simple;
@@ -161,6 +165,7 @@ sub doInventory {
 
                     if ( $data->{Machine} != 0 and $data->{Machine}->{uuid} != 0 ) {
                         my $uuid = $data->{Machine}->{uuid};
+
                         $uuid =~ s/^{?(.{36})}?$/$1/;
                         my $status = "off";
                         foreach my $vmRun (@vmRunnings) {
