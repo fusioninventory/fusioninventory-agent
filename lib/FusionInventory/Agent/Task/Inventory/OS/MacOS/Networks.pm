bb01b89c92f4d5be55d0880a02d8491234bec024
Gonéri Le Bouder <goneri@rulezlan.org>
2010-06-28 20:29:38 +0200

    MacOSX Network: code clean up

--- lib/FusionInventory/Agent/Task/Inventory/OS/MacOS/Networks.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/MacOS/Networks.pm
@@ -101,7 +101,16 @@ sub doInventory {
 
     # for each interface get it's parameters
     foreach my $description (@list) {
-        $ipaddress = $ipmask = $macaddr = $status =  $type = undef;
+        my $ipaddress;
+	my $ipmask;
+	my $macaddr;
+	my $status;
+	my $type;
+	my $binmask;
+	my $binsubnet;
+	my $mask;
+	my $binip;
+
         # search interface infos
         @ifconfig = `ifconfig $description`;
         foreach (@ifconfig){
@@ -111,22 +120,24 @@ sub doInventory {
             $status = 1 if /status:\s+active/i;
             $type = $1 if /media:\s+(\S+)/i;
         }
-        my $binip = &ip_iptobin ($ipaddress ,4);
-        # In BSD, netmask is given in hex form
-        my $binmask = sprintf("%b", oct($ipmask));
-        my $binsubnet = $binip & $binmask;
-        $ipsubnet = ip_bintoip($binsubnet,4);
-        my $mask = ip_bintoip($binmask,4);
+	if ($status) {
+            	$binip = &ip_iptobin ($ipaddress ,4);
+                # In BSD, netmask is given in hex form
+            	$binmask = sprintf("%b", oct($ipmask));
+            	$binsubnet = $binip & $binmask;
+            	$ipsubnet = ip_bintoip($binsubnet,4);
+           	$mask = ip_bintoip($binmask,4);
+	}
         $inventory->addNetwork({
             DESCRIPTION => $description,
-            IPADDRESS => ($status?$ipaddress:undef),
+            IPADDRESS => $ipaddress,
             IPDHCP => _ipdhcp($description),
-            IPGATEWAY => ($status?$ipgateway:undef),
-            IPMASK => ($status?$mask:undef),
-            IPSUBNET => ($status?$ipsubnet:undef),
+            IPGATEWAY => $ipgateway,
+            IPMASK => $mask,
+            IPSUBNET => $ipsubnet,
             MACADDR => $macaddr,
             STATUS => ($status?"Up":"Down"),
-            TYPE => ($status?$type:undef)
+            TYPE => $type
         });
     }
 }
