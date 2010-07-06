dd17342320bd1f5a15c69d7bf4e43e58c780e134
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-06 07:41:04 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Controller.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Controller.pm
@@ -36,20 +36,20 @@ sub doInventory {
     my $config = $params->{config};
 
     foreach my $wmiClass (qw/
-            Win32_FloppyController Win32_IDEController Win32_SCSIController
-            Win32_VideoController
-            Win32_InfraredDevice Win32_USBController Win32_1394Controller
-            Win32_PCMCIAController CIM_LogicalDevice/) {
+        Win32_FloppyController Win32_IDEController Win32_SCSIController
+        Win32_VideoController Win32_InfraredDevice Win32_USBController
+        Win32_1394Controller Win32_PCMCIAController CIM_LogicalDevice
+    /) {
 
-        foreach my $Properties
-            (getWmiProperties($wmiClass,
-qw/Name Manufacturer Caption Description DeviceID HardwareVersion/)) {
+        foreach my $Properties (getWmiProperties($wmiClass, qw/
+            Name Manufacturer Caption Description DeviceID HardwareVersion
+        /)) {
 
             my ($pciid, $pcisubsystemid) = getPciIDFromDeviceID($Properties->{DeviceID});
 
 
-# I scan CIM_LogicalDevice to identify more devices but I don't want
-# everything. Only devices with a PCIID sounds resonable
+            # I scan CIM_LogicalDevice to identify more devices but I don't want
+            # everything. Only devices with a PCIID sounds resonable
             if ($wmiClass eq 'CIM_LogicalDevice') {
                 next unless $pciid;
                 next if $seen{$pciid};
@@ -59,19 +59,17 @@ qw/Name Manufacturer Caption Description DeviceID HardwareVersion/)) {
                 $seen{$pciid} = 1;
             }
             $inventory->addController({
-                    NAME => $Properties->{Name},
-                    MANUFACTURER => $Properties->{Manufacturer},
-                    CAPTION => $Properties->{Caption},
-                    DESCRIPTION => $Properties->{Description},
-                    PCIID => $pciid,
-                    'PCISUBSYSTEMID'=> $pcisubsystemid,
-                    VERSION => $Properties->{HardwareVersion},
-                    TYPE => $Properties->{Caption},
-                    });
-
+                NAME => $Properties->{Name},
+                MANUFACTURER => $Properties->{Manufacturer},
+                CAPTION => $Properties->{Caption},
+                DESCRIPTION => $Properties->{Description},
+                PCIID => $pciid,
+                PCISUBSYSTEMID=> $pcisubsystemid,
+                VERSION => $Properties->{HardwareVersion},
+                TYPE => $Properties->{Caption},
+            });
         }
     }
-
-
 }
+
 1;
