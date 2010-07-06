f0c9762b4d957958fa9fc7db8e6c6855a9c78f15
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-06 07:39:23 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Memory.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Memory.pm
@@ -10,71 +10,67 @@ sub isInventoryEnabled {
 }
 
 my @formFactorVal = qw/
-Unknown 
-Other
-SIP
-DIP
-ZIP
-SOJ
-Proprietary
-SIMM
-DIMM
-TSOP
-PGA
-RIMM
-SODIMM
-SRIMM
-SMD
-SSMP
-QFP
-TQFP
-SOIC
-LCC
-PLCC
-BGA
-FPBGA
-LGA
+    Unknown 
+    Other
+    SIP
+    DIP
+    ZIP
+    SOJ
+    Proprietary
+    SIMM
+    DIMM
+    TSOP
+    PGA
+    RIMM
+    SODIMM
+    SRIMM
+    SMD
+    SSMP
+    QFP
+    TQFP
+    SOIC
+    LCC
+    PLCC
+    BGA
+    FPBGA
+    LGA
 /;
 
-
-
 my @memoryTypeVal = qw/
-Unknown
-Other
-DRAM
-Synchronous DRAM
-Cache DRAM
-EDO
-EDRAM
-VRAM
-SRAM
-RAM
-ROM
-Flash
-EEPROM
-FEPROM
-EPROM
-CDRAM
-3DRAM
-SDRAM
-SGRAM
-RDRAM
-DDR
-DDR-2
+    Unknown
+    Other
+    DRAM
+    Synchronous DRAM
+    Cache DRAM
+    EDO
+    EDRAM
+    VRAM
+    SRAM
+    RAM
+    ROM
+    Flash
+    EEPROM
+    FEPROM
+    EPROM
+    CDRAM
+    3DRAM
+    SDRAM
+    SGRAM
+    RDRAM
+    DDR
+    DDR-2
 /;
 
 my @memoryErrorProtection = ( 
-        undef,
-        'Other',
-        undef,
-        'None',
-        'Parity',
-        'Single-bit ECC',
-        'Multi-bit ECC',
-        'CRC',
-        );
-
-
+    undef,
+    'Other',
+    undef,
+    'None',
+    'Parity',
+    'Single-bit ECC',
+    'Multi-bit ECC',
+    'CRC',
+);
 
 sub doInventory {
     my $params = shift;
@@ -84,10 +80,10 @@ sub doInventory {
     my $cpt = 0;
     my @memories;
 
-        foreach my $Properties
-            (getWmiProperties('Win32_PhysicalMemory',
-qw/Capacity Caption Description FormFactor Removable Speed MemoryType
-SerialNumber/)) {
+    foreach my $Properties (getWmiProperties('Win32_PhysicalMemory', qw/
+        Capacity Caption Description FormFactor Removable Speed MemoryType
+        SerialNumber
+    /)) {
 
         my $capacity = sprintf("%i",$Properties->{Capacity}/(1024*1024));
         my $caption = $Properties->{Caption};
@@ -101,27 +97,24 @@ SerialNumber/)) {
 
         push @memories, {
             CAPACITY => $capacity,
-                     CAPTION => $caption,
-                     DESCRIPTION => $description,
-                     FORMFACTOR => $formfactor,
-                     REMOVABLE => $removable,
-                     SPEED => $speed,
-                     TYPE => $type,
-                     NUMSLOTS => $numslots,
-                     SERIALNUMBER => $serialnumber
+            CAPTION => $caption,
+            DESCRIPTION => $description,
+            FORMFACTOR => $formfactor,
+            REMOVABLE => $removable,
+            SPEED => $speed,
+            TYPE => $type,
+            NUMSLOTS => $numslots,
+            SERIALNUMBER => $serialnumber
         }
-
     }
 
-
-        foreach my $Properties
-            (getWmiProperties('Win32_PhysicalMemoryArray',
-qw/MemoryDevices SerialNumber PhysicalMemoryCorrection/)) {
+    foreach my $Properties (getWmiProperties('Win32_PhysicalMemoryArray', qw/
+        MemoryDevices SerialNumber PhysicalMemoryCorrection
+    /)) {
 
         my $memory = $memories[$Properties->{MemoryDevices} - 1];
         if (!$memory->{SERIALNUMBER}) {
-            $memory->{SERIALNUMBER} =
-                $Properties->{SerialNumber};
+            $memory->{SERIALNUMBER} = $Properties->{SerialNumber};
         }
         $memory->{MEMORYCORRECTION} =
             $memoryErrorProtection[$Properties->{PhysicalMemoryCorrection}];
@@ -131,15 +124,9 @@ qw/MemoryDevices SerialNumber PhysicalMemoryCorrection/)) {
         }
     }
 
-
-
-
     foreach my $memory (@memories) {
         $inventory->addMemory($memory);
     }
-
-
-
 
     my $fullMemory = 0;
     my $swapMemory = 0;
@@ -154,17 +141,10 @@ qw/TotalSwapSpaceSize/)) {
         $swapMemory = $Properties->{TotalSwapSpaceSize};
     }
 
-
-
-
-
     $inventory->setHardware({
-
-            MEMORY =>  int($fullMemory/(1024*1024)),
-            SWAP =>  int($swapMemory/(1024)),
-
-            });
-
+        MEMORY =>  int($fullMemory/(1024*1024)),
+        SWAP =>  int($swapMemory/(1024)),
+    });
 
 }
 
