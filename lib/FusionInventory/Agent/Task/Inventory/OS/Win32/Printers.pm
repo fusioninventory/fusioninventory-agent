0027e43a4360bd19765290110ff39b7782d6a396
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-06 07:44:58 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Printers.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Printers.pm
@@ -6,35 +6,34 @@ use warnings;
 use FusionInventory::Agent::Tools::Win32;
 
 my @status = (
-        'Unknown', # 0 is not defined
-        'Other',
-        'Unknown',
-        'Idle',
-        'Printing',
-        'Warming Up',
-        'Stopped printing',
-        'Offline',
-        );
+    'Unknown', # 0 is not defined
+    'Other',
+    'Unknown',
+    'Idle',
+    'Printing',
+    'Warming Up',
+    'Stopped printing',
+    'Offline',
+);
 
 my @errStatus = (
-        'Unknown',
-        'Other',
-        'No Error',
-        'Low Paper',
-        'No Paper',
-        'Low Toner',
-        'No Toner',
-        'Door Open',
-        'Jammed',
-        'Service Requested',
-        'Output Bin Full',
-        'Paper Problem',
-        'Cannot Print Page',
-        'User Intervention Required',
-        'Out of Memory',
-        'Server Unknown',
-        );
-
+    'Unknown',
+    'Other',
+    'No Error',
+    'Low Paper',
+    'No Paper',
+    'Low Toner',
+    'No Toner',
+    'Door Open',
+    'Jammed',
+    'Service Requested',
+    'Output Bin Full',
+    'Paper Problem',
+    'Cannot Print Page',
+    'User Intervention Required',
+    'Out of Memory',
+    'Server Unknown',
+);
 
 sub isInventoryEnabled {
     return 1;
@@ -51,11 +50,11 @@ sub doInventory {
 
     my @slots;
 
-    foreach my $Properties
-        (getWmiProperties('Win32_Printer',
-qw/ExtendedDetectedErrorState HorizontalResolution VerticalResolution Name Comment DescriptionDriverName
- PortName Network Shared PrinterStatus ServerName ShareName PrintProcessor
-/)) {
+    foreach my $Properties (getWmiProperties('Win32_Printer', qw/
+        ExtendedDetectedErrorState HorizontalResolution VerticalResolution Name
+        Comment DescriptionDriverName PortName Network Shared PrinterStatus
+        ServerName ShareName PrintProcessor
+    /)) {
 
         my $errStatus;
         if ($Properties->{ExtendedDetectedErrorState}) {
@@ -69,21 +68,22 @@ qw/ExtendedDetectedErrorState HorizontalResolution VerticalResolution Name Comme
 $Properties->{HorizontalResolution}."x".$Properties->{VerticalResolution};
         }
         $inventory->addPrinter({
-                NAME => $Properties->{Name},
-                COMMENT => $Properties->{Comment},
-                DESCRIPTION => $Properties->{Description},
-                DRIVER => $Properties->{DriverName},
-                PORT => $Properties->{PortName},
-                RESOLUTION => $resolution,
-                NETWORK => $Properties->{Network},
-                SHARED => $Properties->{Shared},
-                STATUS => $status[$Properties->{PrinterStatus}],
-                ERRSTATUS => $errStatus,
-                SERVERNAME => $Properties->{ServerName},
-                SHARENAME => $Properties->{ShareName},
-                PRINTPROCESSOR => $Properties->{PrintProcessor},
-                });
+            NAME => $Properties->{Name},
+            COMMENT => $Properties->{Comment},
+            DESCRIPTION => $Properties->{Description},
+            DRIVER => $Properties->{DriverName},
+            PORT => $Properties->{PortName},
+            RESOLUTION => $resolution,
+            NETWORK => $Properties->{Network},
+            SHARED => $Properties->{Shared},
+            STATUS => $status[$Properties->{PrinterStatus}],
+            ERRSTATUS => $errStatus,
+            SERVERNAME => $Properties->{ServerName},
+            SHARENAME => $Properties->{ShareName},
+            PRINTPROCESSOR => $Properties->{PrintProcessor},
+        });
 
     }    
 }
+
 1;
