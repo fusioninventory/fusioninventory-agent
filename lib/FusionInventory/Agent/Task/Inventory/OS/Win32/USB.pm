27f87cd93580838dc5b17202e76b55d7364a433b
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-06 07:25:05 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/USB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/USB.pm
@@ -17,34 +17,24 @@ sub doInventory {
     my $inventory = $params->{inventory};
 
     foreach my $wmiClass (qw/CIM_LogicalDevice/) {
+        foreach my $Properties (getWmiProperties($wmiClass, qw/DeviceID Name/)) {
+            next unless $Properties->{DeviceID} =~ /^USB\\VID_(\w+)&PID_(\w+)(\\|$)(.*)/;
 
-        foreach my $Properties
-            (getWmiProperties($wmiClass,
-qw/DeviceID Name/)) {
-                if ($Properties->{DeviceID} =~ /^USB\\VID_(\w+)&PID_(\w+)(\\|$)(.*)/) {
+            my $vendorId = $1;
+            my $productId = $2;
+            my $serial = $4;
 
-                    my $vendorId = $1;
-                    my $productId = $2;
+            $serial =~ s/.*?&//;
+            $serial =~ s/&.*$//;
 
-                    my $serial = $4;
-
-                    $serial =~ s/.*?&//;
-                    $serial =~ s/&.*$//;
-
-                    next if $vendorId =~ /^0+$/;
-            
-                    $inventory->addUSBDevice({
-                            NAME => $Properties->{Name},
-                            VENDORID => $vendorId,
-                            PRODUCTID => $productId,
-                            SERIAL => $serial
-                            });
-
-                }
-
-
-
-
+            next if $vendorId =~ /^0+$/;
+    
+            $inventory->addUSBDevice({
+                NAME => $Properties->{Name},
+                VENDORID => $vendorId,
+                PRODUCTID => $productId,
+                SERIAL => $serial
+            });
         }
     }
 }
