f6cc80ff5ac3507a3c45e88189a08d12e38d35bb
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-06 07:23:13 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Video.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Video.pm
@@ -14,28 +14,31 @@ sub doInventory {
     my $inventory = $params->{inventory};
 
 
-        foreach my $Properties
-            (getWmiProperties('Win32_VideoController',
-qw/CurrentHorizontalResolution CurrentVerticalResolution VideoProcessor
-AdaptaterRAM Name/)) {
+    foreach my $Properties (getWmiProperties(
+        'Win32_VideoController',
+        qw/
+            CurrentHorizontalResolution
+            CurrentVerticalResolution
+            VideoProcessor
+            AdaptaterRAM
+            Name
+        /)) {
 
         my $resolution;
         if ($Properties->{CurrentHorizontalResolution}) {
-            $resolution = $Properties->{CurrentHorizontalResolution} ."x".$Properties->{CurrentVerticalResolution};
+            $resolution =
+                $Properties->{CurrentHorizontalResolution} .
+                "x" .
+                $Properties->{CurrentVerticalResolution};
         }
 
         $inventory->addVideo({
-                CHIPSET => $Properties->{VideoProcessor},
-                MEMORY =>  int($Properties->{AdaptaterRAM} / (1024*1024)),
-                NAME => $Properties->{Name},
-                RESOLUTION => $resolution
-                });
-
+            CHIPSET => $Properties->{VideoProcessor},
+            MEMORY =>  int($Properties->{AdaptaterRAM} / (1024*1024)),
+            NAME => $Properties->{Name},
+            RESOLUTION => $resolution
+        });
     }
-
-
-
-
 }
 
 1;
