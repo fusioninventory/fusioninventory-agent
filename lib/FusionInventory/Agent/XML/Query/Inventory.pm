acb7cdc00cf4f410d66ac457b8856293ccca588c
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-09 07:56:48 +0200

    wrap long lines

--- lib/FusionInventory/Agent/XML/Query/Inventory.pm
+++ lib/FusionInventory/Agent/XML/Query/Inventory.pm
@@ -1020,7 +1020,12 @@ sub getContent {
         $logger->debug('Missing value(s): '.$missing.'. I will send this inventory to the server BUT important value(s) to identify the computer are missing');
     }
 
-    my $content = XMLout( $self->{h}, RootName => 'REQUEST', XMLDecl => '<?xml version="1.0" encoding="UTF-8"?>', SuppressEmpty => undef );
+    my $content = XMLout(
+        $self->{h},
+        RootName => 'REQUEST',
+        XMLDecl => '<?xml version="1.0" encoding="UTF-8"?>',
+        SuppressEmpty => undef
+    );
 
     return $content;
 }
