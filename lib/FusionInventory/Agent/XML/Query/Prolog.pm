701b9f76958fdf397def435041ba2bb55c892f99
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-10 07:30:24 +0200

    abort if no token given, as does simple message without message

--- lib/FusionInventory/Agent/XML/Query/Prolog.pm
+++ lib/FusionInventory/Agent/XML/Query/Prolog.pm
@@ -4,10 +4,13 @@ use strict;
 use warnings;
 use base 'FusionInventory::Agent::XML::Query';
 
+use Carp;
 use XML::Simple;
 
 sub new {
     my ($class, $params) = @_;
+
+    croak "No token" unless $params->{token};
 
     my $self = $class->SUPER::new($params);
 
