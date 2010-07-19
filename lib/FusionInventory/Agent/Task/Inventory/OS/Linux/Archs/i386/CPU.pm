29d325ae0f4f410ee652be89783e56e5d39c8972
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-19 22:37:12 +0200

    move parsing in a distinc function for testing purposes

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Archs/i386/CPU.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Archs/i386/CPU.pm
@@ -45,49 +45,24 @@ sub doInventory {
         }
     }
 
-    my @cpuProcs;
-    my @cpuCoreCpts;
-    if (!open my $handle, '<', '/proc/cpuinfo') {
-        $logger->debug("Can't open /proc/cpuinfo: $ERRNO");
-    } else {
-        my $id=0;
-        my $cpuInfo = {};
-        my $cpuNbr = 0;
-        my $hasPhysicalId;
-        while (<$handle>) {
-            if (/^physical\sid\s*:\s*(\d+)/i) {
-                if ((!defined($cpuCoreCpts[$1]))||$cpuCoreCpts[$1]<$1+1) {
-                    $cpuCoreCpts[$1] = $1+1;
-                }
-                $cpuNbr = $1;
-                $hasPhysicalId = 1;
-            } elsif (/^\s*(\S+.*\S+)\s*:\s*(.+)/i) {
-                $cpuInfo->{$1} = $2;
-            } elsif (/^\s*$/) {
-                $cpuProcs[$cpuNbr]= $cpuInfo;
-                $cpuInfo = {};
-                $cpuNbr++ unless $hasPhysicalId;
-            }
-        }
-        close $handle;
-    }
+    my ($cpuProcs, $cpuCoreCpts) = getInfosFromProc($logger);
 
-    my $maxId = @cpu?@cpu-1:@cpuProcs-1;
+    my $maxId = @cpu?@cpu-1:@$cpuProcs-1;
     foreach my $id (0..$maxId) {
-        if ($cpuProcs[$id]->{vendor_id}) {
-            $cpuProcs[$id]->{vendor_id} =~ s/Genuine//;
-            $cpuProcs[$id]->{vendor_id} =~ s/(TMx86|TransmetaCPU)/Transmeta/;
-            $cpuProcs[$id]->{vendor_id} =~ s/CyrixInstead/Cyrix/;
-            $cpuProcs[$id]->{vendor_id} =~ s/CentaurHauls/VIA/;
+        if ($cpuProcs->[$id]->{vendor_id}) {
+            $cpuProcs->[$id]->{vendor_id} =~ s/Genuine//;
+            $cpuProcs->[$id]->{vendor_id} =~ s/(TMx86|TransmetaCPU)/Transmeta/;
+            $cpuProcs->[$id]->{vendor_id} =~ s/CyrixInstead/Cyrix/;
+            $cpuProcs->[$id]->{vendor_id} =~ s/CentaurHauls/VIA/;
 
-            $cpu[$id]->{MANUFACTURER} = $cpuProcs[$id]->{vendor_id};
+            $cpu[$id]->{MANUFACTURER} = $cpuProcs->[$id]->{vendor_id};
         }
-        $cpu[$id]->{NAME} = $cpuProcs[$id]->{'model name'};
+        $cpu[$id]->{NAME} = $cpuProcs->[$id]->{'model name'};
         if (!$cpu[$id]->{CORE}) {
-            $cpu[$id]->{CORE} = $cpuCoreCpts[$id];
+            $cpu[$id]->{CORE} = $cpuCoreCpts->[$id];
         }
-        if (!$cpu[$id]->{THREAD} && $cpuProcs[$id]->{'siblings'}) {
-            $cpu[$id]->{THREAD} = $cpuProcs[$id]->{'siblings'};
+        if (!$cpu[$id]->{THREAD} && $cpuProcs->[$id]->{'siblings'}) {
+            $cpu[$id]->{THREAD} = $cpuProcs->[$id]->{'siblings'};
         }
         if ($cpu[$id]->{NAME} =~ /([\d\.]+)s*(GHZ)/i) {
             $cpu[$id]->{SPEED} = {
@@ -98,6 +73,46 @@ sub doInventory {
 
         $inventory->addCPU($cpu[$id]);
     }
+}
+
+sub getInfosFromProc {
+    my ($logger, $file, $mode) = @_;
+
+    my ($procs, $cores);
+
+    if (!open my $handle, $mode, $file) {
+        $logger->debug("Can't open $file: $ERRNO");
+    } else {
+        my $cpu;
+        my $cpuNbr = 0;
+        while (my $line = <$handle>) {
+            if ($line =~ /^([^:]+\S) \s* : \s (.+)/x) {
+                $cpu->{$1} = $2;
+            } elsif ($line =~ /^$/) {
+                next unless $cpu;
+
+                my $id = $cpu->{'physical id'};
+                my $hasPhysicalId = 0;
+                if (defined $id) {
+                    if (
+                        !defined($cores->[$id]) ||
+                        $cores->[$id] < $id + 1
+                    ) {
+                        $cores->[$id] = $id + 1;
+                    }
+                    $cpuNbr = $id;
+                    $hasPhysicalId = 1;
+                }
+
+                $procs->[$cpuNbr]= $cpu;
+                $cpuNbr++ unless $hasPhysicalId;
+                undef $cpu;
+            }
+        }
+        close $handle;
+    }
+
+    return $procs, $cores;
 }
 
 1;
