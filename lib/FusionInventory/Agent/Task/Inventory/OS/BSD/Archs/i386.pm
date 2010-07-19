c0ee82fd3c59522ea76e304dbd5deb921ee2497f
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-07-19 21:31:21 +0200

    don't test dmidecode presence again

--- lib/FusionInventory/Agent/Task/Inventory/OS/BSD/Archs/i386.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/BSD/Archs/i386.pm
@@ -1,16 +1,16 @@
 package FusionInventory::Agent::Task::Inventory::OS::BSD::Archs::i386;
-# for i386 in case dmidecode is not available
 
 use strict;
 use warnings;
+
+# Only run this module if dmidecode has not been found
+our $runMeIfTheseChecksFailed =
+    ["FusionInventory::Agent::Task::Inventory::OS::Generic::Dmidecode"];
 
 sub isInventoryEnabled{
     my $arch;
     chomp($arch=`sysctl -n hw.machine`);
     return if (($arch ne "i386") && ($arch ne "amd64"));
-    # dmidecode must not be present
-    `dmidecode 2>&1`;
-    return if ($? >> 8)==0;
     1;
 }
 
