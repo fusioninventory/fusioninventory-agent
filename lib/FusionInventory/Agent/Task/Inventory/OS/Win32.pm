57e701ef1be38a02295ef1e27e4c7ee120e66d9f
Goneri Le Bouder <goneri@rulezlan.org>
2010-08-03 04:09:36 +0400

    W32: use the standard codepage for the registry

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32.pm
@@ -13,6 +13,8 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(getWmiProperties encodeFromWmi encodeFromRegistry);
 
+my $localCodepage;
+
 use Encode;
 
 # We don't need to encode to UTF-8 on Win7
@@ -20,7 +22,7 @@ sub encodeFromWmi {
     my ($string) = @_;
 
 #  return (Win32::GetOSName() ne 'Win7')?encode("UTF-8", $string):$string; 
-    encode("UTF-8", $string); 
+#    return encode("UTF-8", $string); 
 
     return $string;
 }
@@ -28,9 +30,22 @@ sub encodeFromWmi {
 sub encodeFromRegistry {
     my ($string) = @_;
 
-    encode("UTF-8", $string); 
+    return unless $string;
 
-    return $string;
+    if (!$localCodepage) {
+	    no strict; # KEY!READ is nunknown
+        my $lmachine = $Win32::TieRegistry::Registry->Open('LMachine', {
+                Access => Win32::TieRegistry::KEY_READ
+                }) or print "Failed to open LMachine";
+
+        my $codepage =
+	#$lmachine->{"SYSTEM/CurrentControlSet/Control/Nls/CodePage"} or warn;
+            $lmachine->{"SYSTEM\\CurrentControlSet\\Control\\Nls\\CodePage"} or warn;
+
+	    $localCodepage = "cp".$codepage->{ACP};
+    }
+
+    return encode("UTF-8", decode($localCodepage, $string));
 }
 
 sub getWmiProperties {
@@ -63,7 +78,15 @@ sub getWmiProperties {
 
 sub isInventoryEnabled {
     return $OSNAME eq 'MSWin32';
-    eval 'use Win32::OLE; Win32::OLE->Option(CP => Win32::OLE::CP_UTF8);';
+    eval '
+    use Win32::OLE;
+    Win32::OLE->Option(CP => Win32::OLE::CP_UTF8);
+    
+    use constant KEY_WOW64_64KEY => 0x100; 
+    use constant KEY_WOW64_32KEY => 0x200; 
+
+    use Win32::TieRegistry;
+    ';
     return if $EVAL_ERROR;
     return 1;
 }
