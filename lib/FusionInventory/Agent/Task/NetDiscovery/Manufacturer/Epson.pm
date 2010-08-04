4e1e6603dfca9b0c5b92576799dd21c9ad0bd190
David Durieux <d.durieux@siprossii.com>
2010-08-05 00:23:19 +0200

    Add specifications of epson printers

--- lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Epson.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Epson.pm
@@ -15,6 +15,16 @@ sub discovery {
          $description = $description_new;
       }
    }
+
+   if($description =~ m/EPSON Internal 10Base-T/) {
+      my $description_new = $session->snmpGet({
+                        oid => '.1.3.6.1.2.1.25.3.2.1.3.1',
+                        up  => 1,
+                     });
+      if ($description_new ne "null") {
+         $description = $description_new;
+      }
+   }
    return $description;
 }
 
