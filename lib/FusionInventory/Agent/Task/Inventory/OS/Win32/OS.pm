2edf9abdbe8a584f19b08b76cfae84772005f70e
Gonéri Le Bouder <goneri@rulezlan.org>
2010-08-09 21:43:02 +0200

    Win32: retrieve the Windows key on Windows 64 bit
    closes: #267

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/OS.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/OS.pm
@@ -17,6 +17,9 @@ use Win32::TieRegistry (
 
 use FusionInventory::Agent::Task::Inventory::OS::Win32;
 
+sub KEY_WOW64_64KEY () { 0x0100}
+sub KEY_WOW64_32KEY () { 0x0200}
+
 #http://www.perlmonks.org/?node_id=497616
 # Thanks William Gannon && Charles Clarkson
 
@@ -25,9 +28,17 @@ sub getXPkey {
     my ($logger) = @_;
 
     my $machKey = $Registry->Open('LMachine', { Access=> KEY_READ() } )
-	or $logger->fault("Can't open HKEY_LOCAL_MACHINE: $EXTENDED_OS_ERROR");
+    or $logger->fault("Can't open HKEY_LOCAL_MACHINE: $EXTENDED_OS_ERROR");
     my $key     =
-	$machKey->{'Software/Microsoft/Windows NT/CurrentVersion/DigitalProductId'};
+    $machKey->{'Software/Microsoft/Windows NT/CurrentVersion/DigitalProductId'};
+
+    if (!$key) { # 64bit OS?
+        $machKey = $Registry->Open('LMachine', { Access=> KEY_READ()|KEY_WOW64_64KEY() } )
+            or $logger->fault("Can't open HKEY_LOCAL_MACHINE: $EXTENDED_OS_ERROR");
+        $key     =
+        $machKey->{'Software/Microsoft/Windows NT/CurrentVersion/DigitalProductId'};
+    }
+    return unless $key;
     my @encoded = ( unpack 'C*', $key )[ reverse 52 .. 66 ];
 
     # Get indices
