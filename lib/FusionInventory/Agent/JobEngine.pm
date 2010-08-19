5d7a9d22c11f8e2d7bfe62941d4f675638804c9b
Gonéri Le Bouder <goneri@rulezlan.org>
2010-08-19 22:39:28 +0200

    on_child_signal and on_child_close are now anoymous func

--- lib/FusionInventory/Agent/JobEngine.pm
+++ lib/FusionInventory/Agent/JobEngine.pm
@@ -123,7 +123,7 @@ sub run {
 
 
             },
-            got_child_stdout => \&on_child_stdout,
+#            got_child_stdout => \&on_child_stdout,
             got_child_stderr => sub {
                 my ($stderr_line, $wheel_id) = @_[ARG0, ARG1];
 
@@ -137,8 +137,31 @@ sub run {
                     $logger->error($stderr_line);
                 }
             },
-            got_child_close  => \&on_child_close,
-            got_child_signal => \&on_child_signal,
+            got_child_close  => sub {
+                my $wheel_id = $_[ARG0];
+                my $child = delete $_[HEAP]{children_by_wid}{$wheel_id};
+
+                # May have been reaped by on_child_signal().
+                unless (defined $child) {
+                    print "wid $wheel_id closed all pipes.\n";
+                    return;
+                }
+
+                print "pid ", $child->PID, " closed all pipes.\n";
+                delete $_[HEAP]{children_by_pid}{$child->PID};
+                $_[KERNEL]->yield('launchNextTask');
+            },
+            got_child_signal => sub {
+                    print "pid $_[ARG1] exited with status $_[ARG2].\n";
+                    my $child = delete $_[HEAP]{children_by_pid}{$_[ARG1]};
+
+                    # May have been reaped by on_child_close().
+                    return unless defined $child;
+
+                    delete $_[HEAP]{children_by_wid}{$child->ID};
+                }
+
+
         }
     );
 
@@ -260,37 +283,13 @@ sub run {
 #}
 
 # Wheel event, including the wheel's ID.
-sub on_child_stdout {
-    my ($stdout_line, $wheel_id) = @_[ARG0, ARG1];
-    my $child = $_[HEAP]{children_by_wid}{$wheel_id};
-    print "pid ", $child->PID, " STDOUT: $stdout_line\n";
-}
+#sub on_child_stdout {
+#    my ($stdout_line, $wheel_id) = @_[ARG0, ARG1];
+#    my $child = $_[HEAP]{children_by_wid}{$wheel_id};
+#    print "pid ", $child->PID, " STDOUT: $stdout_line\n";
+#}
 
 # Wheel event, including the wheel's ID.
-sub on_child_close {
-    my $wheel_id = $_[ARG0];
-    my $child = delete $_[HEAP]{children_by_wid}{$wheel_id};
-
-    # May have been reaped by on_child_signal().
-    unless (defined $child) {
-        print "wid $wheel_id closed all pipes.\n";
-        return;
-    }
-
-    print "pid ", $child->PID, " closed all pipes.\n";
-    delete $_[HEAP]{children_by_pid}{$child->PID};
-    $_[KERNEL]->yield('launchNextTask');
-}
-
-sub on_child_signal {
-    print "pid $_[ARG1] exited with status $_[ARG2].\n";
-    my $child = delete $_[HEAP]{children_by_pid}{$_[ARG1]};
-
-    # May have been reaped by on_child_close().
-    return unless defined $child;
-
-    delete $_[HEAP]{children_by_wid}{$child->ID};
-}
 
 
 1;
