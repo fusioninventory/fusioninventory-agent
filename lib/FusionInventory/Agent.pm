b7af300c057e1ea88d725d222a815f574071a99f
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-08-28 23:18:41 +0200

    use block-form or eval, rather than string-form

--- lib/FusionInventory/Agent.pm
+++ lib/FusionInventory/Agent.pm
@@ -97,20 +97,24 @@ sub new {
   
 
     if ($OSNAME eq 'MSWin32') {
-        eval '
-use Encode;
-use Win32::API;
+        eval {
+            use Encode;
+            use Win32::API;
 
-	my $GetComputerName = new Win32::API("kernel32", "GetComputerNameExW", ["I", "P", "P"],
-"N");
-my $lpBuffer = "\x00" x 1024;
-my $N=1024;#pack ("c4", 160,0,0,0);
+            my $GetComputerName = Win32::API->new(
+                "kernel32", "GetComputerNameExW", ["I", "P", "P"], "N"
+            );
+            my $lpBuffer = "\x00" x 1024;
+            my $N = 1024; #pack ("c4", 160,0,0,0);
 
-my $return = $GetComputerName->Call(3, $lpBuffer,$N);
+            my $return = $GetComputerName->Call(3, $lpBuffer,$N);
 
-# GetComputerNameExW returns the string in UTF16, we have to change it
-# to UTF8
-$hostname = encode("UTF-8", substr(decode("UCS-2le", $lpBuffer),0,ord $N));';
+            # GetComputerNameExW returns the string in UTF16, we have to change
+            # it # to UTF8
+            $hostname = encode(
+                "UTF-8", substr(decode("UCS-2le", $lpBuffer),0,ord $N)
+            );
+        };
     } else {
         $hostname = hostname();
     }
