e8281169f5ef5544368b7eeee26b0b7576a27a07
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-09-06 00:01:35 +0200

    drop commented debug statements

--- lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Networks.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Networks.pm
@@ -63,7 +63,6 @@ sub doInventory {
     foreach (`netstat -rn`) {
         $ipgateway=$1 if /^default\s+(\S+)/i;
     }
-    #print "Nom :".$zone."*************************\n";
     if ($zone) {
         foreach (`ifconfig -a`) {
             $description = $1 if /^(\S+):/; # Interface name
@@ -79,11 +78,9 @@ sub doInventory {
                 map hex, split /\:/, $1;
             }
             $status = 1 if /<UP,/;
-            #print "DES:".$description."-".$macaddr."-".$nic."-".$num."\n";
             if(($description && $macaddr)) {
                 $nic = $1 if ( $description =~ /^(\S+)(\d+)/);
                 $num = $2 if ( $description =~/^(\S+)(\d+)/);
-                #print "NIC ".$description." :".$nic."-".$num."\n";
                 if ($nic =~ /bge/ ) {
                     $speed = check_bge_nic($nic,$num);
                 } elsif ($nic =~ /ce/) {
@@ -105,7 +102,6 @@ sub doInventory {
                 } else {
                     $speed = check_nic($nic,$num);
                 }
-                #print "SPEED:".$speed ."\n";
                 #HEX TO DEC TO BIN TO IP
                 $ipmask = hex($ipmask);
                 $ipmask = sprintf("%d", $ipmask);
@@ -142,7 +138,6 @@ sub doInventory {
                 $status = 1 if /<UP,/;
                 $type = $1 if /zone\s+(\S+)/i;
             }
-            #Debug
             if ($description && $ipmask) {
                 #HEX TO DEC TO BIN TO IP
                 $ipmask = hex($ipmask);
@@ -153,7 +148,6 @@ sub doInventory {
                 my $binmask = &ip_iptobin ($ipmask ,4);
                 my $binsubnet = $binip & $binmask;
                 $ipsubnet = ip_bintoip($binsubnet,4);
-                #print "INFO2 : ".$description."_". $ipaddress."_".$ipmask."_".$macaddr."\n";
                 $inventory->addNetwork({
                     DESCRIPTION => $description,
                     IPADDRESS => $ipaddress,
@@ -178,11 +172,6 @@ sub doInventory {
                 $description = $1 if /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/; # aggrega
                 $macaddr = $2 if /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/;
                 $speed = $3." ".$4." ".$5 if /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/;
-                #if ($macaddr) {
-                #	  $macaddr = sprintf "%02x:%02x:%02x:%02x:%02x:%02x" ,
-                #	  map hex, split /\:/, $1;
-                #}
-                #$status = $6 if /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/;
                 $status = 1 if /up/;
                 $ipaddress = "0.0.0.0";
                 $inventory->addNetwork({
@@ -214,7 +203,6 @@ sub doInventory {
                 $status = 1 if /online/;
 
                 if ($description &&  $macaddr) {
-                    #print "WWN :".$description."-".$status."-".$type."-".$speed."\n";
                     $inventory->addNetwork({
                         DESCRIPTION => $description,
                         IPADDRESS => $ipaddress,
@@ -247,11 +235,9 @@ sub doInventory {
                 map hex, split /\:/, $1;
             }
             $status = 1 if /<UP,/;
-            #print $description."_".$ipaddress."*****\n";
             if ($description &&  $ipaddress) {
                 $nic = $1 if $description =~ /^(\S+)(\d+):.*$/;
                 $num = $2 if $description =~/^(\S+)(\d+):.*$/;
-                #print "NIC ".$description." :".$nic."-".$num."\n";
                 if ($nic =~ /bge/) {
                     $speed = check_bge_nic($nic,$num);
                 } elsif ($nic =~ /ce/) {
@@ -271,13 +257,11 @@ sub doInventory {
                 } else {
                     $speed = check_nic($nic,$num);
                 }
-                #print "SPEED:".$speed ."\n";
                 #HEX TO DEC TO BIN TO IP
                 $ipmask = hex($ipmask);
                 $ipmask = sprintf("%d", $ipmask);
                 $ipmask = unpack("B*", pack("N", $ipmask));
                 $ipmask = ip_bintoip($ipmask,4);
-                #print $ipmask."\n";
 
                 my $binip = &ip_iptobin ($ipaddress ,4);
                 my $binmask = &ip_iptobin ($ipmask ,4);
