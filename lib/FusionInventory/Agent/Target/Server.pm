bce10ff1d5b6823f70cca466acc32015929f99ac
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-09-17 07:28:19 +0200

    only the server target need this

--- lib/FusionInventory/Agent/Target/Server.pm
+++ lib/FusionInventory/Agent/Target/Server.pm
@@ -54,4 +54,30 @@ sub setAccountInfo {
     return $self->{accountinfo} = $accountinfo;
 }
 
+sub setPrologFreq {
+    my ($self, $prologFreq) = @_;
+
+    my $logger = $self->{logger};
+
+    return unless $prologFreq;
+
+    if ($self->{prologFreq}) {
+        return if $self->{prologFreq} eq $prologFreq;
+        $logger->info(
+            "PROLOG_FREQ has changed since last process ". 
+            "(old=$self->{myData}{prologFreq},new=$prologFreq)"
+        );
+    } else {
+        $logger->info("PROLOG_FREQ has been set: $prologFreq");
+    }
+
+    $self->{prologFreq} = $prologFreq;
+
+    $self->{storage}->save({ data => {
+        prologFreq => $self->{prologFreq},
+        accountInfo => $self->{accountInfo},
+        nextRunDate => $self->{nextRunDate}
+    }});
+}
+
 1;
