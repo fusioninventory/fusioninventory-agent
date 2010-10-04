afb992335dc821f441a9e0e0d168e420d4fb1c55
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-10-04 21:24:35 +0200

    lshal is also a potential info source

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Drives.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Drives.pm
@@ -8,7 +8,9 @@ use English qw(-no_match_vars);
 use FusionInventory::Agent::Tools;
 
 sub isInventoryEnabled {
-    return can_run ("df");
+    return 
+        can_run ('df') ||
+        can_run ('lshal');
 }
 
 sub doInventory {
