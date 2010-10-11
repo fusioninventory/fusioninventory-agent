18b98b19c07aaec1e767c82c03bd835834986712
Gonéri Le Bouder <goneri@rulezlan.org>
2010-10-11 22:21:34 +0200

    import the POE interface from the exp POE branch
    modules were in lib/FusionInventory/Agent/Job

---
+++ lib/FusionInventory/Agent/POE/Logger.pm
@@ -0,0 +1,42 @@
+package FusionInventory::Agent::POE::Logger;
+
+use FusionInventory::Agent::Job;
+
+sub new {
+    my $self = {};
+    
+    bless $self;
+}
+
+sub debug {
+  my ($self, $msg) = @_;
+
+  sendError('debug', $msg);
+}
+
+sub info {
+  my ($self, $msg) = @_;
+
+  sendError('info', $msg);
+}
+
+sub error {
+  my ($self, $msg) = @_;
+
+  sendError('error', $msg);
+}
+
+sub fault {
+  my ($self, $msg) = @_;
+
+  sendError('fault', $msg);
+}
+
+sub user {
+  my ($self, $msg) = @_;
+
+  sendError('user', $msg);
+}
+
+
+1;
---
+++ lib/FusionInventory/Agent/POE/Target.pm
@@ -0,0 +1,45 @@
+package FusionInventory::Agent::POE::Target;
+
+use strict;
+use warnings;
+
+use POE::Component::IKC::ClientLite;
+my $poe;
+
+sub new {
+    my $self = {};
+
+    my $logger = $self->{logger};
+
+    my $name   = "Target$$";
+    $poe = create_ikc_client(
+        port    => 3030,
+        name    => $name,
+        timeout => 10,
+    );
+    die $POE::Component::IKC::ClientLite::error unless $poe;
+
+    tie %$self, __PACKAGE__;
+
+    bless $self;
+}
+
+sub FETCH {
+    my($self, $key) = @_;
+   
+#    warn "Code me\n";
+    my $tmp = {
+        key => $key,
+        moduleName => $ARGV[0]
+    };
+    $poe->post_respond('targetsList/get', $tmp);
+}
+sub TIEHASH  {
+    my $storage = bless {}, shift;
+    warn "New ReportHash created, stored in $storage.\n";
+    $storage
+}
+
+sub DESTROY { }
+
+1;
