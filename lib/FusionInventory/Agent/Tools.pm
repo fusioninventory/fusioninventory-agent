f9ceb7dbbec3e8200d45ea40b7ebf91da0750f4c
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-10-21 22:11:30 +0200

    split unix-specific functions into a separate  module

--- lib/FusionInventory/Agent/Tools.pm
+++ lib/FusionInventory/Agent/Tools.pm
@@ -7,7 +7,6 @@ use base 'Exporter';
 use English qw(-no_match_vars);
 use File::stat;
 use Memoize;
-use Time::Local;
 
 our @EXPORT = qw(
     getFormatedLocalTime
@@ -18,9 +17,6 @@ our @EXPORT = qw(
     getCanonicalSize
     getControllersFromLspci
     getInfosFromDmidecode
-    getIpDhcp
-    getPackagesFromCommand
-    getFilesystemsFromDf
     getDeviceCapacity
     getSanitizedString
     compareVersion
@@ -244,197 +240,6 @@ sub _parseDmidecode {
     return $info;
 }
 
-sub _findDhcpLeaseFile {
-    my ($logger) = @_;
-
-    my @files;
-
-    foreach my $dir qw(
-        /var/db
-        /var/lib/dhcp3
-        /var/lib/dhcp
-    ) {
-        next unless -d $dir;
-
-        push @files,
-            grep { -s $_ }
-            glob("$dir/*.lease");
-
-    }
-
-    return unless @files;
-
-    # sort by creation time
-    @files =
-        map { $_->[0] }
-        sort { $a->[1]->ctime() <=> $b->[1]->ctime() }
-        map { [ $_, stat($_) ] }
-    @files;
-
-    # take the last one
-    return $files[-1];
-}
-
-sub _parseDhcpLeaseFile {
-    my ($logger, $if, $lease_file) = @_;
-
-    my ($server_ip, $expiration_time);
-
-    my $handle;
-    if (!open $handle, '<', $lease_file) {
-        $logger->error("Can't open $lease_file");
-        return;
-    }
-
-    my ($lease, $dhcp);
-
-    # find the last lease for the interface with its expire date
-    while (my $line = <$handle>) {
-        if ($line=~ /^lease/i) {
-            $lease = 1;
-            next;
-        }
-        if ($line=~ /^}/) {
-            $lease = 0;
-            next;
-        }
-
-        next unless $lease;
-
-        # inside a lease section
-        if ($line =~ /interface\s+"([^"]+)"/){
-            $dhcp = ($1 eq $if);
-            next;
-        }
-
-        next unless $dhcp;
-
-        if (
-            $line =~ 
-            /option \s+ dhcp-server-identifier \s+ (\d{1,3}(?:\.\d{1,3}){3})/x
-        ) {
-            # server IP
-            $server_ip = $1;
-        } elsif (
-            $line =~
-            /expire \s+ \d \s+ (\d+)\/(\d+)\/(\d+) \s+ (\d+):(\d+):(\d+)/x
-        ) {
-            $expiration_time = timelocal($6, $5, $4, $3, $2, $1);
-        }
-    }
-    close $handle;
-
-    return unless $expiration_time;
-
-    my $current_time = time();
-
-    return $current_time <= $expiration_time ? $server_ip : undef;
-}
-
-sub getIpDhcp {
-    my ($logger, $if) = @_;
-
-    my $dhcpLeaseFile = _findDhcpLeaseFile($logger);
-
-    return unless $dhcpLeaseFile;
-
-    _parseDhcpLeaseFile($logger, $if, $dhcpLeaseFile);
-}
-
-sub getPackagesFromCommand {
-     my ($logger, $file, $mode, $callback) = @_;
-
-    my $handle;
-    if (!open $handle, $mode, $file) {
-        my $message = $mode eq '-|' ? 
-            "Can't run command $file: $ERRNO" :
-            "Can't open file $file: $ERRNO"   ;
-        $logger->error($message);
-        return;
-    }
-
-    my $packages;
-    
-    while (my $line = <$handle>) {
-        chomp $line;
-        my $package = $callback->($line);
-        push @$packages, $package if $package;
-    }
-
-    close $handle;
-
-    return $packages;
-}
-
-sub getFilesystemsFromDf {
-    my %params = @_;
-
-    my $handle;
-
-    SWITCH: {
-        if ($params{command}) {
-            if (!open $handle, '-|', $params{command}) {
-                $params{logger}->error(
-                    "Can't run command $params{command}: $ERRNO"
-                ) if $params{logger};
-                return;
-            }
-            last SWITCH;
-        }
-        if ($params{file}) {
-            if (!open $handle, '<', $params{file}) {
-                $params{logger}->error(
-                    "Can't open file $params{file}: $ERRNO"
-                ) if $params{logger};
-                return;
-            }
-            last SWITCH;
-        }
-        die "neither command nor file parameter given";
-    }
-
-    my @filesystems;
-    
-    # get headers line first
-    my $line = <$handle>;
-    chomp $line;
-    my @headers = split(/\s+/, $line);
-
-    while (my $line = <$handle>) {
-        chomp $line;
-        my @infos = split(/\s+/, $line);
-
-        # depending of the number of colums, information index change
-        my ($filesystem, $total, $free, $type);
-        if ($headers[1] eq 'Type') {
-            $filesystem = $infos[1];
-            $total      = $infos[2];
-            $free       = $infos[4];
-            $type       = $infos[6];
-        } else {
-            $total = $infos[1];
-            $free  = $infos[3];
-            $type  = $infos[5];
-        }
-
-        # skip some virtual filesystems
-        next if $total !~ /^\d+$/ || $total == 0;
-        next if $free  !~ /^\d+$/ || $free  == 0;
-
-        push @filesystems, {
-            VOLUMN     => $infos[0],
-            FILESYSTEM => $filesystem,
-            TOTAL      => sprintf("%i", $total / 1024),
-            FREE       => sprintf("%i", $free / 1024),
-            TYPE       => $type
-        };
-    }
-
-    close $handle;
-
-    return @filesystems;
-}
-
 sub getDeviceCapacity {
     my ($dev) = @_;
     my $command = `/sbin/fdisk -v` =~ '^GNU' ? 'fdisk -p -s' : 'fdisk -s';
@@ -568,30 +373,6 @@ $info = {
     ],
     ...
 }
-
-=head2 getIpDhcp
-
-Returns an hashref of information for current DHCP lease.
-
-=head2 getPackagesFromCommand
-
-Returns a list of packages as an arrayref of hashref, by parsing given command
-output with given callback.
-
-=head2 getFilesystemsFromDf(%params)
-
-Returns a list of filesystems as a list of hashref, by parsing given df
-command output.
-
-=over
-
-=item logger
-
-=item command
-
-=item file
-
-=back
 
 =head2 getDeviceCapacity($device)
 
