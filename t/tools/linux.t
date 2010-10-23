fusioninventory/fusioninventory-agent

commit 73db5f40057e15729a10c3e6f69cfddb53052870

Linux/PPC: Don't create an empty CPU
On Linux/PPC "fake" CPU section is used to describe the
machine

lib/FusionInventory/Agent/Tools/Linux.pm
@@ -96,13 +96,15 @@ sub getCPUsFromProc {
             $cpu->{$1} = $2;
         } elsif ($line =~ /^$/) {
             next unless $cpu;
-            push @$cpus, $cpu;
+            # On PPC, a "fake" CPU section is used to describe the
+            # machine
+            push @$cpus, $cpu unless $cpu->{'pmac-generation'};
             undef $cpu;
         }
     }
     close $handle;
 
-    push @$cpus, $cpu if $cpu;
+    push @$cpus, $cpu unless $cpu->{'pmac-generation'};
 
     return $cpus;
 }

commit e3a83e3b690777bdc34946517f2803b9f8a2bec0

Don't use positive exclusion criteria ("this tag show that's not an actual cpu"), but negative criteria ("if this tag is missing, that's not an actual cpu"), this allow to filter out more artefact Also fix the regression introduced by 73db5f4 Finally, add linux-ppc-3 test case to the tests

lib/FusionInventory/Agent/Tools/Linux.pm
@@ -109,16 +109,22 @@ sub getCPUsFromProc {
         if ($line =~ /^([^:]+\S) \s* : \s (.+)/x) {
             $cpu->{$1} = $2;
         } elsif ($line =~ /^$/) {
-            next unless $cpu;
-            # On PPC, a "fake" CPU section is used to describe the
-            # machine
-            push @$cpus, $cpu unless $cpu->{'pmac-generation'};
+            # an empty line marks the end of a cpu section
+            # push to the list, but only if it is a valid cpu
+            push @$cpus, $cpu if $cpu &&
+                    exists $cpu->{processor} ||
+                    exists $cpu->{Processor} ||
+                    exists $cpu->{cpu};
             undef $cpu;
         }
     }
     close $handle;
 
-    push @$cpus, $cpu unless $cpu->{'pmac-generation'};
+    # push remaining cpu to the list, if it is valid cpu
+    push @$cpus, $cpu if $cpu &&
+                    exists $cpu->{processor} ||
+                    exists $cpu->{Processor} ||
+                    exists $cpu->{cpu};
 
     return $cpus;
 }

t/tools/linux.t
@@ -158,11 +158,6 @@ my %cpu_tests = (
             'CPU revision' => '0',
             'CPU part' => '0x2e3'
         },
-        {
-            'Revision' => '0000',
-            'Hardware' => 'Thecus N2100',
-            'Serial' => '0000000000000000'
-        }
     ],
     'linux-ia64-1' => [
         {
@@ -238,9 +233,19 @@ my %cpu_tests = (
             'clock' => '1452.000000MHz',
             'processor' => '1'
         },
+    ],
+    'linux-ppc-3' => [
         {
-            'machine' => 'CHRP IBM,7029-6C3',
-            'timebase' => '181495202'
+            'revision' => '3.1 (pvr 003c 0301)',
+            'cpu' => 'PPC970FX, altivec supported',
+            'clock' => '2700.000000MHz',
+            'processor' => '0'
+        },
+        {
+            'revision' => '3.1 (pvr 003c 0301)',
+            'cpu' => 'PPC970FX, altivec supported',
+            'clock' => '2700.000000MHz',
+            'processor' => '1'
         }
     ],
     'linux-sparc-1' => [