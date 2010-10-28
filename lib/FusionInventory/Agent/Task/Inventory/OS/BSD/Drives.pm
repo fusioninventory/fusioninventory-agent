ad3ddb5e4c1d5453702d18704eca545d1c5f040e
Gonéri Le Bouder <goneri@rulezlan.org>
2010-10-28 22:30:14 +0200

    Revert " MacOSX/Drives: use lsvfs to know the FS avalaible"
    This reverts commit 0cbad30bea850afa4a2648d96d191a6d6e35f53a.
    
    Ok, I was on crack. I patched BSD/Drives.pm instead of BSD/Drives.pm...
    
    Conflicts:
    
    	Changes

--- lib/FusionInventory/Agent/Task/Inventory/OS/BSD/Drives.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/BSD/Drives.pm
@@ -3,22 +3,6 @@ package FusionInventory::Agent::Task::Inventory::OS::BSD::Drives;
 use strict;
 use warnings;
 
-sub getVfsFromLsvfs {
-    my ($handle) = @_;
-
-    my %vfs;
-    my $in;
-    while (<$handle>) {
-	if (/^---/) {
-	    $in = 1;
-	} elsif ($in && /^(\S+)/) {
-	    $vfs{$1} = 1;
-	}
-    }
-
-    return %vfs;
-}
-
 sub doInventory {
     my $params = shift;
     my $inventory = $params->{inventory};
@@ -29,17 +13,8 @@ sub doInventory {
     my $type;
     my $volumn;
 
-    my %vfs;
-    if (open my $handle, '-|', 'lsvfs') {
-	%vfs = getVfsFromLsvfs($handle);
-	close $handle;
-    }
 
-# Just in case lsvfs fails
-    $vfs{ffs} = 1;
-    $vfs{ufs} = 1;
-
-    for my $t (keys %vfs) {
+    for my $t ("ffs","ufs") {
 # OpenBSD has no -m option so use -k to obtain results in kilobytes
         for(`df -P -t $t -k 2>&1`){
             if(/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\n/){
