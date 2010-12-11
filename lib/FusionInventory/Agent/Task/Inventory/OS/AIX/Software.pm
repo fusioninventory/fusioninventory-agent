c3775bd3fa097f283fa183003dca56149d2971e2
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-11 18:20:54 +0100

    fix configuration checking

--- lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Software.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/AIX/Software.pm
@@ -6,13 +6,11 @@ use warnings;
 use FusionInventory::Agent::Tools;
 
 sub isInventoryEnabled {
-    my $params = shift;
+    my (%params) = @_;
 
-    # Do not run an package inventory if there is the --nosoft parameter
-    return if ($params->{config}->{nosoft});
-
-    return unless can_run('lslpp');
-    1;
+    return
+        !$params{no_software} &&
+        can_run('lslpp');
 }
 
 sub doInventory {
