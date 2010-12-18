d99f3ff79914d2adc3f9f4efb1f4c574da0a70ad
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-18 15:36:38 +0100

    use an explicit iterator

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Video.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Video.pm
@@ -32,44 +32,50 @@ sub _getDdcprobeData {
 sub _parseXorgFd {
     my ($file) = @_;
 
-    my $xorgData;
-    if (open XORG, $file) {
-        foreach (<XORG>) {
-            if (!$xorgData->{resolution} && /Modeline\s"(\S+?)"/) {
-                $xorgData->{resolution}=$1 
-            } elsif (/Integrated Graphics Chipset:\s+(.*)/) {
-                # Intel
-                $xorgData->{name}=$1;
-            } elsif (/Virtual screen size determined to be (\d+)\s*x\s*(\d+)/) {
-                # Nvidia
-                $xorgData->{resolution}="$1x$2";
-            } elsif (/NVIDIA GPU\s*(.*?)\s*at/) {
-                $xorgData->{name}=$1;
-            } elsif (/VESA VBE OEM:\s*(.*)/) {
-                $xorgData->{name}=$1;
-            } elsif (/VESA VBE OEM Product:\s*(.*)/) {
-                $xorgData->{product}=$1;
-            } elsif (/VESA VBE Total Mem: (\d+)\s*(\w+)/i) {
-                $xorgData->{memory}=$1.$2;
-            } elsif (/RADEON\(0\): Chipset: "(.*?)"/i) {
-                # ATI /Radeon
-                $xorgData->{name}=$1;
-            } elsif (/Virtual size is (\S+)/i) {
-                # VESA / XFree86
-                $xorgData->{resolution}=$1;
-            } elsif (/Primary Device is: PCI (.+)/i) {
-                $xorgData->{pcislot}=$1;
-                # mimic lspci pci slot format
-                $xorgData->{pcislot} =~ s/^00@//;
-                $xorgData->{pcislot} =~ s/(\d{2}):(\d{2}):(\d)$/$1:$2.$3/;
-            } elsif (/NOUVEAU\(0\): Chipset: "(.*)"/) {
-                # Nouveau
-                $xorgData->{product}=$1;
-            }
+    my $handle = getFileHandle(
+        file => $file
+    );
+
+    return unless $handle;
+
+    my $data;
+    foreach my $line (<$handle>) {
+        if ($line =~ /Modeline\s"(\S+?)"/) {
+            $data->{resolution}=$1 if !$data->{resolution};
+        } elsif ($line =~ /Integrated Graphics Chipset:\s+(.*)/) {
+            # Intel
+            $data->{name}=$1;
+        } elsif ($line =~ /Virtual screen size determined to be (\d+)\s*x\s*(\d+)/) {
+            # Nvidia
+            $data->{resolution}="$1x$2";
+        } elsif ($line =~ /NVIDIA GPU\s*(.*?)\s*at/) {
+            $data->{name}=$1;
+        } elsif ($line =~ /VESA VBE OEM:\s*(.*)/) {
+            $data->{name}=$1;
+        } elsif ($line =~ /VESA VBE OEM Product:\s*(.*)/) {
+            $data->{product}=$1;
+        } elsif ($line =~ /VESA VBE Total Mem: (\d+)\s*(\w+)/i) {
+            $data->{memory}=$1.$2;
+        } elsif ($line =~ /RADEON\(0\): Chipset: "(.*?)"/i) {
+            # ATI /Radeon
+            $data->{name}=$1;
+        } elsif ($line =~ /Virtual size is (\S+)/i) {
+            # VESA / XFree86
+            $data->{resolution}=$1;
+        } elsif ($line =~ /Primary Device is: PCI (.+)/i) {
+            $data->{pcislot}=$1;
+            # mimic lspci pci slot format
+            $data->{pcislot} =~ s/^00@//;
+            $data->{pcislot} =~ s/(\d{2}):(\d{2}):(\d)$/$1:$2.$3/;
+        } elsif ($line =~ /NOUVEAU\(0\): Chipset: "(.*)"/) {
+            # Nouveau
+            $data->{product}=$1;
         }
-        close(XORG);
     }
-    return $xorgData;
+
+    close $handle;
+
+    return $data;
 }
 
 sub doInventory {
