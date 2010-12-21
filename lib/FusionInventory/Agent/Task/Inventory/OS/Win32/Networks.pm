471cd0797b1f1a2596f423598487006f3d9f2149
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-21 23:20:58 +0100

    rename variables for consistency with other systems, and fetch current object from array once

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Networks.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Networks.pm
@@ -24,17 +24,19 @@ sub doInventory {
     my $strComputer = '.';
     my $objWMIService = Win32::OLE->GetObject('winmgmts:' . '{impersonationLevel=impersonate}!\\\\' . $strComputer . '\\root\\cimv2');
 
-    my $nics = $objWMIService->ExecQuery('SELECT * FROM Win32_NetworkAdapterConfiguration');
 
     my $defaultGw;
     my @ips;
     my @ip6s;
-    my @netifs;
+    my @interfaces;
     my %defaultgateways;
     my %dns;
+
+    my $nics = $objWMIService->ExecQuery('SELECT * FROM Win32_NetworkAdapterConfiguration');
     foreach my $nic (in $nics) {
-        my $idx = $nic->Index;
-        $netifs[$idx]{description} =  encodeFromWmi($nic->Description);
+        my $interface = $interfaces[$nic->Index];
+
+        $interface->{description} = encodeFromWmi($nic->Description);
 
         foreach (@{$nic->DefaultIPGateway || []}) {
             $defaultgateways{$_} = 1;
@@ -50,67 +52,69 @@ sub doInventory {
                 my $mask = shift @{$nic->IPSubnet};
                 if ($address =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/) {
                     push @ips, $address;
-                    push @{$netifs[$idx]{ipaddress}}, $address;
-                    push @{$netifs[$idx]{ipmask}}, $mask;
-                    push @{$netifs[$idx]{ipsubnet}}, getSubnetAddress($address, $mask);
+                    push @{$interface->{ipaddress}}, $address;
+                    push @{$interface->{ipmask}}, $mask;
+                    push @{$interface->{ipsubnet}}, getSubnetAddress($address, $mask);
                 } elsif ($address =~ /\S+/) {
                     push @ip6s, $address;
-                    push @{$netifs[$idx]{ipaddress6}}, $address;
-                    push @{$netifs[$idx]{ipmask6}}, $mask;
-                    push @{$netifs[$idx]{ipsubnet6}}, getSubnetAddressIPv6($address, $mask);
+                    push @{$interface->{ipaddress6}}, $address;
+                    push @{$interface->{ipmask6}}, $mask;
+                    push @{$interface->{ipsubnet6}}, getSubnetAddressIPv6($address, $mask);
                 }
             }
         }
 
         if ($nic->DefaultIPGateway) {
-            $netifs[$idx]{ipgateway} = $nic->DefaultIPGateway()->[0];
+            $interface->{ipgateway} = $nic->DefaultIPGateway()->[0];
         }
 
-        $netifs[$idx]{status} = $nic->IPEnabled?"Up":"Down";
-        $netifs[$idx]{name} = $nic->Name;
-        $netifs[$idx]{ipdhcp} = $nic->DHCPServer;
-        $netifs[$idx]{macaddr} = $nic->MACAddress;
-        $netifs[$idx]{mtu} = $nic->MTU;
+        $interface->{status}  = $nic->IPEnabled ? "Up" : "Down";
+        $interface->{name}    = $nic->Name;
+        $interface->{ipdhcp}  = $nic->DHCPServer;
+        $interface->{macaddr} = $nic->MACAddress;
+        $interface->{mtu}     = $nic->MTU;
+
     }
 
     $nics = $objWMIService->ExecQuery('SELECT * FROM Win32_NetworkAdapter');
     foreach my $nic (in $nics) {
-        my $idx = $nic->Index;
-        $netifs[$idx]{virtualdev} = $nic->PhysicalAdapter?0:1;
-        $netifs[$idx]{name} = $nic->Name;
-        $netifs[$idx]{macaddr} = $nic->MACAddress;
-        $netifs[$idx]{speed} = $nic->Speed;
-        $netifs[$idx]{pnpdeviceid} = $nic->PNPDeviceID;
+        my $interface = $interfaces[$nic->Index];
+
+        $interface->{virtualdev}  = $nic->PhysicalAdapter?0:1;
+        $interface->{name}        = $nic->Name;
+        $interface->{macaddr}     = $nic->MACAddress;
+        $interface->{speed}       = $nic->Speed;
+        $interface->{pnpdeviceid} = $nic->PNPDeviceID;
     }
 
-    foreach my $netif (@netifs) {
+    foreach my $interface (@interfaces) {
 
         # http://comments.gmane.org/gmane.comp.monitoring.fusion-inventory.devel/34
         next unless $netif->{pnpdeviceid};
 
         next if
-            !$netif->{ipaddress} &&
-            !$netif->{ipaddress6} &&
-            !$netif->{macaddr}
+            !$interface->{ipaddress} &&
+            !$interface->{ipaddress6} &&
+            !$interface->{macaddr}
 
-        my $ipaddress  = $netif->{ipaddress}  ? join('/', @{$netif->{ipaddress})  : undef;
-        my $ipmask     = $netif->{ipmask}     ? join('/', @{$netif->{ipmask})     : undef;
-        my $ipsubnet   = $netif->{ipsubnet}   ? join('/', @{$netif->{ipsubnet})   : undef;
-        my $ipaddress6 = $netif->{ipaddress6} ? join('/', @{$netif->{ipaddress6}) : undef;
+        my $ipaddress  = $interface->{ipaddress}  ? join('/', @{$interface->{ipaddress})  : undef;
+        my $ipmask     = $interface->{ipmask}     ? join('/', @{$interface->{ipmask})     : undef;
+        my $ipsubnet   = $interface->{ipsubnet}   ? join('/', @{$interface->{ipsubnet})   : undef;
+        my $ipaddress6 = $interface->{ipaddress6} ? join('/', @{$interface->{ipaddress6}) : undef;
 
         $inventory->addNetwork({
-            DESCRIPTION => $netif->{description},
+            DESCRIPTION => $interface->{description},
             IPADDRESS => $ipaddress,
-            IPDHCP => $netif->{ipdhcp},
-            IPGATEWAY => $netif->{ipgateway},
+            IPDHCP => $interface->{ipdhcp},
+            IPGATEWAY => $interface->{ipgateway},
             IPMASK => $ipmask,
             IPSUBNET => $ipsubnet,
             IPADDRESS6 => $ipaddress6,
-            MACADDR => $netif->{macaddr},
-            MTU => $netif->{mtu},
-            STATUS => $netif->{status},
-            TYPE => $netif->{type},
-            VIRTUALDEV => $netif->{virtualdev}
+            MACADDR => $interface->{macaddr},
+            MTU => $interface->{mtu},
+            STATUS => $interface->{status},
+            TYPE => $interface->{type},
+            VIRTUALDEV => $interface->{virtualdev}
         });
 
     }
