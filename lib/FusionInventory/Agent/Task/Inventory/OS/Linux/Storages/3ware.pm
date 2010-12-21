47f90a445863f49ca7e5a2935d905639d4704283
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-21 12:48:33 +0100

    split in multiple functions for testing purposes

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/3ware.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/3ware.pm
@@ -32,43 +32,20 @@ sub doInventory {
 
     my $devices = getDevicesFromUdev(logger => $logger);
 
-    # First, getting the cards : c0, c1... etc.
-    foreach (`tw_cli info`) {
-        next unless /^(c\d+)\s+([\w-]+)/;
-        my $card = $1;
-        my $card_model = $2;
-
-        # Second, getting the units : u0, u1... etc.
-        foreach (`tw_cli info $card`) {
-            next unless /^(u(\d+))/;
-            my $unit = $1;
-            my $unit_id = $2;
+    foreach my $card (_getCards()) {
+        foreach my $unit (_getUnits($card)) {
 
             # Try do get unit's serial in order to compare it to what was found
             # in udev db.
             # Works only on newer cards.
             # Allow us to associate a node to a drive : sda -> WD-WMANS1648590
-            my $sn = `tw_cli info $card $unit serial 2> /dev/null`;
+            my $sn =
+                `tw_cli info $card->{id} $unit->{id} serial 2> /dev/null`;
             $sn =~ s/serial number\s=\s(\w*)\s*/$1/;
 
-            # Third, getting the ports : p0, p1... etc.
-            foreach(`tw_cli info $card $unit`) {
-                next unless /(p\d+)/;
-                my $port = $1;
-
+            foreach my $port (_getPorts($card, $unit)) {
                 # Finally, getting drives' values.
-                my $storage;
-                foreach (`tw_cli info $card $port model serial capacity firmware`) {
-                    if (/Model\s=\s(.*)/) {
-                        $storage->{MODEL} = $1;
-                    } elsif (/Serial\s=\s(.*)/) {
-                        $storage->{SERIALNUMBER} = $1;
-                    } elsif (/Capacity\s=\s(\S+)\sGB.*/) {
-                        $storage->{DISKSIZE} = 1024 * $1;
-                    } elsif (/Firmware Version\s=\s(.*)/) {
-                        $storage->{FIRMWARE} = $1
-                    }
-                }
+                my $storage = _getStorage($card, $port);
 
                 foreach my $device (@$devices) {
                     # How does this work with multiple older cards
@@ -77,30 +54,86 @@ sub doInventory {
                     # 3ware cards.
                     if (
                         $device->{SERIALNUMBER} eq 'AMCC_' . $sn ||
-                        $device->{MODEL} eq 'Logical_Disk_' . $unit_id
+                        $device->{MODEL} eq 'Logical_Disk_' . $unit->{index}
                     ) {
                         $storage->{NAME} = $device->{NAME};
                     }
                 }
 
-                # Getting description from card model, very basic
-                # and unreliable
-                # Assuming only IDE drives can be plugged in
-                # 5xxx/6xxx cards and
-                # SATA drives only to 7xxx/8xxx/9xxxx cards
-                $storage->{DESCRIPTION} = 
-                    $card_model =~ /^[56]/  ? 'IDE'  :
-                    $card_model =~ /^[789]/ ? 'SATA' :
-                    undef;
-                $storage->{MANUFACTURER} = getCanonicalManufacturer(
-                    $storage->{MODEL}
-                );
-                $storage->{TYPE} = 'disk';
-
                 $inventory->addStorage($storage);
             }
         }
     }
+}
+
+
+sub _getCards {
+
+    my @cards;
+    foreach (`tw_cli info`) {
+        next unless /^(c\d+)\s+([\w-]+)/;
+        push @cards, { id => $1, model => $2 };
+    }
+
+    return @cards;
+}
+
+sub _getUnits {
+    my ($card) = @_;
+
+    my @units;
+    foreach (`tw_cli info $card->{id}`) {
+        next unless /^(u(\d+))/;
+        push @units, { id => $1, index => $2 };
+    }
+
+    return @units;
+}
+
+sub _getPorts {
+    my ($card, $unit) = @_;
+
+    my @ports;
+    foreach(`tw_cli info $card->{id} $unit->{id}`) {
+        next unless /(p\d+)/;
+        push @ports, { id => $1 }
+    }
+
+    return @ports;
+}
+
+sub _getStorage {
+    my ($card, $port) = @_;
+
+    my $storage;
+    foreach (`tw_cli info $card->{id} $port->{id} model serial capacity firmware`) {
+        if (/Model\s=\s(.*)/) {
+            $storage->{MODEL} = $1;
+        } elsif (/Serial\s=\s(.*)/) {
+            $storage->{SERIALNUMBER} = $1;
+        } elsif (/Capacity\s=\s(\S+)\sGB.*/) {
+            $storage->{DISKSIZE} = 1024 * $1;
+        } elsif (/Firmware Version\s=\s(.*)/) {
+            $storage->{FIRMWARE} = $1
+        }
+    }
+
+    $storage->{MANUFACTURER} = getCanonicalManufacturer(
+        $storage->{MODEL}
+    );
+    $storage->{TYPE} = 'disk';
+
+    # Getting description from card model, very basic
+    # and unreliable
+    # Assuming only IDE drives can be plugged in
+    # 5xxx/6xxx cards and
+    # SATA drives only to 7xxx/8xxx/9xxxx cards
+    $storage->{DESCRIPTION} = 
+        $card->{model} =~ /^[56]/  ? 'IDE'  :
+        $card->{model} =~ /^[789]/ ? 'SATA' :
+        undef;
+
+    return $storage;
 }
 
 1;
