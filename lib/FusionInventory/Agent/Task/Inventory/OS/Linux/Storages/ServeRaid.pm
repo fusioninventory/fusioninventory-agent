48a61ab4311bfde88b32cb4173b3bb74e44a5ff1
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-21 11:58:44 +0100

    unused values

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/ServeRaid.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/ServeRaid.pm
@@ -43,7 +43,7 @@ sub doInventory {
 
         next unless /Controller type.*:\s(.*)/;
         my $name = $1;
-        my ($serial, $capacity, $scsi, $channel, $state);
+        my ($serial, $capacity);
 
         foreach (`ipssend GETCONFIG $slot PD 2>/dev/null`) {
 # Example Output :
@@ -57,16 +57,12 @@ sub doInventory {
 #         Device ID                : IBM-ESXSCBR036C3DFQDB2Q6CDKM
 #         FRU part number          : 32P0729
 
-            $channel        = $1 if /Channel #(.*):/;
-            $scsi           = $1 if /SCSI ID.*:\s(.*)/;
-            $state          = $1 if /State.*\((.*)\)/;            
             $capacity       = $1 if /Size.*:\s(\d*)\/(\d*)/;
             $serial         = $1 if /Device ID.*:\s(.*)/;
 
             if (/FRU part number.*:\s(.*)/) {
                 my $model = $1;
                 my $manufacturer = getCanonicalManufacturer($serial);
-                ## my $fullname = "$name $slot/$channel/$scsi $state";
 
                 $inventory->addStorage({
                     NAME            => "$manufacturer $model",
@@ -79,8 +75,7 @@ sub doInventory {
                     FIRMWARE        => ""}
                 ); 
 
-                # don't undef $channel, appear only once for several drive.
-                $scsi = $state = $capacity = $serial = undef;
+                $capacity = $serial = undef;
             }
         }
     }
