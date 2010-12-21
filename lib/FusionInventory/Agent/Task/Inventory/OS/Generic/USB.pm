921fb51944140caf5105299de1bd630d18a929d1
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-21 23:55:54 +0100

    rework for consistency with other modules

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/USB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/USB.pm
@@ -11,52 +11,43 @@ sub isInventoryEnabled {
     return can_run('lsusb');
 }
 
-sub _addDevice {
-    my ($inventory, $device) = @_;
-
-    my $class = $device->{class};
-    my $subClass = $device->{subClass};
-    my $productId = $device->{productId};
-    my $vendorId = $device->{vendorId};
-    my $serial;
-
-
-    return unless $productId;
-    return unless $vendorId;
-
-    # We ignore the USB Hub
-    return if $productId eq "0001";
-    return if $productId eq "0002";
-
-    if (defined($device->{serial}) && length($device->{serial}) > 4) {
-        $serial =  $device->{serial};
-    }
-    $inventory->addUSBDevice({
-
-            VENDORID => $vendorId,
-            PRODUCTID => $productId,
-            SERIAL => $serial,
-            CLASS => $class,
-            SUBCLASS => $subClass,
-
-        });
-
-}
-
 sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
-    my $in;
+    my $logger    = $params{logger};
+
+    foreach my $device (_getDevices($logger)) {
+        next unless $device->{PRODUCTID};
+        next unless $device->{VENDORID};
+
+        # ignore the USB Hub
+        next if
+            $device->{PRODUCTID} eq "0001" ||
+            $device->{PRODUCTID} eq "0002" ;
+
+        if (defined($device->{SERIAL}) && length($device->{SERIAL}) < 5) {
+            $device->{SERIAL} = undef;
+        }
+
+        $inventory->addUSBDevice($device);
+    }
+}
+
+sub _getDevices {
+    my ($logger) = @_;
+
+    my @devices;
     my $device;
+    my $in;
 
     foreach (`lsusb -v`) {
         if (/^Device/) {
             $in = 1;
         } elsif (/^\s*$/) {
             $in =0;
-            _addDevice($inventory, $device);
-            $device = {};
+            push @devices, $device;
+            undef $device;
         } elsif ($in) {
             if (/^\s*idVendor\s*0x(\w+)/i) {
                 $device->{vendorId}=$1;
@@ -75,7 +66,9 @@ sub doInventory {
             }
         }
     }
-    _addDevice($device);
+    push @devices, $device if $device;
+
+    return @devices;
 }
 
 1;
