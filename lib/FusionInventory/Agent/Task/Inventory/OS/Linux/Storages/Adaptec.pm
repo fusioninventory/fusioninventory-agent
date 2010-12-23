86614ec9b0381897189f5636bcf39c9675b4459b
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-23 16:12:12 +0100

    split /proc parsing into a distinct function for testing purpose

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/Adaptec.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Storages/Adaptec.pm
@@ -26,36 +26,53 @@ sub doInventory {
 
     my $devices = getDevicesFromUdev(logger => $logger);
 
-    foreach my $hd (@$devices) {
-        next unless $hd->{MANUFACTURER};
-        next unless $hd->{MANUFACTURER} eq 'Adaptec';
+    foreach my $device (@$devices) {
+        next unless $device->{MANUFACTURER};
+        next unless $device->{MANUFACTURER} eq 'Adaptec';
 
-        my $handle = getFilehandle(file => '/proc/scsi/scsi');
-        next unless $handle;
-
-        my $count = -1;
-        while (<$handle>) {
-            next unless /^Host:\sscsi$hd->{SCSI_COID}/;
-            $count++;
-            next unless /Model:\s(\S+).*Rev:\s(\S+)/;
-            my $storage = {
-                NAME        => $hd->{NAME},
-                DESCRIPTION => 'SATA',
-                TYPE        => 'disk',
-                MODEL       => $1,
-                FIRMWARE    => $2
-            };
-            next if $storage->{MODEL} =~ 'raid';
-
-            $storage->{MANUFACTURER} = getCanonicalManufacturer(
-                $storage->{MODEL}
-            );
-            $storage->{SERIALNUMBER} = getSerialnumber(device => "/dev/sg$count");
-
+        foreach my $storage (_getStoragesFromProc(device => $device, logger => $logger)) {
             $inventory->addStorage($storage);
         }
-        close $handle;
     }
+}
+
+sub _getStoragesFromProc {
+    my %params = (
+        file => '/proc/scsi/scsi',
+        @_
+    );
+
+    return unless $params{device};
+
+    my $handle = getFilehandle(file => $params{file}, logger => $params{logger});
+    next unless $handle;
+
+    my @storages;
+
+    my $count = -1;
+    while (<$handle>) {
+        next unless /^Host:\sscsi$params{device}->{SCSI_COID}/;
+        $count++;
+        next unless /Model:\s(\S+).*Rev:\s(\S+)/;
+        my $storage = {
+            NAME        => $params{device}->{NAME},
+            DESCRIPTION => 'SATA',
+            TYPE        => 'disk',
+            MODEL       => $1,
+            FIRMWARE    => $2
+        };
+        next if $storage->{MODEL} =~ 'raid';
+
+        $storage->{MANUFACTURER} = getCanonicalManufacturer(
+            $storage->{MODEL}
+        );
+        $storage->{SERIALNUMBER} = getSerialnumber(device => "/dev/sg$count");
+
+        push @storages, $storage;
+    }
+    close $handle;
+
+    return @storages;
 }
 
 1;
