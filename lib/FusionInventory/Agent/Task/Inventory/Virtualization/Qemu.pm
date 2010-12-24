0ad74482a8adf4055dd3c09474a0bdbbdc71d5cf
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-24 13:17:46 +0100

    reduce indentation by breaking loops if condition is not met

--- lib/FusionInventory/Agent/Task/Inventory/Virtualization/Qemu.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Virtualization/Qemu.pm
@@ -19,43 +19,42 @@ sub doInventory {
     my $inventory = $params{inventory};
 
     foreach ( `ps -ef` ) {
-        if (m/^.*((qemu|kvm|(qemu-kvm)).*\-([fh]d[a-d]|cdrom).*)$/) {
-            # match only if an qemu instance
+        # match only if an qemu instance
+        next unless /^.*((qemu|kvm|(qemu-kvm)).*\-([fh]d[a-d]|cdrom).*)$/;
             
-            my $name = "N/A";
-            my $mem = 0;
-            my $uuid;
-            my $vmtype = $2;
-                        
-            my @process = split (/\-/, $1);     #separate options
-            
-            foreach my $option ( @process ) {
-                if ($name eq "N/A" and $option =~ m/^([fh]d[a-d]|cdrom) (\S+)/) {
-                    $name = $2;
-                } elsif ($option =~ m/^name (\S+)/) {
-                    $name = $1;
-                } elsif ($option =~ m/^m (\S+)/) {
-                    $mem = $1;
-                } elsif ($option =~ m/^uuid (\S+)/) {
-                    $uuid = $1;
-                }
+        my $name = "N/A";
+        my $mem = 0;
+        my $uuid;
+        my $vmtype = $2;
+                    
+        my @process = split (/\-/, $1);     #separate options
+        
+        foreach my $option ( @process ) {
+            if ($name eq "N/A" and $option =~ m/^([fh]d[a-d]|cdrom) (\S+)/) {
+                $name = $2;
+            } elsif ($option =~ m/^name (\S+)/) {
+                $name = $1;
+            } elsif ($option =~ m/^m (\S+)/) {
+                $mem = $1;
+            } elsif ($option =~ m/^uuid (\S+)/) {
+                $uuid = $1;
             }
-            
-            if ($mem == 0 ) {
-                # Default value
-                $mem = 128;
-            }
-            
-            $inventory->addVirtualMachine ({
-                NAME      => $name,
-                UUID      => $uuid,
-                VCPU      => 1,
-                MEMORY    => $mem,
-                STATUS    => "running",
-                SUBSYSTEM => $vmtype,
-                VMTYPE    => $vmtype,
-            });
         }
+        
+        if ($mem == 0 ) {
+            # Default value
+            $mem = 128;
+        }
+        
+        $inventory->addVirtualMachine ({
+            NAME      => $name,
+            UUID      => $uuid,
+            VCPU      => 1,
+            MEMORY    => $mem,
+            STATUS    => "running",
+            SUBSYSTEM => $vmtype,
+            VMTYPE    => $vmtype,
+        });
     }
 }
 
