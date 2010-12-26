ed0136fdcdecf56fbe6c91909810c3f7a9b0068b
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-12-26 22:18:08 +0100

    split parsing into distint functions

--- lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Slots.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Slots.pm
@@ -15,97 +15,123 @@ sub doInventory {
 
     my $inventory = $params{inventory};
 
-    my $description;
-    my $designation;
-    my $name;
-    my $status;
-    my @pci;
-    my $flag;
-    my $flag_pci;
-
     my $class = getClass();
 
-    SWITCH: {
-        if ($class == 4) {
-            foreach (`prtdiag`) {
-                if (/pci/) {
-                    @pci = split(/ +/);
-                    $name=$pci[4]." ".$pci[5];
-                    $description=$pci[0]." (".$pci[1].")";
-                    $designation=$pci[3];
-                    $status="";
-                    $inventory->addSlot({
-                        DESCRIPTION =>  $description,
-                        DESIGNATION =>  $designation,
-                        NAME            =>  $name,
-                        STATUS          =>  $status,
-                    });
-                }
-            }
-            last SWITCH;
-        }
+    my @slots = 
+        $class == 4 ? _getSlots4() :
+        $class == 5 ? _getSlots5() :
+                      _getSlotsDefault() ;
 
-        if ($class == 5) {
-            foreach (`prtdiag`) {
-                last if(/^\=+/ && $flag_pci && $flag);
+    foreach my $slot (@slots) {
+        $inventory->addSlot($slot);
+    }
+}
 
-                if($flag && $flag_pci && /^\s+(\d+)/){
-                    $name = "LSB " . $1;
-                }
-                if($flag && $flag_pci && /^\s+\S+\s+(\S+)/){
-                    $description = $1;
-                }
-                if($flag && $flag_pci && /^\s+\S+\s+\S+\s+(\S+)/){
-                    $designation = $1;
-                }
-                $status = " ";
+sub _getSlots4 {
 
-                #Debug
-                #if ($flag && $flag_pci){print "$name" . "||||" . "$designation" . "||" . "$description\n";}
-                #print $_."\n";
+    my @slots;
 
-                if($flag && $flag_pci){
-                    $inventory->addSlot({
-                            DESCRIPTION =>  $description,
-                            DESIGNATION =>  $designation,
-                            NAME            =>  $name,
-                            STATUS          =>  $status,
-                        });
-                }
-                if(/^=+\S+\s+IO Cards/){$flag_pci = 1;  }
-                if($flag_pci && /^-+/){$flag = 1;}
-            }
-            last SWITCH;
-        }
-
-        # default case
-        foreach (`prtdiag`) {
-            last if(/^\=+/ && $flag_pci);
-            next if(/^\s+/ && $flag_pci);
-            if($flag && $flag_pci && /^(\S+)\s+/){
-                $name = $1;
-            }
-            if($flag && $flag_pci && /(\S+)\s*$/){
-                $designation = $1;
-            }
-            if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
-                $description = $1;
-            }
-            if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
-                $status = $1;
-            }
-            if($flag && $flag_pci){
-                $inventory->addSlot({
-                        DESCRIPTION =>  $description,
-                        DESIGNATION =>  $designation,
-                        NAME            =>  $name,
-                        STATUS          =>  $status,
-                    });
-            }
-            if(/^=+\s+IO Cards/){$flag_pci = 1;}
-            if($flag_pci && /^-+/){$flag = 1;}
+    foreach (`prtdiag`) {
+        if (/pci/) {
+            my @pci = split(/ +/);
+            my $name=$pci[4]." ".$pci[5];
+            my $description=$pci[0]." (".$pci[1].")";
+            my $designation=$pci[3];
+            my $status="";
+            push @slots, {
+                DESCRIPTION => $description,
+                DESIGNATION => $designation,
+                NAME        => $name,
+                STATUS      => $status,
+            };
         }
     }
+
+    return @slots;
+}
+
+sub _getSlots5 {
+
+    my @slots;
+    my $flag;
+    my $flag_pci;
+    my $name;
+    my $description;
+    my $designation;
+    my $status;
+
+    foreach (`prtdiag`) {
+        last if(/^\=+/ && $flag_pci && $flag);
+
+        if($flag && $flag_pci && /^\s+(\d+)/){
+            $name = "LSB " . $1;
+        }
+        if($flag && $flag_pci && /^\s+\S+\s+(\S+)/){
+            $description = $1;
+        }
+        if($flag && $flag_pci && /^\s+\S+\s+\S+\s+(\S+)/){
+            $designation = $1;
+        }
+        $status = " ";
+
+        #Debug
+        #if ($flag && $flag_pci){print "$name" . "||||" . "$designation" . "||" . "$description\n";}
+        #print $_."\n";
+
+        if($flag && $flag_pci){
+            push @slots, {
+                DESCRIPTION =>  $description,
+                DESIGNATION =>  $designation,
+                NAME            =>  $name,
+                STATUS          =>  $status,
+            };
+        }
+        if(/^=+\S+\s+IO Cards/){$flag_pci = 1;  }
+        if($flag_pci && /^-+/){$flag = 1;}
+    }
+
+    return @slots;
+}
+
+sub _getSlotsDefault {
+
+    my @slots;
+    my $flag;
+    my $flag_pci;
+    my $name;
+    my $description;
+    my $designation;
+    my $status;
+
+    # default case
+    foreach (`prtdiag`) {
+        last if(/^\=+/ && $flag_pci);
+        next if(/^\s+/ && $flag_pci);
+        if($flag && $flag_pci && /^(\S+)\s+/){
+            $name = $1;
+        }
+        if($flag && $flag_pci && /(\S+)\s*$/){
+            $designation = $1;
+        }
+        if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
+            $description = $1;
+        }
+        if($flag && $flag_pci && /^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/){
+            $status = $1;
+        }
+        if($flag && $flag_pci){
+            push @slots, {
+                DESCRIPTION =>  $description,
+                DESIGNATION =>  $designation,
+                NAME            =>  $name,
+                STATUS          =>  $status,
+            };
+        }
+        if(/^=+\s+IO Cards/){$flag_pci = 1;}
+        if($flag_pci && /^-+/){$flag = 1;}
+    }
+
+    return @slots;
 }
 
 1;
