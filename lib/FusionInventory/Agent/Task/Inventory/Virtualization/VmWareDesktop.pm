8133493c467992105db3be8a87baaa87c7d9d1e4
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-01-15 17:03:29 +0100

    more consistency with VmWareESX

--- lib/FusionInventory/Agent/Task/Inventory/Virtualization/VmWareDesktop.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Virtualization/VmWareDesktop.pm
@@ -20,46 +20,61 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $uuid;
-    my $mem;
-    my $status;
-    my $name;
-    my $i = 0;
-
-    my $commande = "/Library/Application\\ Support/VMware\\ Fusion\/vmrun list";
-    foreach my $vmxpath ( `$commande` ) {
-        next unless $i++ > 0; # Ignore the first line
-        my $handle;
-        if (!open $handle, '<', $vmxpath) {
-            warn "Can't open $vmxpath: $ERRNO";
-            $logger->debug("Can't open $vmxpath\n");
-            next;
-        }
-        my @vminfos = <$handle>;
-        close $handle;
-
-        foreach my $line (@vminfos) {
-            if ($line =~ m/^displayName =\s\"+(.*)\"/) {
-                $name = $1;
-            }
-            elsif ($line =~ m/^memsize =\s\"+(.*)\"/) {
-                $mem = $1;
-            }
-            elsif ($line =~ m/^uuid.bios =\s\"+(.*)\"/) {
-                $uuid = $1;
-            }
-        }
-
-        $inventory->addVirtualMachine ({
-                NAME      => $name,
-                VCPU      => 1,
-                UUID      => $uuid,
-                MEMORY    => $mem,
-                STATUS    => "running",
-                SUBSYSTEM => "VmWare Fusion",
-                VMTYPE    => "VmWare",
-            });
+    foreach my $machine (_getMachines(
+        command => '/Library/Application Support/VMware Fusion/vmrun', logger => $logger
+    )) {
+        $inventory->addVirtualMachine($machine);
     }
+}
+
+sub _getMachines {
+   my %params = @_;
+
+    my $handle = getFileHandle(%params);
+    return unless $handle;
+
+    # skip first line
+    my $line = <$handle>;
+
+    my @machines;
+    while (my $line = <$handle>) {
+        chomp $line;
+        next unless -f $line;
+
+        my %info = _getMachineInfo(file => $line, logger => $params{logger});
+
+        my $machine = {
+            NAME      => $info{'displayName'},
+            VCPU      => 1,
+            UUID      => $info{'uuid.bios'},
+            MEMORY    => $info{'memsize'},
+            STATUS    => "running",
+            SUBSYSTEM => "VmWare Fusion",
+            VMTYPE    => "VmWare",
+        };
+
+        push @machines, $machine;
+    }
+    close $handle;
+
+    return @machines;
+}
+
+sub _getMachineInfo {
+    my $handle = getFileHandle(@_);
+    return unless $handle;
+
+    my %info;
+    while (my $line = <$handle>) {
+        next unless $line = /^(\S+)\s*=\s*(\S+.*)/;
+        my $key = $1;
+        my $value = $2;
+        $value =~ s/(^"|"$)//g;
+        $info{$key} = $value;
+    }
+    close $handle;
+
+    return %info;
 }
 
 1;
