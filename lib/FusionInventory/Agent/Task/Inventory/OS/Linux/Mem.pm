56c8c41de4d4bf12636e3b322ebfb692816865f7
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-01-15 17:41:43 +0100

    use shared functions

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Mem.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Mem.pm
@@ -3,8 +3,6 @@ package FusionInventory::Agent::Task::Inventory::OS::Linux::Mem;
 use strict;
 use warnings;
 
-use English qw(-no_match_vars);
-
 use FusionInventory::Agent::Tools;
 
 sub isInventoryEnabled { 
@@ -15,22 +13,21 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
+    my $logger    = $params{logger};
+
+    my $handle = getFileHandle(file => '/proc/meminfo', logger => $logger);
+    return unless $handle;
+
     my $unit = 1024;
     my $PhysicalMemory;
     my $SwapFileSize;
 
-    # Memory informations
-    if (open my $handle, '<', '/proc/meminfo') {
-        while(<$handle>){
-            $PhysicalMemory=$1 if /^memtotal\s*:\s*(\S+)/i;
-            $SwapFileSize=$1 if /^swaptotal\s*:\s*(\S+)/i;
-        }
-        close $handle;
-    } else {
-        warn "Can't open /proc/meminfo: $ERRNO";
+    while (my $line = <$handle>) {
+        $PhysicalMemory=$1 if /^memtotal\s*:\s*(\S+)/i;
+        $SwapFileSize=$1 if /^swaptotal\s*:\s*(\S+)/i;
     }
+    close $handle;
 
-    # TODO
     $inventory->setHardware(
         MEMORY => sprintf("%i", $PhysicalMemory/$unit),
         SWAP   => sprintf("%i", $SwapFileSize/$unit),
