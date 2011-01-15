b4370e3ec93d49ee3bdd4a82eaab9dc8097c0b78
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-01-15 17:20:52 +0100

    split parsing in distinct functions, and use shared functions

--- lib/FusionInventory/Agent/Task/Inventory/Virtualization/Vserver.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Virtualization/Vserver.pm
@@ -15,23 +15,35 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
-    my $config    = $params{config};
+    my $logger = $params{logger};
+
+    foreach my $machine (_getMachines(
+        command => 'vserver-info', logger => $logger
+    )) {
+        $inventory->addVirtualMachine($machine);
+    }
+}
+
+sub _getMachines {
+    my %params = @_;
+
+    my $handle = getFileHandle(%params);
+    return unless $handle;
 
     my $utilVserver;
     my $cfgDir;
-    foreach (`vserver-info 2>&1`) {
-        $cfgDir = $1 if /^\s+cfg-Directory:\s+(.*)$/;
-        $utilVserver = $1 if /^\s+util-vserver:\s+(.*)$/;
+    while (my $line = <$handle>) {
+        $cfgDir = $1 if $line =~ /^\s+cfg-Directory:\s+(.*)$/;
+        $utilVserver = $1 if $line =~ /^\s+util-vserver:\s+(.*)$/;
     }
+    close $handle;
 
     return unless -d $cfgDir;
 
-    my $handle;
-    if (!opendir $handle, $cfgDir) {
-        warn "Can't open $cfgDir: $ERRNO";
-        return;
-    }
+    $handle = getDirectoryHandle(directory => $cfgDir, logger => $params{logger});
+    return unless $handle;
 
+    my @machines;
     while (my $name = readdir($handle)) {
         next if $name =~ /^\./;
         next unless $name =~ /\S/;
@@ -42,14 +54,18 @@ sub doInventory {
             $line =~ /is running/ ? 'running' :
                                     undef     ;
 
-        $inventory->addVirtualMachine ({
+        my $machine = {
             NAME      => $name,
             STATUS    => $status,
             SUBSYSTEM => $utilVserver,
             VMTYPE    => "vserver",
-        });
+        };
+
+        push @machines, $machine;
     }
     close $handle;
+
+    return @machines;
 }
 
 1;
