2ba60a618f2026e7b9d7cf2e956d58880b86dc46
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-02-09 22:25:33 +0100

    move job creation into Agent.pm, to be shared between worker and server

--- lib/FusionInventory/Agent.pm
+++ lib/FusionInventory/Agent.pm
@@ -6,6 +6,7 @@ use warnings;
 use English qw(-no_match_vars);
 
 use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Job;
 
 our $VERSION = '3.0';
 our $VERSION_STRING =
@@ -91,6 +92,86 @@ sub getDeviceId {
 sub getRandomToken {
     my @chars = ('A'..'Z');
     return join('', map { $chars[rand @chars] } 1..8);
+}
+
+sub createJob {
+    my ($self, $job_name) = @_;
+
+    my $config = $self->{config};
+    my $logger = $self->{logger};
+
+    my $job_config = $config->getBlock($job_name);
+    if (!keys %$job_config) {
+        $logger->error("No configuration for job $job_name, skipping");
+        return;
+    }
+
+    my $target_name = $job_config->{target};
+    if (!$target_name) {
+        $logger->error("No target for job $job_name, skipping");
+        return;
+    }
+
+    my $target_config = $config->getBlock($target_name);
+    if (!keys %$target_config) {
+        $logger->error("No configuration for target $target_name, skipping");
+        return;
+    }
+
+    my $target_type = $target_config->{type};
+    if (!$target_type) {
+        $logger->error("No type for target $target_name, skipping");
+        return;
+    }
+
+    my $target_class =
+        $target_type eq 'stdout' ? 'FusionInventory::Agent::Target::Stdout':
+        $target_type eq 'local'  ? 'FusionInventory::Agent::Target::Local' :
+        $target_type eq 'server' ? 'FusionInventory::Agent::Target::Server':
+                                   undef                                   ;
+    if (!$target_class) {
+        $logger->error("Invalid type $target_type, skipping");
+        return;
+    }
+
+    my $task_name = $job_config->{task};
+    if (!$task_name) {
+        $logger->error("No task for job $job_name, skipping");
+        return;
+    }
+
+    my $task_config = $config->getBlock($task_name);
+    if (!keys %$task_config) {
+        $logger->error("No configuration for task $task_name, skipping");
+        return;
+    }
+
+    my $task_type = $task_config->{type};
+    if (!$task_type) {
+        $logger->error("No type for task $task_name, skipping");
+        return;
+    }
+
+    my $task_class = 'FusionInventory::Agent::Task::' . ucfirst($task_type);
+
+    if (!$task_class->require()) {
+        $logger->error("Unavailable class $task_class, skipping");
+        return;
+    }
+
+    if (!$task_class->isa('FusionInventory::Agent::Task')) {
+        $logger->error("Invalid class $task_class, skipping");
+        return;
+    }
+
+    return FusionInventory::Agent::Job->new(
+        id         => $job_name,
+        task       => $job_config->{task},
+        target     => $job_config->{target},
+        period     => $job_config->{period},
+        logger     => $self->{logger},
+        basevardir => $self->{vardir},
+    );
 }
 
 1;
--- lib/FusionInventory/Agent/Server.pm
+++ lib/FusionInventory/Agent/Server.pm
@@ -9,7 +9,6 @@ use English qw(-no_match_vars);
 use POE;
 
 use FusionInventory::Agent::Config;
-use FusionInventory::Agent::Job;
 use FusionInventory::Agent::Logger;
 use FusionInventory::Agent::Storage;
 use FusionInventory::Agent::Server::HTTPD;
@@ -21,80 +20,8 @@ sub run {
     my $config = $self->{config};
     my $logger = $self->{logger};
 
-    foreach my $job_name (split(' ', $config->getValues('global.jobs'))) {
-        my $job_config = $config->getBlock($job_name);
-        if (!keys %$job_config) {
-            $logger->error("No configuration for job $job_name, skipping");
-            next;
-        }
-
-        my $target_name = $job_config->{target};
-        if (!$target_name) {
-            $logger->error("No target for job $job_name, skipping");
-            next;
-        }
-
-        my $target_config = $config->getBlock($target_name);
-        if (!keys %$target_config) {
-            $logger->error("No configuration for target $target_name, skipping");
-            next;
-        }
-
-        my $target_type = $target_config->{type};
-        if (!$target_type) {
-            $logger->error("No type for target $target_name, skipping");
-            next;
-        }
-
-        my $target_class =
-            $target_type eq 'stdout' ? 'FusionInventory::Agent::Target::Stdout':
-            $target_type eq 'local'  ? 'FusionInventory::Agent::Target::Local' :
-            $target_type eq 'server' ? 'FusionInventory::Agent::Target::Server':
-                                       undef                                   ;
-        if (!$target_class) {
-            $logger->error("Invalid type $target_type, skipping");
-            next;
-        }
-
-        my $task_name = $job_config->{task};
-        if (!$task_name) {
-            $logger->error("No task for job $job_name, skipping");
-            next;
-        }
-
-        my $task_config = $config->getBlock($task_name);
-        if (!keys %$task_config) {
-            $logger->error("No configuration for task $task_name, skipping");
-            next;
-        }
-
-        my $task_type = $task_config->{type};
-        if (!$task_type) {
-            $logger->error("No type for task $task_name, skipping");
-            next;
-        }
-
-        my $task_class = 'FusionInventory::Agent::Task::' . ucfirst($task_type);
-
-        if (!$task_class->require()) {
-            $logger->error("Unavailable class $task_class, skipping");
-            next;
-        }
-        if (!$task_class->isa('FusionInventory::Agent::Task')) {
-            $logger->error("Invalid class $task_class, skipping");
-            next;
-        }
-
-        my $job = FusionInventory::Agent::Job->new(
-            id         => $job_name,
-            task       => $job_config->{task},
-            target     => $job_config->{target},
-            period     => $job_config->{period},
-            logger     => $self->{logger},
-            basevardir => $self->{vardir},
-        );
-
-        push @{$self->{jobs}}, $job;
+    foreach my $job (split(' ', $config->getValues('global.jobs') || '')) {
+        push @{$self->{jobs}}, $self->createJob($job);
     }
 
     die "No jobs defined, aborting" unless $self->{jobs};
@@ -180,7 +107,7 @@ sub runJob {
 sub runAllJobs {
     my ($self) = @_;
 
-    $self->runJob($_) foreach (@{$self->{jobs});
+    $self->runJob($_) foreach (@{$self->{jobs}});
 }
 
 1;
