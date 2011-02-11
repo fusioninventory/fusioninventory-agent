b63711194811abfd487e3b8c6236f17528ed6f06
Gonéri Le Bouder <goneri@rulezlan.org>
2011-02-11 20:00:25 +0100

    add getInfosFromDmidecode() and getCpusFromDmidecode()
    backport from 3.0.x

--- lib/FusionInventory/Agent/Tools.pm
+++ lib/FusionInventory/Agent/Tools.pm
@@ -13,12 +13,15 @@ use File::Basename;
 
 our @EXPORT = qw(
     getCanonicalManufacturer
+    getInfosFromDmidecode
+    getCpusFromDmidecode
     getVersionFromTaskModuleFile
     getFusionInventoryLibdir
     getFusionInventoryTaskList
 );
 
 memoize('getCanonicalManufacturer');
+memoize('getInfosFromDmidecode');
 
 sub getCanonicalManufacturer {
     my ($model) = @_;
@@ -54,6 +57,124 @@ sub getCanonicalManufacturer {
 
     return $manufacturer;
 }
+
+sub getInfosFromDmidecode {
+    my %params = (
+        command => 'dmidecode',
+        @_
+    );
+
+    if ($OSNAME eq 'MSWin32') {
+        my @osver;
+        eval "use Win32; (@osver) = Win32::GetOSVersion();";
+        my $isWin2003 = ($osver[4] == 2 && $osver[1] == 5 && $osver[2] == 2);
+# We get some strange breakage on Win2003. For the moment
+# we don't use dmidecode on this OS.
+        return if $isWin2003;
+    }
+
+    my $handle = getFileHandle(%params);
+
+    my ($info, $block, $type);
+
+    while (my $line = <$handle>) {
+        chomp $line;
+
+        if ($line =~ /DMI type (\d+)/) {
+            # start of block
+
+            # push previous block in list
+            if ($block) {
+                push(@{$info->{$type}}, $block);
+                undef $block;
+            }
+
+            # switch type
+            $type = $1;
+
+            next;
+        }
+
+        next unless defined $type;
+
+        next unless $line =~ /^\s+ ([^:]+) : \s (.*\S)/x;
+
+        next if
+            $2 eq 'N/A'           ||
+            $2 eq 'Not Specified' ||
+            $2 eq 'Not Present'   ;
+
+        $block->{$1} = $2;
+    }
+    close $handle;
+
+    return $info;
+}
+
+
+sub getCpusFromDmidecode {
+    my ($logger, $file) = @_;
+
+    my $infos = getInfosFromDmidecode(logger => $logger, file => $file);
+
+    return unless $infos->{4};
+
+    my @cpus;
+    foreach (@{$infos->{4}}) {
+        next if $_->{Status} && $_->{Status} =~ /Unpopulated/i;
+
+        # VMware
+        if (
+                ($_->{'Processor Manufacturer'} && ($_->{'Processor Manufacturer'} eq '000000000000'))
+                &&
+                ($_->{'Processor Version'} && ($_->{'Processor Version'} eq '00000000000000000000000000000000'))
+           ) {
+            next;
+        }
+
+        my $manufacturer = $_->{'Manufacturer'} || $_->{'Processor Manufacturer'};
+        my $name = (($manufacturer =~ /Intel/ && $_->{'Family'}) || ($_->{'Version'} || $_->{'Processor Family'})) || $_->{'Processor Version'};
+
+        my $speed;
+        if ($_->{Version} && $_->{Version} =~ /([\d\.]+)GHz$/) {
+            $speed = $1*1000;
+        } elsif ($_->{Version} && $_->{Version} =~ /([\d\.]+)MHz$/) {
+            $speed = $1;
+        } elsif ($_->{'Max Speed'}) {
+            if ($_->{'Max Speed'} =~ /^\s*(\d+)\s*Mhz/i) {
+                $speed = $1;
+            } elsif ($_->{'Max Speed'} =~ /^\s*(\d+)\s*Ghz/i) {
+                $speed = $1*1000;
+            }
+        }
+
+
+        my $externalClock;
+        if ($_->{'External Clock'}) {
+            if ($_->{'External Clock'} =~ /^\s*(\d+)\s*Mhz/i) {
+                $externalClock = $1;
+            } elsif ($_->{'External Clock'} =~ /^\s*(\d+)\s*Ghz/i) {
+                $externalClock = $1*1000;
+            }
+        }
+
+        push @cpus, {
+            SERIAL => $_->{'Serial Number'},
+            SPEED => $speed,
+            ID => $_->{ID},
+            MANUFACTURER => $manufacturer,
+            NAME =>  $name,
+            CORE => $_->{'Core Count'} || $_->{'Core Enabled'},
+            THREAD => $_->{'Thread Count'},
+            EXTERNAL_CLOCK => $externalClock
+        }
+
+    }
+
+    return \@cpus;
+}
+
+
 
 sub getVersionFromTaskModuleFile {
     my ($file) = @_;
@@ -159,6 +280,27 @@ This module is a backported from the master git branch.
 =head2 getCanonicalManufacturer($manufacturer)
 
 Returns a normalized manufacturer value for given one.
+
+=head2 getInfosFromDmidecode
+
+Returns a structured view of dmidecode output. Each information block is turned
+into an hashref, block with same DMI type are grouped into a list, and each
+list is indexed by its DMI type into the resulting hashref.
+
+$info = {
+    0 => [
+        { block }
+    ],
+    1 => [
+        { block },
+        { block },
+    ],
+    ...
+}
+
+=head2 getCpusFromDmidecode()
+
+Returns a clean array with the CPU list.
 
 =head2 getVersionFromTaskModuleFile($taskModuleFile)
 
