2f6abf86422d805581545fa2bc2eb39e11561cc0
David Durieux <d.durieux@siprossii.com>
2011-03-23 11:18:32 +0100

    FIx code error on wyse detection

--- lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
@@ -12,7 +12,7 @@ sub discovery {
                         oid => '.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1',
                         up  => 1,
                      });
-      if ($description_new ne "null") {
+      if (($description_new ne "null") && ($description_new ne "noSuchObject")) {
 
          $description_new =~ s/^"//;
          $description_new =~ s/"$//;
