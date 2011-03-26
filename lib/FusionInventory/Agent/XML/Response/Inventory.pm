bb69a3ce57ad37f9a08a5bbab28fadb6c7c3f018
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-03-26 23:15:54 +0100

    drop unused method

--- lib/FusionInventory/Agent/XML/Response/Inventory.pm
+++ lib/FusionInventory/Agent/XML/Response/Inventory.pm
@@ -4,21 +4,4 @@ use strict;
 use warnings;
 use base 'FusionInventory::Agent::XML::Response';
 
-sub isAccountUpdated {
-    my $self = shift;
-
-    my $parsedContent = $self->getParsedContent();
-
-    if (
-        $parsedContent &&
-        exists $parsedContent->{RESPONSE} &&
-        $parsedContent->{RESPONSE} eq 'ACCOUNT_UPDATE'
-    ) {
-	return 1;
-    } else {
-	return 0;
-    }
-
-}
-
 1;
