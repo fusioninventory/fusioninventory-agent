cc04584afc66e1d6c2f3238854a9002f5a8fc3b6
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-03-27 21:14:18 +0200

    drop useless variables and modules

--- lib/FusionInventory/Agent/XML/Query/Prolog.pm
+++ lib/FusionInventory/Agent/XML/Query/Prolog.pm
@@ -4,16 +4,12 @@ use strict;
 use warnings;
 use base 'FusionInventory::Agent::XML::Query';
 
-use Digest::MD5 qw(md5_base64);
 use XML::Simple;
 
 sub new {
     my ($class, $params) = @_;
 
     my $self = $class->SUPER::new($params);
-
-    my $logger = $self->{logger};
-    my $target = $self->{target};
 
     $self->{h}{QUERY} = ['PROLOG'];
     $self->{h}{TOKEN} = [$params->{token}];
