39361dcfe17c073ff52fecaf7adf6f090fef58da
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-03-29 10:48:05 +0200

    use dedicated file locking primitives, rather than thread locking

--- lib/FusionInventory/Agent/Logger/File.pm
+++ lib/FusionInventory/Agent/Logger/File.pm
@@ -3,25 +3,9 @@ package FusionInventory::Agent::Logger::File;
 use strict;
 use warnings;
 
-use Config;
 use English qw(-no_match_vars);
+use Fcntl qw(:flock);
 use File::stat;
-
-BEGIN {
-    # threads and threads::shared must be load before
-    # $lock is initialized
-    if ($Config{usethreads}) {
-        eval {
-            require threads;
-            require threads::shared;
-        };
-        if ($EVAL_ERROR) {
-            print "[error]Failed to use threads!\n"; 
-        }
-    }
-}
-
-my $lock :shared;
 
 sub new {
     my ($class, $params) = @_;
@@ -39,7 +23,6 @@ sub new {
 sub addMsg {
     my ($self, $args) = @_;
 
-    lock($lock);
     my $level = $args->{level};
     my $message = $args->{message};
 
@@ -53,12 +36,21 @@ sub addMsg {
 
     my $handle;
     if (open $handle, '>>', $self->{logfile}) {
-        print $handle "[".localtime()."][$level] $message\n";
+
+        # get an exclusive lock on log file
+        flock($handle, LOCK_EX)
+            or die "can't get an exclusive lock on $self->{logfile}: $ERRNO";
+
+        print {$handle}
+            "[". localtime() ."]" .
+            "[$level]" .
+            " $message\n";
+
+        # closing handle release the lock automatically
         close $handle;
     } else {
         warn "Can't open $self->{logfile}: $ERRNO";
     }
-
 
 }
 
