204ebca6f8edda546d6f0d2e026d11e49871005e
Gonéri Le Bouder <goneri@rulezlan.org>
2011-03-31 16:43:47 +0200

    move the code used to launch job in Server::Run

--- lib/FusionInventory/Agent/Server.pm
+++ lib/FusionInventory/Agent/Server.pm
@@ -7,13 +7,13 @@ use base qw/FusionInventory::Agent/;
 use Cwd;
 use English qw(-no_match_vars);
 use POE;
-use POE qw( Wheel::Run );
 
 use FusionInventory::Agent::Config;
 use FusionInventory::Agent::Logger;
 use FusionInventory::Agent::Storage;
 use FusionInventory::Agent::Server::HTTPD;
 use FusionInventory::Agent::Server::Scheduler;
+use FusionInventory::Agent::Server::Run;
 use FusionInventory::Agent::Target::Server;
 
 sub run {
@@ -165,53 +165,9 @@ sub runJob {
 
     $self->{logger}->debug("[server] running job $job->{id}");
 
-    $job->scheduleNextRun();
-  POE::Session->create(
-    inline_states => {
-      _start           => sub {
-
-      print $_[KERNEL]->alias_resolve("job")."\n";
-      if (defined $_[KERNEL]->alias_resolve("job")) {
-      print "a job is already running.\n";
-      return;
-      }
-
-
-
-        $_[KERNEL]->alias_set("job");
-
-    my $child = POE::Wheel::Run->new(
-      Program => sub {
-        my $taskPkg = "FusionInventory::Agent::Task::".$job->{task};
-        $taskPkg->require();
-        my $task = $taskPkg->new();
-        $task->run(
-                logger => $self->{logger},
-                target => $job->{target},
-                deviceid => $self->{deviceid}
-                );
-      },
-      StdoutEvent  => "got_child_stdout",
-      StderrEvent  => "got_child_stderr",
-      CloseEvent   => "got_child_close",
-    );
-
-    $_[KERNEL]->sig_child($child->PID, "got_child_signal");
-    $_[HEAP]{children_by_wid}{$child->ID} = $child;
-    $_[HEAP]{children_by_pid}{$child->PID} = $child;
-
-    print(
-      "Child pid ", $child->PID,
-      " started as wheel ", $child->ID, ".\n"
-    );
-  },
-#      got_child_stdout => \&on_child_stdout,
-#      got_child_stderr => \&on_child_stderr,
-      got_child_close  => \&on_child_close,
-      got_child_signal => \&on_child_signal,
+    if (FusionInventory::Agent::Server::Run::run($self->{logger}, $job)) {
+        $job->scheduleNextRun();
     }
-  );
-
 }
 
 sub runAllJobs {
@@ -219,47 +175,6 @@ sub runAllJobs {
 
     $self->runJob($_) foreach (@{$self->{jobs}});
 }
-
-
-
-  # Wheel event, including the wheel's ID.
-  sub on_child_stdout {
-    my ($stdout_line, $wheel_id) = @_[ARG0, ARG1];
-    my $child = $_[HEAP]{children_by_wid}{$wheel_id};
-    print "pid ", $child->PID, " STDOUT: $stdout_line\n";
-  }
-
-  # Wheel event, including the wheel's ID.
-  sub on_child_stderr {
-    my ($stderr_line, $wheel_id) = @_[ARG0, ARG1];
-    my $child = $_[HEAP]{children_by_wid}{$wheel_id};
-    print "pid ", $child->PID, " STDERR: $stderr_line\n";
-  }
-
-  # Wheel event, including the wheel's ID.
-  sub on_child_close {
-    my $wheel_id = $_[ARG0];
-    $_[KERNEL]->alias_remove("job");
-    my $child = delete $_[HEAP]{children_by_wid}{$wheel_id};
-
-    # May have been reaped by on_child_signal().
-    unless (defined $child) {
-      print "wid $wheel_id closed all pipes.\n";
-      return;
-    }
-
-    print "pid ", $child->PID, " closed all pipes.\n";
-    delete $_[HEAP]{children_by_pid}{$child->PID};
-  }
-
-  sub on_child_signal {
-    print "pid $_[ARG1] exited with status $_[ARG2].\n";
-    my $child = delete $_[HEAP]{children_by_pid}{$_[ARG1]};
-
-    # May have been reaped by on_child_close().
-    return unless defined $child;
-    delete $_[HEAP]{children_by_wid}{$child->ID};
-  }
 
 
 1;
