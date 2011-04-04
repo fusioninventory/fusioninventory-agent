aa25788516d9e0d0d1b94ae180c4646ea0c50791
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-04 19:31:40 +0200

    backport from master branch

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Packaging/Deb.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Packaging/Deb.pm
@@ -6,25 +6,51 @@ use warnings;
 use FusionInventory::Agent::Tools;
 
 sub isInventoryEnabled {
-    return can_run("dpkg");
+    return can_run('dpkg-query');
 }
 
 sub doInventory {
-    my $params = shift;
-    my $inventory = $params->{inventory};
+    my ($params) = @_;
 
-# use dpkg-query --show --showformat='${Package}|||${Version}\n'
-    foreach(`dpkg-query --show --showformat='\${Package}---\${Version}---\${Installed-Size}---\${Description}\n'`) {
-        if (/^(\S+)---(\S+)---(\S+)---(.*)/) {     	     	
-            $inventory->addSoftware ({
-                'NAME'          => $1,
-                'VERSION'       => $2,
-                'FILESIZE'      => $3,
-                'COMMENTS'      => $4,
-                'FROM'          => 'deb'
-            });
-        }
+    my $inventory = $params->{inventory};
+    my $logger    = $params->{logger};
+
+    my $command =
+        'dpkg-query --show --showformat=\'' .
+        '${Package}\t' .
+        '${Version}\t'.
+        '${Installed-Size}\t' .
+        '${Description}\n' .
+        '\'';
+
+    my $packages = _getPackagesListFromDpkg(
+        logger => $logger, command => $command
+    );
+    foreach my $package (@$packages) {
+        $inventory->addSoftware($package);
     }
+}
+
+sub _getPackagesListFromDpkg {
+    my $handle = getFileHandle(@_);
+
+    my @packages;
+    while (my $line = <$handle>) {
+        # skip descriptions
+        next if $line =~ /^ /;
+        chomp $line;
+        my @infos = split("\t", $line);
+        push @packages, {
+            NAME        => $infos[0],
+            VERSION     => $infos[1],
+            FILESIZE    => $infos[2],
+            COMMENTS    => $infos[3],
+            FROM        => 'deb'
+        };
+    }
+    close $handle;
+
+    return \@packages;
 }
 
 1;
