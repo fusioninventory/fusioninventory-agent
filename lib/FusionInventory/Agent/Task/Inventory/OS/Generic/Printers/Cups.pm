94f35a2ca2e71e11f05f6daa873af4dea12921ad
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-04 11:10:33 +0200

    cleanup

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Printers/Cups.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Printers/Cups.pm
@@ -3,40 +3,43 @@ package FusionInventory::Agent::Task::Inventory::OS::Generic::Printers::Cups;
 use strict;
 use warnings;
 
+use English qw(-no_match_vars);
+
 use FusionInventory::Agent::Tools;
 
 sub isInventoryEnabled {
-    # If we are on a MAC, Mac::SysProfile will do the job
-    return if -r '/usr/sbin/system_profiler';
-    return unless can_load("Net::CUPS");
-    return 0 if ( $Net::CUPS::VERSION < 0.60 );
-    return 1;
+    my (%params) = @_;
+
+    return 
+        # we use system profiler on MacOS
+        $OSNAME ne 'darwin' &&
+        !$params{no_printer} &&
+        can_load("Net::CUPS") &&
+        $Net::CUPS::VERSION >= 0.60;
 }
 
 sub doInventory {
-    my $params = shift;
-    my $inventory = $params->{inventory};
-    my $logger = $params->{logger}; 
-    my $config = $params->{config};
+    my ($params) = @_;
 
-    return if $config->{'no-printer'};
+    my $inventory = $params->{inventory};
 
     my $cups = Net::CUPS->new();
     my @printers = $cups->getDestinations();
 
-    return unless scalar(@printers);
-    foreach my $printer  (@printers) {
-
-        my $printername = $printer->getUri();
-        $printername =~ s/^.*\/\/([^\.]*).*$/$1/eg ;
-        $printername =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+    foreach my $printer (@printers) {
+        my $name = $printer->getUri();
+        $name =~ s/^.*\/\/([^\.]*).*$/$1/eg ;
+        $name =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
         $inventory->addPrinter({
-            NAME    => $printername,
+            NAME        => $name,
             DESCRIPTION => $printer->getDescription(),
-            DRIVER => $printer->getOptionValue("printer-make-and-model"),
-            PORT => $printer->getUri(), 
+            DRIVER      => $printer->getOptionValue(
+                               "printer-make-and-model"
+                           ),
+            PORT        => $printer->getUri(), 
         });
     }
 
 }
+
 1;
