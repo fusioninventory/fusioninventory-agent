a7d45f01ce98dc34ae63df340f517940efa53319
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-04 20:44:54 +0200

    backport from master branch

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Slots.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Dmidecode/Slots.pm
@@ -5,51 +5,45 @@ use warnings;
 
 use FusionInventory::Agent::Tools;
 
+sub isInventoryEnabled {
+    return 1;
+}
+
 sub doInventory {
-    my $params = shift;
+    my ($params) = @_;
+
     my $inventory = $params->{inventory};
+    my $logger    = $params->{logger};
 
-    my $dmidecode = `dmidecode`;
-    # some versions of dmidecode do not separate items with new lines
-    # so add a new line before each handle
-    $dmidecode =~ s/\nHandle/\n\nHandle/g;
-    my @dmidecode = split (/\n/, $dmidecode);
-    # add a new line at the end
-    push @dmidecode, "\n";
+    my $slots = _getSlots($logger);
 
-    s/^\s+// for (@dmidecode);
+    return unless $slots;
 
-    my $flag;
-
-    my $description;
-    my $designation;
-    my $name;
-    my $status;
-
-
-    foreach (@dmidecode) {
-
-        if(/dmi type 9,/i) {
-            $flag=1;
-        } elsif ($flag && /^$/) {
-            $flag=0;
-
-            $inventory->addSlot({
-                DESCRIPTION =>  $description,
-                DESIGNATION =>  $designation,
-                NAME =>  $name,
-                STATUS =>  $status,
-            });
-
-            $description = $designation = $name = $status = undef;
-
-        } elsif ($flag) {
-            $description = $1 if /^type\s*:\s*(.+)/i;
-            $designation = $1 if /^id\s*:\s*(.+)/i;
-            $name = $1 if /^designation\s*:\s*(.+)/i;
-            $status = $1 if /^current usage\s*:\s*(.+)/i;
-        }
+    foreach my $slot (@$slots) {
+        $inventory->addSlot($slot);
     }
+}
+
+sub _getSlots {
+    my ($logger, $file) = @_;
+
+    my $infos = getInfosFromDmidecode(logger => $logger, file => $file);
+
+    return unless $infos->{9};
+
+    my $slots;
+    foreach my $info (@{$infos->{9}}) {
+        my $slot = {
+            DESCRIPTION => $info->{'Type'},
+            DESIGNATION => $info->{'ID'},
+            NAME        => $info->{'Designation'},
+            STATUS      => $info->{'Current Usage'},
+        };
+
+        push @$slots, $slot;
+    }
+
+    return $slots;
 }
 
 1;
