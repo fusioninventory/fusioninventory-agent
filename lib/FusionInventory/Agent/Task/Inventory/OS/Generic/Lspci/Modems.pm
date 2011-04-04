13a0a40f0aacfb05205ad330b15bda117d424cc9
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-04 21:04:01 +0200

    backport from master branch

--- lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Lspci/Modems.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Generic/Lspci/Modems.pm
@@ -4,26 +4,39 @@ use strict;
 use warnings;
 
 use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Tools::Unix;
 
 sub isInventoryEnabled {
     return 1;
 }
 
 sub doInventory {
-    my $params = shift;
+    my ($params) = @_;
+
     my $inventory = $params->{inventory};
+    my $logger    = $params->{logger};
 
-    foreach(`lspci`){
-
-        if(/modem/i && /\d+\s(.+):\s*(.+)$/){
-            my $name = $1;
-            my $description = $2;
-            $inventory->addModem({
-                'DESCRIPTION'  => $description,
-                'NAME'          => $name,
-            });
-        }
+    foreach my $modem (_getModemControllers($logger)) {
+        $inventory->addModem($modem);
     }
+}
+
+sub _getModemControllers {
+    my ($logger, $file) = @_;
+
+    my @modems;
+
+    foreach my $controller (getControllersFromLspci(
+        logger => $logger, file => $file
+    )) {
+        next unless $controller->{NAME} =~ /modem/i;
+        push @modems, {
+            DESCRIPTION => $controller->{NAME},
+            NAME        => $controller->{MANUFACTURER},
+        };
+    }
+
+    return @modems;
 }
 
 1;
