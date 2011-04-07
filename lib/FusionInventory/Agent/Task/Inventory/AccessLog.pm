f5672b1d7d4d1b7c3aa60a54dfd9864d12037d86
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-07 22:52:28 +0200

    backport from master branch

--- lib/FusionInventory/Agent/Task/Inventory/AccessLog.pm
+++ lib/FusionInventory/Agent/Task/Inventory/AccessLog.pm
@@ -10,19 +10,16 @@ sub isInventoryEnabled {
 }
 
 sub doInventory {
-    my $params = shift;
-    my $inventory = $params->{inventory};
+    my (%params) = @_;
 
-    my ($YEAR, $MONTH , $DAY, $HOUR, $MIN, $SEC) = (localtime
-        (time))[5,4,3,2,1,0];
-    my $date=sprintf "%02d-%02d-%02d %02d:%02d:%02d",
-    ($YEAR+1900), ($MONTH+1), $DAY, $HOUR, $MIN, $SEC;
+    my $inventory = $params{inventory};
+
+    my $date = getFormatedLocalTime(time());
 
     $inventory->setAccessLog ({
         USERID => 'N/A',
         LOGDATE => $date
     });
-
 }
 
 1;
