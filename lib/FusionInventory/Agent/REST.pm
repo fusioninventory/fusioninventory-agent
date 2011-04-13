d0e65e3099522e2fb2af6042cdf00cf5da8f6514
Gonéri Le Bouder <goneri@rulezlan.org>
2011-04-13 18:26:07 +0200

    initial import of the REST module

---
+++ lib/FusionInventory/Agent/REST.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+package FusionInventory::Agent::REST;
+
+use strict;
+use warnings;
+
+use FusionInventory::Agent::Network;
+use JSON;
+
+our $AUTOLOAD;
+
+sub new {
+    my $class = shift;
+    my %params = @_; 
+
+    die "missing url key" unless $params{url};
+    die "missing network key" unless $params{network};
+
+    my $self = {
+        url => $params{url},
+        network => $params{network}
+    };
+    bless $self, $class;
+    return $self;
+}
+
+
+sub AUTOLOAD {
+    my $self = shift;
+    my %params = @_;
+
+    my $name = $AUTOLOAD;
+    $name =~ s/.*://; # strip fully-qualified portion
+
+    my $reqUrl = $self->{url}.'?a='.$name;
+    foreach my $k (keys %params) {
+        $reqUrl .= '&'.$k.'='.$params{$k};
+    }
+
+    my $jsonText = $network->get ({
+        source => $reqUrl,
+        timeout => 60,
+        });
+
+
+    return unless $jsonText;
+
+    return from_json( $jsonText, { utf8  => 1 } );
+}
+
+1;
+
+# my $rest = FusionInventory::Agent::REST->new(
+#         "url" => "http://somewhere/",
+#         "network" => $network
+# );
+# my $ret = $rest->getName(param1 => "foo");
+# print Dumper($ret);
