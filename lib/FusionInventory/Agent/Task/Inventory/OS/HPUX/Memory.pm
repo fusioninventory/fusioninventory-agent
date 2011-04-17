8ccd7658b10d0b25485fcb88ae35873aee26f5da
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-17 19:37:37 +0200

    isolate parsing in function, for testing and readibility

--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Memory.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Memory.pm
@@ -16,146 +16,45 @@ sub doInventory {
     my $inventory = $params->{inventory};
     my $logger    = $params->{logger};
 
+    my ($memories, $memorySize, $swapSize);
+
     # HPUX 11.31: http://forge.fusioninventory.org/issues/754
     if (-f '/opt/propplus/bin/cprop' && (`hpvminfo 2>&1` !~ /HPVM/i)) {
-        my ($memories, $totalMem) = _parseCpropMemory(
+        ($memories, $memorySize) = _parseCprop(
             command => '/opt/propplus/bin/cprop -summary -c Memory',
             logger  => $logger
         );
-        $inventory->setHardware({ MEMORY => $totalMem });
-        foreach my $memory (@$memories) {
-            $inventory->addEntry({
-                section => 'MEMORIES',
-                entry   => $memory
-            });
-        }
-        return;
-    }
-
-    my $arch = getFirstLine(command => 'uname -m');
-
-    my $memory;
-
-    if ($arch =~ /ia64/ ) {
-        system("echo 'sc product  IPF_MEMORY;info' | /usr/sbin/cstm");    # enable infolog
-
-        my @lines = getAllLines(
-            command => "echo 'sc product IPF_MEMORY;il' | /usr/sbin/cstm"
-        );
-
-        foreach my $line (@lines) {
-            if ($line =~ /\w+IMM\s+Location/ ) {
-                next
-            }
-
-            if ($line =~ /(\w+IMM)\s+(\w+)\s+(\d+|\-+)\s+(\w+IMM)\s+(\w+)\s+(\d+|\-+)/ ) {
-                $inventory->addEntry({
-                    section => 'MEMORIES',
-                    entry => {
-                        CAPACITY     => $3,
-                        DESCRIPTION  => $1,
-                        CAPTION      => $1 . ' ' . $2,
-                        TYPE         => $1,
-                        NUMSLOTS     => $2,
-                    }
-                });
-                $inventory->addEntry({
-                    section => 'MEMORIES',
-                    entry   => {
-                        CAPACITY     => $6,
-                        DESCRIPTION  => $4,
-                        CAPTION      => $4 . ' ' . $5,
-                        TYPE         => $4,
-                        NUMSLOTS     => $5,
-                    }
-                }); 
-
-            }
-
-            if ($line =~ /Total Configured Memory\s*:\s(\d+)\sMB/i) {
-                $memory = $1;
-            }
-        }
     } else {
-        my $capacity = 0;
-        my $caption; 
-        my $description;
-        my $numslot = 1;
-        my $subnumslot;
-        my $serialnumber = 'No Serial Number available!';
-        my $type;
-        my $ok=0;
+        my $arch = getFirstLine(command => 'uname -m');
 
-        my @lines = getAllLines(
-            command => "echo 'sc product mem;il'| /usr/sbin/cstm"
-        );
-
-        foreach my $line (@lines) {
-
-            if ($line =~ /FRU\sSource\s+=\s+\S+\s+\(memory/ ) {
-                $ok=0;
-            }
-            if ($line =~ /Source\s+Detail\s+=\s4/ ) {
-                $ok=1;
-            }
-            if ($line =~ /Extender\s+Location\s+=\s+(\S+)/ ) {
-                $subnumslot=$1;
-            };
-            if ($line =~ /DIMMS\s+Rank\s+=\s+(\S+)/ ) {
-                $numslot=sprintf("%02x",$1);
-            }
-
-            if ($line =~ /FRU\s+Name\.*:\s+(\S+)/ ) {
-                if ($line =~ /(\S+)_(\S+)/ ) {
-                    $type=$1;
-                    $capacity=$2;
-                } elsif ($line =~ /(\wIMM)(\S+)/ ) {
-                    $ok=1;
-                    $type=$1;
-                    $numslot=$2;
-                }
-            }
-            if ($line =~ /Part\s+Number\.*:\s*(\S+)\s+/ ) {
-                $description=$1;
-            }
-            if ($line =~ /Serial\s+Number\.*:\s*(\S+)\s+/ ) {
-                $serialnumber=$1;
-                if ( $ok eq 1 ) {
-                    if ( $capacity eq 0 ) {
-                        foreach ( @lines ) {
-                            if ( /\s+$numslot\s+(\d+)/ ) {
-                                $capacity=$1;
-                                #print "Capacity $capacity\n";
-                            }
-                        }
-                    }
-                    $inventory->addEntry({
-                        section => 'MEMORIES',
-                        entry   => {
-                            CAPACITY     => $capacity,
-                            DESCRIPTION  => "Part Number $description",
-                            CAPTION      => "Ext $subnumslot Slot $numslot",
-                            TYPE         => $type,
-                            NUMSLOTS     => '1',
-                            SERIALNUMBER => $serialnumber,
-                        }
-                    });
-                    $ok=0;
-                    $capacity=0;
-                } # $ok eq 1
-            } # /Serial\s+Number\.*:\s*(\S+)\s+/ 
-
-            if ($line =~ /Total Configured Memory\s*:\s(\d+)\sMB/i) {
-                $memory = $1;
-            }
-        } # echo 'sc product system;il' | /usr/sbin/cstm
+        if ($arch =~ /ia64/ ) {
+            # enable infolog
+            system("echo 'sc product  IPF_MEMORY;info' | /usr/sbin/cstm");
+            ($memories, $memorySize) = _parseCstm64(
+                command => "echo 'sc product IPF_MEMORY;il' | /usr/sbin/cstm",
+                logger  => $logger
+            );
+        } else {
+            ($memories, $memorySize) = _parseCstm(
+                command => "echo 'sc product mem;il'| /usr/sbin/cstm",
+                logger  => $logger
+            );
+        }
     }
 
-    my $TotalSwapSize = `swapinfo -dt | tail -n1`;
-    $TotalSwapSize =~ s/^total\s+(\d+)\s+\d+\s+\d+\s+\d+%\s+\-\s+\d+\s+\-/$1/i;
+    $swapSize = `swapinfo -dt | tail -n1`;
+    $swapSize =~ s/^total\s+(\d+)\s+\d+\s+\d+\s+\d+%\s+\-\s+\d+\s+\-/$1/i;
+
+    foreach my $memory (@$memories) {
+        $inventory->addEntry({
+            section => 'MEMORIES',
+            entry   => $memory
+        });
+    }
+
     $inventory->setHardware({
-        SWAP   => sprintf("%i", $TotalSwapSize/1024),
-        MEMORY => $memory
+        SWAP   => sprintf("%i", $swapSize/1024),
+        MEMORY => $memorySize
     });
 }
 
@@ -177,13 +76,13 @@ sub _getSizeInMB {
     return $data;
 }
 
-sub _parseCpropMemory {
+sub _parseCprop {
     my $handle = getFileHandle(@_);
 
     return unless $handle;
 
-    my $totalMem = 0;
-    my $memories = [];
+    my $size = 0;
+    my @memories;
     my $instance = {};
     foreach (<$handle>) {
         if (keys (%$instance) && /\[Instance\]: \d+/) {
@@ -197,9 +96,8 @@ sub _parseCpropMemory {
 
         if (keys (%$instance) && /\*\*\*\*/) {
             if ($instance->{Size}) {
-                my $size = _getSizeInMB($instance->{Size}) || 0;
-                $totalMem += $size;
-                push @$memories, {
+                $size += _getSizeInMB($instance->{Size}) || 0;
+                push @memories, {
                     CAPACITY => $size,
                     DESCRIPTION => $instance->{'Part Number'},
                     SERIALNUMBER => $instance->{'Serial Number'},
@@ -211,7 +109,119 @@ sub _parseCpropMemory {
     }
     close $handle;
 
-    return ($memories, $totalMem)
+    return (\@memories, $size);
+}
+
+sub _parseCstm {
+
+    my (@memories, $size);
+
+    my $capacity = 0;
+    my $caption; 
+    my $description;
+    my $numslot = 1;
+    my $subnumslot;
+    my $serialnumber = 'No Serial Number available!';
+    my $type;
+    my $ok=0;
+
+    my @lines = getAllLines(@_);
+
+    foreach my $line (@lines) {
+
+        if ($line =~ /FRU\sSource\s+=\s+\S+\s+\(memory/ ) {
+            $ok=0;
+        }
+        if ($line =~ /Source\s+Detail\s+=\s4/ ) {
+            $ok=1;
+        }
+        if ($line =~ /Extender\s+Location\s+=\s+(\S+)/ ) {
+            $subnumslot=$1;
+        };
+        if ($line =~ /DIMMS\s+Rank\s+=\s+(\S+)/ ) {
+            $numslot=sprintf("%02x",$1);
+        }
+
+        if ($line =~ /FRU\s+Name\.*:\s+(\S+)/ ) {
+            if ($line =~ /(\S+)_(\S+)/ ) {
+                $type=$1;
+                $capacity=$2;
+            } elsif ($line =~ /(\wIMM)(\S+)/ ) {
+                $ok=1;
+                $type=$1;
+                $numslot=$2;
+            }
+        }
+        if ($line =~ /Part\s+Number\.*:\s*(\S+)\s+/ ) {
+            $description=$1;
+        }
+        if ($line =~ /Serial\s+Number\.*:\s*(\S+)\s+/ ) {
+            $serialnumber=$1;
+            if ( $ok eq 1 ) {
+                if ( $capacity eq 0 ) {
+                    foreach ( @lines ) {
+                        if ( /\s+$numslot\s+(\d+)/ ) {
+                            $capacity=$1;
+                            #print "Capacity $capacity\n";
+                        }
+                    }
+                }
+                push @memories, {
+                    CAPACITY     => $capacity,
+                    DESCRIPTION  => "Part Number $description",
+                    CAPTION      => "Ext $subnumslot Slot $numslot",
+                    TYPE         => $type,
+                    NUMSLOTS     => '1',
+                    SERIALNUMBER => $serialnumber,
+                };
+                $ok=0;
+                $capacity=0;
+            } # $ok eq 1
+        } # /Serial\s+Number\.*:\s*(\S+)\s+/ 
+
+        if ($line =~ /Total Configured Memory\s*:\s(\d+)\sMB/i) {
+            $size = $1;
+        }
+    } # echo 'sc product system;il' | /usr/sbin/cstm
+
+    return (\@memories, $size);
+}
+
+sub _parseCstm64 {
+
+    my @lines = getAllLines(@_);
+
+    my (@memories, $size);
+
+    foreach my $line (@lines) {
+        if ($line =~ /\w+IMM\s+Location/ ) {
+            next
+        }
+
+        if ($line =~ /(\w+IMM)\s+(\w+)\s+(\d+|\-+)\s+(\w+IMM)\s+(\w+)\s+(\d+|\-+)/ ) {
+            push @memories, 
+                {
+                    CAPACITY     => $3,
+                    DESCRIPTION  => $1,
+                    CAPTION      => $1 . ' ' . $2,
+                    TYPE         => $1,
+                    NUMSLOTS     => $2,
+                },
+                {
+                    CAPACITY     => $6,
+                    DESCRIPTION  => $4,
+                    CAPTION      => $4 . ' ' . $5,
+                    TYPE         => $4,
+                    NUMSLOTS     => $5,
+                };
+        }
+
+        if ($line =~ /Total Configured Memory\s*:\s(\d+)\sMB/i) {
+            $size = $1;
+        }
+    }
+
+    return (\@memories, $size);
 }
 
 1;
