e5aaa8aafd99b561a6ce2e77b6cda9793fff2764
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-18 17:57:41 +0200

    sync with 2.2.x branch

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/AntiVirus.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/AntiVirus.pm
@@ -5,14 +5,18 @@ use warnings;
 
 use FusionInventory::Agent::Tools::Win32;
 
+sub isInventoryEnabled {
+    return 1;
+}
+
 sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
+    my $logger    = $params{logger};
 
-# Doesn't works on Win2003 Server
-
-# On Win7, we need to use SecurityCenter2
+    # Doesn't works on Win2003 Server
+    # On Win7, we need to use SecurityCenter2
     foreach my $instance (qw/SecurityCenter SecurityCenter2/) {
         my $moniker = "winmgmts:{impersonationLevel=impersonate,(security)}!//./root/$instance";
 
@@ -25,28 +29,34 @@ sub doInventory {
                / ]
         ))) {
 
-            my $enable   = $object->{onAccessScanningEnabled};
+            my $enable = $object->{onAccessScanningEnabled};
             my $uptodate = $object->{productUptoDate};
 
             if ($object->{productState}) {
-                # http://blogs.msdn.com/b/alejacma/archive/2008/05/12/how-to-get-antivirus-information-with-wmi-vbscript.aspx?PageIndex=2#comments
                 my $bin = sprintf( "%b\n", $object->{productState});
+# http://blogs.msdn.com/b/alejacma/archive/2008/05/12/how-to-get-antivirus-information-with-wmi-vbscript.aspx?PageIndex=2#comments
                 if ($bin =~ /(\d)00000(\d)000000(\d)00000$/) {
                     $uptodate = $1 || $2;
                     $enable = $3?0:1;
                 }
+
             }
-            $inventory->addAntiVirus({
-                COMPANY  => $object->{companyName},
-                NAME     => $object->{displayName},
-                GUID     => $object->{instanceGuid},
-                ENABLED  => $enable,
-                UPTODATE => $uptodate,
-                VERSION  => $object->{versionNumber}
-            });
+
+            $inventory->addEntry(
+                section => 'ANTIVIRUS',
+                entry   => {
+                    COMPANY  => $object->{companyName},
+                    NAME     => $object->{displayName},
+                    GUID     => $object->{instanceGuid},
+                    ENABLED  => $enable,
+                    UPTODATE => $uptodate,
+                    VERSION  => $object->{versionNumber}
+                },
+                noDuplicated => 1
+            );
         }
     }
-
 }
+
 1;
 
