8f2998ce59e7b5c602a478d7f1b30c7c02cedcc4
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-21 23:04:40 +0200

    avoid using undefined handles or arrayref

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Softwares/Deb.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux/Softwares/Deb.pm
@@ -26,6 +26,8 @@ sub doInventory {
     my $packages = _getPackagesList(
         logger => $logger, command => $command
     );
+    return unless $packages;
+
     foreach my $package (@$packages) {
         $inventory->addEntry(
             section => 'SOFTWARES',
@@ -36,6 +38,7 @@ sub doInventory {
 
 sub _getPackagesList {
     my $handle = getFileHandle(@_);
+    return unless $handle;
 
     my @packages;
     while (my $line = <$handle>) {
