c2bc292e720dfbee239b2cdf76c8fb9898b062a8
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-25 10:05:25 +0200

    unroll loop to avoid tests and reduce indentation level

--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Storages.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Storages.pm
@@ -28,62 +28,65 @@ sub doInventory {
     my $devrdsk;
     my $revlvl;
 
-    foreach my $type (qw/tape disk/) {
-        foreach ( `ioscan -kFnC $type | cut -d ':' -f 1,11,18` ) {
-            if ( /(\S+)\:(\S+)\:(\S+)\s+(\S+)/ ) {
-                $description = $1;
-                $path = $2;
-                $vendor = $3;
-                $ref = $4;
-            }
-            if (  $type eq "disk" ) {
-                my $alternate = 0 ;
-                if ( /\s+(\/dev\/dsk\/\S+)\s+(\/dev\/rdsk\/\S+)/ ) {
-                    $devdsk=$1;
-                    $devrdsk=$2;
-                    # We look if whe are on an alternate link
-                    foreach ( `pvdisplay $devdsk 2> /dev/null` ) {
-                        if ( /$devdsk\.+lternate/ ) {
-                            $alternate=1;
-                        }
-                    }
-
-                    # skip alternate link
-                    next if $alternate;
-
-                    foreach ( `diskinfo -v $devrdsk 2>/dev/null`) {
-                        if ( /^\s+size:\s+(\S+)/ ) {
-                            $size=$1;
-                            $size = int ( $size/1024 ) if $size;
-                        }
-                        if ( /^\s+rev level:\s+(\S+)/ ) {
-                            $revlvl=$1;
-                        }
-                    }
-                    $inventory->addStorage({
-                        MANUFACTURER => $vendor,
-                        MODEL => $ref,
-                        NAME => $devdsk,
-                        DESCRIPTION => $description,
-                        TYPE => $type,
-                        DISKSIZE => $size,
-                        FIRMWARE => $revlvl,
-                    });
-                }
-            } else {
-                # We look for tapes
-                if ( /^\s+(\/dev\/rmt\/\Sm)\s+/ ) {
-                    $devdsk=$1;
-                    $inventory->addStorage({
-                        MANUFACTURER => $vendor,
-                        MODEL => $ref,
-                        NAME => $devdsk,
-                        DESCRIPTION => $description,
-                        TYPE => $type,
-                        DISKSIZE => ''
-                    });
+    foreach ( `ioscan -kFnC disk | cut -d ':' -f 1,11,18` ) {
+        if ( /(\S+)\:(\S+)\:(\S+)\s+(\S+)/ ) {
+            $description = $1;
+            $path = $2;
+            $vendor = $3;
+            $ref = $4;
+        }
+        my $alternate = 0 ;
+        if ( /\s+(\/dev\/dsk\/\S+)\s+(\/dev\/rdsk\/\S+)/ ) {
+            $devdsk=$1;
+            $devrdsk=$2;
+            # We look if whe are on an alternate link
+            foreach ( `pvdisplay $devdsk 2> /dev/null` ) {
+                if ( /$devdsk\.+lternate/ ) {
+                    $alternate=1;
                 }
             }
+
+            # skip alternate link
+            next if $alternate;
+
+            foreach ( `diskinfo -v $devrdsk 2>/dev/null`) {
+                if ( /^\s+size:\s+(\S+)/ ) {
+                    $size=$1;
+                    $size = int ( $size/1024 ) if $size;
+                }
+                if ( /^\s+rev level:\s+(\S+)/ ) {
+                    $revlvl=$1;
+                }
+            }
+            $inventory->addStorage({
+                MANUFACTURER => $vendor,
+                MODEL => $ref,
+                NAME => $devdsk,
+                DESCRIPTION => $description,
+                TYPE => 'disk',
+                DISKSIZE => $size,
+                FIRMWARE => $revlvl,
+            });
+        }
+    }
+
+    foreach ( `ioscan -kFnC tape | cut -d ':' -f 1,11,18` ) {
+        if ( /(\S+)\:(\S+)\:(\S+)\s+(\S+)/ ) {
+            $description = $1;
+            $path = $2;
+            $vendor = $3;
+            $ref = $4;
+        }
+        if ( /^\s+(\/dev\/rmt\/\Sm)\s+/ ) {
+            $devdsk=$1;
+            $inventory->addStorage({
+                MANUFACTURER => $vendor,
+                MODEL => $ref,
+                NAME => $devdsk,
+                DESCRIPTION => $description,
+                TYPE => 'tape',
+                DISKSIZE => ''
+            });
         }
     }
 }
