051c2bd68aa5d939d9442d7a3aada3e246f04468
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-25 22:41:38 +0200

    isolate parsing into its own function

--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Controllers.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Controllers.pm
@@ -13,20 +13,37 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
+    my $logger    = $params{logger};
 
     foreach my $type (qw/ext_bus fc psi/) {
-        foreach (`ioscan -kFC $type| cut -d ':' -f 9,11,17,18`) {
-            next unless /(\S+):(\S+):(\S+):(.+)/;
+        foreach my $controller (_getControllers(
+            command => "ioscan -kFC $type| cut -d ':' -f 9,11,17,18",
+            logger  => $logger
+        )) {
             $inventory->addEntry(
                 section => 'CONTROLLERS',
-                entry   => {
-                    NAME         => $2,
-                    MANUFACTURER => "$3 $4",
-                    TYPE         => $1,
-                }
+                entry   => $controller
             );
         }
     }
+}
+
+sub _getControllers {
+    my $handle = getFileHandle(@_);
+    return unless $handle;
+
+    my @controllers;
+    while (my $line = <$handle>) {
+        next unless $line =~ /(\S+):(\S+):(\S+):(.+)/;
+        push @controllers, {
+            NAME         => $2,
+            MANUFACTURER => "$3 $4",
+            TYPE         => $1,
+        };
+    }
+    close $handle;
+
+    return @controllers;
 }
 
 1;
--- lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Slots.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/HPUX/Slots.pm
@@ -13,21 +13,38 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
+    my $logger    = $params{logger};
 
     foreach my $type (qw/ioa ba/) {
-        foreach my $line (`ioscan -kFC $type| cut -d ':' -f 9,11,17,18` ) {
-            next unless $line =~ /(\S+):(\S+):(\S+):(.+)/;
+        foreach my $slot (_getSlots(
+            command => "ioscan -kFC $type| cut -d ':' -f 9,11,17,18",
+            logger  => $logger
+        )) {
             $inventory->addEntry(
                 section => 'SLOTS',
-                entry   => {
-                    DESCRIPTION => $2,
-                    DESIGNATION => "$3 $4",
-                    NAME        => $1,
-                    STATUS      => "OK",
-                }
+                entry   => $slot
             );
         }
     }
+}
+
+sub _getSlots {
+    my $handle = getFileHandle(@_);
+    return unless $handle;
+
+    my @slots;
+    while (my $line = <$handle>) {
+        next unless $line =~ /(\S+):(\S+):(\S+):(.+)/;
+        push @slots, {
+            DESCRIPTION => $2,
+            DESIGNATION => "$3 $4",
+            NAME        => $1,
+            STATUS      => "OK",
+        };
+    }
+    close $handle;
+
+    return @slots;
 }
 
 1;
