7bf03eb5d68ee43f69cabb4855e6ef6495b19233
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-04-27 09:40:21 +0200

    move private functions at the end  of the file

--- lib/FusionInventory/Agent/Task/Inventory/OS/Linux.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Linux.pm
@@ -11,23 +11,6 @@ use FusionInventory::Agent::Tools;
 
 our $runAfter = ["FusionInventory::Agent::Task::Inventory::OS::Generic"];
 
-# Get RedHat Network SystemId
-sub _getRHNSystemId {
-    my ($file) = @_;
-
-    return unless -f $file;
-    my $tpp = XML::TreePP->new();
-    my $h = $tpp->parsefile($file);
-    use Data::Dumper;
-    my $v;
-    eval {
-        foreach (@{$h->{params}{param}{value}{struct}{member}}) {
-            next unless $_->{name} eq 'system_id';
-            return $_->{value}{string};
-        }
-    }
-}
-
 sub isInventoryEnabled {
     return $OSNAME eq 'linux';
 }
@@ -56,6 +39,23 @@ sub doInventory {
         WINPRODID          => _getRHNSystemId('/etc/sysconfig/rhn/systemid')
     });
 
+}
+
+# Get RedHat Network SystemId
+sub _getRHNSystemId {
+    my ($file) = @_;
+
+    return unless -f $file;
+    my $tpp = XML::TreePP->new();
+    my $h = $tpp->parsefile($file);
+    use Data::Dumper;
+    my $v;
+    eval {
+        foreach (@{$h->{params}{param}{value}{struct}{member}}) {
+            next unless $_->{name} eq 'system_id';
+            return $_->{value}{string};
+        }
+    }
 }
 
 1;
