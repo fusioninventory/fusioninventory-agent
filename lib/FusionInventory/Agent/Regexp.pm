8e102bfabd29e2682133edf738a6ef390861be2c
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-06-07 11:20:07 +0200

    add hex version of the patterns

--- lib/FusionInventory/Agent/Regexp.pm
+++ lib/FusionInventory/Agent/Regexp.pm
@@ -7,6 +7,8 @@ use base 'Exporter';
 our @EXPORT = qw(
     $mac_address_pattern
     $ip_address_pattern
+    $hex_mac_address_pattern
+    $hex_ip_address_pattern
 );
 
 my $hex_byte = qr/[0-9A-F]{2}/i;
@@ -18,6 +20,14 @@ our $mac_address_pattern = qr/
 
 our $ip_address_pattern = qr/
     $dec_byte \. $dec_byte \. $dec_byte \. $dec_byte
+/x;
+
+our $hex_mac_address_pattern = qr/
+    $hex_byte $hex_byte $hex_byte $hex_byte $hex_byte $hex_byte
+/x;
+
+our $hex_ip_address_pattern = qr/
+    $hex_byte $hex_byte $hex_byte $hex_byte
 /x;
 
 1;
@@ -41,3 +51,11 @@ This pattern matches a MAC address.
 =head2 ip_address_pattern
 
 This pattern matches an IP address.
+
+=head2 hax_mac_address_pattern
+
+This pattern matches a MAC address in hexadecimal form.
+
+=head2 hax_ip_address_pattern
+
+This pattern matches an IP address in hexadecimal form.
