guillomovitch/fusioninventory-agent-task-netdiscovery

a bit more OO interface

lib/FusionInventory/Agent/Task/NetDiscovery.pm
@@ -130,7 +130,7 @@ sub StartThreads {
     });
 
     if ( (!defined($dico)) || !(%$dico)) {
-        $dico = FusionInventory::Agent::Task::NetDiscovery::Dico::loadDico();
+        $dico = FusionInventory::Agent::Task::NetDiscovery::Dico->new();
         $storage->save({
             idx => 999998,
             data => $dico

fusioninventory/fusioninventory-agent

lib/FusionInventory/Agent/Task/NetDiscovery.pm
@@ -368,7 +368,7 @@ sub _getDictionnary {
 
     # fallback on builtin dictionnary
     if (!$dictionnary || ref $dictionnary ne "HASH") {
-        $dictionnary = FusionInventory::Agent::Task::NetDiscovery::Dico::loadDico();
+        $dictionnary = FusionInventory::Agent::Task::NetDiscovery::Dico->new();
         $hash        = md5_hex($dictionnary);
     }
 