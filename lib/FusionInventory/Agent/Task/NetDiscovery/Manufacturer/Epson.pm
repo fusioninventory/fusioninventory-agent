guillomovitch/fusioninventory-agent-task-netdiscovery

use a static dispatch table for hardware-specific method, instead of discovering modules at runtime, allowing to only load and run the ones really needed

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Epson.pm
@@ -3,30 +3,26 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Epson;
 use strict;
 use warnings;
 
-sub discovery {
-    my ($description, $session) = @_;
+sub getDescriptionBuiltin {
+    my ($session) = @_;
 
-    if ($description =~ m/EPSON Built-in/) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.1248.1.1.3.1.3.8.0',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description = $description_new;
-        }
-    }
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.1248.1.1.3.1.3.8.0',
+        up  => 1,
+    });
 
-    if ($description =~ m/EPSON Internal 10Base-T/) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.2.1.25.3.2.1.3.1',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description = $description_new;
-        }
-    }
+    return $result;
+}
 
-    return $description;
+sub getDescriptionInternal {
+    my ($session) = @_;
+
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.2.1.25.3.2.1.3.1',
+        up  => 1,
+    });
+
+    return $result;
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Kyocera.pm
@@ -3,68 +3,59 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Kyocera;
 use strict;
 use warnings;
 
-sub discovery {
-    my ($description, $session) = @_;
+sub getDescriptionHP {
+    my ($session) = @_;
 
-    if ($description =~ m/,HP,JETDIRECT,J/) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.1229.2.2.2.1.15.1',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description = $description_new;
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.1229.2.2.2.1.15.1',
+        up  => 1,
+    });
+
+    return $result;
+}
+
+sub getDescriptionOther {
+    my ($session) = @_;
+
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.1347.42.5.1.1.2.1',
+        up  => 1,
+    });
+
+    return $result if $result;
+
+    $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.1347.43.5.1.1.1.1',
+        up  => 1,
+    });
+
+    return $result if $result;
+
+    $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.1347.43.5.1.1.1.1',
+        up  => 1,
+    });
+
+    return $result if $result;
+
+    $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.11.2.3.9.1.1.7.0',
+        up  => 1,
+    });
+
+    return unless $result;
+
+    foreach my $info (split(/;/, $result)) {
+        if ($info =~ /^MDL:/) {
+            $info =~ s/MDL://;
+            return $info;
+        } elsif ($info =~ /^MODEL:/) {
+            $info =~ s/MODEL://;
+            return $info;
         }
     }
-    
-    if (
-        $description eq "KYOCERA MITA Printing System" ||
-        $description eq "KYOCERA Printer I/F" ||
-        $description eq "SB-110"
-    ) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.1347.42.5.1.1.2.1',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description = $description_new;
-        } else {
-            my $description_new = $session->snmpGet({
-                oid => '.1.3.6.1.4.1.1347.43.5.1.1.1.1',
-                up  => 1,
-            });
-            if ($description_new) {
-                $description = $description_new;
-            } else {
-                my $description_new = $session->snmpGet({
-                    oid => '.1.3.6.1.4.1.1347.43.5.1.1.1.1',
-                    up  => 1,
-                });
-                if ($description_new) {
-                    $description = $description_new;
-                } else {
-                    my $description_new = $session->snmpGet({
-                        oid => '.1.3.6.1.4.1.11.2.3.9.1.1.7.0',
-                        up  => 1,
-                    });
-                    if ($description_new) {
-                        my @infos = split(/;/,$description_new);
-                        foreach (@infos) {
-                            if ($_ =~ /^MDL:/) {
-                                $_ =~ s/MDL://;
-                                $description = $_;
-                                last;
-                            } elsif ($_ =~ /^MODEL:/) {
-                                $_ =~ s/MODEL://;
-                                $description = $_;
-                                last;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return $description;
+
+    return;
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Samsung.pm
@@ -3,20 +3,15 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Samsung;
 use strict;
 use warnings;
 
-sub discovery {
-    my ($description, $session) = @_;
+sub getDescription {
+    my ($session) = @_;
 
-    if ($description =~ m/SAMSUNG NETWORK PRINTER,ROM/) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.236.11.5.1.1.1.1.0',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description = $description_new;
-        }
-    }
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.236.11.5.1.1.1.1.0',
+        up  => 1,
+    });
 
-    return $description;
+    return $result;
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
@@ -3,26 +3,21 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Wyse;
 use strict;
 use warnings;
 
-sub discovery {
-    my ($description, $session) = @_;
+sub getDescription {
+    my ($session) = @_;
 
-    if ($description =~ m/Linux/) {
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1',
+        up  => 1,
+    });
 
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1',
-            up  => 1,
-        });
-        if ($description_new) {
-            $description_new =~ s/^"//;
-            $description_new =~ s/"$//;
-            $description = "Wyse ".$description_new;
-        }
+    if ($result) {
+        $result =~ s/^"//;
+        $result =~ s/"$//;
+        $result = "Wyse $result";
     }
 
-    # OR ($description{'.1.3.6.1.2.1.1.1.0'} =~ m/Windows/))
-    # In other oid for Windows
-
-    return $description;
+    return $result;
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Zebranet.pm
@@ -3,30 +3,27 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Zebranet;
 use strict;
 use warnings;
 
-sub discovery {
-    my ($description, $session) = @_;
+sub getDescription {
+    my ($session) = @_;
 
-    if ($description =~ m/ZebraNet PrintServer/) {
-        my $description_new = $session->snmpGet({
-            oid => '.1.3.6.1.4.1.11.2.3.9.1.1.7.0',
-            up  => 1,
-        });
-        if ($description_new) {
-            my @infos = split(/;/,$description_new);
-            foreach (@infos) {
-                if ($_ =~ /^MDL:/) {
-                    $_ =~ s/MDL://;
-                    $description = $_;
-                    last;
-                } elsif ($_ =~ /^MODEL:/) {
-                    $_ =~ s/MODEL://;
-                    $description = $_;
-                    last;
-                }
-            }
+    my $result = $session->snmpGet({
+        oid => '.1.3.6.1.4.1.11.2.3.9.1.1.7.0',
+        up  => 1,
+    });
+
+    return unless $result;
+
+    foreach my $info (split(/;/, $result)) {
+        if ($info =~ /^MDL:/) {
+            $info =~ s/MDL://;
+            return $info;
+        } elsif ($info =~ /^MODEL:/) {
+            $info =~ s/MODEL://;
+            return $info;
         }
     }
-    return $description;
+
+    return;
 }
 
 1;

fusioninventory/fusioninventory-agent

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Epson.pm
@@ -3,18 +3,16 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Epson;
 use strict;
 use warnings;
 
-sub getBetterDescription {
-    my ($description, $snmp) = @_;
+sub getDescriptionBuiltin {
+    my ($snmp) = @_;
 
-    if ($description =~ m/EPSON Built-in/) {
-        return $snmp->get('.1.3.6.1.4.1.1248.1.1.3.1.3.8.0');
-    }
+    return $snmp->get('.1.3.6.1.4.1.1248.1.1.3.1.3.8.0');
+}
 
-    if ($description =~ m/EPSON Internal 10Base-T/) {
-        return $snmp->get('.1.3.6.1.2.1.25.3.2.1.3.1');
-    }
+sub getDescriptionInternal {
+    my ($snmp) = @_;
 
-    return;
+    return $snmp->get('.1.3.6.1.2.1.25.3.2.1.3.1');
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Kyocera.pm
@@ -3,40 +3,34 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Kyocera;
 use strict;
 use warnings;
 
-sub getBetterDescription {
-    my ($description, $snmp) = @_;
+sub getDescriptionHP {
+    my ($snmp) = @_;
 
-    if ($description =~ m/,HP,JETDIRECT,J/) {
-        return $snmp->get('.1.3.6.1.4.1.1229.2.2.2.1.15.1');
-    }
+    return $snmp->get('.1.3.6.1.4.1.1229.2.2.2.1.15.1');
+}
 
-    if (
-        $description eq "KYOCERA MITA Printing System" or
-        $description eq "KYOCERA Printer I/F"          or 
-        $description eq "SB-110"
-    ) {
-        my $result = $snmp->get('.1.3.6.1.4.1.1347.42.5.1.1.2.1');
-        return $result if $result;
+sub getDescriptionOther {
+    my ($snmp) = @_;
 
-        $result = $snmp->get('.1.3.6.1.4.1.1347.43.5.1.1.1.1');
-        return $result if $result;
+    my $result = $snmp->get('.1.3.6.1.4.1.1347.42.5.1.1.2.1');
+    return $result if $result;
 
-        $result = $snmp->get('.1.3.6.1.4.1.11.2.3.9.1.1.7.0');
-        return unless $result;
+    $result = $snmp->get('.1.3.6.1.4.1.1347.43.5.1.1.1.1');
+    return $result if $result;
 
-        my @infos = split(/;/, $result);
-        foreach (@infos) {
-            if ($_ =~ /^MDL:/) {
-                $_ =~ s/MDL://;
-                return $_;
-            } elsif ($_ =~ /^MODEL:/) {
-                $_ =~ s/MODEL://;
-                return $_;
-            }
+    $result = $snmp->get('.1.3.6.1.4.1.11.2.3.9.1.1.7.0');
+    return unless $result;
+
+    my @infos = split(/;/, $result);
+    foreach (@infos) {
+        if ($_ =~ /^MDL:/) {
+            $_ =~ s/MDL://;
+            return $_;
+        } elsif ($_ =~ /^MODEL:/) {
+            $_ =~ s/MODEL://;
+            return $_;
         }
     }
-
-    return;
 }
 
 1;

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Samsung.pm
@@ -3,10 +3,9 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Samsung;
 use strict;
 use warnings;
 
-sub getBetterDescription {
-    my ($description, $snmp) = @_;
+sub getDescription {
+    my ($snmp) = @_;
 
-    return unless $description =~ m/SAMSUNG NETWORK PRINTER,ROM/;
     return $snmp->get('.1.3.6.1.4.1.236.11.5.1.1.1.1.0');
 }
 

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
@@ -3,16 +3,15 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Wyse;
 use strict;
 use warnings;
 
-sub getBetterDescription {
-    my ($description, $snmp) = @_;
+sub getDescription {
+    my ($snmp) = @_;
 
-    return unless $description =~ m/Linux/;
     my $result = $snmp->get('.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1');
-    if ($result) {
-        $result =~ s/^"//;
-        $result =~ s/"$//;
-        $result = "Wyse $result";
-    }
+    return unless $result;
+
+    $result =~ s/^"//;
+    $result =~ s/"$//;
+    $result = "Wyse $result";
     return $result;
 }
 

lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Zebranet.pm
@@ -3,25 +3,23 @@ package FusionInventory::Agent::Task::NetDiscovery::Manufacturer::Zebranet;
 use strict;
 use warnings;
 
-sub getBetterDescription {
-    my ($description, $snmp) = @_;
+sub getDescription {
+    my ($snmp) = @_;
 
-    return unless $description =~ m/ZebraNet PrintServer/;
     my $result = $snmp->get('.1.3.6.1.4.1.11.2.3.9.1.1.7.0');
-    if ($result) {
-        my @infos = split(/;/, $result);
-        foreach (@infos) {
-            if ($_ =~ /^MDL:/) {
-                $_ =~ s/MDL://;
-                return $_;
-            } elsif ($_ =~ /^MODEL:/) {
-                $_ =~ s/MODEL://;
-                return $_;
-            }
+    return unless $result;
+
+    my @infos = split(/;/, $result);
+    foreach (@infos) {
+        if ($_ =~ /^MDL:/) {
+            $_ =~ s/MDL://;
+            return $_;
+        } elsif ($_ =~ /^MODEL:/) {
+            $_ =~ s/MODEL://;
+            return $_;
         }
     }
 
-    return;
 }
 
 1;