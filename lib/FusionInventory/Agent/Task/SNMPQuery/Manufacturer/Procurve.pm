cdb68802b7f117e3c1cef95fc335e5bd3a2a57c1
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-07-22 23:26:33 +0200

    no need to test ip pattern specifically for Procurve

--- lib/FusionInventory/Agent/Task/SNMPQuery/Manufacturer/Procurve.pm
+++ lib/FusionInventory/Agent/Task/SNMPQuery/Manufacturer/Procurve.pm
@@ -66,7 +66,7 @@ sub setCDPPorts {
             $short_number =~ s/$walks->{cdpCacheAddress}->{OID}//;
             my @array = split(/\./, $short_number);
             my $ip = hex2quad($ip_hex);
-            if (($ip ne "0.0.0.0") && ($ip =~ /^([O1]?\d\d?|2[0-4]\d|25[0-5])\.([O1]?\d\d?|2[0-4]\d|25[0-5])\.([O1]?\d\d?|2[0-4]\d|25[0-5])\.([O1]?\d\d?|2[0-4]\d|25[0-5])$/)){
+            if ($ip ne "0.0.0.0") {
                 $port_number[$array[1]] = 1;
                 $datadevice->{PORTS}->{PORT}->[$ports->{$array[1]}]->{CONNECTIONS}->{CONNECTION}->{IP} = $ip;
                 $datadevice->{PORTS}->{PORT}->[$ports->{$array[1]}]->{CONNECTIONS}->{CDP} = "1";
