317d3e3b2e5936060582046e9359c84a8e316d3d
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-07-22 23:09:21 +0200

    useless local variable

--- lib/FusionInventory/Agent/Task/SNMPQuery/Manufacturer/Nortel.pm
+++ lib/FusionInventory/Agent/Task/SNMPQuery/Manufacturer/Nortel.pm
@@ -67,13 +67,11 @@ sub setCDPPorts {
     return unless ref $results->{lldpRemChassisId} eq "HASH";
 
     my $short_number;
-    my @port_number;
 
     while (my ($number, $chassisname) = each %{$results->{lldpRemChassisId}}) {
         $short_number = $number;
         $short_number =~ s/$walks->{lldpRemChassisId}->{OID}//;
         my @array = split(/\./, $short_number);
-        next if defined $port_number[$array[2]];
 
         my @arraymac = split(/(\S{2})/, $chassisname);
         $datadevice->{PORTS}->{PORT}->[$ports->{$array[2]}]->{CONNECTIONS}->{CONNECTION}->{SYSMAC} = $arraymac[3].":".$arraymac[5].":".$arraymac[7].":".$arraymac[9].":".$arraymac[11].":".$arraymac[13];
