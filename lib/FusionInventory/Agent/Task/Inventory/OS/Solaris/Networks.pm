9555808724b4a8d34525e2ba3784d0684fbd939c
Gonéri Le Bouder <goneri@rulezlan.org>
2011-08-16 15:51:14 +0200

    getRoutingTable is in F::I::Tools::Unix

--- lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Networks.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Networks.pm
@@ -17,6 +17,7 @@ use warnings;
 use FusionInventory::Agent::Tools;
 use FusionInventory::Agent::Tools::Solaris;
 use FusionInventory::Agent::Tools::Network;
+use FusionInventory::Agent::Tools::Unix;
 
 sub isEnabled {
     return canRun('ifconfig');
