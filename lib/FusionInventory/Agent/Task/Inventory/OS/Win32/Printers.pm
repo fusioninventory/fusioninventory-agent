41bfe8518a26affed204a8dc8bfec0f030cf90ec
unknown <guillaume@.(none)>
2011-08-18 22:10:23 +0200

    function name capitalization

--- lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Printers.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Win32/Printers.pm
@@ -75,7 +75,7 @@ sub doInventory {
                 $object->{VerticalResolution};
         }
 
-        $object->{Serial} = _getSerialbyUsb($object->{PortName})
+        $object->{Serial} = _getSerialByUSB($object->{PortName})
             if $object->{PortName} && $object->{PortName} =~ /USB/;
 
         $inventory->addEntry(
@@ -102,7 +102,7 @@ sub doInventory {
 }
 
 # Search serial when connected in USB
-sub _getSerialbyUsb {
+sub _getSerialByUSB {
     my ($portName) = @_;
 
     my $machKey = $Registry->Open('LMachine', { 
