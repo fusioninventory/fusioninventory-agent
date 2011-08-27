c26037b6d49d3a42af18f097cccf2fa3c4740dc1
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-08-27 19:25:36 +0200

    fix _getDfCommand to use -P option only with GNU df

--- lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Drives.pm
+++ lib/FusionInventory/Agent/Task/Inventory/OS/Solaris/Drives.pm
@@ -15,9 +15,9 @@ sub _getDfCmd {
         command => "df --version"
     );
 
-    my $ret = ($line =~ /GNU/)?"df -P -k":"df -P -k";
-
-    return $ret;
+    return $line =~ /GNU/ ? 
+        "df -P -k" :
+        "df -k";
 }
 
 sub doInventory {
