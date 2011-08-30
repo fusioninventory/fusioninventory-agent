cc403235f7a8fe6bcb42e5c9eca91254c3c6d725
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2011-08-30 21:31:24 +0200

    initial import

---
+++ t/fake/windows/Win32/OLE.pm
@@ -0,0 +1,23 @@
+package Win32::OLE;
+
+use strict;
+use warnings;
+
+$INC{'Win32/OLE/Const.pm'} = 1;
+$INC{'Win32/OLE/Enum.pm'} = 1;
+$INC{'Win32/OLE/Variant.pm'} = 1;
+
+sub import {
+    my $callpkg = caller();
+    no strict 'refs';
+
+    *{"$callpkg\::in"}       = sub {};
+    *{"$callpkg\::CP_UTF8"}  = sub {};
+    *{"$callpkg\::VT_BYREF"} = sub {};
+    *{"$callpkg\::VT_BSTR"}  = sub {};
+}
+
+sub Option {
+}
+
+1;
