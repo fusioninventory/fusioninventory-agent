1a0a1ad98ea64ed551c25ab7ed55020c50eb41ca
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-09-04 16:19:10 +0200

    cleanup: - make the comment match the code (this it not an hash...) - use 'first' function instead of a loop - find the exact distribution only once, before extracting data - use dedicated file function

--- lib/FusionInventory/Agent/Task/Inventory/Input/Linux/Distro/NonLSB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Linux/Distro/NonLSB.pm
@@ -7,45 +7,48 @@ use English qw(-no_match_vars);
 
 use FusionInventory::Agent::Tools;
 
-# This hash contains the following fields:
-#     File to read             => Distro name => Regex to get distro version => Full version
-my @files = (
+# This array contains four items for each distribution:
+# - release file
+# - distribution name,
+# - regex to get the version
+# - template to get the full name
+my @distributions = (
     # vmware-release contains something like "VMware ESX Server 3" or "VMware ESX 4.0 (Kandinsky)"
-    [ '/etc/vmware-release'    => 'VMWare'    => '.* ([0-9\.]+).*'          => '%s' ],
+    [ '/etc/vmware-release',    'VMWare',                     '.* ([0-9\.]+).*',         '%s' ],
 
-    [ '/etc/arch-release'      => 'ArchLinux' => '(.*)'                     => 'ArchLinux %s' ],
+    [ '/etc/arch-release',      'ArchLinux',                  '(.*)',                    'ArchLinux %s' ],
 
-    [ '/etc/debian_version'    => 'Debian'    => '(.*)'                     => 'Debian GNU/Linux %s'],
+    [ '/etc/debian_version',    'Debian',                     '(.*)',                    'Debian GNU/Linux %s'],
 
     # fedora-release contains something like "Fedora release 9 (Sulphur)"
-    [ '/etc/fedora-release'    => 'Fedora'    => '.* release ([0-9\.]+).*'  => '%s' ],
+    [ '/etc/fedora-release',    'Fedora',                     '.* release ([0-9\.]+).*', '%s' ],
 
-    [ '/etc/gentoo-release'    => 'Gentoo'    => '(.*)'                     => 'Gentoo Linux %s' ],
+    [ '/etc/gentoo-release',    'Gentoo',                     '(.*)',                    'Gentoo Linux %s' ],
 
     # knoppix_version contains something like "3.2 2003-04-15".
     # Note: several 3.2 releases can be made, with different dates, so we need to keep the date suffix
-    [ '/etc/knoppix_version'   => 'Knoppix'   => '(.*)'                     => 'Knoppix GNU/Linux %s' ],
+    [ '/etc/knoppix_version',   'Knoppix',                    '(.*)',                    'Knoppix GNU/Linux %s' ],
 
     # mandriva-release contains something like "Mandriva Linux release 2010.1 (Official) for x86_64"
-    [ '/etc/mandriva-release'  => 'Mandriva'  => '.* release ([0-9\.]+).*'  => '%s'],
+    [ '/etc/mandriva-release',  'Mandriva',                   '.* release ([0-9\.]+).*', '%s'],
 
     # mandrake-release contains something like "Mandrakelinux release 10.1 (Community) for i586"
-    [ '/etc/mandrake-release'  => 'Mandrake'  => '.* release ([0-9\.]+).*'  => '%s'],
+    [ '/etc/mandrake-release',  'Mandrake',                   '.* release ([0-9\.]+).*', '%s'],
 
     # redhat-release contains something like "Red Hat Enterprise Linux Server release 5 (Tikanga)"
-    [ '/etc/redhat-release'    => 'RedHat'    => '.* release ([0-9\.]+).*'  => '%s' ],
+    [ '/etc/redhat-release',    'RedHat',                     '.* release ([0-9\.]+).*', '%s' ],
 
-    [ '/etc/slackware-version' => 'Slackware' => 'Slackware (.*)'           => '%s' ],
+    [ '/etc/slackware-version', 'Slackware',                  'Slackware (.*)'         , '%s' ],
 
     # SuSE-release contains something like "SUSE Linux Enterprise Server 11 (x86_64)"
     # Note: it may contain several extra lines
-    [ '/etc/SuSE-release'      => 'SuSE'      => '.* ([0-9\.]+).*'          => '%s' ],
+    [ '/etc/SuSE-release',      'SuSE',                       '.* ([0-9\.]+).*',         '%s' ],
 
     # trustix-release contains something like "Trustix Secure Linux release 2.0 (Cloud)"
-    [ '/etc/trustix-release'   => 'Trustix'   => '.* release ([0-9\.]+).*'  => '%s' ],
+    [ '/etc/trustix-release',   'Trustix',                    '.* release ([0-9\.]+).*', '%s' ],
 
     # Fallback
-    [ '/etc/issue'             => 'Unknown Linux distribution' => '.* ([0-9\.]+).*' => '%s' ],
+    [ '/etc/issue',             'Unknown Linux distribution', '.* ([0-9\.]+).*'        , '%s' ],
 
     # Note: Ubuntu is not listed here as it does not have a
     # ubuntu-{release,version} file, but it should always have the lsb_release
@@ -64,81 +67,50 @@ sub doInventory {
 
     my $inventory = $params{inventory};
 
+    my $distribution = first { -f $_->[0] } @distributions;
+    return unless $distribution;
+
     $inventory->setHardware({
-        OSNAME => _findRelease(),
+        OSNAME => _findRelease($distribution),
     });
 
-    $inventory->setOperatingSystem(_getDistroData());
+    $inventory->setOperatingSystem(_getDistroData($distribution));
 }
 
 sub _getDistroData {
-    my $data;
+    my ($distribution) = @_;
 
-    foreach (@files) {
-        my $file = $_->[0];
-        next unless -f $file;
+    my $name     = $distribution->[1];
+    my $regexp   = $distribution->[2];
+    my $template = $distribution->[3];
 
-        my $distroName = $_->[1];
-        my $distroVersRegex = $_->[2];
-        my $distroFullName  = $_->[3];
+    my $line       = getFirstLine(file => $distribution->[0]);
+    my $release    = sprintf $template, $line;
+    my ($version)  = $line =~ /^$regexp/;
 
-        my $handle;
-        if (!open $handle, '<', $file) {
-            warn "Can't open $file: $ERRNO";
-            return;
-        }
-        my $version = <$handle>;
-        chomp $version;
-        close $handle;
+    my $data = {
+        NAME      => $name,
+        VERSION   => $version,
+        FULL_NAME => $release
+    };
 
-        my $commercialFullName = sprintf $distroFullName, $version;
-        if ($version =~ /^$distroVersRegex/) {
-            my $distroVersion = $1;
-
-            # Now we have found the distro name and version, let's set them
-            $data = {
-                NAME                 => $distroName,
-                VERSION              => $distroVersion,
-                FULL_NAME            => $commercialFullName
-            };
-
-            # We found what we need, no need to continue checking
-            last;
-        }
+    if ($name eq 'SuSE') {
+        $data->{SERVICE_PACK} = getFirstMatch(
+            file    => '/etc/SuSE-release',
+            pattern => qr/^PATCHLEVEL = ([0-9]+)/
+        );
     }
-
-    # TODO: improve, integrate test-suite
-    if (-f "/etc/SuSE-release") {
-        my $handle;
-        if (!open $handle, '<', "/etc/SuSE-release") {
-            warn "Can't open /etc/SuSE-release: $ERRNO";
-            return;
-        }
-        while (<$handle>) {
-            if (/^PATCHLEVEL = ([0-9]+)/) {
-                $data->{SERVICE_PACK} = $1;
-            }
-        }
-        close $handle;
-    }
-
 
     return $data;
 }
 
-
 sub _findRelease {
-    my $release;
+    my ($distribution) = @_;
 
-    foreach (@files) {
-        my $file = $_->[0];
-        my $distro = $_->[1];
+    my $template = $distribution->[3];
 
-        next unless canRead($file);
-        my $version = getFirstLine(file => $file);
-        $release = sprintf $distro, $version;
-        last;
-    }
+    my $line    = getFirstLine(file => $distribution->[0]);
+    my $release = sprintf $template, $line;
 
     return $release;
 }
