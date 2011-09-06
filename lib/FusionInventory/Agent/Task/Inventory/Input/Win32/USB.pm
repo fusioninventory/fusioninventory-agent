8bfc95b554783ce0c97f8e3a91178816be764491
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-09-06 23:35:31 +0200

    more sanitizion

--- lib/FusionInventory/Agent/Task/Inventory/Input/Win32/USB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Win32/USB.pm
@@ -45,7 +45,7 @@ sub doInventory {
         class      => 'CIM_LogicalDevice',
         properties => [ qw/DeviceID Name/ ]
     )) {
-        next unless $object->{DeviceID} =~ /^USB\\VID_(\w+)&PID_(\w+)(.*)?/;
+        next unless $object->{DeviceID} =~ /^USB\\VID_(\w+)&PID_(\w+)(.*)/;
 
         my $vendorId  = $1;
         my $productId = $2;
