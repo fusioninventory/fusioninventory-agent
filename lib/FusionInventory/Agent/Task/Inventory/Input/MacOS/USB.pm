1df91e59ca1e66355fe2e2e60a035b37f8d207ee
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-09-08 05:12:27 +0200

    move samples from comments to a resource file

--- lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/USB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/USB.pm
@@ -5,62 +5,6 @@ use warnings;
 
 use FusionInventory::Agent::Tools;
 
-#          {
-#            'IOGeneralInterest' => 'IOCommand is not serializable',
-#            'USB Address' => '3',
-#            'Requested Power' => '10',
-#            'idProduct' => '545',
-#            'bMaxPacketSize0' => '8',
-#            'USB Vendor Name' => 'Apple, Inc',
-#            'sessionID' => '1472072547',
-#            'bNumConfigurations' => '1',
-#            'bDeviceProtocol' => '0',
-#            'Bus Power Available' => '50',
-#            'PortUsingExtraPowerForWake' => '0',
-#            'Device Speed' => '0',
-#            'IOCFPlugInTypes' => '{9dc7b780-9ec0-11d4-a54f-000a27052861"="IOUSBFamily.kext/Contents/PlugIns/IOUSBLib.bundle}',
-#            'iProduct' => '2',
-#            'IOUserClientClass' => 'IOUSBDeviceUserClientV2',
-#            'USB Product Name' => 'Apple Keyboard',
-#            'bDeviceSubClass' => '0',
-#            'bDeviceClass' => '0',
-#            'PortNum' => '2',
-#            'non-removable' => 'yes',
-#            'locationID' => '18446744073611116544',
-#            'bcdDevice' => '105',
-#            'Low Power Displayed' => 'No',
-#            'iManufacturer' => '1',
-#            'iSerialNumber' => '0',
-#            'idVendor' => '1452'
-#          },
-#          {
-#            'IOGeneralInterest' => 'IOCommand is not serializable',
-#            'USB Address' => '4',
-#            'Requested Power' => '50',
-#            'idProduct' => '49174',
-#            'bMaxPacketSize0' => '8',
-#            'USB Vendor Name' => 'Logitech',
-#            'sessionID' => '1586211098',
-#            'bNumConfigurations' => '1',
-#            'bDeviceProtocol' => '0',
-#            'Bus Power Available' => '50',
-#            'PortUsingExtraPowerForWake' => '0',
-#            'Device Speed' => '0',
-#            'IOCFPlugInTypes' => '{9dc7b780-9ec0-11d4-a54f-000a27052861"="IOUSBFamily.kext/Contents/PlugIns/IOUSBLib.bundle}',
-#            'iProduct' => '2',
-#            'IOUserClientClass' => 'IOUSBDeviceUserClientV2',
-#            'USB Product Name' => 'Optical USB Mouse',
-#            'bDeviceSubClass' => '0',
-#            'bDeviceClass' => '0',
-#            'PortNum' => '3',
-#            'bcdDevice' => '832',
-#            'locationID' => '18446744073611182080',
-#            'Low Power Displayed' => 'No',
-#            'iManufacturer' => '1',
-#            'iSerialNumber' => '0',
-#            'idVendor' => '1133'
-#          }
-
 sub isEnabled {
     return 1;
 }
