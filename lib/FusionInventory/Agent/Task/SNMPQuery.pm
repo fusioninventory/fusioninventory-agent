d139e457ac6c7eb59ffce0d62e4afe27c973ccaa
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-09-11 22:26:38 +0200

    don't forget Tools module, for any() function

--- lib/FusionInventory/Agent/Task/SNMPQuery.pm
+++ lib/FusionInventory/Agent/Task/SNMPQuery.pm
@@ -19,6 +19,7 @@ use English qw(-no_match_vars);
 
 use FusionInventory::Agent::SNMP;
 use FusionInventory::Agent::XML::Query;
+use FusionInventory::Agent::Tools;
 use FusionInventory::Agent::Tools::Network;
 
 our $VERSION = '2.0';
