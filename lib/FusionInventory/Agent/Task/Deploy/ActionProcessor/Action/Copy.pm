44aed96e6df22776484cbdf866449f5a3c88ecd3
Gonéri Le Bouder <goneri@rulezlan.org>
2011-09-11 21:55:21 +0200

    avoid File::Copy::Recursive::*_glob functions
    See: https://rt.cpan.org/Public/Bug/Display.html?id=70326

--- lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Copy.pm
+++ lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Copy.pm
@@ -5,20 +5,24 @@ use warnings;
 
 $File::Copy::Recursive::CPRFComp = 1;
 use File::Copy::Recursive qw(rcopy);
-
+use File::Glob;
 
 sub do {
     my ($params) = @_;
 
     my $log = [];
-    my $status;
-    $status = rcopy($params->{from}, $params->{to});
-    if (!$status) {
-        $log = [ "Failed to move file: '".$params->{from}."' to '".$params->{to}."'", $! ];
+    my $status = 1;
+    foreach (File::Glob::glob($params->{from})) {
+        if (!File::Copy::Recursive::rcopy($_, $params->{to})) {
+            push @$log, "Failed to copy: `".$params->{from}."' to '".$params->{to};
+            push @$log, $!;
+
+            $status = 0;
+        }
     }
     return {
-    status => $status,
-    log => $log,
+        status => $status,
+        log => $log,
     };
 }
 
--- lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Move.pm
+++ lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Move.pm
@@ -5,21 +5,25 @@ use warnings;
 
 $File::Copy::Recursive::CPRFComp = 1;
 use File::Copy::Recursive;
+use File::Glob;
 
 
 sub do {
     my ($params) = @_;
 
     my $log = [];
-    my $status;
-    $status = File::Copy::Recursive::rmove_glob($params->{from}, $params->{to});
-print $!;
-    if (!$status) {
-        $log = [ "Failed to move file: `".$params->{from}."' to '".$params->{to}, $! ];
+    my $status = 1;
+    foreach (File::Glob::glob($params->{from})) {
+        if (!File::Copy::Recursive::rmove($_, $params->{to})) {
+            push @$log, "Failed to move: `".$params->{from}."' to '".$params->{to};
+            push @$log, $!;
+
+            $status = 0;
+        }
     }
     return {
-    status => $status,
-    log => $log,
+        status => $status,
+        log => $log,
     };
 }
 
