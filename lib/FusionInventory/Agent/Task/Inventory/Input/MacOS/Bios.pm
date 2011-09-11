c6760061b35308600a5a6381076b64ccbe56f49f
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-09-11 10:52:45 +0200

    factorize ioreg parsing into a new shared function

--- lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/Bios.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/Bios.pm
@@ -15,6 +15,7 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
+    my $logger    = $params{logger};
 
     my $prof = Mac::SysProfile->new();
     my $info = $prof->gettype('SPHardwareDataType');
@@ -22,56 +23,25 @@ sub doInventory {
 
     $info = $info->{'Hardware Overview'};
 
-    my $ioregInfo;
-#+-o iMac7,1  <class IOPlatformExpertDevice, registered, matched, active, busy 0, retain 24>
-#    {
-#      "IOBusyInterest" = "IOCommand is not serializable"
-#      "IOInterruptControllers" = ("io-apic-0")
-#      "IOPlatformSerialNumber" = "0"
-#      "clock-frequency" = <00e1f505>
-#      "version" = <"1.0">
-#      "product-name" = <"iMac7,1">
-#      "serial-number" = <30003000000000000000000000300000000000000000000000000000000000000000000000000000000000>
-#      "IOInterruptSpecifiers" = (<0900000007000000>)
-#      "model" = <"iMac7,1">
-#      "IOPlatformUUID" = "00000000-0000-1000-8000-0800276E729D"
-#      "manufacturer" = <"innotek GmbH">
-#      "IOPlatformArgs" = <00b0ac0000a0680010cfb80000000000>
-#      "name" = <"/">
-#      "compatible" = <"iMac7,1">
-#      "IOPolledInterface" = "SMCPolledInterface is not serializable"
-#    }
-    my $in;
-    foreach (`ioreg -l`) {
-       $in = 1 if /<class IOPlatformExpertDevice/;
-       if ($in) {
-           if (/"(\S+)"\s*=\s*(.*)/) {
-               my $k = $1;
-               my $t = $2;
-               $t =~ s/<(.*)>/$1/;
-               $t =~ s/"(.*)"/$1/;
-               $ioregInfo->{$k} = $t;
-           } elsif (/^[\|\s]*}\s*$/) {
-               $in=0;
-               last; 
-           }
-        }
-    }
+    my ($device) = getIODevices(
+        class => 'IOPlatformExpertDevice',
+        logger => $logger
+    );
 
     # set the bios informaiton from the apple system profiler
     $inventory->setBios({
-        SMANUFACTURER => $ioregInfo->{'manufacturer'} || 'Apple Inc', # duh
+        SMANUFACTURER => $device->{'manufacturer'} || 'Apple Inc', # duh
         SMODEL        => $info->{'Model Identifier'} || $info->{'Machine Model'},
         #       SSN             => $h->{'Serial Number'}
         # New method to get the SSN, because of MacOS 10.5.7 update
         # system_profiler gives 'Serial Number (system): XXXXX' where 10.5.6
         # and lower give 'Serial Number: XXXXX'
-        SSN           => $info->{'Serial Number'} || $info->{'Serial Number (system)'} || $ioregInfo->{'serial-number'},
+        SSN           => $info->{'Serial Number'} || $info->{'Serial Number (system)'} || $device->{'serial-number'},
         BVERSION      => $info->{'Boot ROM Version'},
     });
 
     $inventory->setHardware({
-        UUID => $info->{'Hardware UUID'} || $ioregInfo->{'IOPlatformUUID'}
+        UUID => $info->{'Hardware UUID'} || $device->{'IOPlatformUUID'}
     });
 }
 
--- lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/USB.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/USB.pm
@@ -15,7 +15,7 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    foreach my $device (_getDevices(command => 'ioreg -l', logger => $logger)) {
+    foreach my $device (_getDevices(logger => $logger)) {
         $inventory->addEntry(
             section => 'USBDEVICES',
             entry   => $device,
@@ -25,46 +25,19 @@ sub doInventory {
 }
 
 sub _getDevices {
-    my $handle = getFileHandle(@_);
-    return unless $handle;
 
-    my $state = 0;
-    my @devices;  
-    my $device;   
-
-    while (my $line = <$handle>) {
-        $line =~ s/^[\|\s]*//;     
-        $state = 1 if $line =~ /<class IOUSBDevice,/;
-        $state = 2 if $state == 1 && $line =~ /^\{/;
-
-        if ($state == 2 && $line =~ /^\}/) {
-            $state = 0;   
-            push @devices, {
-                VENDORID  => sprintf("%x", $device->{'idVendor'}),
-                PRODUCTID => sprintf("%x", $device->{'idProduct'}),
-                SERIAL    => $device->{'USB Serial Number'},
-                NAME      => $device->{'USB Product Name'},
-                CLASS     => $device->{'bDeviceClass'},
-                SUBCLASS  => $device->{'bDeviceSubClass'}
-            } if keys %$device;
-            $device = {};
-        } 
-
-        if ($state == 2) {
-            if (/(.*?)\s=\s(.*)/) {                                                                                                        
-                my $key = $1;
-                my $val = $2;
-
-                $key =~ s/"(.*)"/$1/;
-                $val =~ s/"(.*)"/$1/;
-
-                $device->{$key}=$val;
-            } 
-        } 
-    } 
-    close $handle;
-
-    return @devices;
+    return 
+        map {
+            {
+                VENDORID  => sprintf("%x", $_->{'idVendor'}),
+                PRODUCTID => sprintf("%x", $_->{'idProduct'}),
+                SERIAL    => $_->{'USB Serial Number'},
+                NAME      => $_->{'USB Product Name'},
+                CLASS     => $_->{'bDeviceClass'},
+                SUBCLASS  => $_->{'bDeviceSubClass'}
+            }
+        }
+        getIODevices(class => 'IOUSBDevice', @_);
 }
 
 1;
