e189d1fc39421d98f342079a4b4410f2be5ecb09
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-10-02 22:59:10 +0200

    log message homogeneisation

--- lib/FusionInventory/Agent/Task/SNMPQuery.pm
+++ lib/FusionInventory/Agent/Task/SNMPQuery.pm
@@ -179,13 +179,13 @@ sub run {
     my ($self) = @_;
 
     if (!$self->{target}->isa('FusionInventory::Agent::Target::Server')) {
-        $self->{logger}->debug("No server. Exiting...");
+        $self->{logger}->debug("No server available, exiting");
         return;
     }
 
     my $response = $self->{prologresp};
     if (!$response) {
-        $self->{logger}->debug("No server response. Exiting...");
+        $self->{logger}->debug("No server response, exiting");
         return;
     }
 
