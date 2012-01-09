3468c1623764e98600d40bbf2fb560f97cd8f1d4
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2012-01-09 23:09:58 +0100

    split code in functions for readability, and use plain english variable names

--- lib/FusionInventory/Agent/Task/Deploy/Datastore.pm
+++ lib/FusionInventory/Agent/Task/Deploy/Datastore.pm
@@ -82,82 +82,105 @@ sub diskIsFull {
 
     my $logger = $self->{logger};
 
-    my $spaceFree;
-    if ($OSNAME eq 'MSWin32') {
+    my $freeSpace =
+        $OSNAME eq 'MSWin32' ? _getFreeSpaceWindows() :
+        $OSNAME eq 'solaris' ? _getFreeSpaceSolaris() :
+                               _getFreeSpace()        ;
 
-        if (!eval ('
-                use Win32::OLE qw(in CP_UTF8);
-                use Win32::OLE::Const;
-
-                Win32::OLE->Option(CP => CP_UTF8);
-
-                1')) {
-            $logger->error("Failed to load Win32::OLE: $@") if $logger;
-        }
-
-
-        my $letter;
-        if ($self->{path} !~ /^(\w):./) {
-            $logger->error("Path parse error: ".$self->{path}) if $logger;
-            return;
-        }
-        $letter = $1.':';
-
-
-        my $WMIServices = Win32::OLE->GetObject(
-            "winmgmts:{impersonationLevel=impersonate,(security)}!//./" );
-
-        if (!$WMIServices) {
-            $logger->error(Win32::OLE->LastError()) if $logger;
-            return;
-        }
-
-        foreach my $properties ( Win32::OLE::in(
-                $WMIServices->InstancesOf('Win32_LogicalDisk'))) {
-
-            next unless lc($properties->{Caption}) eq lc($letter);
-            my $t = $properties->{FreeSpace};
-            if ($t && $t =~ /(\d+)\d{6}$/) {
-                $spaceFree = $1;
-            }
-        }
-    } elsif ($OSNAME eq 'solaris') {
-        my $dfFh;
-        return unless -d $self->{path};
-        if (open($dfFh, '-|', "df", '-b', $self->{path})) {
-            foreach(<$dfFh>) {
-                if (/^\S+\s+(\d+)\d{3}[^\d]/) {
-                    $spaceFree = $1;
-                }
-            }
-            close $dfFh
-        } elsif ($logger) {
-            $logger->error("Failed to exec df");
-        }
-    } else {
-        my $dfFh;
-        return unless -d $self->{path};
-        if (open($dfFh, '-|', "df", '-Pk', $self->{path})) {
-            foreach(<$dfFh>) {
-                if (/^\S+\s+\S+\s+\S+\s+(\d+)\d{3}[^\d]/) {
-                    $spaceFree = $1;
-                }
-            }
-            close $dfFh
-        } elsif ($logger) {
-            $logger->error("Failed to exec df");
-        }
-    }
-
-    if(!$spaceFree) {
+    if(!$freeSpace) {
 	$logger->debug('$spaceFree is undef!') if $logger;
-	$spaceFree=0;
+	$freeSpace=0;
     }
 
-    print "Freespace on ".$self->{path}." : ".$spaceFree."\n";
+    print "Freespace on ".$self->{path}." : ".$freeSpace."\n";
     # 400MB Free, should be set by a config option
-    return ($spaceFree < 2000);
+    return ($freeSpace < 2000);
 }
 
+sub _getFreeSpaceWindows {
+    my ($self) = @_;
+
+    my $logger = $self->{logger};
+
+    if (!eval ('
+            use Win32::OLE qw(in CP_UTF8);
+            use Win32::OLE::Const;
+
+            Win32::OLE->Option(CP => CP_UTF8);
+
+            1')) {
+        $logger->error("Failed to load Win32::OLE: $@") if $logger;
+    }
+
+
+    my $letter;
+    if ($self->{path} !~ /^(\w):./) {
+        $logger->error("Path parse error: ".$self->{path}) if $logger;
+        return;
+    }
+    $letter = $1.':';
+
+
+    my $WMIServices = Win32::OLE->GetObject(
+        "winmgmts:{impersonationLevel=impersonate,(security)}!//./" );
+
+    if (!$WMIServices) {
+        $logger->error(Win32::OLE->LastError()) if $logger;
+        return;
+    }
+
+    my $freeSpace;
+    foreach my $properties ( Win32::OLE::in(
+            $WMIServices->InstancesOf('Win32_LogicalDisk'))) {
+
+        next unless lc($properties->{Caption}) eq lc($letter);
+        my $t = $properties->{FreeSpace};
+        if ($t && $t =~ /(\d+)\d{6}$/) {
+            $freeSpace = $1;
+        }
+    }
+
+    return $freeSpace;
+}
+
+sub _getFreeSpaceSolaris {
+    my ($self) = @_;
+
+    return unless -d $self->{path};
+
+    my $freeSpace;
+    if (open(my $handle, '-|', "df", '-b', $self->{path})) {
+        foreach(<$handle>) {
+            if (/^\S+\s+(\d+)\d{3}[^\d]/) {
+                $freeSpace = $1;
+            }
+        }
+        close $handle
+    } else {
+        $self->{logger}->error("Failed to exec df") if $self->{logger};
+    }
+
+    return $freeSpace;
+}
+
+sub _getFreeSpace {
+    my ($self) = @_;
+
+    return unless -d $self->{path};
+
+    my $freeSpace;
+    if (open(my $handle, '-|', "df", '-Pk', $self->{path})) {
+        foreach(<$handle>) {
+            if (/^\S+\s+\S+\s+\S+\s+(\d+)\d{3}[^\d]/) {
+                $freeSpace = $1;
+            }
+        }
+        close $handle
+    } else {
+        $self->{logger}->error("Failed to exec df") if $self->{logger};
+    }
+
+    return $freeSpace;
+}
 
 1;
