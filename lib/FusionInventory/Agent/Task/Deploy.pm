5ae92c2ed7ecaa95c4971a20d33701e3ed5f5def
Gonéri Le Bouder <goneri@rulezlan.org>
2012-02-01 18:32:42 +0100

    prepare 1.0.9901 release

--- lib/FusionInventory/Agent/Task/Deploy.pm
+++ lib/FusionInventory/Agent/Task/Deploy.pm
@@ -19,7 +19,7 @@ use FusionInventory::Agent::Task::Deploy::Datastore;
 use FusionInventory::Agent::Task::Deploy::File;
 use FusionInventory::Agent::Task::Deploy::Job;
 
-our $VERSION = '1.0.3';
+our $VERSION = '1.0.9901';
 
 sub isEnabled {
     my ($self, $response) = @_;
