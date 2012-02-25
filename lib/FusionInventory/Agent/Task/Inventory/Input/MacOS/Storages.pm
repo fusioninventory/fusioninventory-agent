9d99310386d83fc7d0c6ca43d12d071ff496be4d
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2012-02-25 23:26:10 +0100

    rewrite, using system_profiler parser from Tools::MacOs

--- lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/Storages.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/MacOS/Storages.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Tools::MacOS;
 
 sub isEnabled {
     return 1;
@@ -15,116 +16,99 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $devices = {};
-
-    # Get SATA Drives
-    my $sata = _getDiskInfo();
-
-    foreach my $device ( @$sata ) {
-        my $description;
-        if (!$device->{'Protocol'}) {
-            $description = 'Disk drive';
-        } elsif ( ($device->{'Protocol'} eq 'ATAPI')
-                ||
-                ($device->{'Drive Type'}) ) {
-            $description = 'CD-ROM Drive';
-        }
-
-        my $size = $device->{'Capacity'};
-        if ($size) {
-            #e.g: Capacity: 320,07 GB (320 072 933 376 bytes)
-            $size =~ s/\s*\(.*//;
-            $size =~ s/ GB//;
-            $size =~ s/,/./;
-            $size = int($size * 1024);
-        }
-
-        my $manufacturer = getCanonicalManufacturer($device->{'Name'});
-
-        my $model = $device->{'Model'};
-        if ($model) {
-            $model =~ s/\s*$manufacturer\s*//i;
-        }
-
+    foreach my $storage (_getStorages(logger => $logger)) {
         $inventory->addEntry(
             section => 'STORAGES',
-            entry   => {
-                NAME         => $device->{'Name'},
-                SERIAL       => $device->{'Serial Number'},
-                DISKSIZE     => $size,
-                FIRMWARE     => $device->{'Revision'},
-                MANUFACTURER => $manufacturer,
-                DESCRIPTION  => $description,
-                MODEL        => $model,
-                TYPE         => $device->{'Type'}
-            }
+            entry   => $storage
         );
     }
-
-
 }
 
-sub _getDiskInfo {
-    my ($section) = @_;
+sub _getStorages {
+    my $infos = getSystemProfilerInfos(@_);
 
-    my $wasEmpty;
-    my $revIndent = '';
-    my @infos;
-    my $info;
-    my $name;
-    my $type;
-    foreach (`/usr/sbin/system_profiler SPSerialATADataType`,
-        `/usr/sbin/system_profiler SPParallelATADataType`,
-        `/usr/sbin/system_profiler SPUSBDataType`,
-        `/usr/sbin/system_profiler SPFireWireDataType`) {
-        if (/^\s*$/) {
-            $wasEmpty=1;
-            next;
-        }
+    # system profiler info is organized as
+    # bus => {
+    #     controller => {
+    #         key1 => value1,
+    #         device => {
+    #             key1 => value1
+    #             subdevice => {
+    #                 key1 => value1,
+    #             }
+    #         }
+    #     }
+    # }
+    my @storages;
 
-        next unless /^(\s*)/;
-        if ($1 ne $revIndent) {
-            $name = $1 if (/^\s+(\S+.*\S+):\s*$/ && $wasEmpty);
-            $revIndent = $1;
+    foreach my $bus_name (qw/ATA SERIAL-ATA USB FireWire/) {
+        my $bus = $infos->{$bus_name};
+        next unless $bus;
+        foreach my $controller_name (keys %{$bus}) {
+            my $controller = $bus->{$controller_name};
+            foreach my $device_name (keys %{$controller}) {
+                my $device = $controller->{$device_name};
+                next unless ref $device eq 'HASH';
+                if (_isStorage($device)) {
+                    push @storages,
+                        _getStorage($device, $device_name, $bus_name);
+                } else {
+                    foreach my $subdevice_name (keys %{$device}) {
+                        my $subdevice = $device->{$subdevice_name};
+                        next unless ref $subdevice eq 'HASH';
+                        push @storages,
+                            _getStorage($subdevice, $subdevice_name, $bus_name)
+                            if _isStorage($subdevice);
+                    }
+                }
 
-# We use the Protocol key to know if it a storage section or not
-            if (
-                # disk
-                ($info->{'BSD Name'} && $info->{'BSD Name'} =~ /^disk\d+$/)
-                ||
-                # USB Disk
-#                ($info->{'BSD Name'} && $info->{'Product ID'})
-#                ||
-                # CDROM
-                ($info->{'Protocol'} && $info->{'Socket Type'})
-            ) {
-                $info->{Type} = $type;
-                push @infos, $info;
-                $name = '';
-            }
-            $info = {};
-        }
-        if (/^\s+(\S+.*?):\s+(\S.*)/) {
-            $info->{$1}=$2;
-            $info->{Name} = $name;
-        }
-
-        $type = '' if /^(\S+)/;
-        if (/^(\S+):$/) {
-            $type=$1;
-            if ($type eq 'FireWire') {
-                $type = '1394';
             }
         }
-
-
-        $wasEmpty=0;
     }
-# The last one
-    if ($info->{Protocol}) {
-        push @infos, $info;
+
+    return @storages;
+}
+
+sub _isStorage {
+    my ($device) = @_;
+
+    return 
+        ($device->{'BSD Name'} && $device->{'BSD Name'} =~ /^disk\d+$/) || 
+        ($device->{'Protocol'} && $device->{'Socket Type'});
+}
+
+sub _getStorage {
+    my ($device, $device_name, $bus_name) = @_;
+
+    my $storage = {
+        NAME         => $device_name,
+        MANUFACTURER => getCanonicalManufacturer($device_name),
+        TYPE         => $bus_name eq 'FireWire' ? '1394' : $bus_name,
+        SERIAL       => $device->{'Serial Number'},
+        FIRMWARE     => $device->{'Revision'},
+        MODEL        => $device->{'Model'},
+        DISKSIZE     => $device->{'Capacity'}
+    };
+
+    if (!$device->{'Protocol'}) {
+        $storage->{DESCRIPTION} = 'Disk drive';
+    } elsif ($device->{'Protocol'} eq 'ATAPI' || $device->{'Drive Type'}) {
+        $storage->{DESCRIPTION} = 'CD-ROM Drive';
     }
-    return \@infos;
+
+    if ($storage->{DISKSIZE}) {
+        #e.g: Capacity: 320,07 GB (320 072 933 376 bytes)
+        $storage->{DISKSIZE} =~ s/\s*\(.*//;
+        $storage->{DISKSIZE} =~ s/ GB//;
+        $storage->{DISKSIZE} =~ s/,/./;
+        $storage->{DISKSIZE} = int($storage->{DISKSIZE} * 1024);
+    }
+
+    if ($storage->{MODEL}) {
+        $storage->{MODEL} =~ s/\s*$storage->{MANUFACTURER}\s*//i;
+    }
+
+    return $storage;
 }
 
 1;
