619bfd9e276e7cfc788c40f70dfeb1bb5f7bd0c0
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2012-03-04 15:54:51 +0100

    code factorisation

--- t/FusionInventory/Test/Utils.pm
+++ t/FusionInventory/Test/Utils.pm
@@ -4,10 +4,12 @@ use strict;
 use warnings;
 use base 'Exporter';
 
+use English qw(-no_match_vars);
 use Socket;
 
 our @EXPORT = qw(
     test_port
+    mockGetWmiObjects
 );
 
 sub test_port {
@@ -24,4 +26,46 @@ sub test_port {
     }
 
     return 0;
+}
+
+sub mockGetWmiObjects {
+    my ($test) = @_;
+
+    return sub {
+        my (%params) = @_;
+
+        my $file = "resources/win32/wmi/$test-$params{class}.wmi";
+        open (my $handle, '<', $file) or die "can't open $file: $ERRNO";
+
+        # this is a windows file
+        binmode $handle, ':encoding(UTF-16LE)';
+        local $INPUT_RECORD_SEPARATOR="\r\n";
+
+        # build a list of desired properties indexes
+        my %properties = map { $_ => 1 } @{$params{properties}};
+
+        my @objects;
+        my $object;
+        while (my $line = <$handle>) {
+            chomp $line;
+
+            if ($line =~ /^ (\w+) = (.+) $/x) {
+                my $key = $1;
+                my $value = $2;
+                next unless $properties{$key};
+                $value =~ s/&amp;/&/g;
+                $object->{$key} = $value;
+                next;
+            }
+
+            if ($line =~ /^$/) {
+                push @objects, $object if $object;
+                undef $object;
+                next;
+            }
+        }
+        close $handle;
+
+        return @objects;
+    };
 }
