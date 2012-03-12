bd8f5e37dd23f3ec993078722e6167b0c9726f31
Gonéri Le Bouder <goneri@rulezlan.org>
2012-03-12 10:33:42 +0100

    fixes for Windows

--- tools/createExtLinks.sh
+++ tools/createExtLinks.sh
@@ -6,16 +6,16 @@ for task in Deploy SNMPQuery NetDiscovery ESX; do
     taskLcName=`perl -e" print lc \"$task\""`
     taskFile=$PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task.pm
     taskDir=$PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task
-    if [ -f $taskFile ] && [ ! -e lib/FusionInventory/Agent/Task/$task.pm ]; then
+    if [ -f "$taskFile" ] && [ ! -e lib/FusionInventory/Agent/Task/$task.pm ]; then
         echo "create link for $task"
-        ln -s $PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task.pm lib/FusionInventory/Agent/Task/
-        if [ -d $taskDir ] && [ ! -e lib/FusionInventory/Agent/Task/$task ]; then
-            ln -s $PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task lib/FusionInventory/Agent/Task/
+        ln -s "$PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task.pm" lib/FusionInventory/Agent/Task/
+        if [ -d "$taskDir" ] && [ ! -e lib/FusionInventory/Agent/Task/$task ]; then
+            ln -s "$PWD/../agent-task-$taskLcName/lib/FusionInventory/Agent/Task/$task" lib/FusionInventory/Agent/Task/$task
         fi
     fi
 done
 if [ ! -e lib/FusionInventory/VMware ]; then
-    ln -s $PWD/../agent-task-esx/lib/FusionInventory/VMware lib/FusionInventory/
+    ln -s "$PWD/../agent-task-esx/lib/FusionInventory/VMware" lib/FusionInventory/VMware
 fi
 if [ ! -e lib/FusionInventory/Agent/SNMP.pm ]; then
     ln -s $PWD/../agent-task-netdiscovery/lib/FusionInventory/Agent/SNMP.pm lib/FusionInventory/Agent/SNMP.pm
