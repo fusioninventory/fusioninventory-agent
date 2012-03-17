21e5456bc4b28dbf71481333bcfd59a97bcca376
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2012-03-17 16:14:25 +0100

    drop copyright notice
    the related code is actually in FusionInventory::Agent::Tools::Screen
    package

--- lib/FusionInventory/Agent/Task/Inventory/Input/Generic/Screen.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Generic/Screen.pm
@@ -1,23 +1,5 @@
 package FusionInventory::Agent::Task::Inventory::Input::Generic::Screen;
-#     Copyright (C) 2005 Mandriva
-#     Copyright (C) 2007 Gonéri Le Bouder <goneri@rulezlan.org> 
-#     This program is free software; you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation; either version 2 of the License, or
-#     (at your option) any later version.
 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-
-#     You should have received a copy of the GNU General Public License
-#     along with this program; if not, write to the Free Software
-#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Some part come from Mandriva's (great) monitor-edid
-# http://svn.mandriva.com/cgi-bin/viewvc.cgi/soft/monitor-edid/trunk/
-#
 use strict;
 use warnings;
 
