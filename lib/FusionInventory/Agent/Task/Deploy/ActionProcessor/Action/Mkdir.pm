3eb802c0ffd877f52658718aa3c10daf85dafa98
Gonéri Le Bouder <goneri@rulezlan.org>
2012-03-19 00:18:26 +0100

    avoid a failure if mkdir is done on an existing dir

--- lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Mkdir.pm
+++ lib/FusionInventory/Agent/Task/Deploy/ActionProcessor/Action/Mkdir.pm
@@ -12,11 +12,18 @@ sub do {
     my $msg = [];
     my $status = 1;
     foreach (@{$params->{list}}) {
-        File::Path::mkpath($_);
-        $status = 0 unless -d $_;
-        my $m = "Failed to create $_ directory";
-        push @$msg, $m;
-        $logger->debug($m);
+        my $dir = $_;
+        if (-d $dir) {
+            my $m = "Directory $dir already exists";
+            push @$msg, $m;
+            $logger->debug($m);
+        } else {
+            File::Path::mkpath($dir);
+            $status = 0 unless -d $dir;
+            my $m = "Failed to create $dir directory";
+            push @$msg, $m;
+            $logger->debug($m);
+        }
     }
     return {
     status => $status,
