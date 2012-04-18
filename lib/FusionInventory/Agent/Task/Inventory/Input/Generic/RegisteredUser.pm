08c860b95c236a7c87cb0434e7d1f01cc2aeb66b
Vincent Membré <vincent.membre@normation.com>
2012-04-18 13:11:13 +0200

    clean code

--- lib/FusionInventory/Agent/Task/Inventory/Input/Generic/RegisteredUser.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Generic/RegisteredUser.pm
@@ -29,45 +29,48 @@ sub doInventory {
 
             next unless $line =~ /^(\S+)/;
 
-	    my @splitted_line = split (/:|\n/,$line);
- 
-            my @shadow_line = getshadow(user=>$splitted_line[0],logger  => $logger);    
- 
+            my @splitted_line = split (/:|\n/,$line);
 
-	    my %password_data=( 
-		LAST_CHANGE_DATE => $shadow_line[2],
-		MINIMUM_AGE => $shadow_line[3],
-		MAXIMUM_AGE => $shadow_line[4],
-		WARNING_PERIOD => $shadow_line[5],
-		INACTIVITY => $shadow_line[6]);
+            my @shadow_line = getshadow(
+                user=>$splitted_line[0],
+                logger  => $logger
+                );    
 
-	    my $password = treat_datas(
-		datas => \%password_data
-		);
-	    
-	    my %user_data= ( 
-		NAME => $splitted_line[0],
-		UID => $splitted_line[2],
-		GID => $splitted_line[3],
-		PASSWORD => $password,
-		HOMEDIR => $splitted_line[5],
-		COMMAND_INTERPRETER => $splitted_line[6],
-		EXPIRATION_DATE => $shadow_line[7],
-		REALNAME => $splitted_line[4]
-		);
-	
-	    my $user = treat_datas(
-		datas => \%user_data
-		);
+	    my %password_data= (
+                LAST_CHANGE_DATE => $shadow_line[2],
+                MINIMUM_AGE      => $shadow_line[3],
+                MAXIMUM_AGE      => $shadow_line[4],
+                WARNING_PERIOD   => $shadow_line[5],
+                INACTIVITY       => $shadow_line[6]
+                );
+
+            my $password = treat_datas(
+                datas => \%password_data
+                );
+
+            my %user_data= (
+                NAME                => $splitted_line[0],
+                UID                 => $splitted_line[2],
+                GID                 => $splitted_line[3],
+                PASSWORD            => $password,
+                HOMEDIR             => $splitted_line[5],
+                COMMAND_INTERPRETER => $splitted_line[6],
+                EXPIRATION_DATE     => $shadow_line[7],
+                REALNAME            => $splitted_line[4]
+                );
+
+            my $user = treat_datas(
+                datas => \%user_data
+                );
 
 	    push @users,$user;
 	}
 
-	my $registered = { USER => \@users};
+        my $registered = { USER => \@users};
 
-	$inventory->addEntry(
-	    section => 'REGISTERED_USERS',
-	    entry   => $registered
+        $inventory->addEntry(
+            section => 'REGISTERED_USERS',
+            entry   => $registered
             );
     }
     close $handle;
@@ -82,15 +85,14 @@ sub getshadow{
     my $shadow = getFileHandle(
         logger  => $logger,
         file => '/etc/shadow'
-	);
+        );
 
-
-
-    while(my $line = <$shadow>){
+    while (my $line = <$shadow>) {
 
      @shadow_line = split (/:|\n/,$line);
+
      if ($shadow_line[0] eq $user) {
-	return @shadow_line;
+        return @shadow_line;
      }
     }
 
@@ -98,30 +100,34 @@ sub getshadow{
 }
 
 sub treat_datas {
+
     my (%params) = @_;
     my $target;
     my $datas = $params{datas};
+
     while (my ($key,$value)= each %{$datas}){
-		
-	$target= treat_param(
-	    target => $target,
-	    key => $key,
-	    value => $value
-	    );
+
+        $target= treat_param(
+            target => $target,
+            key => $key,
+            value => $value
+            );
     }
 
     return $target;
-    
 }
+
 sub treat_param{
 
     my (%params) = @_;
     my $target = $params{target};
     my $value = $params{value};
     my $key = $params{key};
-    if(defined $value) {
-	$target->{$key} = $value unless $value eq '';
+
+   if(defined $value) {
+        $target->{$key} = $value unless $value eq '';
     }
+
     return $target;
 }
 
