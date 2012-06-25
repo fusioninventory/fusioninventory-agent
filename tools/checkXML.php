aab8c30637d5360636977e5e5bfa641867d9cf8b
Gonéri Le Bouder <goneri@rulezlan.org>
2012-06-25 20:53:26 +0200

    PHP script to check PHP XML parsing

---
+++ tools/checkXML.php
@@ -0,0 +1,59 @@
+#!/usr/bin/php
+<?php
+
+if (count($argv)<2) {
+  print "Usage: $argv[0] filename.ocs\n";
+  exit(1);
+}
+
+$file = $argv[1];
+
+if (!file_exists($file)) {
+  print "Usage: can't read $file\n";
+  exit(1);
+}
+
+
+$doc = libxml_use_internal_errors(true);
+simplexml_load_file($file,'SimpleXMLElement', LIBXML_NOCDATA);
+$xml = explode("\n", $xmlstr);
+
+if (!$doc) {
+    $errors = libxml_get_errors();
+
+    foreach ($errors as $error) {
+        echo display_xml_error($error, $xml);
+    }
+
+    libxml_clear_errors();
+}
+
+
+function display_xml_error($error, $xml)
+{
+    $return  = $xml[$error->line - 1] . "\n";
+    $return .= str_repeat('-', $error->column) . "^\n";
+
+    switch ($error->level) {
+        case LIBXML_ERR_WARNING:
+            $return .= "Warning $error->code: ";
+            break;
+         case LIBXML_ERR_ERROR:
+            $return .= "Error $error->code: ";
+            break;
+        case LIBXML_ERR_FATAL:
+            $return .= "Fatal Error $error->code: ";
+            break;
+    }
+
+    $return .= trim($error->message) .
+               "\n  Line: $error->line" .
+               "\n  Column: $error->column";
+
+    if ($error->file) {
+        $return .= "\n  File: $error->file";
+    }
+
+    return "$return\n\n--------------------------------------------\n\n";
+}
+
