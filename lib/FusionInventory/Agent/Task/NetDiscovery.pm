c7f9657e7b20d36a12de761f8abf25218682f5a7
Guillaume Rousse <guillomovitch@gmail.com>
2012-07-20 08:48:58 +0200

    document used oids

--- lib/FusionInventory/Agent/Task/NetDiscovery.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery.pm
@@ -607,6 +607,7 @@ sub _scanAddressBySNMP {
             next;
         }
 
+        # SNMPv2-MIB::sysDescr.0
         my $sysdescr = $snmp->get('.1.3.6.1.2.1.1.1.0');
 
         $self->{logger}->debug2(
@@ -660,6 +661,7 @@ sub _scanAddressBySNMP {
         }
 
         $device{AUTHSNMP}     = $credential->{ID};
+        # SNMPv2-MIB::sysName.0
         $device{SNMPHOSTNAME} = $snmp->get('.1.3.6.1.2.1.1.5.0');
         $device{DESCRIPTION}  = $sysdescr if !$device{DESCRIPTION};
 
@@ -703,9 +705,11 @@ sub _getMacAddress {
     } else {
         # use default oids
 
+        # SNMPv2-SMI::mib-2.17.1.1.0
         $macAddress = $snmp->getMacAddress(".1.3.6.1.2.1.17.1.1.0");
 
         if (!$macAddress || $macAddress !~ /^$mac_address_pattern$/) {
+            # IF-MIB::ifPhysAddress
             my $macs = $snmp->walkMacAddresses(".1.3.6.1.2.1.2.2.1.6");
             foreach my $value (values %{$macs}) {
                 next if !$value;
