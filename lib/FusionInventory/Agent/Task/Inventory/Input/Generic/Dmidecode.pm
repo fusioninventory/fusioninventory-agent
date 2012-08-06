9afeae541b5fd4ce62fdcdf42c2ef4fb494d9365
Gonéri Le Bouder <goneri@rulezlan.org>
2012-08-06 13:56:37 +0200

    don't run dmidecode.exe on Win2003
    dmidecode.exe generates segfault on Win2003. We ensure we are on Win2003+
    Windows before the execution.
    
    see: #379
    closes: #1746
    
    Reported on the forum, please use the bug tracker Instead.
    http://forum.fusioninventory.org/viewtopic.php?pid=8889

--- lib/FusionInventory/Agent/Task/Inventory/Input/Generic/Dmidecode.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Generic/Dmidecode.pm
@@ -6,27 +6,12 @@ use warnings;
 use English qw(-no_match_vars);
 use UNIVERSAL::require;
 
-use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Tools::Generic;
 
 sub isEnabled {
 
-    if ($OSNAME eq 'MSWin32') {
-        # don't run dmidecode on Win2003
-        # http://forge.fusioninventory.org/issues/379
-        Win32->require();
-        my @osver = Win32::GetOSVersion();
-        return if
-            $osver[4] == 2 &&
-            $osver[1] == 5 &&
-            $osver[2] == 2;
-    }
+    return unless getDmidecodeInfos();
 
-    return unless canRun('dmidecode');
-
-    my $count = getLinesCount(
-        command => "dmidecode"
-    );
-    return $count > 10;
 }
 
 sub doInventory {}
