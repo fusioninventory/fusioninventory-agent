2b6b669e16fc8093e7943c93ea9ec3261d8f6f2e
Guillaume Rousse <guillomovitch@gmail.com>
2012-08-16 20:13:12 +0200

    unused functions

--- lib/FusionInventory/Agent/Manufacturer.pm
+++ lib/FusionInventory/Agent/Manufacturer.pm
@@ -6,18 +6,6 @@ use warnings;
 use FusionInventory::Agent::Tools::Network;
 use FusionInventory::Agent::Tools::SNMP;
 
-sub setDescription {
-    my ($device, $snmp, $oid) = @_;
-
-    return $snmp->get('.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0');
-}
-
-sub setVendor {
-    my ($snmp) = @_;
-
-    return $snmp->get('.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0');
-}
-
 sub setConnectedDevicesMacAddresses {
     my (%params) = @_;
 
