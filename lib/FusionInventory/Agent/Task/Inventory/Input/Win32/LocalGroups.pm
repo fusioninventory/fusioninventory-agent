272e23659b9c093e84309499371e1699b4cc77f8
Vincent Membré <vincent.membre@normation.com>
2012-10-29 18:17:25 +0100

    win32 V1.0

---
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Win32/LocalGroups.pm
@@ -0,0 +1,55 @@
+package FusionInventory::Agent::Task::Inventory::Input::Win32::LocalGroups;
+
+use strict;
+use warnings;
+
+use constant wbemFlagReturnImmediately => 0x10;
+use constant wbemFlagForwardOnly => 0x20;
+
+use English qw(-no_match_vars);
+use Win32::OLE;
+use Win32::OLE::Variant;
+use Win32::TieRegistry (
+    Delimiter   => '/',
+    ArrayValues => 0,
+    qw/KEY_READ/
+);
+
+Win32::OLE->Option(CP => Win32::OLE::CP_UTF8);
+
+
+use FusionInventory::Agent::Tools::Win32;
+
+my $seen;
+
+sub isEnabled {
+    return 1;
+}
+
+sub doInventory {
+    my (%params) = @_;
+
+    my $inventory = $params{inventory};
+
+    my $WMIService = Win32::OLE->GetObject("winmgmts:\\\\.\\root\\CIMV2")
+        or die "WMI connection failed: " . Win32::OLE->LastError();
+
+    my $processes = $WMIService->ExecQuery(
+        "SELECT * FROM Win32_Group  Where LocalAccount = True", "WQL",
+        wbemFlagReturnImmediately | wbemFlagForwardOnly ## no critic (ProhibitBitwise)
+    );
+
+    foreach my $group (in $processes) {
+
+
+
+      print "Local Account: $group->{LocalAccount}\n";
+      print "Name: $group->{Name}\n";
+      print "SID: $group->{SID}\n";
+
+     
+    }
+
+}
+
+1;
