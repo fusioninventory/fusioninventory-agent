93927088fd6211ada6a2531cb84bc230cc6ed24b
Guillaume Rousse <guillomovitch@gmail.com>
2012-11-07 09:04:25 +0100

    keep module public interface first

--- lib/FusionInventory/Agent/Task/Inventory/Input/Win32/License.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Win32/License.pm
@@ -10,11 +10,31 @@ use Win32::TieRegistry (
     qw/KEY_READ/
 );
 
-
 use FusionInventory::Agent::Tools::Win32;
 
 sub isEnabled {
     return 1;
+}
+
+sub doInventory {
+    my (%params) = @_;
+
+    # TODO: 64/32 bit support
+    my $machKey = $Registry->Open('LMachine', {
+            Access => KEY_READ ## no critic (ProhibitBitwise)
+            }) or $params{logger}->error("Can't open HKEY_LOCAL_MACHINE key: $EXTENDED_OS_ERROR");
+
+    my $office =
+        $machKey->{"SOFTWARE/Microsoft/Office"};
+
+    my @found;
+
+    _scanOffice($office, \@found);
+
+    foreach my $license (@found) {
+        $params{inventory}->addEntry(section => 'LICENSEINFOS', entry => $license);
+    }
+
 }
 
 sub _scanOffice {
@@ -67,26 +87,5 @@ sub _scanOffice {
     }
 }
 
-
-sub doInventory {
-    my (%params) = @_;
-
-    # TODO: 64/32 bit support
-    my $machKey = $Registry->Open('LMachine', {
-            Access => KEY_READ ## no critic (ProhibitBitwise)
-            }) or $params{logger}->error("Can't open HKEY_LOCAL_MACHINE key: $EXTENDED_OS_ERROR");
-
-    my $office =
-        $machKey->{"SOFTWARE/Microsoft/Office"};
-
-    my @found;
-
-    _scanOffice($office, \@found);
-
-    foreach my $license (@found) {
-        $params{inventory}->addEntry(section => 'LICENSEINFOS', entry => $license);
-    }
-
-}
 
 1;
