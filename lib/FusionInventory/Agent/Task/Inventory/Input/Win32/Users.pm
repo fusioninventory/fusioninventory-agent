8563495d7b62104f0bd9437c2097c582ac4e735d
Guillaume Rousse <guillomovitch@gmail.com>
2012-11-16 17:46:07 +0100

    isolate code in specific functions

--- lib/FusionInventory/Agent/Task/Inventory/Input/Win32/Users.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Input/Win32/Users.pm
@@ -20,8 +20,6 @@ Win32::OLE->Option(CP => Win32::OLE::CP_UTF8);
 
 use FusionInventory::Agent::Tools::Win32;
 
-my $seen;
-
 sub isEnabled {
     return 1;
 }
@@ -39,57 +37,16 @@ sub doInventory {
         );
     }
 
-    my $WMIService = Win32::OLE->GetObject("winmgmts:\\\\.\\root\\CIMV2")
-        or die "WMI connection failed: " . Win32::OLE->LastError();
-
-    my $processes = $WMIService->ExecQuery(
-        "SELECT * FROM Win32_Process", "WQL",
-        wbemFlagReturnImmediately | wbemFlagForwardOnly ## no critic (ProhibitBitwise)
-    );
-
-    foreach my $process (in $processes) {
-        next unless
-            $process->{ExecutablePath} &&
-            $process->{ExecutablePath} =~ /\\Explorer\.exe$/i;
-
-        ## no critic (ProhibitBitwise)
-        my $name = Variant(VT_BYREF | VT_BSTR, '');
-        my $domain = Variant(VT_BYREF | VT_BSTR, '');
-
-        $process->GetOwner($name, $domain);
-
-        my $user = {
-            LOGIN => $name->Get(),
-            DOMAIN => $domain->Get()
-        };
-
-        utf8::upgrade($user->{LOGIN});
-        utf8::upgrade($user->{DOMAIN});
-
-        next if $seen->{$user->{LOGIN}}++;
-
+    foreach my $user (_getLoggedUsers(logger => $logger)) {
         $inventory->addEntry(
             section => 'USERS',
             entry   => $user
         );
     }
 
-    my $machKey = $Registry->Open('LMachine', {
-        Access => KEY_READ
-    }) or die "Can't open HKEY_LOCAL_MACHINE key: $EXTENDED_OS_ERROR";
-
-    foreach my $key (
-        "SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/DefaultUserName",
-        "SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/LastLoggedOnUser"
-    ) {
-        my $user = encodeFromRegistry($machKey->{$key});
-        next unless $user;
-        $user =~ s,.*\\,,;
-        $inventory->setHardware({
-           LASTLOGGEDUSER => $user
-        });
-    }
-
+    $inventory->setHardware(
+        _getLastUser(logger => $logger)
+    );
 }
 
 sub _getLocalUsers {
@@ -110,6 +67,64 @@ sub _getLocalUsers {
     }
 
     return @users;
+}
+
+sub _getLoggedUsers {
+
+    my $WMIService = Win32::OLE->GetObject("winmgmts:\\\\.\\root\\CIMV2")
+        or die "WMI connection failed: " . Win32::OLE->LastError();
+
+    my $processes = $WMIService->ExecQuery(
+        "SELECT * FROM Win32_Process", "WQL",
+        wbemFlagReturnImmediately | wbemFlagForwardOnly ## no critic (ProhibitBitwise)
+    );
+
+    my @users;
+    my $seen;
+
+    foreach my $process (in $processes) {
+        next unless
+            $process->{ExecutablePath} &&
+            $process->{ExecutablePath} =~ /\\Explorer\.exe$/i;
+
+        ## no critic (ProhibitBitwise)
+        my $name = Variant(VT_BYREF | VT_BSTR, '');
+        my $domain = Variant(VT_BYREF | VT_BSTR, '');
+
+        $process->GetOwner($name, $domain);
+
+        my $user = {
+            LOGIN  => $name->Get(),
+            DOMAIN => $domain->Get()
+        };
+
+        utf8::upgrade($user->{LOGIN});
+        utf8::upgrade($user->{DOMAIN});
+
+        next if $seen->{$user->{LOGIN}}++;
+    }
+
+    return @users;
+}
+
+sub _getLastUser {
+
+    my $machKey = $Registry->Open('LMachine', {
+        Access => KEY_READ
+    }) or die "Can't open HKEY_LOCAL_MACHINE key: $EXTENDED_OS_ERROR";
+
+    my $user;
+
+    foreach my $key (
+        "SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/DefaultUserName",
+        "SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/LastLoggedOnUser"
+    ) {
+        $user = encodeFromRegistry($machKey->{$key});
+        next unless $user;
+        $user =~ s,.*\\,,;
+    }
+
+    return $user;
 }
 
 1;
