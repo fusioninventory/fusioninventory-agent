321cd887c4ef35570ad8aebde6e89b43408da98b
Guillaume Rousse <guillomovitch@gmail.com>
2013-02-27 18:57:51 +0100

    not needed anymore

--- lib/FusionInventory/Agent/Tools/Solaris.pm
+++ lib/FusionInventory/Agent/Tools/Solaris.pm
@@ -4,15 +4,6 @@ use strict;
 use warnings;
 use base 'Exporter';
 
-use constant SOLARIS_UNKNOWN      => 0;
-use constant SOLARIS_FIRE         => 1;
-use constant SOLARIS_FIRE_V       => 2;
-use constant SOLARIS_FIRE_T       => 3;
-use constant SOLARIS_ENTERPRISE_T => 4;
-use constant SOLARIS_ENTERPRISE   => 5;
-use constant SOLARIS_I86PC        => 6;
-use constant SOLARIS_CONTAINER    => 7;
-
 use English qw(-no_match_vars);
 
 use FusionInventory::Agent::Tools;
@@ -20,73 +11,16 @@ use Memoize;
 
 our @EXPORT = qw(
     getZone
-    getModel
-    getClass
     getPrtconfInfos
     getPrtdiagInfos
-    SOLARIS_UNKNOWN
-    SOLARIS_FIRE
-    SOLARIS_FIRE_V
-    SOLARIS_FIRE_T
-    SOLARIS_ENTERPRISE_T
-    SOLARIS_ENTERPRISE
-    SOLARIS_I86PC
-    SOLARIS_CONTAINER
 );
 
 memoize('getZone');
-memoize('getModel');
-memoize('getClass');
 
 sub getZone {
     return canRun('zonename') ?
         getFirstLine(command => 'zonename') : # actual zone name
         'global';                             # outside zone name
-}
-
-sub getModel {
-    return getZone() eq 'global' ?
-        getFirstLine(command => 'uname -i') :
-        'Solaris Containers';
-}
-
-sub getClass {
-    my $model = getModel();
-
-    if ($model =~ /SUNW,Sun-Fire-\d/ || $model =~ /SUNW,Sun-Fire-V490/) {
-        return SOLARIS_FIRE;
-    }
-
-    if (
-        $model =~ /SUNW,Sun-Fire-V/ or
-        $model =~ /SUNW,Netra-T/    or
-        $model =~ /SUNW,Ultra-250/
-    ) {
-        return SOLARIS_FIRE_V;
-    }
-
-    if (
-        $model =~ /SUNW,Sun-Fire-T\d/ or
-        $model =~ /SUNW,T\d/
-    ) {
-        return SOLARIS_FIRE_T;
-    }
-
-    if ($model =~ /SUNW,SPARC-Enterprise-T\d/) {
-        return SOLARIS_ENTERPRISE_T;
-    }
-    if ($model =~ /SUNW,SPARC-Enterprise/) {
-        return SOLARIS_ENTERPRISE;
-    }
-    if ($model eq "i86pc") {
-        return SOLARIS_I86PC;
-    }
-    if ($model =~ /Solaris Containers/) {
-        return SOLARIS_CONTAINER;
-    }
-
-    # unknown class
-    return SOLARIS_UNKNOWN;
 }
 
 sub getPrtconfInfos {
