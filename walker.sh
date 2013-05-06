e335cc43a2590337eb2e4e49840e43e82ab44ddc
Gonéri Le Bouder <goneri@rulezlan.org>
2013-05-06 07:02:09 +0200

    don't inject empty file

--- walker.sh
+++ walker.sh
@@ -3,5 +3,7 @@
 for walk in `locate *.walk`; do
   echo processing $walk
   ./bin/fusioninventory-netinventory --models-dir /home/goneri/models+dev-20130501-1400 --file $walk > /tmp/myxml
-  ./bin/fusioninventory-injector -f /tmp/myxml -u http://localhost/~goneri/glpigit/plugins/fusioninventory/ -v
+  if [ -s "/tmp/myxml" ]; then
+      ./bin/fusioninventory-injector -f /tmp/myxml -u http://localhost/~goneri/glpigit/plugins/fusioninventory/ -v
+  fi
 done
