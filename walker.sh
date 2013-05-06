e48a6705e11da020c1426b7dced18d0fe5410671
Gonéri Le Bouder <goneri@rulezlan.org>
2013-05-06 20:14:07 +0200

    use Carp::Always::Color

--- walker.sh
+++ walker.sh
@@ -2,7 +2,7 @@
 
 for walk in `locate *.walk`; do
   echo processing $walk
-  ./bin/fusioninventory-netinventory --models-dir /home/goneri/models+dev-20130501-1400 --file $walk > /tmp/myxml
+  perl -MCarp::Always::Color ./bin/fusioninventory-netinventory --models-dir /home/goneri/models+dev-20130501-1400 --file $walk > /tmp/myxml
   if [ -s "/tmp/myxml" ]; then
       ./bin/fusioninventory-injector -f /tmp/myxml -u http://localhost/~goneri/glpigit/plugins/fusioninventory/ -v
   fi
