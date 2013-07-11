fusioninventory/fusioninventory-agent

commit 252a52bf24af77b8b046bcac6112e13cfa8d46e1

add a missing alt2canonical()

lib/FusionInventory/Agent/Manufacturer.pm
@@ -80,9 +80,9 @@ sub setConnectedDevicesUsingCDP {
             SYSDESCR => $results->{cdpCacheVersion}->{
                 $walks->{cdpCacheVersion}->{OID} . "." . $port_number
             },
-            SYSNAME  => $results->{cdpCacheDeviceId}->{
+            SYSNAME  => alt2canonical($results->{cdpCacheDeviceId}->{
                 $walks->{cdpCacheDeviceId}->{OID} . "." . $port_number
-            },
+            }),
             MODEL => $results->{cdpCachePlatform}->{
                 $walks->{cdpCachePlatform}->{OID} . "." . $port_number
             }

commit 78df01bee01306257fdc9c04f1e68d0f455592a6

CDP: deal with special hostname SIP$MAC
this patch also fix a regression introduce by 252a52bf24af77b8b046.

lib/FusionInventory/Agent/Manufacturer.pm
@@ -72,17 +72,22 @@ sub setConnectedDevicesUsingCDP {
             getElement($oid, -2) . "." .
             getElement($oid, -1);
 
+        my $mac;
+        my $sysname = $results->{cdpCacheDeviceId}->{$walks->{cdpCacheDeviceId}->{OID} . "." . $port_number};
+        if ($sysname =~ /^SIP([A-F0-9a-f]*)$/) {
+            $mac = alt2canonical("0x".$1);
+        }
+
         my $connection = {
             IP      => $ip,
+            MAC     => $mac,
             IFDESCR => $results->{cdpCacheDevicePort}->{
                 $walks->{cdpCacheDevicePort}->{OID} . "." . $port_number
             },
             SYSDESCR => $results->{cdpCacheVersion}->{
                 $walks->{cdpCacheVersion}->{OID} . "." . $port_number
             },
-            SYSNAME  => alt2canonical($results->{cdpCacheDeviceId}->{
-                $walks->{cdpCacheDeviceId}->{OID} . "." . $port_number
-            }),
+            SYSNAME  => $sysname,
             MODEL => $results->{cdpCachePlatform}->{
                 $walks->{cdpCachePlatform}->{OID} . "." . $port_number
             }

t/agent/manufacturer.t
@@ -45,6 +45,7 @@ my @connected_devices_tests = (
                 CONNECTIONS => {
                     CONNECTION => {
                         IP       => '192.168.20.139',
+                        MAC      => 'E0:5F:B9:81:A7:A7',
                         IFDESCR  => 'Port 1',
                         SYSDESCR => '7.4.9c',
                         SYSNAME  => 'SIPE05FB981A7A7',