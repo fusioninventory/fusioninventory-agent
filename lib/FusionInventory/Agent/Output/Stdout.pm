4e346a043391a7845cb801a939dd956d6dcbf30a
Guillaume Rousse <guillomovitch@zarb.org>
2013-09-03 22:55:12 +0200

    factorize output handler code

---
+++ lib/FusionInventory/Agent/Output/Stdout.pm
@@ -0,0 +1,24 @@
+package FusionInventory::Agent::Output::Stdout;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, %params) = @_;
+
+    return bless {
+        verbose => $params{verbose}
+    }, $class;
+}
+
+sub send {
+    my ($self, %params) = @_;
+
+    # don't display control message by default
+    return unless $self->{verbose}
+        or $params{message}->{h}->{CONTENT}->{DEVICE};
+
+    print $params{message}->getContent();
+}
+
+1;
