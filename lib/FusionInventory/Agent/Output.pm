416a0cfcabb64f1f551a78917df22a018e8c841f
Guillaume Rousse <guillomovitch@zarb.org>
2013-09-04 00:05:24 +0200

    add server target type

--- lib/FusionInventory/Agent/Output.pm
+++ lib/FusionInventory/Agent/Output.pm
@@ -8,18 +8,15 @@ use UNIVERSAL::require;
 sub create {
     my ($class, %params) = @_;
 
-    if ($params{target}) {
+    if ($params{target} && $params{target} =~ m{^https?://}) {
+        FusionInventory::Agent::Output::Server->require();
+        return FusionInventory::Agent::Output::Server->new(%params);
+    } elsif ($params{target}) {
         FusionInventory::Agent::Output::Directory->require();
-        return FusionInventory::Agent::Output::Directory->new(
-            path    => $params{target},
-            task    => $params{task},
-            verbose => $params{verbose}
-        );
+        return FusionInventory::Agent::Output::Directory->new(%params);
     } else {
         FusionInventory::Agent::Output::Stdout->require();
-        return FusionInventory::Agent::Output::Stdout->new(
-            verbose => $params{verbose}
-        );
+        return FusionInventory::Agent::Output::Stdout->new(%params);
     }
 }
 
---
+++ lib/FusionInventory/Agent/Output/Server.pm
@@ -0,0 +1,37 @@
+package FusionInventory::Agent::Output::Server;
+
+use strict;
+use warnings;
+
+use FusionInventory::Agent::HTTP::Client::OCS;
+
+sub new {
+    my ($class, %params) = @_;
+
+    die "missing target parameter" unless $params{target};
+
+    my $client = FusionInventory::Agent::HTTP::Client::OCS->new(
+        user         => $params{user},
+        password     => $params{password},
+        proxy        => $params{proxy},
+        ca_cert_file => $params{ca_cert_file},
+        ca_cert_dir  => $params{ca_cert_dir},
+        no_ssl_check => $params{no_ssl_check},
+    );
+
+    return bless {
+        url    => $params{target},
+        client => $client
+    }, $class;
+}
+
+sub send {
+    my ($self, %params) = @_;
+
+    $self->{client}->send(
+        url     => $self->{url},
+        message => $params{message}
+    );
+}
+
+1;
