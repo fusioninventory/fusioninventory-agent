9e1bb847d67971ea1e6d5e817ec02def83ff571b
Guillaume Rousse <guillomovitch@zarb.org>
2014-01-08 23:20:33 +0100

    prevent warning for malformed group file
    see: #2384

--- lib/FusionInventory/Agent/Task/Inventory/Generic/Users.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Generic/Users.pm
@@ -108,6 +108,8 @@ sub _getLocalGroups {
         chomp $line;
         my ($name, undef, $gid, $members) = split(/:/, $line);
 
+        # prevent warning for malformed group file (#2384)
+        next unless $members
         my @members = split(/,/, $members);
 
         push @groups, {
