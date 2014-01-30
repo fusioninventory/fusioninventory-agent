ef30c6145add308b9a3861a57ca030b88bab0918
Guillaume Rousse <guillomovitch@zarb.org>
2014-01-30 13:26:22 +0100

    simplification
    The task->new(), task->configure(), task->run() model is simpler to
    read.

--- lib/FusionInventory/Agent.pm
+++ lib/FusionInventory/Agent.pm
@@ -265,20 +265,6 @@ sub runRequestedTasks {
     }
 }
 
-sub runSingleTask {
-    my ($self, %params) = @_;
-
-    $params{task}->run(
-        user         => $self->{config}->{user},
-        password     => $self->{config}->{password},
-        proxy        => $self->{config}->{proxy},
-        ca_cert_file => $self->{config}->{'ca-cert-file'},
-        ca_cert_dir  => $self->{config}->{'ca-cert-dir'},
-        no_ssl_check => $self->{config}->{'no-ssl-check'},
-        recipient    => $params{recipient},
-    );
-}
-
 sub _runController {
     my ($self, $controller) = @_;
 
