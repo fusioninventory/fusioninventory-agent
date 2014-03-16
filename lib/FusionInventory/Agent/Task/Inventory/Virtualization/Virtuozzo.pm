1c43a544a1e75f2c5c6e38e9121ad7eee8d0c6ba
Guillaume Rousse <guillomovitch@zarb.org>
2014-03-16 15:31:27 +0100

    simpler and more consistent with Qemu case
    Moreover, it avoids a warning if virsh is not available.

--- lib/FusionInventory/Agent/Task/Inventory/Virtualization/Virtuozzo.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Virtualization/Virtuozzo.pm
@@ -7,11 +7,7 @@ use FusionInventory::Agent::Tools;
 
 sub isEnabled {
     # Avoid duplicated entry with libvirt
-    return if getFirstMatch(
-        command => "virsh uri",
-        pattern => "openvz:///system"
-    );
-
+    return if canRun('virsh');
 
     return canRun('vzlist');
 }
