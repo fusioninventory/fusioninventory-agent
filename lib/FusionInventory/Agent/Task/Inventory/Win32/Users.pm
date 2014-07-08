4babf3a526844fac7fb239ec651dddb6151bdda6
Guillaume Rousse <guillomovitch@zarb.org>
2014-07-08 14:09:20 +0200

    fix typo

--- lib/FusionInventory/Agent/Task/Inventory/Win32/Users.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/Users.pm
@@ -156,7 +156,7 @@ sub _getLastUser {
     my $user;
 
     foreach my $key (
-        "SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/LastLoggedOnUser"
+        "SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/LastLoggedOnUser",
         "SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/DefaultUserName",
     ) {
         $user = encodeFromRegistry($machKey->{$key});
