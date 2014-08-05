0ba671e8aa4291b7331b487651fa61c02bd9a01f
Guillaume Rousse <guillomovitch@zarb.org>
2014-08-05 12:51:25 +0200

    more flexibility in message filtering
    new 'verbosity' parameter replaces 'debug' one, and allows finer
    selection.

--- lib/FusionInventory/Agent.pm
+++ lib/FusionInventory/Agent.pm
@@ -63,10 +63,14 @@ sub init {
     );
     $self->{config} = $config;
 
+    my $verbosity = $config->{debug} && $config->{debug} == 1 ? LOG_DEBUG  :
+                    $config->{debug} && $config->{debug} == 2 ? LOG_DEBUG2 :
+                                                                LOG_INFO   ;
+
     my $logger = FusionInventory::Agent::Logger->new(
         config   => $config,
         backends => $config->{logger},
-        debug    => $config->{debug}
+        level    => $verbosity
     );
     $self->{logger} = $logger;
 
