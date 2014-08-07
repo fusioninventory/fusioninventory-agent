96ca5e858a1f5c657aa4a1cc5b617bc521a33c22
Guillaume Rousse <guillomovitch@zarb.org>
2014-08-07 19:02:14 +0200

    use -r operator directly

--- lib/FusionInventory/Agent/Task/Inventory/Linux/Rudder.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Linux/Rudder.pm
@@ -7,10 +7,7 @@ use FusionInventory::Agent::Tools;
 use FusionInventory::Agent::Tools::Unix;
 
 sub isEnabled {
-
-    #If you can read /opt/rudder/etc/uuid.hive then you can do that inventory
-    return
-        canRead("/opt/rudder/etc/uuid.hive");
+    return -r '/opt/rudder/etc/uuid.hive';
 }
 
 sub doInventory {
