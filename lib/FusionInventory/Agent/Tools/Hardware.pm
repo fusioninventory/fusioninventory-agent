9aca22244171cc9e4b3a897f0c9e00aca13ae442
Guillaume Rousse <guillomovitch@gmail.com>
2014-10-21 20:34:11 +0200

    decode hex-encoded strings in CDP info
    see: #2670,#2746,#2754

--- lib/FusionInventory/Agent/Tools/Hardware.pm
+++ lib/FusionInventory/Agent/Tools/Hardware.pm
@@ -1233,8 +1233,8 @@ sub _getCDPInfo {
 
         my $connection = {
             IP       => $ip,
-            SYSDESCR => $cdpCacheVersion->{$suffix},
-            MODEL    => $cdpCachePlatform->{$suffix}
+            SYSDESCR => hex2char($cdpCacheVersion->{$suffix}),
+            MODEL    => hex2char($cdpCachePlatform->{$suffix})
         };
 
         # cdpCacheDevicePort is either a port number or a port description
