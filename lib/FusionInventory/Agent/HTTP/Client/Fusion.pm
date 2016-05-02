c4ee5bff99cfc3d1c72183bd7a850b1938216b81
tom <tlornet@teclib.com>
2016-05-02 16:28:49 +0200

    revert the reverted... oops...

--- lib/FusionInventory/Agent/HTTP/Client/Fusion.pm
+++ lib/FusionInventory/Agent/HTTP/Client/Fusion.pm
@@ -97,7 +97,7 @@ sub send { ## no critic (ProhibitBuiltinHomonyms)
 
     my $response = $self->request($request);
 
-    return unless $response;
+    return unless $response->is_success();
 
     $self->{_cookies}->extract_cookies($response);
 
