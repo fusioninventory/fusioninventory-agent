ad20f30ca6c3c21834b36c6f07e990aaf6163c45
pzeidler-dlr <petra.zeidler@dlr.de>
2016-07-26 12:42:52 +0200

    recognize Solaris 11 and later and use its own packaging tool

--- lib/FusionInventory/Agent/Task/Inventory/Solaris/Softwares.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Solaris/Softwares.pm
@@ -4,13 +4,21 @@ use strict;
 use warnings;
 
 use FusionInventory::Agent::Tools;
+use FusionInventory::Agent::Tools::Solaris;
 
 sub isEnabled {
     my (%params) = @_;
 
-    return
-        !$params{no_category}->{software} &&
-        canRun('pkginfo');
+    my $info = getReleaseInfo();
+    if ($info->{version} > 10) {
+    	return
+    	    !$params{no_category}->{software} &&
+    	    canRun('pkg');
+    } else {
+    	return
+    	    !$params{no_category}->{software} &&
+    	    canRun('pkginfo');
+    }
 }
 
 sub doInventory {
@@ -19,30 +27,59 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $handle = getFileHandle(
-        command => 'pkginfo -l',
-        logger  => $logger,
-    );
+    my $handle;
+    my $info = getReleaseInfo();
+    if ($info->{version} > 10) {
+    	$handle = getFileHandle(
+    	    command => 'pkg info',
+    	    logger  => $logger,
+    	);
+    } else {
+    	$handle = getFileHandle(
+    	    command => 'pkginfo -l',
+    	    logger  => $logger,
+    	);
+    }
 
     return unless $handle;
 
     my $software;
-    while (my $line = <$handle>) {
-        if ($line =~ /^\s*$/) {
-            $inventory->addEntry(
-                section => 'SOFTWARES',
-                entry   =>  $software
-            );
-            undef $software;
-        } elsif ($line =~ /PKGINST:\s+(.+)/) {
-            $software->{NAME} = $1;
-        } elsif ($line =~ /VERSION:\s+(.+)/) {
-            $software->{VERSION} = $1;
-        } elsif ($line =~ /VENDOR:\s+(.+)/) {
-            $software->{PUBLISHER} = $1;
-        } elsif ($line =~  /DESC:\s+(.+)/) {
-            $software->{COMMENTS} = $1;
-        }
+    if ($info->{version} > 10) {
+    	while (my $line = <$handle>) {
+    	    if ($line =~ /^\s*$/) {
+        		$inventory->addEntry(
+        		    section => 'SOFTWARES',
+        		    entry   =>  $software
+        		);
+    		    undef $software;
+    	    } elsif ($line =~ /Name:\s+(.+)/) {
+    		    $software->{NAME} = $1;
+    	    } elsif ($line =~ /FMRI:\s+.+\@(.+)/) {
+    		    $software->{VERSION} = $1;
+    	    } elsif ($line =~ /Publisher:\s+(.+)/) {
+    		    $software->{PUBLISHER} = $1;
+    	    } elsif ($line =~  /Summary:\s+(.+)/) {
+    		    $software->{COMMENTS} = $1;
+    	    }
+    	}
+    } else {
+    	while (my $line = <$handle>) {
+    	    if ($line =~ /^\s*$/) {
+        		$inventory->addEntry(
+        		    section => 'SOFTWARES',
+        		    entry   =>  $software
+        		);
+        		undef $software;
+    	    } elsif ($line =~ /PKGINST:\s+(.+)/) {
+        		$software->{NAME} = $1;
+    	    } elsif ($line =~ /VERSION:\s+(.+)/) {
+        		$software->{VERSION} = $1;
+    	    } elsif ($line =~ /VENDOR:\s+(.+)/) {
+        		$software->{PUBLISHER} = $1;
+    	    } elsif ($line =~  /DESC:\s+(.+)/) {
+        		$software->{COMMENTS} = $1;
+    	    }
+    	}
     }
 
     close $handle;
