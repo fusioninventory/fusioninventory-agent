f91605cde89fc2d76c2c612dffcbddadab706316
Petra Zeidler, DLR <petra.zeidler@dlr.de>
2016-07-27 18:30:15 +0200

    make this module fit for testing

--- lib/FusionInventory/Agent/Task/Inventory/Solaris/Softwares.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Solaris/Softwares.pm
@@ -19,65 +19,69 @@ sub doInventory {
     my $inventory = $params{inventory};
     my $logger    = $params{logger};
 
-    my $handle;
-    
-    my $usepkg = 0;
-    $usepkg = 1 if (canRun('pkg'));
-    
-    if ($usepkg) {
-    	$handle = getFileHandle(
-    	    command => 'pkg info',
-    	    logger  => $logger,
-    	);
-    } else {
-    	$handle = getFileHandle(
-    	    command => 'pkginfo -l',
-    	    logger  => $logger,
-    	);
+    my $pkgs = _parse_pkgs(logger => $logger);
+    return unless $pkgs;
+
+    foreach my $pkg (@$pkgs) {
+        $inventory->addEntry(
+            section => 'SOFTWARES',
+            entry   =>  $pkg
+        );
+    }
+}
+
+sub _parse_pkgs {
+    my (%params) = @_;
+
+    if (!defined $params{command}) {
+        if (canRun('pkg')) {
+            $params{command} = 'pkg info';
+        } else {
+            $params{command} = 'pkginfo -l';
+        }
     }
 
+    my $handle = getFileHandle(%params);
     return unless $handle;
 
+    my @softwares;
     my $software;
-    if ($usepkg) {
-    	while (my $line = <$handle>) {
-    	    if ($line =~ /^\s*$/) {
-        		$inventory->addEntry(
-        		    section => 'SOFTWARES',
-        		    entry   =>  $software
-        		);
-    		    undef $software;
-    	    } elsif ($line =~ /Name:\s+(.+)/) {
-    		    $software->{NAME} = $1;
-    	    } elsif ($line =~ /FMRI:\s+.+\@(.+)/) {
-    		    $software->{VERSION} = $1;
-    	    } elsif ($line =~ /Publisher:\s+(.+)/) {
-    		    $software->{PUBLISHER} = $1;
-    	    } elsif ($line =~  /Summary:\s+(.+)/) {
-    		    $software->{COMMENTS} = $1;
-    	    }
-    	}
+    if ($params{command} =~ /pkg info/) {
+        while (my $line = <$handle>) {
+            if ($line =~ /^\s*$/) {
+                push @softwares, $software if $software;
+                undef $software;
+            } elsif ($line =~ /Name:\s+(.+)/) {
+                $software->{NAME} = $1;
+            } elsif ($line =~ /FMRI:\s+.+\@(.+)/) {
+                $software->{VERSION} = $1;
+            } elsif ($line =~ /Publisher:\s+(.+)/) {
+                $software->{PUBLISHER} = $1;
+            } elsif ($line =~  /Summary:\s+(.+)/) {
+                $software->{COMMENTS} = $1;
+            }
+        }
     } else {
-    	while (my $line = <$handle>) {
-    	    if ($line =~ /^\s*$/) {
-        		$inventory->addEntry(
-        		    section => 'SOFTWARES',
-        		    entry   =>  $software
-        		);
-        		undef $software;
-    	    } elsif ($line =~ /PKGINST:\s+(.+)/) {
-        		$software->{NAME} = $1;
-    	    } elsif ($line =~ /VERSION:\s+(.+)/) {
-        		$software->{VERSION} = $1;
-    	    } elsif ($line =~ /VENDOR:\s+(.+)/) {
-        		$software->{PUBLISHER} = $1;
-    	    } elsif ($line =~  /DESC:\s+(.+)/) {
-        		$software->{COMMENTS} = $1;
-    	    }
-    	}
+        while (my $line = <$handle>) {
+            if ($line =~ /^\s*$/) {
+                push @softwares, $software if $software;
+                undef $software;
+            } elsif ($line =~ /PKGINST:\s+(.+)/) {
+                $software->{NAME} = $1;
+            } elsif ($line =~ /VERSION:\s+(.+)/) {
+                $software->{VERSION} = $1;
+            } elsif ($line =~ /VENDOR:\s+(.+)/) {
+                $software->{PUBLISHER} = $1;
+            } elsif ($line =~  /DESC:\s+(.+)/) {
+                $software->{COMMENTS} = $1;
+            }
+        }
     }
 
+    push @softwares, $software if $software;
+
     close $handle;
+    return \@softwares;
 }
 
 1;
