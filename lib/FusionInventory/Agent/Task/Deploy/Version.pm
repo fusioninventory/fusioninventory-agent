9317a3d6bd26beba31d3830c3555eb1703225227
Guillaume Bougard <gbougard@teclib.com>
2017-05-30 16:10:45 +0200

    Bump version to 2.3.20 Bump Inventory, Deploy and WakeOnLan tasks version Updated Changes

--- lib/FusionInventory/Agent/Task/Deploy/Version.pm
+++ lib/FusionInventory/Agent/Task/Deploy/Version.pm
@@ -3,6 +3,6 @@ package FusionInventory::Agent::Task::Deploy::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "2.2";
+use constant VERSION => "2.3";
 
 1;
--- lib/FusionInventory/Agent/Task/Inventory/Version.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Version.pm
@@ -3,6 +3,6 @@ package FusionInventory::Agent::Task::Inventory::Version;
 use strict;
 use warnings;
 
-use constant VERSION => "1.1";
+use constant VERSION => "1.2";
 
 1;
