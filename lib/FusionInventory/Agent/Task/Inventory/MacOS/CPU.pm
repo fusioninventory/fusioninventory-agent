b3c445887f2fbc89fc4c4902f8ad991efd6125f0
evg54 <sadeless@gmail.com>
2017-06-02 01:43:36 +0700

    [MacOSX] Update CPU.pm (#304)
    Add discovery CPU type from sysctl.
    Fix unittest for updated CPU name

--- lib/FusionInventory/Agent/Task/Inventory/MacOS/CPU.pm
+++ lib/FusionInventory/Agent/Task/Inventory/MacOS/CPU.pm
@@ -53,7 +53,8 @@ sub _getCpus {
     }
     close $handle;
 
-    my $type  = $sysprofile_info->{'Processor Name'} ||
+    my $type  = $sysctl_info->{'machdep.cpu.brand_string'} || 
+                $sysprofile_info->{'Processor Name'} ||
                 $sysprofile_info->{'CPU Type'};
     my $procs = $sysprofile_info->{'Number Of Processors'} ||
                 $sysprofile_info->{'Number Of CPUs'}       ||
