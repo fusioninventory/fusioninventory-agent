46ea2a17c48413cdc15d510ef8a732ad34b593db
Guillaume Bougard <gbougard@teclib.com>
2017-06-27 18:35:01 +0200

    Also compute libdir in setup This permits to register and use agent as win32 service directly from sources by just running 'perl bin/fusioninventory-win32-service --register'

--- lib/setup.pm
+++ lib/setup.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use base qw(Exporter);
 
+use File::Spec;
+
 our @EXPORT = ('%setup');
 
 our %setup = (
@@ -12,5 +14,10 @@ our %setup = (
     libdir  => './lib',
     vardir  => './var',
 );
+
+# Compute directly libdir from this setup file as it should be installed
+# in expected directory
+$setup{libdir} = File::Spec->rel2abs('..', __FILE__)
+    unless ($setup{libdir} && File::Spec->file_name_is_absolute($setup{libdir}));
 
 1;
