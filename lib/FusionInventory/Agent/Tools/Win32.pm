9f013aff263e5ec2e26f1535c3968aee778faef6
Guillaume Bougard <gbougard@teclib.com>
2017-10-04 14:19:27 +0200

    Clean comment

--- lib/FusionInventory/Agent/Tools/Win32.pm
+++ lib/FusionInventory/Agent/Tools/Win32.pm
@@ -1327,7 +1327,7 @@ sub getWMIService {
 
         my $connected = _call_win32_ole_dependent_api($win32_ole_dependent_api);
 
-        # Only set $wmiService as connected status in main thread only if worker is active
+        # Only set $wmiService as connected status in main thread if worker is active
         $wmiService = $connected if (defined($worker));
     }
 
