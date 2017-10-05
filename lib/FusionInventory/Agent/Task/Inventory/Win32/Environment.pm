d33bfd5eb7d9b148ac8114f14ca9f04143008d8e
Guillaume Bougard <gbougard@teclib.com>
2017-10-05 19:57:29 +0200

    Revert Win32:Environment to limit core changes

--- lib/FusionInventory/Agent/Task/Inventory/Win32/Environment.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/Environment.pm
@@ -3,8 +3,6 @@ package FusionInventory::Agent::Task::Inventory::Win32::Environment;
 use strict;
 use warnings;
 
-use Storable 'dclone';
-
 use FusionInventory::Agent::Tools::Win32;
 
 sub isEnabled {
@@ -17,37 +15,22 @@ sub doInventory {
     my (%params) = @_;
 
     my $inventory = $params{inventory};
-    my $wmiParams = {};
-    $wmiParams->{WMIService} = dclone ($params{inventory}->{WMIService}) if $params{inventory}->{WMIService};
-    my @envVars = getEnvironmentValues(%params, %$wmiParams);
-    foreach my $envVar (@envVars) {
-        $inventory->addEntry(
-            section => 'ENVS',
-            entry   => $envVar
-        );
-    }
-}
 
-sub getEnvironmentValues {
-    my (%params) = @_;
-
-    my @envVars = ();
     foreach my $object (getWMIObjects(
-        %params,
         class      => 'Win32_Environment',
         properties => [ qw/SystemVariable Name VariableValue/ ]
     )) {
 
         next unless $object->{SystemVariable};
 
-        my $envVar = {
-            KEY => $object->{Name},
-            VAL => $object->{VariableValue}
-        };
-        push @envVars, $envVar;
+        $inventory->addEntry(
+            section => 'ENVS',
+            entry   => {
+                KEY => $object->{Name},
+                VAL => $object->{VariableValue}
+            }
+        );
     }
-
-    return @envVars;
 }
 
 1;
