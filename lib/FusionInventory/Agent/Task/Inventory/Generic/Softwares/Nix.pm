402fb16d04475ced385a950f82f9bac797bcd36d
phile314-fh <phile314-fh@users.noreply.github.com>
2017-11-15 14:02:31 +0100

    Add NixOS support for Software inventiory task (#397)

---
+++ lib/FusionInventory/Agent/Task/Inventory/Generic/Softwares/Nix.pm
@@ -0,0 +1,58 @@
+package FusionInventory::Agent::Task::Inventory::Generic::Softwares::Nix;
+
+use strict;
+use warnings;
+
+use FusionInventory::Agent::Tools;
+
+sub isEnabled {
+    return canRun('nix-store');
+}
+
+sub doInventory {
+    my (%params) = @_;
+
+    my $inventory = $params{inventory};
+    my $logger    = $params{logger};
+
+    my $command = 'nix-store --gc --print-live';
+    my $packages = _getPackagesList(
+        logger => $logger, command => $command
+    );
+    return unless $packages;
+
+    foreach my $package (@$packages) {
+        $inventory->addEntry(
+            section => 'SOFTWARES',
+            entry   => $package
+        );
+    }
+}
+
+sub _getPackagesList {
+    my $handle = getFileHandle(@_);
+    return unless $handle;
+
+    my @packages;
+    my %seen   = ();
+    while (my $line = <$handle>) {
+        chomp $line;
+        next unless $line =~ m%^/nix/store/[^-]+-(.+)-(\d+(\.\d+)*)$%;
+
+        my $package = {
+            NAME        => $1,
+            VERSION     => $2,
+            FROM        => 'nix'
+        };
+
+        next if $seen{$package->{NAME} . '-' . $package->{VERSION}}++;
+
+        push @packages, $package;
+    }
+
+    close $handle;
+
+    return \@packages;
+}
+
+1;
