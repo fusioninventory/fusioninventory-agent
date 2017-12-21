bd2ae01f64fa2a53b8b0e1caf5e0a91cb31cec30
Guillaume Bougard <gbougard@teclib.com>
2017-12-21 16:45:49 +0100

    Safer WMI task class Include prototype for server support

--- lib/FusionInventory/Agent/Task/WMI.pm
+++ lib/FusionInventory/Agent/Task/WMI.pm
@@ -8,13 +8,38 @@ use parent 'FusionInventory::Agent::Task::Inventory';
 use UNIVERSAL::require;
 use English qw(-no_match_vars);
 
-use FusionInventory::Agent::Tools::Win32;
-
 sub isEnabled {
-    my ($self) = @_;
+    my ($self, $response) = @_;
 
-    # TODO Fix to handle requests from server
-    return defined $self->{service};
+    # Task only supported on MSWin32 at the moment
+    return 0 if $OSNAME ne 'MSWin32';
+
+    # Enabled for local target only is still connected
+    return defined($self->{service}) unless
+        $self->{target}->isa('FusionInventory::Agent::Target::Server');
+
+    my $content = $response->getContent();
+    if (!$content || !$content->{RESPONSE} || $content->{RESPONSE} ne 'SEND') {
+        if ($self->{config}->{force}) {
+            $self->{logger}->debug("WMI inventory task execution not requested, but execution forced");
+        } else {
+            $self->{logger}->debug("WMI inventory task execution not requested");
+            return 0;
+        }
+    }
+
+    # TODO: This is only a POC as nothing is implemented server-side and we need
+    # to safely pass credentials
+    my %connection = (
+        host => $response->getOptionsInfoByName('REMOTEHOST'),
+        user => $response->getOptionsInfoByName('REMOTEUSER'),
+        pass => $response->getOptionsInfoByName('REMOTEPASS'),
+    );
+
+    $self->{registry} = [ $response->getOptionsInfoByName('REGISTRY') ];
+
+    # Finally enable task only is can connect
+    return $self->connect(%connection);
 }
 
 sub connect {
@@ -26,6 +51,8 @@ sub connect {
     my $pass   = $params{pass} || '';
 
     $logger->debug2("Connecting via wmi to ".($user?"$user@":"").$host) if $logger;
+
+    FusionInventory::Agent::Tools::Win32->use();
 
     $self->{service} = getWMIService(
         host    => $host,
