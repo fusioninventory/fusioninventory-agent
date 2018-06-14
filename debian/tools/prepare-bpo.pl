bc2295b2f65c8934f1b4f107fd6b19a83eb1ee07
Guillaume Bougard <gbougard@teclib.com>
2018-06-14 13:35:43 +0200

    prepare-bpo.pl script refacto and update

--- debian/tools/prepare-bpo.pl
+++ debian/tools/prepare-bpo.pl
@@ -5,40 +5,17 @@ use Data::Dumper;
 use Dpkg::Control::Info;
 use Dpkg::Changelog::Debian;
 
-$ENV{'DEBFULLNAME'} = 'Gonéri Le Bouder';
-$ENV{'EMAIL'}       = 'goneri@rulezlan.org';
-
-my %dropTest = (
-    'fusioninventory-agent' => [
-        't/components/client/ocs/response.t', 't/xml/response.t',
-        't/components/logger.t',              't/components/client/ssl.t',
-        't/components/client/connection.t',   't/01compile.t',
-        't/apps/agent.t',
-    ],
-    'libfusioninventory-agent-task-deploy-perl' => [ 't/server.t', ]
-
-);
+$ENV{'DEBFULLNAME'} = 'Debian Perl Group';
+$ENV{'EMAIL'}       = 'pkg-perl-maintainers@lists.alioth.debian.org';
 
 my %newDeps = (
-    'Depends' => {
-        'libfusioninventory-agent-task-esx-perl' => [ 'libio-socket-ssl-perl' ],
-    },
-    'Build-Depends' => {
-        'libfusioninventory-agent-task-esx-perl' => [ 'libio-socket-ssl-perl' ]
-
-      }
-
+    #'Depends' => {
+    #    'libfusioninventory-agent-task-esx-perl' => [ 'libio-socket-ssl-perl' ],
+    #},
+    #'Build-Depends' => {
+    #    'libfusioninventory-agent-task-esx-perl' => [ 'libio-socket-ssl-perl' ]
+    #}
 );
-
-open RULES, "<debian/rules";
-my @origin_rules = <RULES>;
-close RULES;
-open RULES, ">debian/rules";
-foreach (@origin_rules) {
-    s/BACKPORT = no/BACKPORT = yes/;
-    print RULES;
-}
-close RULES;
 
 sub bpDeps {
     my ($pkg) = @_;
@@ -52,9 +29,9 @@ sub bpDeps {
         my @listFinal;
         foreach (@list) {
             s/^\s*//;
-            next if $_ eq "libhttp-daemon-perl";
-            next if $_ eq "libhttp-server-simple-authen-perl";
-            next if $_ eq "libhttp-cookies-perl";
+            #next if $_ eq "libhttp-daemon-perl";
+            #next if $_ eq "libhttp-server-simple-authen-perl";
+            #next if $_ eq "libhttp-cookies-perl";
             push @listFinal, $_;
         }
         if ( $newDeps{$section}->{$name} ) {
@@ -62,30 +39,24 @@ sub bpDeps {
         }
         $pkg->{$section} = join( ",\n", @listFinal );
     }
-
 }
 
 my $c = Dpkg::Control::Info->new("debian/control");
 
 my $s = $c->get_source();
 bpDeps($s);
-use Data::Dumper;
 
 my @pList = $c->get_packages();
 foreach my $p (@pList) {
     bpDeps($p);
 }
 
-open my $f, ">debian/control";
+open my $f, ">debian/control" or die "Can't open control file: $!\n";
 $c->output($f);
-
-foreach ( @{ $dropTest{ $s->{Source} } } ) {
-    unlink($_);
-}
+close($f);
 
 my $changelog = Dpkg::Changelog::Debian->new();
 $changelog->load('debian/changelog');
-use Data::Dumper;
 if ( $changelog->[0]->get_distributions() ne 'squeeze-backports' ) {
     system("dch --bpo");
 }
