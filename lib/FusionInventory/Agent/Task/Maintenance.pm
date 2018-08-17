556126f3af1c3cc6a923597c61a430cce92c1c08
Guillaume Bougard <gbougard@teclib.com>
2018-08-17 15:28:38 +0200

    feature(maintenance): scan for task maintenance modules
    disable Maintenance task if no maintenance module could be used
    move Deploy maintenance under ::Deploy::Maintenance module

--- lib/FusionInventory/Agent/Task/Maintenance.pm
+++ lib/FusionInventory/Agent/Task/Maintenance.pm
@@ -5,8 +5,10 @@ use warnings;
 
 use parent 'FusionInventory::Agent::Task';
 
-use FusionInventory::Agent::Storage;
-use FusionInventory::Agent::Task::Deploy::Datastore;
+use English qw(-no_match_vars);
+use UNIVERSAL::require;
+use File::Basename;
+
 use FusionInventory::Agent::Task::Maintenance::Version;
 
 our $VERSION = FusionInventory::Agent::Task::Maintenance::Version::VERSION;
@@ -14,25 +16,58 @@ our $VERSION = FusionInventory::Agent::Task::Maintenance::Version::VERSION;
 sub isEnabled {
     my ($self) = @_;
 
-    if (!$self->{target}->isa('FusionInventory::Agent::Target::Scheduler')) {
+    if (!$self->{target}->isType('scheduler')) {
         $self->{logger}->debug("Maintenance task only compatible with Scheduler target");
         return;
     }
 
-    return 1;
+    my $found = 0;
+    # Lookup for each target task if a Maintenance module exists
+    foreach my $task ($self->{target}->otherTasks()) {
+        my $taskdir = dirname(__FILE__) .'/'. ucfirst($task);
+        next unless -d $taskdir;
+
+        my $maintenance = $taskdir.'/Maintenance.pm';
+        next unless -e $maintenance;
+
+        $found ++;
+        last;
+    }
+
+    return $found;
 }
 
 sub run {
-    my ($self, %params) = @_;
+    my ($self) = @_;
 
     my $logger = $self->{logger};
-    my $folder = $self->{target}->getStorage()->getDirectory();
-    my $datastore = FusionInventory::Agent::Task::Deploy::Datastore->new(
-        config => $self->{config},
-        path   => $folder.'/deploy',
-        logger => $logger
-    );
-    $datastore->cleanUp( force => $datastore->diskIsFull() );
+    my @taskclass = split('::', __PACKAGE__);
+    pop @taskclass;
+
+    # Lookup for each target task if a Maintenance module exists and run
+    # the its doMaintenance() API
+    foreach my $task ($self->{target}->otherTasks()) {
+        my $taskdir = dirname(__FILE__) .'/'. ucfirst($task);
+        next unless -d $taskdir;
+
+        my $file = $taskdir.'/Maintenance.pm';
+        next unless -e $file;
+
+        my $module = join('::', @taskclass, ucfirst($task), 'Maintenance');
+        $module->require();
+        if ($EVAL_ERROR) {
+            $logger->debug("failed to load $task maintenance module: $EVAL_ERROR");
+           next;
+        }
+
+        $logger->debug2("Doing $task Maintenance");
+        my $maintenance = $module->new(
+            target  => $self->{target},
+            config  => $self->{config},
+            logger  => $logger,
+        );
+        $maintenance->doMaintenance();
+    }
 }
 
 1;
@@ -52,8 +87,11 @@ and safe.
 
 =head2 isEnabled ( $self )
 
-Returns true if the task is enabled.
+Lookup for a Maintenance module for each target enabled tasks.
 
-=head2 run ( $self, %params )
+Returns true if the task should be finally enabled.
 
-Run the task.
+=head2 run ( $self )
+
+Run the Maintenance task by calling each doMaintenance() API from each
+task Maintenance found modules.
