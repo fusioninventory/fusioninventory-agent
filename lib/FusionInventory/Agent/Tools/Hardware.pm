aaafc586708155fc11a17235a58542133d384aec
Guillaume Bougard <gbougard@teclib.com>
2018-10-02 12:03:14 +0200

    feature: add other LLDP/CDP connection match checks

--- lib/FusionInventory/Agent/Tools/Hardware.pm
+++ lib/FusionInventory/Agent/Tools/Hardware.pm
@@ -1033,7 +1033,21 @@ sub _setConnectedDevices {
             my $cdp_connection  = $cdp_info->{$interface_id};
 
             if ($lldp_connection) {
-                if ($cdp_connection->{SYSDESCR} eq $lldp_connection->{SYSDESCR}) {
+                my $match = 0;
+
+                # Try different case to find LLDP/CDP connection match
+                if ($lldp_connection->{SYSDESCR} && $cdp_connection->{SYSDESCR} eq $lldp_connection->{SYSDESCR}) {
+                    $match ++;
+                } elsif ($lldp_connection->{SYSNAME} && $cdp_connection->{SYSNAME}) {
+                    my $cdp_test = getCanonicalMacAddress($cdp_connection->{SYSNAME});
+                    if ($cdp_connection->{SYSNAME} eq $lldp_connection->{SYSNAME}) {
+                        $match ++;
+                    } elsif ($lldp_connection->{SYSMAC} && $cdp_test && $lldp_connection->{SYSMAC} eq $cdp_test) {
+                        $match ++;
+                    }
+                }
+
+                if ($match) {
                     # same device, everything OK
                     foreach my $key (qw/IP MODEL/) {
                         $lldp_connection->{$key} = $cdp_connection->{$key};
