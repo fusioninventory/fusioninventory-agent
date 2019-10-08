9a9386382ab1f6834ba9291fc25ebbdff48a1584
Pierre Renaudet <53604301+PR-gh@users.noreply.github.com>
2019-10-08 10:06:40 +0200

    Fix FireWall Win32 Unintialized value in concatenation

--- lib/FusionInventory/Agent/Task/Inventory/Win32/Firewall.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Win32/Firewall.pm
@@ -108,7 +108,7 @@ sub _makeProfileAndConnectionsAssociation {
         next if ($interface->{STATUS} ne 'Up');
 
         my $profile;
-        my $domainSettings = $DNSRegisteredAdapters->{$interface->{GUID}.'/'};
+        my $domainSettings = $DNSRegisteredAdapters->{$interface->{GUID}.'/'} if exists $interface->{GUID};
         # check if connection with domain
         if ($domainSettings) {
             $profile = _retrieveFirewallProfileWithdomain(
