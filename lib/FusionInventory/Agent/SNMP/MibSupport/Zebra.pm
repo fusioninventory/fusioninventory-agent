be786d6a3651c4a31aaed3eb4aef82d9fb03e311
Guillaume Bougard <gbougard@teclib.com>
2020-06-29 15:10:52 +0200

    feat: Update Zebra printer support

--- lib/FusionInventory/Agent/SNMP/MibSupport/Zebra.pm
+++ lib/FusionInventory/Agent/SNMP/MibSupport/Zebra.pm
@@ -15,9 +15,15 @@ use constant    model2  => esi . '.6.2.3.2.1.15.1' ;
 use constant    serial  => esi . '.1.5.0' ;
 use constant    fw2     => esi . '.1.9.0' ;
 
+# See ZEBRA-QL-MIB
+
 use constant    zebra   => '.1.3.6.1.4.1.10642' ;
 use constant    model1  => zebra . '.1.1.0' ;
 use constant    fw1     => zebra . '.1.2.0' ;
+
+use constant    zql_zebra_ql    => zebra . '.200' ;
+use constant    model3  => zql_zebra_ql . '.19.7.0' ;
+use constant    serial3 => zql_zebra_ql . '.19.5.0' ;
 
 our $mibSupport = [
     {
@@ -29,13 +35,14 @@ our $mibSupport = [
 sub getSerial {
     my ($self) = @_;
 
-    return hex2char($self->get(serial));
+    # serial3 is more accurate than serial on GK420 & ZE500
+    return hex2char($self->get(serial3) || $self->get(serial));
 }
 
 sub getModel {
     my ($self) = @_;
 
-    return hex2char($self->get(model1) || $self->get(model2));
+    return hex2char($self->get(model1) || $self->get(model2) || $self->get(model3));
 }
 
 sub getFirmware {
