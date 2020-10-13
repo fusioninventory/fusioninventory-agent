53f3b042cbdb690474e4ff611039b8124f99dc3b
Vadim Pisarev <plv@techiemedia.net>
2020-10-13 11:40:54 -0400

    Explicitly disabled Win32 in FusionInventory::Agent::Task::Inventory::Generic::Storages::HP

--- lib/FusionInventory/Agent/Task/Inventory/Generic/Storages/HP.pm
+++ lib/FusionInventory/Agent/Task/Inventory/Generic/Storages/HP.pm
@@ -5,13 +5,16 @@ use warnings;
 
 use parent 'FusionInventory::Agent::Task::Inventory::Module';
 
+use English qw(-no_match_vars);
+
 use FusionInventory::Agent::Tools;
 use FusionInventory::Agent::Tools::Storages::HP;
 
 our $runMeIfTheseChecksFailed = ['FusionInventory::Agent::Task::Inventory::Generic::Storages::HpWithSmartctl'];
 
 sub isEnabled {
-    return canRun('hpacucli');
+    # MSWin32 has its Win32::Storages::HP dedicated module
+    return canRun('hpacucli') && $OSNAME ne 'MsWin32';
 }
 
 sub doInventory {
